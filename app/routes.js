89ff4d7115011112f3b8f958756b50097a11f940
Planeshifter <pburckhardt@outlook.com>
2016-07-11 23:38:34 +0200

    Add route for settings page

--- app/routes.js
+++ app/routes.js
@@ -5,7 +5,7 @@ import { Route, IndexRoute } from 'react-router';
 import App from './containers/App';
 import Editor from './containers/Editor';
 import Documentation from './containers/Documentation';
-
+import Settings from './containers/Settings';
 
 // EXPORTS //
 
@@ -13,5 +13,6 @@ export default (
 	<Route path="/" component={App}>
 		<IndexRoute component={Editor} />
 		<Route path="/docs" component={Documentation} />
+		<Route path="/settings" component={Settings} />
 	</Route>
 );
