a818baf41b39aca528a8903de98ebcd3e70736e6
Planeshifter <pburckhardt@outlook.com>
2016-07-22 00:10:01 +0200

    Add styling of settings page

--- app/containers/Settings.js
+++ app/containers/Settings.js
@@ -2,6 +2,7 @@
 
 import React, { Component } from 'react';
 import NumberField from 'react-number-field';
+import { Panel } from 'react-bootstrap';
 import { Link } from 'react-router';
 
 
@@ -21,7 +22,9 @@ class Settings extends Component {
 		return (
 			<div
 				style={{
-					marginLeft: '20px'
+					marginLeft: '20px',
+					marginTop: '20px',
+					marginRight: '20px'
 				}}
 			>
 				<Link
@@ -36,16 +39,19 @@ class Settings extends Component {
 						zIndex: 2
 					}}
 				>Back to Editor</Link>
-				<h1>Settings</h1>
-				<h3>Font Size:</h3>
-				<p>Editor text height in pixels</p>
-				<NumberField
-					onChange={handleFontSizeChange}
-					min={8}
-					max={56}
-					step={1}
-					defaultValue={localStorage.getItem( 'fontSize' ) || 14}
-				/>
+				<br />
+				<br />
+				<Panel header={<h1>Settings</h1>}>
+					<h4>Font Size:</h4>
+					<p>Editor text height in pixels</p>
+					<NumberField
+						onChange={handleFontSizeChange}
+						min={8}
+						max={56}
+						step={1}
+						defaultValue={localStorage.getItem( 'fontSize' ) || 14}
+					/>
+				</Panel>
 			</div>
 		);
 	}
