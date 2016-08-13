fa59affd0cce5e748f0ad6a9f47233823b44b78e
Planeshifter <pburckhardt@outlook.com>
2016-08-14 00:12:40 +0200

    Resize WebView when browser window height or width changed

--- app/containers/Documentation.js
+++ app/containers/Documentation.js
@@ -15,16 +15,40 @@ const openBrowser = ( url ) => {
 // DOCUMENTATION //
 
 class Documentation extends Component {
+
+	constructor( props ) {
+		super( props );
+		this.state = {};
+
+		this.resize = () => {
+			this.setState({
+				width: window.innerWidth,
+				height: window.innerHeight
+			});
+		};
+
+	}
+
 	componentDidMount() {
 		const webview = document.getElementById( 'DocumentationView' );
 		webview.addEventListener( 'new-window', ( e ) => {
 			try {
 				openBrowser( e.url );
-			} catch( error ) {
+			} catch ( error ) {
 				alert( `Ignoring ${e.url} due to ${error.message}` );
 			}
 		});
+		window.addEventListener( 'resize', this.resize );
 	}
+
+	componentWillMount() {
+		this.resize();
+	}
+
+	componentWillUnmount() {
+		window.removeEventListener( 'resize', this.resize );
+	}
+
 	render() {
 		return (
 			<div>
@@ -43,10 +67,14 @@ class Documentation extends Component {
 				<webview
 					id="DocumentationView"
 					src="http://isledocs.philipp-burckhardt.com/"
-					style={{ display: 'flex', minHeight: '1600px' }}
+					style={{
+						display: 'flex',
+						height: this.state.height,
+						window: this.state.width
+					}}
 					autosize="on"
 					disablewebsecurity
-				></webview>
+				/>
 			</div>
 		);
 	}
