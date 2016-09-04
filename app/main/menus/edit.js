c8fd757d4214efeb4869c6d8dbbf74efbc1d0b01
Planeshifter <pburckhardt@outlook.com>
2016-09-04 19:53:13 -0400

    Add Undo and Redo menu items

--- app/main/menus/edit.js
+++ app/main/menus/edit.js
@@ -8,9 +8,41 @@ import * as actions from './../actions';
 export default {
 	label: 'Edit',
 	submenu: [
+		{
+			label: 'Undo',
+			accelerator: 'CmdOrCtrl+Z',
+			click: ( menuItem, browserWindow ) => {
+				browserWindow.webContents.sendInputEvent({
+					type: 'keyDown',
+					keyCode: 'Ctrl'
+				});
+				browserWindow.webContents.sendInputEvent({
+					type: 'keyDown',
+					keyCode: 'Z',
+					modifiers: [ 'control' ]
+				});
+			}
+		},
+		{
+			label: 'Redo',
+			accelerator: 'CmdOrCtrl+Y',
+			click: ( menuItem, browserWindow ) => {
+				browserWindow.webContents.sendInputEvent({
+					type: 'keyDown',
+					keyCode: 'Ctrl'
+				});
+				browserWindow.webContents.sendInputEvent({
+					type: 'keyDown',
+					keyCode: 'Y',
+					modifiers: [ 'control' ]
+				});
+			}
+		},
+		{ type: 'separator' },
 		{ role: 'cut' },
 		{ role: 'copy' },
 		{ role: 'paste' },
-		{ role: 'delete' }
+		{ role: 'delete' },
+		{ role: 'selectall' }
 	]
 };
