49612b9d80ef7482dbb3e24bd6001b52347f5bdd
Planeshifter <pburckhardt@outlook.com>
2016-09-26 22:11:22 -0400

    Setup testing

---
+++ test/components/header.spec.js
@@ -0,0 +1,18 @@
+// MODULES //
+
+import test from 'tape';
+import React from 'react';
+import { Toolbar, Space, Heading, NavItem } from 'rebass';
+import { shallow, mount } from 'enzyme';
+import Header from '../../app/components/Header.js';
+
+
+// TESTS //
+
+test( 'the component renders two <Toolbar /> components', t => {
+	const wrapper = shallow( <Header /> );
+	const toolbars = wrapper.find( Toolbar );
+	t.equal( toolbars.length, 2, 'length is equal to two' );
+	t.end();
+});
+
