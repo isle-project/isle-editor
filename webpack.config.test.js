874ed327a7dc5cb53b93fa6657baf492f67fea41
Planeshifter <pburckhardt@outlook.com>
2017-02-06 21:40:46 -0500

    Fix code due to breaking change in Webpack

--- webpack.config.test.js
+++ webpack.config.test.js
@@ -12,7 +12,7 @@ module.exports = {
 		libraryTarget: 'commonjs2'
 	},
 	module: {
-		loaders: devConfigs.module.loaders.slice( 1 ) // remove babel-loader
+		loaders: devConfigs.module.rules.slice( 1 ) // remove babel-loader
 	},
 	resolve: {
 		modulesDirectories: [
