3a895eff23e78e619b4f60237d3e1eca6b09cd7a
Planeshifter <pburckhardt@outlook.com>
2017-02-07 13:19:26 -0500

    Fix bar heights

--- app/components/d3/density-plot/index.js
+++ app/components/d3/density-plot/index.js
@@ -156,7 +156,7 @@ class DensityPlot extends D3Plot {
 							.attr( 'x', 1 )
 							.attr( 'transform', d => 'translate(' + x( d.x0 ) + ',' + y( d.length / data.length ) + ')' )
 							.attr( 'width', d => x( d.x1 ) - x( d.x0 ) - 1 )
-							.attr( 'height', d => ( height - y( d.length ) ) )
+							.attr( 'height', d => ( height - y( d.length / data.length ) ) )
 							.style( 'fill', '#bbb' )
 							.style( 'shape-rendering', 'crispEdges' );
 				}
