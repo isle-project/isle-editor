459fd1332f47b6d8dcdd5ea73caaedac3112a8d5
Planeshifter <pburckhardt@outlook.com>
2017-03-02 03:24:57 -0500

    Support math delimiters

--- app/utils/markdown-to-html/index.js
+++ app/utils/markdown-to-html/index.js
@@ -7,20 +7,49 @@ const md = require( 'markdown-it' )({
 	breaks: true,
 	typographer: false
 });
+const mdInline = require( 'markdown-it' )({
+	html: true,
+	xhtmlOut: true,
+	breaks: false,
+	typographer: false
+});
+
+
+// VARIABLES //
+
+const DEFAULT_INLINE_MATH_DELIMITERS = [
+	/\\\((.*?)\\\)/g,
+];
+const DEFAULT_DISPLAY_MATH_DELIMITERS = [
+	/\$\$([\s\S]*?)\$\$/g,
+	/\\\[([\s\S]*?)\\\]/g
+];
 
 
 // MAIN //
 
 function toMarkdown( str ) {
 	const tokenizer = new Tokenizer();
-	const arr = tokenizer.parse( str );
 
+	// Replace math delimiters by ISLE component
+	DEFAULT_INLINE_MATH_DELIMITERS.forEach( regexp => {
+		str = str.replace( regexp, '<TeX raw="$1" />' );
+	});
+	DEFAULT_DISPLAY_MATH_DELIMITERS.forEach( regexp => {
+		str = str.replace( regexp, '<TeX raw="$1" displayMode />' );
+	});
+
+	const arr = tokenizer.parse( str );
+	for ( let i = 0; i < arr.length; i++ ) {
+		arr[ i ] = mdInline.renderInline( arr[ i ]);
+	}
 	str = arr.join( '' );
 	str = md.render( str );
 
 	for ( let key in tokenizer.divHash ) {
 		str = str.replace( key, tokenizer.divHash[ key ]);
 	}
+
 	return str;
 } // end FUNCTION toMarkdown()
 
