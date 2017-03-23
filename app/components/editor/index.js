bc68ee035d8773c6282ea558e525dd965a648f23
Planeshifter <pburckhardt@outlook.com>
2017-03-23 01:18:43 -0400

    Use custom markdown rendering

--- app/components/editor/index.js
+++ app/components/editor/index.js
@@ -2,6 +2,14 @@
 
 import React, { Component, PropTypes } from 'react';
 import SimpleMDE from 'simplemde';
+import replace from '@stdlib/string/replace';
+
+const md = require( 'markdown-it' )({
+	html: true,
+	xhtmlOut: true,
+	breaks: true,
+	typographer: false
+});
 
 
 // MAIN //
@@ -12,16 +20,54 @@ class MarkdownEditor extends Component {
 		super( props );
 
 		this.state = {
-			value: props.defaultValue
+			value: props.defaultValue,
+			hash: {}
+		};
+
+		this.previewRender = ( plainText ) => {
+			const { hash } = this.state;
+			for ( let key in hash ) {
+				console.log( key )
+				plainText = replace( plainText, key, hash[ key ]);
+			}
+			console.log( plainText )
+			return md.render( plainText );
 		};
 	}
 
 	componentDidMount () {
-		this.simplemde = new SimpleMDE({ element: this.refs.simplemde });
+		this.simplemde = new SimpleMDE({
+			element: this.refs.simplemde,
+			initialValue: this.props.defaultValue,
+			previewRender: this.previewRender,
+			toolbar: [
+				'bold', 'italic', 'strikethrough', '|', 'heading', 'quote', 'unordered-list', 'ordered-list', 'link', '|', 'preview', 'side-by-side', 'fullscreen', '|', 'guide',
+				{
+					name: 'custom',
+					action: function customFunction( editor ){
+						// Add your own code
+					},
+					className: "fa fa-save",
+					title: "Save HTML File"
+				}
+			]
+		});
 
+		// Add event listeners:
 		this.simplemde.codemirror.on( 'change', () => {
 			this.setState({
 				value: this.simplemde.value()
+			});
+		});
+		this.simplemde.codemirror.on( 'drop', ( instance, event ) => {
+			event.preventDefault();
+			const key = event.dataTransfer.getData( 'text/plain' );
+			const html = event.dataTransfer.getData( 'text/html' );
+			const { hash } = this.state;
+			hash[ key ] = html;
+			instance.replaceSelection( key );
+			this.setState({
+				hash
 			});
 		});
 	}
@@ -29,9 +75,13 @@ class MarkdownEditor extends Component {
 	componentWillUnmount () {
 	}
 
+	allowDrop( event ) {
+		event.preventDefault();
+	}
+
 	render() {
 		return (
-			<textarea ref="simplemde" defaultValue={this.props.defaultValue} autoComplete="off" />
+			<textarea ref="simplemde" autoComplete="off" />
 		);
 	}
 }
