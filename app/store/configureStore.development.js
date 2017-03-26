27a7abec2596980b762e7055268715d0d8686fc5
Planeshifter <pburckhardt@outlook.com>
2017-03-26 00:40:44 -0400

    Accommodate breaking change in redux-logger

--- app/store/configureStore.development.js
+++ app/store/configureStore.development.js
@@ -3,7 +3,7 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import { persistState } from 'redux-devtools';
 import thunk from 'redux-thunk';
-import createLogger from 'redux-logger';
+import { createLogger } from 'redux-logger';
 import { hashHistory } from 'react-router';
 import { routerMiddleware } from 'react-router-redux';
 import rootReducer from 'reducers';
