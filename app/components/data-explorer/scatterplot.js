f384562474bf98413922ee2f3a1dc028f134c3ae
Planeshifter <pburckhardt@outlook.com>
2017-07-10 11:43:30 -0400

    Add option to map variable to dot size

--- app/components/data-explorer/scatterplot.js
+++ app/components/data-explorer/scatterplot.js
@@ -15,13 +15,16 @@ class Scatterplot extends Component {
 		super( props );
 	}
 
-	generateScatterplot( xval, yval, color, type ) {
+	generateScatterplot( xval, yval, color, type, size ) {
 		let aes = 'aes( x = xval, y = yval';
 		if ( color ) {
 			aes += ', color = color';
 		}
 		if ( type ) {
 			aes += ', shape = type';
+		}
+		if ( size ) {
+			aes += ', size = size';
 		}
 		aes += ' )';
 
@@ -32,16 +35,36 @@ class Scatterplot extends Component {
 		if ( type ) {
 			labs += `, shape = "${type}"`;
 		}
+		if ( size ) {
+			labs += `, size = "${size}"`;
+		}
 		labs += ' )';
+
+		let sizeData = this.props.data[ size ];
+		if ( size ) {
+			// Make size variable categorical in case it is binary:
+			let sizeIsBinary = true;
+			for ( let i = 0; i < sizeData.length; i++ ) {
+				if ( sizeData[ i ] !== 0 && sizeData[ i ] !== 1 ) {
+					sizeIsBinary = false;
+					break;
+				}
+			}
+			if ( sizeIsBinary ) {
+				sizeData = sizeData.map( e => `"${e}"` );
+			}
+		}
 
 		let code = `dat = data.frame(
 			xval = c(${this.props.data[ xval ]}),
 			yval = c(${this.props.data[ yval ]})
 			${ color ? `, color = c(${this.props.data[ color ].map( e => `"${e}"` )})` : '' }
 			${ type ? `, type = c(${this.props.data[ type ].map( e => `"${e}"` )})` : '' }
+			${ size ? `, size = c( ${sizeData} )` : '' }
 		)
 		ggplot( data = dat, ${aes}) +
-		geom_point( size = 1.5 ) + ${labs}`;
+		geom_point( ${ !size ? 'size = 1.5' : '' } ) + ${labs}`;
+		console.log( code );
 
 		const output = {
 			variable: `${xval} against ${yval}`,
@@ -50,14 +73,14 @@ class Scatterplot extends Component {
 				<label>{`${xval} against ${yval}`}: </label>
 				<RPlot
 					code={code}
-					prependCode={[ 'theme_set(theme_gray(base_size = 18))' ]}
+					prependCode={[ 'theme_set(theme_bw(base_size = 20))' ]}
 					libraries={[ 'ggplot2' ]}
 					onDone={this.props.onPlotDone}
 				/>
 			</div>
 		};
 		this.props.logAction( 'DATA_EXPLORER:SCATTERPLOT', {
-			xval, yval, color, type
+			xval, yval, color, type, size
 		});
 		this.props.onCreated( output );
 	}
@@ -83,10 +106,17 @@ class Scatterplot extends Component {
 				<SelectInput
 					legend="Color By:"
 					options={groupingVariables}
+					clearable={true}
 				/>
 				<SelectInput
 					legend="Point Type By:"
 					options={groupingVariables}
+					clearable={true}
+				/>
+				<SelectInput
+					legend="Point Size By:"
+					options={variables}
+					clearable={true}
 				/>
 			</Dashboard>
 		);
