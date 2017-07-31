fcde6d76995721845a7e187f2099fd5e787b2fbb
Planeshifter <pburckhardt@outlook.com>
2017-07-30 22:25:28 -0400

    Export components

--- app/components/learn/index.js
+++ app/components/learn/index.js
@@ -4,8 +4,23 @@ import ContinuousCLT from 'components/learn/clt/continuous.js';
 import DiscreteCLT from 'components/learn/clt/discrete.js';
 import SampleCLT from 'components/learn/clt/sample.js';
 import CrossValidation from 'components/learn/cross-validation';
+import MeanTest from 'components/learn/hypothesis-testing/mean.js';
+import ProportionTest from 'components/learn/hypothesis-testing/proportion.js';
+import ExponentialDistribution from 'components/learn/distribution/exponential';
+import NormalDistribution from 'components/learn/distribution/normal';
+import UniformDistribution from 'components/learn/distribution/uniform';
 
 
 // EXPORTS //
 
-export { ContinuousCLT, DiscreteCLT, SampleCLT, CrossValidation };
+export {
+	ContinuousCLT,
+	DiscreteCLT,
+	SampleCLT,
+	CrossValidation,
+	ProportionTest,
+	MeanTest,
+	ExponentialDistribution,
+	NormalDistribution,
+	UniformDistribution
+};
