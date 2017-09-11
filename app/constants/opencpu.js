240054d42fa7fa9089e674485f862d68256f25bf
Planeshifter <pburckhardt@outlook.com>
2017-09-11 17:49:34 -0400

    Use OpenCPU R server until we have HTTPS for phd-serv5

--- app/constants/opencpu.js
+++ app/constants/opencpu.js
@@ -1,2 +1,2 @@
-export const OPEN_CPU_DEFAULT_SERVER = 'http://phd-serv5.heinz.cmu.edu';
+export const OPEN_CPU_DEFAULT_SERVER = 'https://cloud.opencpu.org';
 export const OPEN_CPU_IDENTITY = '/ocpu/library/base/R/identity/identity-d';
