fb23c5e068adbc63bd089d8d00ae5231653ad69f
Planeshifter <pburckhardt@outlook.com>
2017-10-18 19:30:27 -0400

    Add d3 tree component

---
+++ app/components/d3/tree/link/index.js
@@ -0,0 +1,134 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { svg, select } from 'd3';
+
+import './style.css';
+
+// FUNCTIONS //
+
+function linkVertical(d) {
+	return "M" + d.source.x + "," + d.source.y
+		+ "C" + (d.source.x + d.target.x) / 2 + "," + d.source.y
+		+ " " + (d.source.x + d.target.x) / 2 + "," + d.target.y
+		+ " " + d.target.x + "," + d.target.y;
+}
+
+function linkHorizontal(d) {
+	return "M" + d.source.y + "," + d.source.x
+		+ "C" + (d.source.y + d.target.y) / 2 + "," + d.source.x
+		+ " " + (d.source.y + d.target.y) / 2 + "," + d.target.x
+		+ " " + d.target.y + "," + d.target.x;
+}
+
+// MAIN //
+
+export default class Link extends React.PureComponent {
+	constructor(props) {
+		super(props);
+		this.state = {
+			initialStyle: {
+				opacity: 0,
+			},
+		};
+	}
+
+	componentDidMount() {
+		this.applyOpacity(1);
+	}
+
+	componentWillLeave(done) {
+		this.applyOpacity(0, done);
+	}
+
+	applyOpacity(opacity, done = () => {}) {
+		const { transitionDuration } = this.props;
+
+		select( this.link )
+			.transition()
+			.duration( transitionDuration )
+			.style( 'opacity', opacity )
+			.on( 'end', done );
+	}
+
+	diagonalPath( linkData, orientation ) {
+		if ( orientation === 'horizontal' ) {
+			return linkHorizontal( linkData );
+		}
+		return linkVertical( linkData );
+	}
+
+	straightPath(linkData, orientation) {
+		const straight = svg
+			.line()
+			.interpolate('basis')
+			.x(d => d.x)
+			.y(d => d.y);
+
+		let data = [
+			{ x: linkData.source.x, y: linkData.source.y },
+			{ x: linkData.target.x, y: linkData.target.y },
+		];
+
+		if (orientation === 'horizontal') {
+			data = [
+				{ x: linkData.source.y, y: linkData.source.x },
+				{ x: linkData.target.y, y: linkData.target.x },
+			];
+		}
+
+		return straight(data);
+	}
+
+	elbowPath(d, orientation) {
+		return orientation === 'horizontal'
+			? `M${d.source.y},${d.source.x}V${d.target.x}H${d.target.y}`
+			: `M${d.source.x},${d.source.y}V${d.target.y}H${d.target.x}`;
+	}
+
+	drawPath() {
+		const { linkData, orientation, pathFunc } = this.props;
+
+		if (typeof pathFunc === 'function') {
+			return pathFunc(linkData, orientation);
+		}
+
+		if (pathFunc === 'elbow') {
+			return this.elbowPath(linkData, orientation);
+		}
+
+		if (pathFunc === 'straight') {
+			return this.straightPath(linkData, orientation);
+		}
+
+		return this.diagonalPath(linkData, orientation);
+	}
+
+	render() {
+		const { styles } = this.props;
+		return (
+			<path
+				ref={l => {
+					this.link = l;
+				}}
+				style={{ ...this.state.initialStyle, ...styles }}
+				className="linkBase"
+				d={this.drawPath()}
+			/>
+		);
+	}
+}
+
+Link.defaultProps = {
+	styles: {},
+};
+
+Link.propTypes = {
+	linkData: PropTypes.object.isRequired,
+	orientation: PropTypes.oneOf(['horizontal', 'vertical']).isRequired,
+	pathFunc: PropTypes.oneOfType([
+		PropTypes.oneOf(['diagonal', 'elbow', 'straight']),
+		PropTypes.func,
+	]).isRequired,
+	transitionDuration: PropTypes.number.isRequired,
+	styles: PropTypes.object,
+};
---
+++ app/components/d3/tree/node/index.js
@@ -0,0 +1,154 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import uuid from 'uuid';
+import { select } from 'd3';
+
+import './style.css';
+
+export default class Node extends React.Component {
+	constructor(props) {
+		super(props);
+		const { parent } = props.nodeData;
+		const originX = parent ? parent.x : 0;
+		const originY = parent ? parent.y : 0;
+
+		this.state = {
+			transform: this.setTransformOrientation(originX, originY),
+			initialStyle: {
+				opacity: 0,
+			},
+		};
+
+		this.handleClick = this.handleClick.bind(this);
+	}
+
+	componentDidMount() {
+		const { x, y } = this.props.nodeData;
+		const transform = this.setTransformOrientation(x, y);
+
+		this.applyTransform(transform);
+	}
+
+	componentWillUpdate(nextProps) {
+		const transform = this.setTransformOrientation(
+			nextProps.nodeData.x,
+			nextProps.nodeData.y,
+		);
+		this.applyTransform(transform);
+	}
+
+	setTransformOrientation(x, y) {
+		return this.props.orientation === 'horizontal'
+			? `translate(${y},${x})`
+			: `translate(${x},${y})`;
+	}
+
+	applyTransform(transform, opacity = 1, done = () => {}) {
+		const { transitionDuration } = this.props;
+
+		select(this.node)
+			.transition()
+			.duration(transitionDuration)
+			.attr('transform', transform)
+			.style('opacity', opacity)
+			.on('end', done);
+	}
+
+	handleClick() {
+		console.log( this.props );
+		this.props.onClick( this.props.nodeData.data.id );
+	}
+
+	componentWillLeave(done) {
+		const { parent } = this.props.nodeData;
+		const originX = parent ? parent.x : 0;
+		const originY = parent ? parent.y : 0;
+		const transform = this.setTransformOrientation(originX, originY);
+
+		this.applyTransform(transform, 0, done);
+	}
+
+	render() {
+		const { nodeData, nodeSvgShape, textLayout, styles } = this.props;
+		const nodeStyle = nodeData._children
+			? { ...styles.node }
+			: { ...styles.leafNode };
+		return (
+			<g
+				id={nodeData.id}
+				ref={n => {
+					this.node = n;
+				}}
+				style={this.state.initialStyle}
+				className={nodeData._children ? 'nodeBase' : 'leafNodeBase'}
+				transform={this.state.transform}
+				onClick={this.handleClick}
+			>
+				{/* TODO: DEPRECATE <circle /> */}
+				{this.props.circleRadius ? (
+					<circle r={this.props.circleRadius} style={nodeStyle.circle} />
+				) : (
+					React.createElement(nodeSvgShape.shape, {
+						...nodeSvgShape.shapeProps,
+						...nodeStyle.circle,
+					})
+				)}
+
+				<text
+					className="nodeNameBase"
+					style={nodeStyle.name}
+					textAnchor={textLayout.textAnchor}
+					x={textLayout.x}
+					y={textLayout.y}
+					dy=".35em"
+				>
+					{this.props.name}
+				</text>
+				<text
+					className="nodeAttributesBase"
+					y={textLayout.y + 10}
+					textAnchor={textLayout.textAnchor}
+					style={nodeStyle.attributes}
+				>
+					{this.props.attributes &&
+						Object.keys(this.props.attributes).map(labelKey => (
+							<tspan x={textLayout.x} dy="1.2em" key={uuid.v4()}>
+								{labelKey}: {this.props.attributes[labelKey]}
+							</tspan>
+						))}
+				</text>
+			</g>
+		);
+	}
+}
+
+Node.defaultProps = {
+	textAnchor: 'start',
+	attributes: undefined,
+	circleRadius: undefined,
+	styles: {
+		node: {
+			circle: {},
+			name: {},
+			attributes: {},
+		},
+		leafNode: {
+			circle: {},
+			name: {},
+			attributes: {},
+		},
+	},
+};
+
+Node.propTypes = {
+	nodeData: PropTypes.object.isRequired,
+	nodeSvgShape: PropTypes.object.isRequired,
+	orientation: PropTypes.oneOf(['horizontal', 'vertical']).isRequired,
+	transitionDuration: PropTypes.number.isRequired,
+	onClick: PropTypes.func.isRequired,
+	name: PropTypes.string.isRequired,
+	attributes: PropTypes.object,
+	textLayout: PropTypes.object.isRequired,
+	circleRadius: PropTypes.number,
+	styles: PropTypes.object,
+};
