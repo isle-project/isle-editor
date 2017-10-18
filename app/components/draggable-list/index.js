1207765fa7e191b9822cc5ba49d38d9ccfb3a4e0
Planeshifter <pburckhardt@outlook.com>
2017-10-17 23:05:47 -0400

    Avoid using deprecated react function

--- app/components/draggable-list/index.js
+++ app/components/draggable-list/index.js
@@ -2,7 +2,7 @@
 
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import update from 'react/lib/update';
+import update from 'immutability-helper';
 import Card from './card';
 import { DragDropContext } from 'react-dnd';
 import HTML5Backend from 'react-dnd-html5-backend';
