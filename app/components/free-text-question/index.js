faaa1908cb5e9aecef980d6548928e57f62897fd
Planeshifter <pburckhardt@outlook.com>
2017-10-31 02:18:43 -0400

    Make answers persistent

--- app/components/free-text-question/index.js
+++ app/components/free-text-question/index.js
@@ -3,6 +3,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Button, ButtonToolbar, FormControl, OverlayTrigger, Panel, Tooltip } from 'react-bootstrap';
+import isArray from '@stdlib/assert/is-array';
 import ChatButton from 'components/chat-button';
 import InstructorBar from 'components/instructor-bar';
 const debug = require( 'debug' )( 'isle:free-text-question' );
@@ -26,73 +27,73 @@ class FreeTextQuestion extends Component {
 			solutionDisplayed: false,
 			submitted: false
 		};
+	}
 
-		/*
-		* Event handler invoked when text area value changes. Updates `value` and invokes
-		* `onChange` callback with the new text as its first argument
-		*/
-		this.handleChange = ( event ) => {
-			const newValue = event.target.value;
-			this.setState({ value: newValue });
-			this.props.onChange( newValue );
-		};
+	/*
+	* Event handler invoked when text area value changes. Updates `value` and invokes
+	* `onChange` callback with the new text as its first argument
+	*/
+	handleChange = ( event ) => {
+		const newValue = event.target.value;
+		this.setState({ value: newValue });
+		this.props.onChange( newValue );
+	};
 
-		this.submitHandler = ( event ) => {
-			const { session } = this.context;
-			if ( this.state.submitted ) {
-				session.addNotification({
-					title: 'Answer re-submitted.',
-					message: this.props.resubmissionMsg,
-					level: 'success',
-					position: 'tr'
-				});
-			} else {
-				let submissionMsg = this.props.submissionMsg;
-				if ( submissionMsg === '' ) {
-					submissionMsg = this.props.solution ?
-						'Compare your answer with solution using the "Show Solution" button. You can then change your answer and re-submit if necessary.' :
-						'You can change your answer and re-submit if you want to.';
-				}
-				session.addNotification({
-					title: 'Answer submitted.',
-					message: submissionMsg,
-					level: 'success',
-					position: 'tr'
-				});
-			}
-
-			this.setState({
-				submitted: true
+	submitHandler = ( event ) => {
+		const { session } = this.context;
+		if ( this.state.submitted ) {
+			session.addNotification({
+				title: 'Answer re-submitted.',
+				message: this.props.resubmissionMsg,
+				level: 'success',
+				position: 'tr'
 			});
-			if ( this.props.id ) {
-				session.log({
-					id: this.props.id,
-					type: 'FREE_TEXT_QUESTION_SUBMIT_ANSWER',
-					value: this.state.value
-				});
+		} else {
+			let submissionMsg = this.props.submissionMsg;
+			if ( submissionMsg === '' ) {
+				submissionMsg = this.props.solution ?
+					'Compare your answer with solution using the "Show Solution" button. You can then change your answer and re-submit if necessary.' :
+					'You can change your answer and re-submit if you want to.';
 			}
-		};
+			session.addNotification({
+				title: 'Answer submitted.',
+				message: submissionMsg,
+				level: 'success',
+				position: 'tr'
+			});
+		}
 
-		this.handleSolutionClick = () => {
-			const { session } = this.context;
-			if ( this.state.solutionDisplayed ) {
-				this.setState({
-					solutionDisplayed: false,
-					value: this.state.studentAnswer
-				});
-			} else {
-				session.log({
-					id: this.props.id,
-					type: 'FREE_TEXT_QUESTION_DISPLAY_SOLUTION',
-					value: null
-				});
-				this.setState({
-					solutionDisplayed: true,
-					studentAnswer: this.state.value,
-					value: this.props.solution
-				});
-			}
-		};
+		this.setState({
+			submitted: true
+		});
+		if ( this.props.id ) {
+			session.log({
+				id: this.props.id,
+				type: 'FREE_TEXT_QUESTION_SUBMIT_ANSWER',
+				value: this.state.value
+			});
+		}
+	};
+
+	handleSolutionClick = () => {
+		const { session } = this.context;
+		if ( this.state.solutionDisplayed ) {
+			this.setState({
+				solutionDisplayed: false,
+				value: this.state.studentAnswer
+			});
+		} else {
+			session.log({
+				id: this.props.id,
+				type: 'FREE_TEXT_QUESTION_DISPLAY_SOLUTION',
+				value: null
+			});
+			this.setState({
+				solutionDisplayed: true,
+				studentAnswer: this.state.value,
+				value: this.props.solution
+			});
+		}
 	}
 
 	componentWillReceiveProps( nextProps ) {
@@ -104,6 +105,31 @@ class FreeTextQuestion extends Component {
 			});
 			debug( 'Component will receive props.' );
 		}
+	}
+
+	componentDidMount() {
+		const { session } = this.context;
+		this.unsubscribe = session.subscribe( ( type, val ) => {
+			if ( type === 'retrieved_current_user_actions' ) {
+				let actions = val[ this.props.id ];
+				if ( isArray( actions ) ) {
+					actions = actions.filter( action => {
+						return action.type === 'FREE_TEXT_QUESTION_SUBMIT_ANSWER';
+					});
+					if ( actions.length > 0 ) {
+						const lastAction = actions[ 0 ].value;
+						this.setState({
+							value: lastAction,
+							submitted: true
+						});
+					}
+				}
+			}
+		});
+	}
+
+	componentWillUnmount() {
+		this.unsubscribe();
 	}
 
 	/*
