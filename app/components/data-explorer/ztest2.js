cce4ecbffbb0c13c44f6bbaaaf869c8cee36217d
Planeshifter <pburckhardt@outlook.com>
2017-11-27 17:40:57 -0500

    Avoid usage of dashboad and update styling

--- app/components/data-explorer/ztest2.js
+++ app/components/data-explorer/ztest2.js
@@ -1,11 +1,10 @@
 // MODULES //
 
 import React, { Component } from 'react';
-import { Form } from 'react-bootstrap';
 import PropTypes from 'prop-types';
+import { Button, Panel, Row, Col } from 'react-bootstrap';
 import NumberInput from 'components/input/number';
 import SelectInput from 'components/input/select';
-import Dashboard from 'components/dashboard';
 import TeX from 'components/tex';
 import isArray from '@stdlib/assert/is-array';
 import ztest2 from '@stdlib/math/stats/ztest2';
@@ -31,109 +30,116 @@ class ZTest2 extends Component {
 			categories = [];
 		}
 		this.state = {
-			categories: categories
+			categories: categories,
+			var1: props.continuous[ 0 ],
+			grouping: null,
+			var2: null,
+			diff: 0,
+			direction: 'two-sided',
+			alpha: 0.05
 		};
+	}
 
-		this.calculateTwoSampleZTest = ( var1, grouping, var2, diff, direction, alpha ) => {
-			const { data } = this.props;
-			let secondCategory;
-			let firstCategory;
-			let value;
-			let x;
-			let y;
+	calculateTwoSampleZTest = () => {
+		const { var1, grouping, var2, diff, direction, alpha } = this.state;
+		const { data } = this.props;
+		let secondCategory;
+		let firstCategory;
+		let value;
+		let x;
+		let y;
 
-			if ( grouping ) {
-				let categories = data[ grouping ];
-				firstCategory = categories[ 0 ];
-				for ( let i = 1; i < categories.length; i++ ) {
-					if ( categories[ i ] !== firstCategory ) {
-						secondCategory = categories[ i ];
-						break;
-					}
+		if ( grouping ) {
+			let categories = data[ grouping ];
+			firstCategory = categories[ 0 ];
+			for ( let i = 1; i < categories.length; i++ ) {
+				if ( categories[ i ] !== firstCategory ) {
+					secondCategory = categories[ i ];
+					break;
 				}
-				const splitted = bifurcateBy( data[ var1 ], function( x, idx ) {
-					return categories[ idx ] === firstCategory;
-				});
-				x = splitted[ 0 ];
-				y = splitted[ 1 ];
-				const result = ztest2( x, y, stdev( x ), stdev( y ), {
-					'alpha': alpha,
-					'alternative': direction,
-					'difference': diff
-				});
-				let arrow = '\\ne';
-				if ( direction === 'less' ) {
-					arrow = '<';
-				} else if ( direction === 'greater' ){
-					arrow = '>';
-				}
-				value = <div>
-					<label>Hypothesis test for {var1} between {grouping}:</label>
-					<TeX
-						displayMode
-						raw={`H_0: \\mu_{\\text{${grouping}:${firstCategory}}} - \\mu_{\\text{${grouping}:${secondCategory}}} = ${diff}`}
-						tag=""
-					/>
-					<span> vs. </span>
-					<TeX
-						displayMode
-						raw={`H_1: \\mu_{\\text{${grouping}:${firstCategory}}} - \\mu_{\\text{${grouping}:${secondCategory}}} ${arrow} ${diff}`}
-						tag="" />
-					<pre>
-						{result.print()}
-					</pre>
-				</div>;
-			} else if ( var2 ) {
-				x = data[ var1 ];
-				y = data[ var2 ];
-				const result = ztest2( x, y, stdev( x ), stdev( y ), {
-					'alpha': alpha,
-					'alternative': direction,
-					'difference': diff
-				});
-				let arrow = '\\ne';
-				if ( direction === 'less' ) {
-					arrow = '<';
-				} else if ( direction === 'greater' ){
-					arrow = '>';
-				}
-				value = <div>
-					<label>Hypothesis test for {var1} against {var2}:</label>
-					<TeX
-						displayMode
-						raw={`H_0: \\mu_{${var1}} - \\mu_{${var2}} = ${diff}`}
-						tag=""
-					/>
-					<span>vs.</span>
-					<TeX
-						displayMode
-						raw={`\\; H_1: \\mu_{${var1}} - \\mu_{${var2}} ${arrow} ${diff}`}
-						tag=""
-					/>
-					<pre>
-						{result.print()}
-					</pre>
-				</div>;
-			} else {
-				this.props.session.addNotification({
-					title: 'Action required',
-					message: `Please select either a grouping variable or a second variable to compare ${var1} against.`,
-					level: 'warning',
-					position: 'tr'
-				});
 			}
-			if ( value ) {
-				const output = {
-					variable: `Two-Sample Z-Test`,
-					type: 'Test',
-					value: value
-				};
-				this.props.logAction( 'DATA_EXPLORER:TESTS:TWO_SAMPLE_ZTEST', {
-					var1, grouping, var2, diff, direction, alpha
-				});
-				this.props.onCreated( output );
+			const splitted = bifurcateBy( data[ var1 ], function( x, idx ) {
+				return categories[ idx ] === firstCategory;
+			});
+			x = splitted[ 0 ];
+			y = splitted[ 1 ];
+			const result = ztest2( x, y, stdev( x ), stdev( y ), {
+				'alpha': alpha,
+				'alternative': direction,
+				'difference': diff
+			});
+			let arrow = '\\ne';
+			if ( direction === 'less' ) {
+				arrow = '<';
+			} else if ( direction === 'greater' ){
+				arrow = '>';
 			}
-		};
+			value = <div>
+				<label>Hypothesis test for {var1} between {grouping}:</label>
+				<TeX
+					displayMode
+					raw={`H_0: \\mu_{\\text{${grouping}:${firstCategory}}} - \\mu_{\\text{${grouping}:${secondCategory}}} = ${diff}`}
+					tag=""
+				/>
+				<span> vs. </span>
+				<TeX
+					displayMode
+					raw={`H_1: \\mu_{\\text{${grouping}:${firstCategory}}} - \\mu_{\\text{${grouping}:${secondCategory}}} ${arrow} ${diff}`}
+					tag="" />
+				<pre style={{ fontSize: '11px' }}>
+					{result.print()}
+				</pre>
+			</div>;
+		} else if ( var2 ) {
+			x = data[ var1 ];
+			y = data[ var2 ];
+			const result = ztest2( x, y, stdev( x ), stdev( y ), {
+				'alpha': alpha,
+				'alternative': direction,
+				'difference': diff
+			});
+			let arrow = '\\ne';
+			if ( direction === 'less' ) {
+				arrow = '<';
+			} else if ( direction === 'greater' ){
+				arrow = '>';
+			}
+			value = <div>
+				<label>Hypothesis test for {var1} against {var2}:</label>
+				<TeX
+					displayMode
+					raw={`H_0: \\mu_{${var1}} - \\mu_{${var2}} = ${diff}`}
+					tag=""
+				/>
+				<span>vs.</span>
+				<TeX
+					displayMode
+					raw={`\\; H_1: \\mu_{${var1}} - \\mu_{${var2}} ${arrow} ${diff}`}
+					tag=""
+				/>
+				<pre style={{ fontSize: '11px' }}>
+					{result.print()}
+				</pre>
+			</div>;
+		} else {
+			this.props.session.addNotification({
+				title: 'Action required',
+				message: `Please select either a grouping variable or a second variable to compare ${var1} against.`,
+				level: 'warning',
+				position: 'tr'
+			});
+		}
+		if ( value ) {
+			const output = {
+				variable: `Two-Sample Z-Test`,
+				type: 'Test',
+				value: value
+			};
+			this.props.logAction( 'DATA_EXPLORER:TESTS:TWO_SAMPLE_ZTEST', {
+				var1, grouping, var2, diff, direction, alpha
+			});
+			this.props.onCreated( output );
+		}
 	}
 
 	getBinaryVars( vars ) {
@@ -152,60 +158,87 @@ class ZTest2 extends Component {
 		const { continuous, categorical } = this.props;
 		const binary = this.getBinaryVars( categorical );
 		return (
-			<Dashboard
-				title="Two-Sample Z-Test"
-				label="Calculate"
-				autoStart={false}
-				onGenerate={this.calculateTwoSampleZTest}
+			<Panel
+				header={<h4>Two-Sample Z-Test</h4>}
+				style={{ fontSize: '14px' }}
 			>
 				<SelectInput
 					legend="Variable:"
-					defaultValue={continuous[ 0 ]}
+					defaultValue={this.state.var1}
 					options={continuous}
+					onChange={( value ) => {
+						this.setState({
+							var1: value
+						});
+					}}
 				/>
-				<Form>
-					<label>Groups: </label>
-					<SelectInput
-						options={binary}
-						ref={ input => this.ztest2groupSelect = input }
-						clearable
-						onChange={ () => {
-							this.ztest2varSelect.setState({
-								value: null
-							});
-						}}
-					/>
-					{` OR `}
-					<label>Second Variable: </label>
-					<SelectInput
-						options={continuous}
-						ref={ input => this.ztest2varSelect = input }
-						clearable
-						onChange={ () => {
-							this.ztest2groupSelect.setState({
-								value: null
-							});
-						}}
-					/>
-				</Form>
+				<Row>
+					<Col md={5}>
+						<SelectInput
+							legend="Groups:"
+							options={binary}
+							defaultValue={this.state.grouping}
+							clearable
+							onChange={( value ) => {
+								this.setState({
+									grouping: value,
+									var2: null
+								});
+							}}
+						/>
+					</Col>
+					<Col md={2}>
+						<p>OR</p>
+					</Col>
+					<Col md={5}>
+						<SelectInput
+							legend="Second Variable:"
+							options={continuous}
+							defaultValue={this.state.var2}
+							clearable
+							onChange={ ( value ) => {
+								this.setState({
+									var2: value,
+									grouping: null
+								});
+							}}
+						/>
+					</Col>
+				</Row>
 				<NumberInput
 					legend="Difference under H0"
-					defaultValue={0.0}
+					defaultValue={this.state.diff}
 					step="any"
+					onChange={ ( value ) => {
+						this.setState({
+							diff: value
+						});
+					}}
 				/>
 				<SelectInput
 					legend="Direction:"
-					defaultValue="two-sided"
+					defaultValue={this.state.direction}
 					options={[ 'less', 'greater', 'two-sided' ]}
+					onChange={ ( value ) => {
+						this.setState({
+							direction: value
+						});
+					}}
 				/>
 				<NumberInput
 					legend={<span>Significance level <TeX raw="\alpha" /></span>}
-					defaultValue={0.05}
+					defaultValue={this.state.alpha}
 					min={0.0}
 					max={1.0}
 					step="any"
+					onChange={ ( value ) => {
+						this.setState({
+							alpha: value
+						});
+					}}
 				/>
-			</Dashboard>
+				<Button bsStyle="primary" block onClick={this.calculateTwoSampleZTest}>Calculate</Button>
+			</Panel>
 		);
 	}
 }
