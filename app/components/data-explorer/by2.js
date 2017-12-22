5cbfb716101fabba4b817cf92262a62299068387
Philipp Burckhardt <pburckhardt@outlook.com>
2017-12-22 17:41:35 +0100

    Add by2 function

---
+++ app/components/data-explorer/by2.js
@@ -0,0 +1,24 @@
+import isArray from '@stdlib/assert/is-array';
+import hasOwnProp from '@stdlib/assert/has-own-property';
+
+function by2( arr1, arr2, factor, fun ) {
+	let out = {};
+	let ret1 = {};
+	let ret2 = {};
+	for ( let i = 0; i < factor.length; i++ ) {
+		if ( !isArray( ret1[ factor[ i ] ]) ) {
+			ret1[ factor[ i ] ] = [];
+			ret2[ factor[ i ] ] = [];
+		}
+		ret1[ factor[ i ] ].push( arr1[ i ]);
+		ret2[ factor[ i ] ].push( arr2[ i ]);
+	}
+	for ( let key in ret1 ) {
+		if ( hasOwnProp( ret1, key ) ) {
+			out[ key ] = fun( ret1[ key ], ret2[ key ]);
+		}
+	}
+	return out;
+}
+
+export default by2;
