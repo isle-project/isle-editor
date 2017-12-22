5e427535d51e7baec493918aa3a07e5308445b4f
Planeshifter <pburckhardt@outlook.com>
2017-12-22 16:57:40 +0100

    Refactor component

--- app/components/learn/conditional-probability/index.js
+++ app/components/learn/conditional-probability/index.js
@@ -184,80 +184,88 @@ class ConditionalProbability extends Component {
 		});
 	}
 
+	renderTable() {
+		const { A, B, pA, pB, pAB, pAnotB } = this.state;
+		return (
+			<table className="table-bordered">
+				<tbody>
+					<tr>
+						<th></th>
+						<th>{B}</th>
+						<th>not {B}</th>
+					</tr>
+					<tr>
+						<th>{A}</th>
+						<td>
+							<NumberInput
+								inline
+								min={roundn( max( pA - ( 1-pB ), 0 ), -10 )}
+								max={roundn( min( pA, pB ), -10 )}
+								step={0.01}
+								onChange={this.changeProbAB}
+								defaultValue={pAB}
+							/>
+						</td>
+						<td>
+							<NumberInput
+								inline
+								min={roundn( max( pA - pB, 0 ), -10 )}
+								max={roundn( min( pA, 1-pB ), -10 )}
+								step={0.01}
+								onChange={this.changeProbAnotB}
+								defaultValue={pAnotB}
+							/>
+						</td>
+						<td>{pA}</td>
+					</tr>
+					<tr>
+						<th>not {A}</th>
+						<td>
+							<NumberInput
+								inline
+								min={roundn( max( pB - min( pA, pB ), 0 ), -10 )}
+								max={roundn( min( 1-pA, pB ), -10 )}
+								step={0.01}
+								onChange={this.changeProbNotAB}
+								defaultValue={this.state.pnotAB}
+							/>
+						</td>
+						<td>
+							<NumberInput
+								inline
+								min={roundn( max( 1 - pA - pB, 0 ), -10 )}
+								max={roundn( min( 1-pA, 1-pB ), -10 )}
+								step={0.01}
+								onChange={this.changeProbNotAnotB}
+								defaultValue={this.state.pnotAnotB}
+							/>
+						</td>
+						<td>{roundn( 1-pA, -3 )}</td>
+					</tr>
+					<tr>
+						<th></th>
+						<td>{pB}</td>
+						<td>{roundn( 1-pB, -3 )}</td>
+						<td>1.0</td>
+					</tr>
+				</tbody>
+			</table>
+		);
+	}
+
 	render() {
+		const { A, B, pA, pB } = this.state;
 		return (
 			<div>
-				<p>Consider events <TextInput inline defaultValue={this.state.A} onChange={( A ) => this.setState({ A })} /> and <TextInput inline defaultValue={this.state.B} onChange={( B ) => this.setState({ B })} />. Let <TeX raw={`P(\\text{${this.state.A}}) = `} /><NumberInput min={0} max={1} step={0.01} defaultValue={0.5} inline onChange={this.changeProbA} /> and  <TeX raw={`P(\\text{${this.state.B}}) = `} /><NumberInput min={0} max={1} step={0.01} defaultValue={0.5} inline onChange={this.changeProbB} />. Then we have <TeX raw={`P(\\text{${this.state.A}}^C) = ${roundn( 1-this.state.pA, -4 )}`} /> and <TeX raw={`P(\\text{${this.state.B}}^C) = ${roundn( 1-this.state.pB, -4 )}`} />. These form the marginals in the following probability table:</p>
+				<p>Consider events <TextInput inline defaultValue={A} onChange={( A ) => this.setState({ A })} /> and <TextInput inline defaultValue={B} onChange={( B ) => this.setState({ B })} />. Let <TeX raw={`P(\\text{${A}}) = `} /><NumberInput min={0} max={1} step={0.01} defaultValue={0.5} inline onChange={this.changeProbA} /> and  <TeX raw={`P(\\text{${B}}) = `} /><NumberInput min={0} max={1} step={0.01} defaultValue={0.5} inline onChange={this.changeProbB} />. Then we have <TeX raw={`P(\\text{${A}}^C) = ${roundn( 1-pA, -4 )}`} /> and <TeX raw={`P(\\text{${B}}^C) = ${roundn( 1-pB, -4 )}`} />. These form the marginals in the following probability table:</p>
 				<Panel>
 					<Grid>
 						<Row>
 							<Col md={4}>
-								<p>What are the inner cells equal to? If the events are random, we have <TeX raw={`P(\\text{${this.state.A}} \\cap \\text{${this.state.B}} ) = P(\\text{${this.state.A}}) \\cdot P(\\text{${this.state.B}}) = ${roundn( this.state.pA*this.state.pB, -4 )}`} />. </p>
-								<table className="table-bordered">
-									<tbody>
-										<tr>
-											<th></th>
-											<th>{this.state.B}</th>
-											<th>not {this.state.B}</th>
-										</tr>
-										<tr>
-											<th>{this.state.A}</th>
-											<td>
-												<NumberInput
-													inline
-													min={roundn( max( this.state.pA - ( 1-this.state.pB ), 0 ), -10 )}
-													max={roundn( min( this.state.pA, this.state.pB ), -10 )}
-													step={0.01}
-													onChange={this.changeProbAB}
-													defaultValue={this.state.pAB}
-												/>
-											</td>
-											<td>
-												<NumberInput
-													inline
-													min={roundn( max( this.state.pA - this.state.pB, 0 ), -10 )}
-													max={roundn( min( this.state.pA, 1-this.state.pB ), -10 )}
-													step={0.01}
-													onChange={this.changeProbAnotB}
-													defaultValue={this.state.pAnotB}
-												/>
-											</td>
-											<td>{this.state.pA}</td>
-										</tr>
-										<tr>
-											<th>not {this.state.A}</th>
-											<td>
-												<NumberInput
-													inline
-													min={roundn( max( this.state.pB - min( this.state.pA, this.state.pB ), 0 ), -10 )}
-													max={roundn( min( 1-this.state.pA, this.state.pB ), -10 )}
-													step={0.01}
-													onChange={this.changeProbNotAB}
-													defaultValue={this.state.pnotAB}
-												/>
-											</td>
-											<td>
-												<NumberInput
-													inline
-													min={roundn( max( 1 - this.state.pA - this.state.pB, 0 ), -10 )}
-													max={roundn( min( 1-this.state.pA, 1-this.state.pB ), -10 )}
-													step={0.01}
-													onChange={this.changeProbNotAnotB}
-													defaultValue={this.state.pnotAnotB}
-												/>
-											</td>
-											<td>{roundn( 1-this.state.pA, -3 )}</td>
-										</tr>
-										<tr>
-											<th></th>
-											<td>{this.state.pB}</td>
-											<td>{roundn( 1-this.state.pB, -3 )}</td>
-											<td>1.0</td>
-										</tr>
-									</tbody>
-								</table>
+								<p>What are the inner cells equal to? If the events are random, we have <TeX raw={`P(\\text{${A}} \\cap \\text{${B}} ) = P(\\text{${A}}) \\cdot P(\\text{${B}}) = ${roundn( pA*pB, -4 )}`} />. </p>
+								{this.renderTable()}
 								<br />
-								{probabilityTableLabels({ A: this.state.A, B: this.state.B })}
+								{probabilityTableLabels({ A: A, B: B })}
 								<br />
 								<FeedbackButtons
 									for="independence_applet"
@@ -265,10 +273,10 @@ class ConditionalProbability extends Component {
 							</Col>
 							<Col md={8}>
 								<p>What about the conditional probabilities?</p>
-								{ this.state.pB === 0 || this.state.pA === 0 ?
+								{ pB === 0 || pA === 0 ?
 									<Panel>
-										{ this.state.pB === 0 ? <span>The conditional probability is undefined when <TeX raw={`P( \\text{${this.state.B}}) = 0`} /></span> : <span>
-												When <TeX raw={`P( ${this.state.A}) = 0`} />, we must have <TeX raw={`P( \\text{${this.state.A}} \\mid \\text{${this.state.B}} ) = P( \\text{${this.state.A}} \\mid \\text{${this.state.B}}^C ) = 0`} /> in case B has non-zero probability.
+										{ pB === 0 ? <span>The conditional probability is undefined when <TeX raw={`P( \\text{${B}}) = 0`} /></span> : <span>
+												When <TeX raw={`P( ${A}) = 0`} />, we must have <TeX raw={`P( \\text{${A}} \\mid \\text{${B}} ) = P( \\text{${A}} \\mid \\text{${B}}^C ) = 0`} /> in case B has non-zero probability.
 										</span>}
 									</Panel> :
 									<Grid>
@@ -285,30 +293,30 @@ class ConditionalProbability extends Component {
 													<VictoryAxis dependentAxis tickFormat={( x ) => x/100} />
 													<VictoryAxis />
 													<VictoryBar
-														data={[ {x: `P( ${this.state.A}|${this.state.B})`, y: 100*this.state.pAgivenB }, {x: `P( ${this.state.A}|not ${this.state.B} )`, y: this.state.pAgivenNotB*100 } ]}
+														data={[ {x: `P( ${A}|${B})`, y: 100*this.state.pAgivenB }, {x: `P( ${A}|not ${B} )`, y: this.state.pAgivenNotB*100 } ]}
 														domain={{ y: [ 0, 100 ] }}
 														x="x"
 														y="y"
 														labels={( d ) => roundn( d.y/100, -3 )}
 													/>
-													{ this.state.independent ? <VictoryLine data={[ {x: `P( ${this.state.A}| ${this.state.B})`, y: 100*this.state.pAgivenB }, {x: `P( ${this.state.A}|not  ${this.state.B})`, y: this.state.pAgivenNotB*100 } ]} /> : null }
+													{ this.state.independent ? <VictoryLine data={[ {x: `P( ${A}| ${B})`, y: 100*this.state.pAgivenB }, {x: `P( ${A}|not  ${B})`, y: this.state.pAgivenNotB*100 } ]} /> : null }
 												</VictoryChart>
 											</Col>
 										</Row>
 										<Row>
 											<NumberInput
-												legend={<span>Choose <TeX raw={`P(\\text{${this.state.A}} \\mid \\text{${this.state.B})}`} /></span>}
-												min={max( ( this.state.pA-1+this.state.pB )/this.state.pB, 0 )}
-												max={min( this.state.pA/this.state.pB, 1 )}
+												legend={<span>Choose <TeX raw={`P(\\text{${A}} \\mid \\text{${B})}`} /></span>}
+												min={max( ( pA-1+pB )/pB, 0 )}
+												max={min( pA/pB, 1 )}
 												step={0.001}
 												onChange={this.changeProbAgivenB}
 												defaultValue={this.state.pAgivenB}
 												inline
 											/>
 											<NumberInput
-												legend={<span>Choose <TeX raw={`P( \\text{${this.state.A}} \\mid \\text{ ${this.state.B} }^C)`} /></span>}
-												min={max( ( this.state.pA-this.state.pB ) / ( 1-this.state.pB ), 0 )}
-												max={min( this.state.pA/( 1-this.state.pB ), 1 )}
+												legend={<span>Choose <TeX raw={`P( \\text{${A}} \\mid \\text{ ${B} }^C)`} /></span>}
+												min={max( ( pA-pB ) / ( 1-pB ), 0 )}
+												max={min( pA/( 1-pB ), 1 )}
 												step={0.001}
 												onChange={this.changeProbAgivenNotB}
 												defaultValue={this.state.pAgivenNotB}
@@ -317,7 +325,7 @@ class ConditionalProbability extends Component {
 										</Row>
 										<Row>
 											{ this.state.independent ?
-												<span>The events are independent because <TeX raw={`P( \\text{${this.state.A}} \\mid \\text{ ${this.state.B} } ) = P( \\text{${this.state.A}} \\mid \\text{ ${this.state.B} }^C )`} /></span> : <span>The events are <b>not</b> independent because <TeX raw={this.state.pAgivenB === 0 && this.state.pAgivenNotB === 0 ? `P( \\text{ ${this.state.A} } \\mid \\text{ ${this.state.B} } ) = P( \\text{${this.state.A}} \\mid \\text{ ${this.state.B} }^C \\neq P( \\text{${this.state.A}})` : `P( \\text{ ${this.state.A} } \\mid \\text{ ${this.state.B} } ) \\neq P( \\text{${this.state.A}} \\mid \\text{ ${this.state.B} }^C )`} /></span>
+												<span>The events are independent because <TeX raw={`P( \\text{${A}} \\mid \\text{ ${B} } ) = P( \\text{${A}} \\mid \\text{ ${B} }^C )`} /></span> : <span>The events are <b>not</b> independent because <TeX raw={this.state.pAgivenB === 0 && this.state.pAgivenNotB === 0 ? `P( \\text{ ${A} } \\mid \\text{ ${B} } ) = P( \\text{${A}} \\mid \\text{ ${B} }^C \\neq P( \\text{${A}})` : `P( \\text{ ${A} } \\mid \\text{ ${B} } ) \\neq P( \\text{${A}} \\mid \\text{ ${B} }^C )`} /></span>
 											}
 										</Row>
 									</Grid>
