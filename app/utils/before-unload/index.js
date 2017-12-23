12ea8ff16b97fff5fbf7b81ada4d426b2f33099f
Planeshifter <pburckhardt@outlook.com>
2017-12-23 15:21:27 +0100

    Disable lint rule

--- app/utils/before-unload/index.js
+++ app/utils/before-unload/index.js
@@ -1,7 +1,7 @@
-// BEFORE UNLOAD //
+// MAIN //
 
 function beforeUnload( e ) {
-	let confirmationMessage = '\o/';
+	let confirmationMessage = '\o/'; // eslint-disable-line no-useless-escape
 	e.returnValue = confirmationMessage; // Gecko, Trident, Chrome 34+
 	return confirmationMessage; // Gecko, WebKit, Chrome <34
 } // end FUNCTION beforeUnload()
