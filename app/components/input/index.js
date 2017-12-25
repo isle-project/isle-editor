208ed8293caf37e2468f9d843a84ae1e8c22ee9d
Planeshifter <pburckhardt@outlook.com>
2017-12-25 21:16:12 +0100

    Add voice input

--- app/components/input/index.js
+++ app/components/input/index.js
@@ -5,8 +5,9 @@ import NumberInput from 'components/input/number';
 import SelectInput from 'components/input/select';
 import SliderInput from 'components/input/slider';
 import TextInput from 'components/input/text';
+import VoiceInput from 'components/input/voice';
 
 
 // EXPORTS //
 
-export { CheckboxInput, NumberInput, SelectInput, SliderInput, TextInput };
+export { CheckboxInput, NumberInput, SelectInput, SliderInput, TextInput, VoiceInput };
