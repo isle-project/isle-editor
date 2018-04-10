c7612d8e30aaf60cc939c2d8a6448645bb416643
Frank Kovacs <fran70kk@gmail.com>
2018-04-10 16:07:09 -0400

    Started working on calculator, pushing to get on my real computer

---
+++ app/components/calculator/index.js
@@ -0,0 +1,91 @@
+// MODULES //
+
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+
+// MAIN //
+
+class Calculator extends Component {
+	/**
+	* Constructor function
+	*/
+	constructor() {
+		super();
+
+		// Set initial component states...
+		this.state = {
+			visible: true
+		};
+	}
+	/*
+	* Invoked once on client after the initial rendering. Sets up component timer.
+	*/
+	componentDidMount() {
+		if ( this.props.active ) {
+			this.setTimer();
+		}
+	}
+	/**
+	* Invoked when component is receiving new children, this method sets
+	*  up the time and sets the component visible before rendering.
+	*
+	* @param {Object} newProps - new properties
+	*/
+	componentWillReceiveProps( newProps ) {
+		if ( newProps.children !== this.props.children ) {
+			this.setTimer();
+			this.setState({ visible: true });
+		}
+		if ( newProps.active ) {
+			this.setTimer();
+		}
+	}
+	/*
+	* Invoked immediately before component is unmounted from DOM. Performs clean-up of
+	* component timer.
+	*/
+	componentWillUnmount() {
+		clearTimeout( this._timer );
+	}
+
+	setTimer() {
+		if ( this._timer !== null ) {
+			clearTimeout( this._timer );
+		}
+		this._timer = setTimeout( () => {
+			this.setState({ visible: false });
+			this._timer = null;
+		}, this.props.delay );
+	}
+
+	/**
+	* React component render method
+	*/
+	render() {
+		return this.state.visible ?
+			<div>{this.props.children}</div> :
+			<span />;
+	}
+}
+
+
+// DEFAULT PROPERTIES //
+
+Calculator.defaultProps = {
+	active: false,
+	delay: 1000
+};
+
+
+// PROPERTY TYPES //
+
+Calculator.propTypes = {
+	active: PropTypes.bool,
+	delay: PropTypes.number
+};
+
+
+// EXPORTS //
+
+export default Calculator;
