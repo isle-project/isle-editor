7fdd6f25e1de9bcfec87864c388bb7736583578a
Frank Kovacs <fkovacs@andrew.cmu.edu>
2018-05-27 16:55:49 -0400

    Added instructor notes update

--- app/components/statusbar/instructor-view/index.js
+++ app/components/statusbar/instructor-view/index.js
@@ -17,7 +17,8 @@ import max from '@stdlib/math/base/special/max';
 import isElectron from 'utils/is-electron';
 import ActionLog from 'components/statusbar/action-log';
 import RangePicker from 'components/range-picker';
-import MarkdownEditor from 'components/markdown-editor';
+// import MarkdownEditor from 'components/markdown-editor';
+import InstructorNotes from 'components/statusbar/instructor-notes';
 import animatePosition from 'utils/animate-position';
 import UserList from './user_list.js';
 import './instructor_view.css';
@@ -231,8 +232,10 @@ class InstructorView extends Component {
 						{<span>Instructor Notes</span>}
 					</Panel.Title>
 				</Panel.Heading>
-				<Panel.Body collapsible>
-					<MarkdownEditor id={'instructor_view'} submitButton />
+				<Panel.Body className='panel-body panel_instructor' collapsible >
+					<InstructorNotes
+						id="instructor_notes"
+					/>
 				</Panel.Body>
 			</Panel>
 		</PanelGroup> );
