659982469739e87927e2969e6d596c37b9a04c30
Planeshifter <pburckhardt@outlook.com>
2018-06-06 21:38:32 +0200

    Add panel for PDF options in save modal

--- app/components/markdown-editor/save_modal.js
+++ app/components/markdown-editor/save_modal.js
@@ -1,39 +1,75 @@
 // MODULES //
 
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Button from 'react-bootstrap/lib/Button';
 import Modal from 'react-bootstrap/lib/Modal';
+import Panel from 'react-bootstrap/lib/Panel';
 
 
 // MAIN //
 
-const SaveModal = ( props ) => {
-	return ( <Modal
-		onHide={props.onHide}
-		show={props.show}
-	>
-		<Modal.Header closeButton>
-			<Modal.Title>Save Report</Modal.Title>
-		</Modal.Header>
-		<Modal.Body>
-			<div className="well">
-				<Button onClick={props.handleSave} bsStyle="primary" bsSize="large" block>
-					Save (in browser)
-				</Button>
-				<Button onClick={props.exportHTML} bsStyle="primary" bsSize="large" block>
-					Export as HTML
-				</Button>
-				<Button onClick={props.exportPDF} bsStyle="primary" bsSize="large" block>
-					Export as PDF
-				</Button>
-				<Button onClick={props.saveMarkdown} bsSize="large" block>
-					Export Markdown Source (to restore later)
-				</Button>
-			</div>
-		</Modal.Body>
-	</Modal> );
-};
+class SaveModal extends Component {
+	constructor( props ) {
+		super( props );
+
+		this.state = {
+			openPDF: false
+		};
+	}
+
+	togglePDFMenu = () => {
+		this.setState({
+			openPDF: !this.state.openPDF
+		});
+	}
+
+	savePDF = () => {
+		this.props.exportPDF();
+	}
+
+	clickHide = () => {
+		this.setState({
+			openPDF: false
+		}, () => {
+			this.props.onHide();
+		});
+	}
+
+	render() {
+		return ( <Modal
+			onHide={this.clickHide}
+			show={this.props.show}
+		>
+			<Modal.Header closeButton>
+				<Modal.Title>Save Report</Modal.Title>
+			</Modal.Header>
+			<Modal.Body>
+				<div className="well">
+					<Button onClick={this.props.handleSave} bsStyle="primary" bsSize="large" block>
+						Save (in browser)
+					</Button>
+					<Button onClick={this.props.exportHTML} bsStyle="primary" bsSize="large" block>
+						Export as HTML
+					</Button>
+					<Button onClick={this.togglePDFMenu} bsStyle="primary" bsSize="large" block>
+						Export as PDF
+					</Button>
+					<Panel id="export-pdf-panel" expanded={this.state.openPDF}>
+						<Panel.Collapse>
+							<Panel.Body>
+								<Button onClick={this.savePDF} >Save</Button>
+							</Panel.Body>
+						</Panel.Collapse>
+					</Panel>
+					<Button onClick={this.props.saveMarkdown} bsSize="large" block>
+						Export Markdown Source (to restore later)
+					</Button>
+				</div>
+			</Modal.Body>
+		</Modal> );
+	}
+}
 
 
 // PROPERTY TYPES //
