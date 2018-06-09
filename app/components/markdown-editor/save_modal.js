bc4392ced3d452d048630a8b73dc70e02cf1d203
Frank Kovacs <fkovacs@andrew.cmu.edu>
2018-06-09 12:48:54 -0400

    Updated layout of save modal

--- app/components/markdown-editor/save_modal.js
+++ app/components/markdown-editor/save_modal.js
@@ -5,9 +5,13 @@ import PropTypes from 'prop-types';
 import Button from 'react-bootstrap/lib/Button';
 import Modal from 'react-bootstrap/lib/Modal';
 import Panel from 'react-bootstrap/lib/Panel';
+import Col from 'react-bootstrap/lib/Col';
+import Row from 'react-bootstrap/lib/Row';
 import SelectInput from 'components/input/select';
 import CheckboxInput from 'components/input/checkbox';
 import NumberInput from 'components/input/number';
+import pageSizes from './page_sizes.json';
+
 
 // MAIN //
 
@@ -19,17 +23,29 @@ class SaveModal extends Component {
 			openPDF: false,
 			pageSize: 'LETTER',
 			customSize: false,
-			customWidth: 11 * 72,
-			customHeight: 8.5 * 72,
+			showPageOptions: false,
+			pageOptionConfig: 'Predefined',
+			customWidth: 8.5 * 72,
+			customHeight: 11 * 72,
 			useString: true,
-			pageOrientation: 'portrait'
+			pageOrientation: 'portrait',
+			visibleWidth: 8.5,
+			visibleHeight: 11
 		};
 	}
 
 	togglePDFMenu = () => {
-		this.setState({
-			openPDF: !this.state.openPDF
-		});
+		// Do some control flow to hide pageOptions too
+		if ( this.state.openPDF && this.state.showPageOptions ) {
+			this.setState({
+				openPDF: !this.state.openPDF,
+				showPageOptions: !this.state.showPageOptions
+			});
+		} else {
+			this.setState({
+				openPDF: !this.state.openPDF
+			});
+		}
 	}
 
 	savePDF = () => {
@@ -38,7 +54,8 @@ class SaveModal extends Component {
 		if ( this.state.useString ) {
 			// If we use the string make it the string
 			pageDims = this.state.pageSize;
-		} else {
+		}
+		if ( !this.state.useString || this.state.pageSize === 'POSTER' ) {
 			pageDims = {};
 			pageDims.height = this.state.customHeight;
 			pageDims.width = this.state.customWidth;
@@ -51,7 +68,8 @@ class SaveModal extends Component {
 	clickHide = () => {
 		this.setState({
 			openPDF: false,
-			customSize: false
+			customSize: false,
+			showPageOptions: false
 		}, () => {
 			this.props.onHide();
 		});
@@ -67,92 +85,134 @@ class SaveModal extends Component {
 			</Modal.Header>
 			<Modal.Body>
 				<div className="well">
-					<Button onClick={this.props.handleSave} bsStyle="primary" bsSize="large" block>
-						Save (in browser)
-					</Button>
-					<Button onClick={this.props.exportHTML} bsStyle="primary" bsSize="large" block>
-						Export as HTML
-					</Button>
-					<Button onClick={this.togglePDFMenu} bsStyle="primary" bsSize="large" block>
-						Export as PDF
-					</Button>
-					<Panel id="export-pdf-panel" expanded={this.state.openPDF}>
-						<Panel.Collapse>
-							<Panel.Body>
-								<Button onClick={this.savePDF} >Save</Button>
-								<CheckboxInput
-									legend="Custom Page Dimensions?"
-									defaultValue={false}
-									onChange={( value )=>{
-										this.setState({
-											customSize: value
-										});
-									}}
-								/>
-								<SelectInput
-									legend="Orientation"
-									defaultValue={'portrait'}
-									options={['portrait', 'landscape']}
-									onChange={( value )=>{
-										this.setState({
-											pageOrientation: value
-										});
-									}}
-								/>
-								<Panel expanded={this.state.customSize}>
-									<Panel.Collapse>
-										<Panel.Body>
-											<div>
+					<div>
+						<Button onClick={this.props.handleSave} bsStyle="primary" bsSize="large" block>
+							Save (in browser)
+						</Button>
+					</div>
+					<div>
+						<Button onClick={this.props.exportHTML} bsStyle="primary" bsSize="large" block>
+							Export as HTML
+						</Button>
+					</div>
+					<div>
+						<Button onClick={this.togglePDFMenu} bsStyle="primary" bsSize="large" block>
+							Export as PDF
+						</Button>
+						<Panel id="export-pdf-panel" expanded={this.state.openPDF}>
+							<Panel.Collapse>
+								<Panel.Body>
+									<Row className="showDimensions">
+										<Col xs={3} md={3}>
+											<CheckboxInput
+												legend="Choose Page Dimensions?"
+												defaultValue={false}
+												onChange={( value )=>{
+													this.setState({
+														customSize: value
+													});
+												}}
+											/>
+										</Col>
+										<Col xs={2} md={2}>
+											<SelectInput
+												legend="Page Sizing"
+												disabled={!this.state.customSize}
+												defaultValue={'Predefined'}
+												options={['Predefined', 'Custom']}
+												onChange={( value )=>{
+													this.setState({
+														pageOptionConfig: value
+													});
+												}}
+											/>
+										</Col>
+										<Col xs={3} md={3}>
+											<SelectInput
+												legend="Orientation"
+												defaultValue={'portrait'}
+												options={['portrait', 'landscape']}
+												onChange={( value )=>{
+													var oldWidth = this.state.visibleWidth;
+													var oldHeight = this.state.visibleHeight;
+													this.setState({
+														pageOrientation: value,
+														visibleWidth: oldHeight,
+														visibleHeight: oldWidth
+													});
+												}}
+											/>
+										</Col>
+										<Col xs={4} md={4}>
+											<p>Current Page Size: {this.state.visibleWidth} x {this.state.visibleHeight}</p>
+										</Col>
+									</Row>
+									<Panel expanded={(this.state.pageOptionConfig === 'Predefined') && this.state.customSize}>
+										<Panel.Collapse>
+											<Panel.Body>
 												<SelectInput
 													legend="Pick a predefined value"
 													defaultValue={'LETTER'}
-													options={['LETTER', 'A4', 'A5', 'A10', 'LEGAL', 'EXECUTIVE', 'TABLOID']}
+													options={['LETTER', 'LEGAL', 'A4', 'B5', 'TABLOID', 'EXECUTIVE', 'POSTER']}
 													onChange={( value )=>{
 														this.setState({
 															pageSize: value,
-															useString: true
+															useString: true,
+															visibleHeight: pageSizes[value].height,
+															visibleWidth: pageSizes[value].width,
+															customHeight: pageSizes[value].height * 72,
+															customWidth: pageSizes[value].width * 72
 														});
 													}}
 												/>
-											</div>
-											<div>
+											</Panel.Body>
+										</Panel.Collapse>
+									</Panel>
+									<Panel expanded={(this.state.pageOptionConfig === 'Custom') && this.state.customSize}>
+										<Panel.Collapse>
+											<Panel.Body>
 												<p>Custom Sizes</p>
 												<NumberInput
 													legend="Pick the width (Inches)"
-													defaultValue={8.5}
+													defaultValue={this.state.visibleWidth}
 													min={1}
 													max={50}
 													step={0.5}
 													onChange={( value ) =>{
 														this.setState({
 															customWidth: 72 * value,
-															useString: false
+															useString: false,
+															visibleWidth: value
 														});
 													}}
 												/>
 												<NumberInput
 													legend="Pick the height (Inches)"
-													defaultValue={11.5}
+													defaultValue={this.state.visibleHeight}
 													min={1}
 													max={50}
 													step={0.5}
 													onChange={( value ) =>{
 														this.setState({
 															customHeight: 72 * value,
-															useString: false
+															useString: false,
+															visibleHeight: value
 														});
 													}}
 												/>
-											</div>
-										</Panel.Body>
-									</Panel.Collapse>
-								</Panel>
-							</Panel.Body>
-						</Panel.Collapse>
-					</Panel>
+											</Panel.Body>
+										</Panel.Collapse>
+									</Panel>
+									<Button onClick={this.savePDF} block>Save</Button>
+								</Panel.Body>
+							</Panel.Collapse>
+						</Panel>
+					</div>
+					<div>
 					<Button onClick={this.props.saveMarkdown} bsSize="large" block>
 						Export Markdown Source (to restore later)
 					</Button>
+					</div>
 				</div>
 			</Modal.Body>
 		</Modal> );
