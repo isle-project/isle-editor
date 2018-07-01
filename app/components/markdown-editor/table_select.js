b8add6c71185c2bb68793c306e2612dcea84657e
Planeshifter <pburckhardt@outlook.com>
2018-06-30 22:31:56 -0400

    Clean up component

--- app/components/markdown-editor/table_select.js
+++ app/components/markdown-editor/table_select.js
@@ -4,8 +4,6 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Button from 'react-bootstrap/lib/Button';
 import Modal from 'react-bootstrap/lib/Modal';
-import ReactDom from 'react-dom';
-import ReactTable from 'react-table';
 import noop from '@stdlib/utils/noop';
 import './table-select.css';
 
@@ -20,22 +18,16 @@ class TableSelect extends Component {
 		this.state = {
 			rows: 4,
 			cols: 3,
-			shouldUpdate: true,
 			hoverRow: 0,
 			hoverCol: 0
 		};
-	}
-
-	shouldComponentUpdate(nextProps, nextState, nextContext) {
-		return this.state.shouldUpdate;
 	}
 
 	onClickFactory = (row, col) => {
 		return () => {
 			this.setState({
 				rows: row,
-				cols: col,
-				shouldUpdate: true
+				cols: col
 			});
 		};
 	}
@@ -44,39 +36,42 @@ class TableSelect extends Component {
 		return () => {
 			this.setState({
 				hoverRow: row,
-				hoverCol: col,
-				shouldUpdate: true
+				hoverCol: col
 			});
 		};
 	}
 
 	createTable = () => {
-		var rows = new Array(8);
-		var cols;
-		for ( var i = 9; i > 0; i-- ) {
+		const rows = new Array( 8 );
+		let cols;
+		for ( let i = 9; i > 0; i-- ) {
 			// Index is i - 1
-			cols = new Array(8);
-			for ( var j = 9; j > 0; j-- ) {
-				if ( (j <= this.state.cols) && (i <= this.state.rows) ) {
-					cols[j - 1] = <td className="grid_select"
+			cols = new Array( 8 );
+			for ( let j = 9; j > 0; j-- ) {
+				if ( j <= this.state.cols && i <= this.state.rows ) {
+					cols[ j - 1 ] = <td
+						key={`${i}-${j}`}
+						className="grid_select"
 						onClick={this.onClickFactory(i, j)}
 						onMouseOver={this.onMouseOverFactory(i, j)}
-						>Cell</td>; // eslint-disable-line
+					>Cell</td>;
 				} else {
-					cols[j - 1] = <td className="cell"
+					cols[ j - 1 ] = <td
+						key={`${i}-${j}`}
+						className="cell"
 						onClick={this.onClickFactory(i, j)}
 						onMouseOver={this.onMouseOverFactory(i, j)}
-						>Cell</td>; // eslint-disable-line
+					>Cell</td>;
 				}
 			}
 			rows[i - 1] = <tr>{cols}</tr>;
 		}
-		var header = new Array(9);
-		for ( var z = 0; z < header.length; z++ ) {
+		const header = new Array( 9 );
+		for ( let z = 0; z < header.length; z++ ) {
 			if ( z <= this.state.cols - 1 ) {
-				header[z] = <th className="selected_cols">Col{z + 1}</th>;
+				header[z] = <th key={z} className="selected_cols">Col{z + 1}</th>;
 			} else {
-				header[z] = <th className="un_selected_cols">Col{z + 1}</th>;
+				header[z] = <th key={z} className="un_selected_cols">Col{z + 1}</th>;
 			}
 		}
 		return <table className="tableSelect" align="center"><thead>{header}</thead><tbody>{rows}</tbody></table>;
@@ -85,33 +80,34 @@ class TableSelect extends Component {
 	insertTableText = () => {
 		var tableStr = '|';
 
-		// First make the header
-		for ( var colStrIndex = 0; colStrIndex < this.state.cols; colStrIndex++ ) {
+		// First make the header:
+		for ( let colStrIndex = 0; colStrIndex < this.state.cols; colStrIndex++ ) {
 			tableStr += ' Column' + (colStrIndex + 1) + ' |';
 		}
 		tableStr += '\n';
 
-		// Now make the split
-		for ( var splitIndex = 0; splitIndex < this.state.cols; splitIndex++ ) {
+		// Make the split:
+		for ( let splitIndex = 0; splitIndex < this.state.cols; splitIndex++ ) {
 			tableStr += '| --- ';
 		}
 		tableStr += '|\n';
 
-		// Now add the cells
-		for ( var rowCount = 0; rowCount < this.state.rows; rowCount++ ) {
-			for ( var colInRow = 0; colInRow < this.state.cols; colInRow++ ) {
+		// Add the cells:
+		for ( let rowCount = 0; rowCount < this.state.rows; rowCount++ ) {
+			for ( let colInRow = 0; colInRow < this.state.cols; colInRow++ ) {
 				tableStr += '| Cell ';
 			}
 			tableStr += '|\n';
 		}
 		tableStr += '\n';
-		// Pass up props to parent
-		this.props.onClick(tableStr);
-		// Reset state
+
+		// Pass up props to parent:
+		this.props.onClick( tableStr );
+
+		// Reset state:
 		this.setState({
 			rows: 4,
 			cols: 3,
-			shouldUpdate: true,
 			hoverRow: 0,
 			hoverCol: 0
 		});
