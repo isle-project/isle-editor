e7d935e76e2f0be621e2ffc585c5201be14b4910
Planeshifter <pburckhardt@outlook.com>
2018-07-02 00:21:01 -0400

    Add tooltip

--- app/components/data-explorer/clear_button.js
+++ app/components/data-explorer/clear_button.js
@@ -3,17 +3,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Button from 'react-bootstrap/lib/Button';
+import OverlayTrigger from 'react-bootstrap/lib/OverlayTrigger';
+import Tooltip from 'react-bootstrap/lib/Tooltip';
+
+
+// VARIABLES //
+
+const tooltip = <Tooltip
+	id="clear_tooltip"
+>
+	Delete
+</Tooltip>;
 
 
 // MAIN //
 
-const ClearButton = ( props ) => ( <Button
-	bsSize="xs"
-	style={{ float: 'right' }}
-	onClick={props.onClick}
->
-	<span>&times;</span>
-</Button> );
+const ClearButton = ( props ) => (
+	<OverlayTrigger
+		placement="bottom"
+		overlay={tooltip}
+		rootClose={true}
+	>
+		<Button
+			bsSize="xs"
+			style={{ float: 'right' }}
+			onClick={props.onClick}
+		>
+			<span>&times;</span>
+		</Button>
+	</OverlayTrigger>
+);
 
 ClearButton.propTypes = {
 	'onClick': PropTypes.func.isRequired
