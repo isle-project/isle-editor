bd4bee1da31d75fce3a07047a486e51220865159
Planeshifter <pburckhardt@outlook.com>
2018-07-17 00:47:56 -0400

    Parse Markdown code correctly

--- app/utils/markdown-to-html/tokenizer.js
+++ app/utils/markdown-to-html/tokenizer.js
@@ -7,6 +7,7 @@ const IN_STRING_ATTRIBUTE = 3;
 const IN_JSX_ATTRIBUTE = 4;
 const IN_BETWEEN_TAGS = 5;
 const IN_JSX_EXPRESSION = 6;
+const IN_CODE = 7;
 
 
 // FUNCTIONS //
@@ -60,6 +61,20 @@ class Tokenizer{
 			this._state = IN_OPENING_TAG;
 			this.tokens.push( this._current );
 			this._level += 1;
+			this._current = char;
+		} else if ( char === '`' && this._buffer.charAt( i+1 ) !== '`' ) {
+			this._state = IN_CODE;
+			this.tokens.push( this._current );
+			this._current = char;
+		} else {
+			this._current += char;
+		}
+	}
+
+	_inCode( char, i ) {
+		if ( char === '`' ) {
+			this._state = IN_BASE;
+			this.tokens.push( this._current );
 			this._current = char;
 		} else {
 			this._current += char;
@@ -163,6 +178,9 @@ class Tokenizer{
 			switch ( this._state ) {
 			case IN_BASE:
 				this._inBase( char, i );
+				break;
+			case IN_CODE:
+				this._inCode( char, i );
 				break;
 			case IN_OPENING_TAG:
 				this._inOpeningTag( char, i );
