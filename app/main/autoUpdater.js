cbe15cccd26dfb4e2ad5043c391e2824a2d6c753
Planeshifter <pburckhardt@outlook.com>
2018-07-26 17:45:36 -0400

    Use native electron ClientRequest

--- app/main/autoUpdater.js
+++ app/main/autoUpdater.js
@@ -1,22 +1,38 @@
 // MODULES //
 
 import semver from 'semver';
+import { net } from 'electron';
 import { version as currentVersion } from './../../package.json';
+import logger from 'debug';
 
+
+// VARIABLES //
+
+const debug = logger( 'isle-editor:auto-updater' );
 
 // MAIN //
 
 function autoUpdater( callback ) {
-	const link = 'https://raw.githubusercontent.com/planeshifter/isle-editor/master/package.json';
-	fetch( link )
-		.then( res => res.json() )
-		.then( body => {
-			const newVersion = body.version;
+	const request = net.request({
+		method: 'GET',
+		url: 'https://raw.githubusercontent.com/planeshifter/isle-editor/master/package.json'
+	});
+	request.on( 'response', response => {
+		let body = '';
+		response.on( 'data', ( chunk ) => {
+			body += chunk;
+		});
+		request.on( 'error', error => callback( error ) );
+		response.on( 'end', () => {
+			let json = JSON.parse( body );
+			const newVersion = json.version;
+			debug( 'Newest version is '+newVersion );
 			if ( semver.gt( newVersion, currentVersion ) ) {
 				return callback( null, newVersion );
 			}
-		})
-		.catch( error => callback( error ) );
+		});
+	});
+	request.end();
 }
 
 
