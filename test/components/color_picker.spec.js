3411910f4cee036385fd37991906c9fcb5375f1b
Planeshifter <pburckhardt@outlook.com>
2018-08-03 15:21:00 -0400

    Migrate tests to Jest

--- test/components/color_picker.spec.js
+++ test/components/color_picker.spec.js
@@ -1,25 +1,31 @@
 // MODULES //
 
-import test from 'tape';
 import React from 'react';
-import { shallow } from 'enzyme';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import ColorPicker from 'components/color-picker';
 import { SketchPicker } from 'react-color';
 
 
+// VARIABLES //
+
+Enzyme.configure({ adapter: new Adapter() });
+
+
 // TESTS //
 
-test( 'the component renders a <SketchPicker /> from react-color', t => {
-	const wrapper = shallow( <ColorPicker /> );
-	const picker = wrapper.find( SketchPicker );
-	t.equal( picker.length, 1, 'length is equal to one' );
-	t.end();
+describe( '<ColorPicker />', function test() {
+	it( 'renders a <SketchPicker /> from react-color', () => {
+		const wrapper = shallow( <ColorPicker /> );
+		const picker = wrapper.find( SketchPicker );
+		expect( picker.length ).toBe( 1 );
+	});
+
+	it( 'passes down props to <SketchPicker />', () => {
+		const wrapper = shallow( <ColorPicker color="#fff" /> );
+		const picker = wrapper.find( SketchPicker );
+		const props = picker.props();
+		expect( props.color ).toBe( '#fff' );
+	});
 });
 
-test( 'the component passes down props to <SketchPicker />', t => {
-	const wrapper = shallow( <ColorPicker color="#fff" /> );
-	const picker = wrapper.find( SketchPicker );
-	const props = picker.props();
-	t.equal( props.color, '#fff', 'color is equal to #fff' );
-	t.end();
-});
