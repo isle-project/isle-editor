ddad5d2221664867257b92cde20701e0b165e713
Planeshifter <pburckhardt@outlook.com>
2018-08-15 16:47:44 -0400

    Update styling and fix learning component bugs

--- app/components/learn/conditional-probability/index.js
+++ app/components/learn/conditional-probability/index.js
@@ -1,6 +1,6 @@
 // MODULES //
 
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import Col from 'react-bootstrap/lib/Col';
 import Grid from 'react-bootstrap/lib/Grid';
@@ -10,6 +10,7 @@ import { VictoryAxis, VictoryBar, VictoryChart, VictoryLine, VictoryTheme } from
 import roundn from '@stdlib/math/base/special/roundn';
 import max from '@stdlib/math/base/special/max';
 import min from '@stdlib/math/base/special/min';
+import epsdiff from '@stdlib/math/base/utils/float64-epsilon-difference';
 import FeedbackButtons from 'components/feedback';
 import NumberInput from 'components/input/number';
 import TextInput from 'components/input/text';
@@ -170,8 +171,8 @@ class ConditionalProbability extends Component {
 		const pnotAB = this.state.pB - pAB;
 		const pnotAnotB = 1 - pAB - pAnotB - pnotAB;
 		const pAgivenNotB = ( this.state.pA - pAgivenB * this.state.pB ) / ( 1-this.state.pB );
-		const independent = ( pAgivenB === this.state.pAgivenNotB ) &&
-			( this.state.pAgivenNotB === this.state.pA );
+		const independent = epsdiff( pAgivenB, pAgivenNotB ) < 2.0 &&
+			epsdiff( pAgivenNotB, this.state.pA ) < 2.0;
 		this.setState({
 			pAgivenB, pAB, pAnotB, pnotAB, pnotAnotB, pAgivenNotB, independent
 		});
@@ -185,8 +186,8 @@ class ConditionalProbability extends Component {
 		const pAB = this.state.pA - pAnotB;
 		const pnotAB = this.state.pB - pAB;
 		const pAgivenB = ( this.state.pA - pAgivenNotB * pnotB ) / this.state.pB;
-		const independent = ( pAgivenNotB === this.state.pAgivenB ) &&
-			( this.state.pAgivenB === this.state.pA );
+		const independent = epsdiff( pAgivenNotB, pAgivenB ) < 2.0 &&
+			epsdiff( pAgivenB, this.state.pA ) < 2.0;
 		this.setState({
 			pAgivenNotB, pAB, pAnotB, pnotAB, pnotAnotB, pAgivenB, independent
 		});
@@ -263,86 +264,94 @@ class ConditionalProbability extends Component {
 
 	render() {
 		const { A, B, pA, pB } = this.state;
+		const victoryData = [ { x: `P(${A}|${B})`, y: 100*this.state.pAgivenB }, { x: `P(${A}|not ${B})`, y: this.state.pAgivenNotB*100 } ];
+		const victoryLineData = [ { x: `P(${A}|${B})`, y: 100*this.state.pAgivenB }, { x: `P(${A}|not ${B})`, y: this.state.pAgivenNotB*100 } ];
 		return (
-			<div>
-				<p>Consider events <TextInput inline defaultValue={A} onChange={( A ) => this.setState({ A })} /> and <TextInput inline defaultValue={B} onChange={( B ) => this.setState({ B })} />. Let <TeX raw={`P(\\text{${A}}) = `} /><NumberInput min={0} max={1} step={0.01} defaultValue={0.5} inline onChange={this.changeProbA} /> and  <TeX raw={`P(\\text{${B}}) = `} /><NumberInput min={0} max={1} step={0.01} defaultValue={0.5} inline onChange={this.changeProbB} />. Then we have <TeX raw={`P(\\text{${A}}^C) = ${roundn( 1-pA, -4 )}`} /> and <TeX raw={`P(\\text{${B}}^C) = ${roundn( 1-pB, -4 )}`} />. These form the marginals in the following probability table:</p>
-				<Panel>
-					<Grid>
-						<Row>
-							<Col md={4}>
-								<p>What are the inner cells equal to? If the events are random, we have <TeX raw={`P(\\text{${A}} \\cap \\text{${B}} ) = P(\\text{${A}}) \\cdot P(\\text{${B}}) = ${roundn( pA*pB, -4 )}`} />. </p>
-								{this.renderTable()}
-								<br />
-								{probabilityTableLabels({ A: A, B: B })}
-								<br />
-								<FeedbackButtons
-									for="independence_applet"
+			<Panel>
+				<Panel.Body>
+				<p>Consider events <TextInput inline defaultValue={A} onChange={( A ) => this.setState({ A })} /> and <TextInput inline defaultValue={B} onChange={( B ) => this.setState({ B })} />. Let <TeX raw={`P(\\text{${A}}) = `} /><NumberInput min={0} max={1} step={0.01} defaultValue={0.5} inline onChange={this.changeProbA} /> and  <TeX raw={`P(\\text{${B}}) = `} /><NumberInput min={0} max={1} step={0.01} defaultValue={0.5} inline onChange={this.changeProbB} />. Then we have <TeX raw={`P(\\text{${A}}^C) = ${roundn( 1-pA, -4 )}`} /> and <TeX raw={`P(\\text{${B}}^C) = ${roundn( 1-pB, -4 )}`} />. These form the marginals in the following probability table.</p>
+				<Grid>
+					<Row>
+						<Col sm={6}>
+							<p>What are the inner cells equal to? If the events are random, we have <TeX raw={`P(\\text{${A}} \\cap \\text{${B}} ) = P(\\text{${A}}) \\cdot P(\\text{${B}}) = ${roundn( pA*pB, -4 )}`} />. </p>
+							{this.renderTable()}
+							<br />
+							{probabilityTableLabels({ A: A, B: B })}
+							<br />
+							<Row>
+								<span>Choose</span>
+								<NumberInput
+									legend={<TeX raw={`P(\\text{${A}} \\mid \\text{${B})}`} />}
+									min={max( ( pA-1+pB )/pB, 0 )}
+									max={min( pA/pB, 1 )}
+									step={0.001}
+									onChange={this.changeProbAgivenB}
+									defaultValue={this.state.pAgivenB}
+									inline
+								/><span>and</span>
+								<NumberInput
+									legend={<TeX raw={`P( \\text{${A}} \\mid \\text{ ${B} }^C)`} />}
+									min={max( ( pA-pB ) / ( 1-pB ), 0 )}
+									max={min( pA/( 1-pB ), 1 )}
+									step={0.001}
+									onChange={this.changeProbAgivenNotB}
+									defaultValue={this.state.pAgivenNotB}
+									inline
 								/>
-							</Col>
-							<Col md={8}>
-								<p>What about the conditional probabilities?</p>
-								{ pB === 0 || pA === 0 ?
-									<Panel>
-										{ pB === 0 ? <span>The conditional probability is undefined when <TeX raw={`P( \\text{${B}}) = 0`} /></span> : <span>
-												When <TeX raw={`P( ${A}) = 0`} />, we must have <TeX raw={`P( \\text{${A}} \\mid \\text{${B}} ) = P( \\text{${A}} \\mid \\text{${B}}^C ) = 0`} /> in case B has non-zero probability.
-										</span>}
-									</Panel> :
-									<Grid>
-										<Row>
-											<Col md={4}>
-												<VictoryChart
-													theme={VictoryTheme.material}
-													domainPadding={{ x: 50, y: [ 10, 20 ]}}
-													style={{
-														height: 100,
-														width: 200
-													}}
-												>
-													<VictoryAxis dependentAxis tickFormat={( x ) => x/100} />
-													<VictoryAxis />
-													<VictoryBar
-														data={[ { x: `P( ${A}|${B})`, y: 100*this.state.pAgivenB }, { x: `P( ${A}|not ${B} )`, y: this.state.pAgivenNotB*100 } ]}
-														domain={{ y: [ 0, 100 ]}}
-														x="x"
-														y="y"
-														labels={( d ) => roundn( d.y/100, -3 )}
-													/>
-													{ this.state.independent ? <VictoryLine data={[ { x: `P( ${A}| ${B})`, y: 100*this.state.pAgivenB }, { x: `P( ${A}|not  ${B})`, y: this.state.pAgivenNotB*100 } ]} /> : null }
-												</VictoryChart>
-											</Col>
-										</Row>
-										<Row>
-											<NumberInput
-												legend={<span>Choose <TeX raw={`P(\\text{${A}} \\mid \\text{${B})}`} /></span>}
-												min={max( ( pA-1+pB )/pB, 0 )}
-												max={min( pA/pB, 1 )}
-												step={0.001}
-												onChange={this.changeProbAgivenB}
-												defaultValue={this.state.pAgivenB}
-												inline
-											/>
-											<NumberInput
-												legend={<span>Choose <TeX raw={`P( \\text{${A}} \\mid \\text{ ${B} }^C)`} /></span>}
-												min={max( ( pA-pB ) / ( 1-pB ), 0 )}
-												max={min( pA/( 1-pB ), 1 )}
-												step={0.001}
-												onChange={this.changeProbAgivenNotB}
-												defaultValue={this.state.pAgivenNotB}
-												inline
-											/>
-										</Row>
-										<Row>
-											{ this.state.independent ?
-												<span>The events are independent because <TeX raw={`P( \\text{${A}} \\mid \\text{ ${B} } ) = P( \\text{${A}} \\mid \\text{ ${B} }^C )`} /></span> : <span>The events are <b>not</b> independent because <TeX raw={this.state.pAgivenB === 0 && this.state.pAgivenNotB === 0 ? `P( \\text{ ${A} } \\mid \\text{ ${B} } ) = P( \\text{${A}} \\mid \\text{ ${B} }^C \\neq P( \\text{${A}})` : `P( \\text{ ${A} } \\mid \\text{ ${B} } ) \\neq P( \\text{${A}} \\mid \\text{ ${B} }^C )`} /></span>
-											}
-										</Row>
-									</Grid>
-								}
-							</Col>
-						</Row>
-					</Grid>
-				</Panel>
-			</div>
+							</Row>
+						</Col>
+						<Col sm={5}>
+							<p>What about the conditional probabilities?</p>
+							{ pB === 0 || pA === 0 ?
+								<Panel>
+									{ pB === 0 ? <span>The conditional probability is undefined when <TeX raw={`P( \\text{${B}}) = 0`} /></span> : <span>
+											When <TeX raw={`P( ${A}) = 0`} />, we must have <TeX raw={`P( \\text{${A}} \\mid \\text{${B}} ) = P( \\text{${A}} \\mid \\text{${B}}^C ) = 0`} /> in case B has non-zero probability.
+									</span>}
+								</Panel> :
+								<Grid>
+									<Row>
+										<Col md={4}>
+											<VictoryChart
+												theme={VictoryTheme.material}
+												domainPadding={{ x: 50, y: [ 10, 20 ]}}
+												style={{
+													height: 100,
+													width: 250
+												}}
+											>
+												<VictoryAxis dependentAxis tickFormat={( x ) => x/100} />
+												<VictoryAxis />
+												<VictoryBar
+													data={victoryData}
+													domain={{ y: [ 0, 100 ]}}
+													x="x"
+													y="y"
+													labels={( d ) => roundn( d.y/100, -3 )}
+												/>
+												{ this.state.independent ? <VictoryLine data={victoryLineData} /> : null }
+											</VictoryChart>
+										</Col>
+									</Row>
+									<Row>
+										{ this.state.independent ?
+											<Fragment><p>The events are independent because</p><TeX raw={`P( \\text{${A}} \\mid \\text{ ${B} } ) = P( \\text{${A}} \\mid \\text{ ${B} }^C )`} /></Fragment> :
+											<Fragment><p>The events are <b>not</b> independent because </p>
+											<TeX raw={this.state.pAgivenB === 0 && this.state.pAgivenNotB === 0 ? `P( \\text{ ${A} } \\mid \\text{ ${B} } ) = P( \\text{${A}} \\mid \\text{ ${B} }^C \\neq P( \\text{${A}})` : `P( \\text{ ${A} } \\mid \\text{ ${B} } ) \\neq P( \\text{${A}} \\mid \\text{ ${B} }^C )`} /></Fragment>
+										}
+									</Row>
+								</Grid>
+							}
+						</Col>
+						<Col sm="1">
+							<FeedbackButtons
+								for="independence_applet"
+								vertical
+							/>
+						</Col>
+					</Row>
+				</Grid>
+				</Panel.Body>
+			</Panel>
 		);
 	}
 }
