fe3c2bff27df136217c4b901117583242f99e084
Planeshifter <pburckhardt@outlook.com>
2018-08-17 20:41:06 -0400

    Add utility function to process JSON HTML elements

---
+++ app/utils/json-to-jsx/index.js
@@ -0,0 +1,37 @@
+// MODULES //
+
+import { createElement } from 'react';
+import logger from 'debug';
+import isArray from '@stdlib/assert/is-array';
+import isPlainObject from '@stdlib/assert/is-plain-object';
+
+
+// VARIABLES //
+
+const debug = logger( 'isle-editor:convert-json' );
+
+
+// MAIN //
+
+function convertJSONtoJSX( config ) {
+	console.log( config );
+	debug( `Convert JSON ${config.component} object to React element...` );
+	let children = config.children;
+	if ( isArray( children ) ) {
+		for ( let i = 0; i < children.length; i++ ) {
+			const child = children[ i ];
+			if ( isPlainObject( child ) ) {
+				children[ i ] = convertJSONtoJSX( child );
+			}
+		}
+	} else if ( isPlainObject( children ) ) {
+		children = convertJSONtoJSX( children );
+	}
+	const props = config.props || {};
+	return createElement( config.component, props, children );
+}
+
+
+// EXPORTS //
+
+export default convertJSONtoJSX;
