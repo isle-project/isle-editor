fa93d7d91bff738926f64b5dd5e33017a70b975e
Planeshifter <pburckhardt@outlook.com>
2018-08-18 11:46:18 -0400

    Add custom overlay trigger

---
+++ app/components/overlay-trigger/index.js
@@ -0,0 +1,38 @@
+// MODULES //
+
+import React, { Component } from 'react';
+import OverlayTrigger from 'react-bootstrap/lib/OverlayTrigger';
+
+
+// FUNCTIONS //
+
+function isTouchDevice() {
+	return (
+		'ontouchstart' in window ||
+		navigator.MaxTouchPoints > 0 ||
+		navigator.msMaxTouchPoints > 0
+	);
+}
+
+
+// VARIABLES //
+
+const IS_TOUCH = isTouchDevice();
+
+
+// MAIN //
+
+class Placeholder extends Component {
+	render() {
+		if ( IS_TOUCH ) {
+			// Do not render tooltips on touch devices to prevent users from having to double tap buttons:
+			return this.props.children;
+		}
+		return <OverlayTrigger {...this.props} >{this.props.children}</OverlayTrigger>;
+	}
+}
+
+
+// EXPORTS //
+
+export default Placeholder;
