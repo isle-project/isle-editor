c0ff915e9bfe6ba31ce250ec4f93c279391b5e8a
Planeshifter <pburckhardt@outlook.com>
2018-08-26 15:29:46 -0400

    Force update when user joins or disconnects

--- app/components/statusbar/instructor-view/user_list.js
+++ app/components/statusbar/instructor-view/user_list.js
@@ -2,8 +2,14 @@
 
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import logger from 'debug';
 import ListGroup from 'react-bootstrap/lib/ListGroup';
 import ListGroupItem from 'react-bootstrap/lib/ListGroupItem';
+
+
+// VARIABLES //
+
+const debug = logger( 'isle-editor:user-list' );
 
 
 // MAIN //
@@ -13,12 +19,31 @@ class UserList extends Component {
 		super( props );
 	}
 
+	componentDidMount() {
+		const { session } = this.props;
+		this.unsubscribe = session.subscribe( ( type ) => {
+			if (
+				type === 'received_users' ||
+				type === 'user_joined' ||
+				type === 'user_left'
+			) {
+				debug( 'Should render the user list...' );
+				this.forceUpdate();
+			}
+		});
+	}
+
+	componentWillUnmount() {
+		this.unsubscribe();
+	}
+
 	render() {
 		const { session } = this.props;
 		return (
 			<ListGroup style={{ overflowY: 'scroll', height: window.innerHeight / 2, marginLeft: 0, fontSize: 15, fontFamily: 'Open Sans' }}>
 				{session.userList.map( ( user, idx ) => {
-					return ( <ListGroupItem key={idx} style={{ background: user.inactive ? 'lightgrey' : 'transparent' }}>
+					const color = user.owner ? '#3c763d' : 'black';
+					return ( <ListGroupItem key={idx} style={{ background: user.inactive ? 'lightgrey' : 'transparent', color: color }}>
 						{user.name} ({user.email}) | {user.joinTime} - {user.exitTime}
 					</ListGroupItem> );
 				})}
