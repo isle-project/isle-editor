273bd734977b1d24aefd88687f5891b1e6d12c05
Planeshifter <pburckhardt@outlook.com>
2018-09-15 22:46:21 -0400

    Use custom component to restore clear buttons

--- app/components/data-explorer/clear_button.js
+++ app/components/data-explorer/clear_button.js
@@ -3,7 +3,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Button from 'react-bootstrap/lib/Button';
-import Tooltip from 'react-bootstrap/lib/Tooltip';
+import Tooltip from 'components/tooltip';
 
 
 // MAIN //
