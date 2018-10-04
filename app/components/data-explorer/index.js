81a62e1f6de0ed22813b691e992b6c220fee5867
Planeshifter <pburckhardt@outlook.com>
2018-10-04 19:27:51 -0400

    Remove unused import

--- app/components/data-explorer/index.js
+++ app/components/data-explorer/index.js
@@ -1,6 +1,6 @@
 // MODULES //
 
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import logger from 'debug';
 import Draggable from 'react-draggable';
