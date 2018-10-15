085670ba26d76c3313fc8e97ab444c088d762779
Planeshifter <pburckhardt@outlook.com>
2018-10-15 13:14:08 -0400

    Add JSDoc comment

--- app/utils/base64-to-blob/index.js
+++ app/utils/base64-to-blob/index.js
@@ -1,5 +1,13 @@
 // MAIN //
 
+/**
+* Converts a base-64 encoded string to a blob.
+*
+* @param {string} b64Data - base-64 encoded string
+* @param {string} [contentType=''] - content type
+* @param {integer} [sliceSize=512] - slice size
+* @returns {Blob} created blob
+*/
 function base64toBlob( b64Data, contentType = '', sliceSize = 512 ) {
 	const byteCharacters = atob( b64Data );
 	const byteArrays = [];
