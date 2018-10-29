16c6522154768c000af0233339b1e70ca5c5aaf9
Planeshifter <pburckhardt@outlook.com>
2018-10-29 11:49:22 -0400

    Change question display

---
+++ app/components/index.js
@@ -0,0 +1,6 @@
+// EXPORTS //
+
+export FreeTextQuestion from './free-text-question';
+export MultipleChoiceQuestion from './multiple-choice-question';
+export Recorder from './recorder';
+export WordCloud from './word-cloud';
