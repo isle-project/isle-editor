3cd5136c5c42a7fd6c76f26a74572cc4ab783b4b
Phalanstere <elias@ludicphilosophy.com>
2018-12-04 18:45:15 +0100

    Fix logical error in the memory game

--- app/components/multi-cards/memory.js
+++ app/components/multi-cards/memory.js
@@ -3,6 +3,14 @@ var MemoryGame = {
     pairs: [],
     drawCt: 0,
     counter: 0,
+    solved: [],
+
+    resetGame() {
+        this.solved = [];
+        this.drawCt = 0;
+        this.counter = 0;
+        this.storedCard = null;
+    },
 
     establishPairs( type ) {
         var found = false;
@@ -38,12 +46,16 @@ var MemoryGame = {
         } else {
             this.drawCt = 0;
             if (this.storedCard.type === type) {
+                this.solved.push(ndx);
+                this.solved.push( this.storedCard.ndx);
+                console.log('SOLVED ' + this.solved);
                 this.checkSuccess();
                 return true;
             }
             return false;
         }
     },
+
 
     checkSuccess(){
         this.counter += 1;
@@ -54,6 +66,8 @@ var MemoryGame = {
                 list.push( (i*2) +1);
             }
         this.callback( list );
+        global.memory = this;
+        this.resetGame();
         }
     },
 
@@ -66,6 +80,21 @@ var MemoryGame = {
         return false;
     },
 
+    checkSolved(ndx) {
+        var present = false;
+        for (var i = 0; i< this.solved.length; i++) {
+            if (ndx === this.solved[i]) present = true;
+        }
+        return present;
+    },
+
+    reset(ndx) {
+        let matrix = this.parent.state.cardMatrix.slice( 0 );
+        matrix[ndx] = !matrix[ndx];
+        this.parent.setState({
+          cardMatrix: matrix
+          });
+    },
 
     draw(item, ndx, callback) {
         this.callback = callback;
@@ -78,6 +107,11 @@ var MemoryGame = {
 				cardMatrix: matrix
                 });
 
+              return null;
+            }
+
+            if (this.checkSolved(ndx) === true) {
+              this.reset(ndx);
               return null;
             }
 
