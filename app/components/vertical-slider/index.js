7c3166882dffe280abbde152e43811cb4bbeda61
Philipp Burckhardt <pburckhardt@outlook.com>
2018-12-22 18:51:24 +0100

    Update vertical slider

--- app/components/vertical-slider/index.js
+++ app/components/vertical-slider/index.js
@@ -2,47 +2,71 @@
 
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import logger from 'debug';
+import isBoolean from '@stdlib/assert/is-boolean';
+import isNull from '@stdlib/assert/is-null';
 import VSHeader from './vs-header.js';
 import VSBody from './vs-body.js';
 import './vertical-slider.css';
+
+
+// VARIABLES //
+
+const debug = logger( 'isle:vertical-slider' );
+
+
+// FUNCTIONS //
+
+function getClassName( val ) {
+	if ( val === false ) {
+		return 'vs-body vs-hidden';
+	}
+	return 'vs-body vs-display';
+}
 
 
 // MAIN //
 
 /**
 * An ISLE component that allows to slideDown and slideUp - JQuery stale
-* @property {string} groupID - if set, the slider will refer to a group of sliders. This will allow to control a multitude of sliders
-* @property {boolean} visible - if set, the body is visible right from the start
+*
+* @property {boolean} defaultVisible - if set, the body is visible right from the start
+* @property {boolean} visible - controls whether slider is visible; turns it into a controlled component where state needs to be handled externally
+* @property {string} header - header string
 * @property {string} headerClassName - this overrules the given className of the header
 * @property {object} headerStyle - one may also assign a style to the header bar
 */
 class VerticalSlider extends Component {
 	constructor( props ) {
 		super( props );
+		const visible = props.visible || props.defaultVisible;
 		this.state = {
-			visible: props.visible,
-			className: this.getClassName(props.visible)
+			visible: visible,
+			className: getClassName( visible )
 		};
 	}
 
-	register() {
-		if ( this.props.groupID ) console.log('this should register the component');
-		// not implemented yet
-	}
-
-	getClassName( val ) {
-		if ( val === false ) {
-			return 'vs-body vs-hidden';
+	static getDerivedStateFromProps( nextProps, prevState ) {
+		if ( !isNull( nextProps.visible ) && nextProps.visible !== prevState.visible ) {
+			debug( 'Override visible state...' );
+			const newState = {
+				visible: nextProps.visible,
+				className: getClassName( nextProps.visible )
+			};
+			return newState;
 		}
-		return 'vs-body vs-display';
+		return null;
 	}
 
 	toggleVisibility = () => {
-		const v = !this.state.visible;
-		this.setState({
-			visible: v,
-			className: this.getClassName(v)
-		});
+		this.props.onClick();
+		if ( !isBoolean( this.props.visible ) ) {
+			const v = !this.state.visible;
+			this.setState({
+				visible: v,
+				className: getClassName( v )
+			});
+		}
 	}
 
 	renderHeader() {
@@ -57,10 +81,10 @@ class VerticalSlider extends Component {
 				</VSHeader>
 			);
 		}
-
 		return (
 			<VSHeader
-				onClick={this.toggleVisibility}>
+				onClick={this.toggleVisibility}
+			>
 				<h1 style={this.props.headerStyle}>{this.props.header}</h1>
 			</VSHeader>
 		);
@@ -82,19 +106,21 @@ class VerticalSlider extends Component {
 // PROPERTIES //
 
 VerticalSlider.propTypes = {
-	groupID: PropTypes.string,
 	header: PropTypes.string,
 	headerStyle: PropTypes.object,
 	headerClassName: PropTypes.string,
-	visible: PropTypes.bool
+	defaultVisible: PropTypes.bool,
+	visible: PropTypes.bool,
+	onClick: PropTypes.func
 };
 
 VerticalSlider.defaultProps = {
-	groupID: null,
 	header: 'Header',
 	headerClassName: null,
 	headerStyle: null,
-	visible: false
+	defaultVisible: false,
+	visible: null,
+	onClick() {}
 };
 
 
