c8b27f4453fc384da71a880c2db0414e2d2617d2
Planeshifter <pburckhardt@outlook.com>
2019-01-24 01:30:15 -0500

    Use spinner for loadable

--- app/editor-components/loadable/index.js
+++ app/editor-components/loadable/index.js
@@ -14,7 +14,18 @@ const Loading = ( props ) => {
 			<button onClick={props.retry}>Retry</button>
 		</div> );
 	}
-	return <div>Loading...</div>;
+	const spinner = <div className="sk-cube-grid">
+		<div className="sk-cube sk-cube1"></div>
+		<div className="sk-cube sk-cube2"></div>
+		<div className="sk-cube sk-cube3"></div>
+		<div className="sk-cube sk-cube4"></div>
+		<div className="sk-cube sk-cube5"></div>
+		<div className="sk-cube sk-cube6"></div>
+		<div className="sk-cube sk-cube7"></div>
+		<div className="sk-cube sk-cube8"></div>
+		<div className="sk-cube sk-cube9"></div>
+	</div>;
+	return spinner;
 };
 
 Loading.propTypes = {
