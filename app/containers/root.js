b6b08ea19fef08011c8bab076d8430c47f2a274d
Planeshifter <pburckhardt@outlook.com>
2019-03-05 20:00:08 -0500

    Improve startup by rendering white screen instantly

--- app/containers/root.js
+++ app/containers/root.js
@@ -1,13 +1,13 @@
 // MODULES //
 
-import React, { Component } from 'react';
+import React, { Component, lazy, Suspense } from 'react';
 import { Provider } from 'react-redux';
 import { Router, Route } from 'react-router-dom';
 import { hot } from 'react-hot-loader';
-import Editor from 'containers/editor.js';
-import Documentation from 'containers/documentation.js';
-import Settings from 'containers/settings.js';
-import Export from 'containers/export.js';
+const Editor = lazy( () => import( 'containers/editor.js' ) );
+const Documentation = lazy( () => import( 'containers/documentation.js' ) );
+const Settings = lazy( () => import( 'containers/settings.js' ) );
+const Export = lazy( () => import( 'containers/export.js' ) );
 
 
 // VARIABLES //
@@ -22,20 +22,22 @@ class Root extends Component {
 		const { store, history } = this.props;
 		return (
 			<Provider store={store}>
-				<Router history={history}>
-					<div className="App">
-						<Route exact path="/" children={( match ) => {
-							// Account for routes of spectacle slides...
-							if ( PATH_REGEX.test( match.location.pathname ) ) {
-								return <Editor />;
-							}
-							return null;
-						}} />
-						<Route path="/docs" component={Documentation} />
-						<Route path="/settings" component={Settings} />
-						<Route path="/export" component={Export} />
-					</div>
-				</Router>
+				<Suspense fallback={<div className="welcome-screen" />}>
+					<Router history={history}>
+						<div className="App">
+							<Route exact path="/" children={( match ) => {
+								// Account for routes of spectacle slides...
+								if ( PATH_REGEX.test( match.location.pathname ) ) {
+									return <Editor />;
+								}
+								return null;
+							}} />
+							<Route path="/docs" component={Documentation} />
+							<Route path="/settings" component={Settings} />
+							<Route path="/export" component={Export} />
+						</div>
+					</Router>
+				</Suspense>
 			</Provider>
 		);
 	}
