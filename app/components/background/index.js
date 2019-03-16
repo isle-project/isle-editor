4fbea526e04aee718bfc9e4d1879ae7c7b33645d
Planeshifter <pburckhardt@outlook.com>
2019-03-16 18:43:01 -0400

    Update background and add docs

--- app/components/background/index.js
+++ app/components/background/index.js
@@ -5,46 +5,43 @@ import PropTypes from 'prop-types';
 import './background.css';
 
 
-// VARIABLES //
-
-let bgArray = [
-	'https://isle.heinz.cmu.edu/Summertime_YoungGamer.jpeg',
-	'https://isle.heinz.cmu.edu/Summertime_cogs.jpeg'
-];
-
-
 // MAIN //
 
+/**
+* A dynamic background component that changes a background image at a specified interval.
+*
+* @property {Array} images - list of image URLs to be displayed
+* @property {number} time - duration each background image is displayed (in seconds)
+* @property {string} in - entrance effect name (either `fade-in`, `slide-left-in`, and `flip-hor-bottom-in`)
+* @property {string} out - exit effect name (either `fade-out`, `slide-right-out`, and `flip-hor-bottom-out`)
+* @property {number} inTime - length of the enter effect (in seconds)
+* @property {number} outTime - length of the exit effect (in seconds)
+*/
 class Background extends Component {
 	constructor( props ) {
 		super( props );
 
-		this.imageList=[];
-
-		if (props.imageList) bgArray = this.props.imageList;
-
+		this.imageList = [];
 		this.state = {
 			ratio: 1.777,
 			fading: true,
-			backgroundImage: bgArray[0],
-			foregroundImage: bgArray[1],
+			backgroundImage: props.images[0],
+			foregroundImage: props.images[1],
 			foregroundRatio: 1.777,
 			backgroundRatio: 1.777,
-			counter: 0,
-			in: props.in,
-			inTime: props.inTime,
-			outTime: props.outTime,
-			out: props.out
+			counter: 0
 		};
 	}
 
 	componentDidMount() {
-		if ( this.props.time ) {
-			this.getRatio();
-			this.preload();
-			this.setImages();
-			window.addEventListener( 'resize', this.getRatio );
-		}
+		this.getRatio();
+		this.preload();
+		this.changeBackground();
+		window.addEventListener( 'resize', this.getRatio );
+	}
+
+	componentWillUnmount() {
+		window.removeEventListener( 'resize', this.getRatio );
 	}
 
 	getRatio = () => {
@@ -58,113 +55,84 @@ class Background extends Component {
 	}
 
 	preload() {
-		if ( this.bgArray !== null ) {
-			for ( let i = 0; i < bgArray.length; i++ ) {
-				const img = new Image();
-				img.src = bgArray[ i ];
-				this.imageList.push( img );
-			}
+		for ( let i = 0; i < this.props.images.length; i++ ) {
+			const img = new Image();
+			img.src = this.props.images[ i ];
+			this.imageList.push( img );
 		}
 	}
 
 	getImageRatio(ct) {
-		if (this.imageList[ct]) {
+		if ( this.imageList[ct] ) {
 			let img = this.imageList[ct];
-			return img.width/img.height;
+			return img.width / img.height;
 		}
-	}
-
-	setImages() {
-		let ct = this.state.counter;
-
-		if (this.state.fading) {
-			let bct = ct -1;
-			let fgRatio = this.getImageRatio(ct);
-			if (ct === 0) bct = bgArray.length -1;
-			let bgRatio = this.getImageRatio(bct);
-			this.setState({
-					foregroundImage: bgArray[ct],
-					backgroundImage: bgArray[bct],
-					foregroundRatio: fgRatio,
-					backgroundRatio: bgRatio,
-					fading: false
-				});
-		} else {
-				let newCt = ct+2;
-				if (newCt >= bgArray.length-1) newCt = 0;
-
-				let fgRatio = this.getImageRatio(ct);
-				let bgRatio = this.getImageRatio(ct+1);
-
-				this.setState({
-					foregroundImage: bgArray[ct+1],
-					backgroundImage: bgArray[ct],
-					foregroundRatio: fgRatio,
-					backgroundRatio: bgRatio,
-					fading: true,
-					counter: newCt
-				});
-		}
-
-		setTimeout(this.changeBackground, this.props.time);
 	}
 
 	changeBackground = () => {
-		this.setImages();
+		let ct = this.state.counter;
+		if ( this.state.fading ) {
+			let bct = ct - 1;
+			const fgRatio = this.getImageRatio( ct );
+			if ( ct === 0 ) {
+				bct = this.props.images.length -1;
+			}
+			const bgRatio = this.getImageRatio( bct );
+			this.setState({
+				foregroundImage: this.props.images[ct],
+				backgroundImage: this.props.images[bct],
+				foregroundRatio: fgRatio,
+				backgroundRatio: bgRatio,
+				fading: false
+			});
+		} else {
+			let newCt = ct+2;
+			if ( newCt >= this.props.images.length-1 ) {
+				newCt = 0;
+			}
+			const fgRatio = this.getImageRatio(ct);
+			const bgRatio = this.getImageRatio(ct+1);
+			this.setState({
+				foregroundImage: this.props.images[ct+1],
+				backgroundImage: this.props.images[ct],
+				foregroundRatio: fgRatio,
+				backgroundRatio: bgRatio,
+				fading: true,
+				counter: newCt
+			});
+		}
+		setTimeout( this.changeBackground, this.props.time*1000 );
 	}
 
 	resizeImage( ratio ) {
-		let style = {
-			width: 'auto',
-			height: '100%'
-		};
-
-		if (ratio < this.state.ratio) {
+		let style;
+		if ( ratio < this.state.ratio ) {
 			style = {
 				width: '100%',
 				height: 'auto'
+			};
+		} else {
+			style = {
+				width: 'auto',
+				height: '100%'
 			};
 		}
 		return style;
 	}
 
 	exit() {
-		const ani = this.state.out + ' ' + this.state.outTime + 's forwards';
-		const foreground = {
-			position: 'absolute',
-			left: 0,
-			right: 0,
-			top: 0,
-			opacity: 1,
-			width: '100%',
-			height: '100%',
-			overflow: 'hidden',
-			fontFamily: 'Open Sans',
-			backgroundColor: 'white',
-			zIndex: -2
-		};
+		const ani = this.props.out + ' ' + this.props.outTime + 's forwards';
 		const background = {
-			position: 'absolute',
-			left: 0,
-			right: 0,
-			top: 0,
-			opacity: 1,
-			width: '100%',
-			height: '100%',
-			overflow: 'hidden',
-			fontFamily: 'Open Sans',
-			zIndex: -1,
 			animation: ani
 		};
-
-		const low = this.resizeImage(this.state.foregroundRatio);
-		const lowB = this.resizeImage(this.state.backgroundRatio);
+		const low = this.resizeImage( this.state.foregroundRatio );
+		const lowB = this.resizeImage( this.state.backgroundRatio );
 		return (
 			<div>
-				<div className="NeueAnimation" style={foreground} >
+				<div className="exit-foreground">
 					<img style={low} src={this.state.foregroundImage} />
 				</div>
-				<div style={background} >
+				<div className="exit-background" style={background} >
 					<img style={lowB} src={this.state.backgroundImage} />
 				</div>
 			</div>
@@ -172,39 +140,18 @@ class Background extends Component {
 	}
 
 	enter() {
-		const ani = this.state.in + ' ' + this.state.inTime + 's forwards';
+		const ani = this.props.in + ' ' + this.props.inTime + 's forwards';
 		const foreground = {
-			position: 'absolute',
-			left: 0,
-			right: 0,
-			top: 0,
-			opacity: 1,
-			width: '100%',
-			height: '100%',
-			overflow: 'hidden',
-			zIndex: -1,
 			animation: ani
 		};
-		const background = {
-			position: 'absolute',
-			left: 0,
-			right: 0,
-			top: 0,
-			opacity: 1,
-			width: '100%',
-			height: '100%',
-			overflow: 'hidden',
-			zIndex: -2
-		};
-
-		const low = this.resizeImage(this.state.foregroundRatio);
-		const lowB = this.resizeImage(this.state.backgroundRatio);
+		const low = this.resizeImage( this.state.foregroundRatio );
+		const lowB = this.resizeImage( this.state.backgroundRatio );
 		return (
 			<div>
-				<div style={foreground} >
+				<div className="enter-foreground" style={foreground} >
 					<img style={low} src={this.state.foregroundImage} />
 				</div>
-				<div style={background} >
+				<div className="enter-background">
 					<img style={lowB} src={this.state.backgroundImage} />
 				</div>
 			</div>
@@ -212,12 +159,11 @@ class Background extends Component {
 	}
 
 	render() {
-		if (this.state.fading === false) {
+		if ( this.state.fading === false ) {
 			return (
 				<div>{ this.enter() }</div>
 			);
 		}
-
 		return (
 			<div>{ this.exit() }</div>
 		);
@@ -228,7 +174,7 @@ class Background extends Component {
 // PROPERTIES //
 
 Background.propTypes = {
-	imageList: PropTypes.arrayOf(PropTypes.string),
+	images: PropTypes.arrayOf( PropTypes.string ),
 	time: PropTypes.number,
 	in: PropTypes.string,
 	out: PropTypes.string,
@@ -237,8 +183,8 @@ Background.propTypes = {
 };
 
 Background.defaultProps = {
-	imageList: [],
-	time: 3000,
+	images: [],
+	time: 3,
 	in: 'fade-in',
 	out: 'fade-out',
 	inTime: 1,
