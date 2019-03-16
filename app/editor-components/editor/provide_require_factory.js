cdb23fae37f9ee16fa3fff2a3b80a5e16f104847
Planeshifter <pburckhardt@outlook.com>
2019-03-16 18:42:47 -0400

    Fix bug when no require in preamble

--- app/editor-components/editor/provide_require_factory.js
+++ app/editor-components/editor/provide_require_factory.js
@@ -13,30 +13,32 @@ function factory( monaco ) {
 			lineNumber: position.lineNumber,
 			column: position.column - 1
 		});
-		const range = model.findMatches( 'require:' )[ 0 ].range;
+		const match = model.findMatches( 'require:' )[ 0 ];
 		const suggestions = [];
-		for ( let i = 0; i < NAMESPACE.length; i++ ) {
-			const x = NAMESPACE[ i ];
-			if (
-				x.alias === word &&
-				model.findMatches( x.path ).length === 0 // not already required
-			) {
-				suggestions.push({
-					label: x.alias,
-					documentation: `Require ${x.alias} in preamble`,
-					kind: monaco.languages.CompletionItemKind.Reference,
-					insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
-					insertText: '',
-					additionalTextEdits: [
-						{
-							range: range,
-							text: `require:\n    ${x.alias}: "${x.path}"`
-						}
-					]
-				});
+		if ( match ) {
+			const range = match.range;
+			for ( let i = 0; i < NAMESPACE.length; i++ ) {
+				const x = NAMESPACE[ i ];
+				if (
+					x.alias === word &&
+					model.findMatches( x.path ).length === 0 // not already required
+				) {
+					suggestions.push({
+						label: x.alias,
+						documentation: `Require ${x.alias} in preamble`,
+						kind: monaco.languages.CompletionItemKind.Reference,
+						insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+						insertText: '',
+						additionalTextEdits: [
+							{
+								range: range,
+								text: `require:\n    ${x.alias}: "${x.path}"`
+							}
+						]
+					});
+				}
 			}
 		}
-		console.log( suggestions );
 		return {
 			suggestions: suggestions,
 			incomplete: false
