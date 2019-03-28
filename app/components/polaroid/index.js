33b23d5fe4d31654590a55694596b2cc0c82330c
Phalanstere <elias@ludicphilosophy.com>
2019-03-28 11:40:21 +0100

    Add callback and remove functionality

--- app/components/polaroid/index.js
+++ app/components/polaroid/index.js
@@ -11,6 +11,12 @@ import './polaroid.css';
 * A dynamic background component that changes a background image at a specified interval.
 *
 * @property {Array} image - the image which is displayed in the polaroid frame
+* @property {Bool} removable - indicates whether the image is removable when clicking on the pin, default: false
+* @property {Bool} id - this will allow a parent component to identify the image
+* @property {Bool} image - image url
+* @property {Function} callback - callback function (receives the id of the Polaroid as parameter)
+* @property {Bool} showPin - shows a pin, default: false
+* @property {Object} style - css definitions, override the style of the component
 */
 class Polaroid extends Component {
 	constructor( props ) {
@@ -18,7 +24,8 @@ class Polaroid extends Component {
 
 		this.state = {
 			height: '100%',
-			width: 'auto'
+			width: 'auto',
+			exit: false
 		};
 	}
 
@@ -49,6 +56,21 @@ class Polaroid extends Component {
 		});
 	}
 
+	remove = (evt) => {
+		evt.stopPropagation();
+		this.setState({
+			exit: true
+		});
+	}
+
+
+	trigger = (event) => {
+		if (this.props.callback) {
+			this.props.callback(this.props.id);
+		}
+	}
+
+
 	render() {
 		const format = this.state.width + ' ' + this.state.height;
 		const background = {
@@ -57,12 +79,23 @@ class Polaroid extends Component {
 			backgroundPosition: 'center',
 			filter: 'sepia(50%)'
 		};
+
+		let imageClass = 'Polaroid';
+		if ( this.props.id !== '') {
+			imageClass = 'Polaroid clickablePolaroid';
+		}
+
+		if (this.state.exit === true) {
+			imageClass = 'Polaroid PolaroidExit';
+		}
+
 		return (
-			<div style={this.props.style} className="Polaroid">
+			<div id={this.props.id} onClick={this.trigger} style={this.props.style} className={imageClass}>
 				{ this.props.stain ? <div className={'PolaroidStain'} /> : null }
 				<div style={background} className="PolaroidImage">
 				</div>
 				{ this.props.showPin ? <div className={'PolaroidPin'} /> : null }
+				{ this.props.removable ? <div onClick={this.remove} className={'PinImageMap'} /> : null }
 			</div>
 		);
 	}
@@ -72,14 +105,20 @@ class Polaroid extends Component {
 // PROPERTIES //
 
 Polaroid.propTypes = {
+	id: PropTypes.string,
 	style: PropTypes.object,
 	image: PropTypes.string,
+	callback: PropTypes.func,
+	removable: PropTypes.bool,
 	showPin: PropTypes.bool
 
 };
 
 Polaroid.defaultProps = {
+	id: '',
 	image: null,
+	callback: null,
+	removable: false,
 	showPin: false,
 	style: {}
 };
