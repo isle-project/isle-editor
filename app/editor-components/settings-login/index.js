9823ff81990d3d35b2a2d47c09cdc6b1f27dfbf9
Planeshifter <pburckhardt@outlook.com>
2019-03-31 11:21:19 -0400

    Add key press and update styling

--- app/editor-components/settings-login/index.js
+++ app/editor-components/settings-login/index.js
@@ -20,64 +20,70 @@ class SettingsLogin extends Component {
 			password: localStorage.getItem( 'password' ) || '',
 			encounteredError: null
 		};
+	}
 
-		this.handleInputChange = ( event ) => {
-			const target = event.target;
-			const value = target.value;
-			const name = target.name;
-			this.setState({
-				[ name ]: value
-			}, () => {
-				localStorage.setItem( name, value );
-			});
+	handleInputChange = ( event ) => {
+		const target = event.target;
+		const value = target.value;
+		const name = target.name;
+		this.setState({
+			[ name ]: value
+		}, () => {
+			localStorage.setItem( name, value );
+		});
+	}
+
+	unlink = () => {
+		localStorage.removeItem( 'token' );
+		this.forceUpdate();
+	}
+
+	connectToServer = () => {
+		let form = {
+			password: this.state.password,
+			email: this.state.email
 		};
-
-		this.unlink = () => {
-			localStorage.removeItem( 'token' );
-			this.forceUpdate();
-		};
-
-		this.connectToServer = () => {
-			let form = {
-				password: this.state.password,
-				email: this.state.email
-			};
-			fetch( this.state.server+'/login', {
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json'
-				},
-				body: JSON.stringify( form )
-			})
-			.then( res => res.json() )
-			.then( body => {
-				try {
-					if ( body.type === 'incorrect_password' ) {
-						return this.setState({
-							encounteredError: new Error( body.message )
-						});
-					}
-					localStorage.setItem( 'token', body.token );
-					this.forceUpdate();
-				} catch ( error ) {
-					this.setState({
-						encounteredError: 'Couldn\'t login to server. Please check the address and port.'
+		fetch( this.state.server+'/login', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify( form )
+		})
+		.then( res => res.json() )
+		.then( body => {
+			try {
+				if ( body.type === 'incorrect_password' ) {
+					return this.setState({
+						encounteredError: new Error( body.message )
 					});
 				}
-			})
-			.catch( err => {
+				localStorage.setItem( 'token', body.token );
+				this.forceUpdate();
+			} catch ( error ) {
 				this.setState({
-					encounteredError: err
+					encounteredError: 'Couldn\'t login to server. Please check the address and port.'
 				});
+			}
+		})
+		.catch( err => {
+			this.setState({
+				encounteredError: err
 			});
-		};
+		});
+	}
+
+	handleKeyPress = ( event ) => {
+		if ( event.charCode === 13 ) {
+			this.connectToServer( event );
+		}
 	}
 
 	render() {
 		return (
 			<Card>
 				<Card.Header as="h5">
-						Connect to ISLE server
+					Connect to ISLE server
 				</Card.Header>
 				<Card.Body>
 					{ localStorage.getItem( 'token' ) === null ?
@@ -90,6 +96,7 @@ class SettingsLogin extends Component {
 									placeholder="Enter text"
 									onChange={this.handleInputChange}
 									value={this.state.server}
+									onKeyPress={this.handleKeyPress}
 								/>
 							</FormGroup>
 							<FormGroup>
@@ -100,6 +107,7 @@ class SettingsLogin extends Component {
 									placeholder="Enter email address"
 									onChange={this.handleInputChange}
 									value={this.state.email}
+									onKeyPress={this.handleKeyPress}
 								/>
 							</FormGroup>
 							<FormGroup>
@@ -110,6 +118,7 @@ class SettingsLogin extends Component {
 									placeholder="Enter password"
 									onChange={this.handleInputChange}
 									value={this.state.password}
+									onKeyPress={this.handleKeyPress}
 								/>
 							</FormGroup>
 							<Button
@@ -131,7 +140,7 @@ class SettingsLogin extends Component {
 						</Form> :
 						<Card border="success">
 							<Card.Body>
-								<p>You are linked to the ISLE server at {this.state.server} with user ${this.state.email}.</p>
+								<p>You are linked to the ISLE server at <b>{this.state.server}</b> with user <b>{this.state.email}</b>.</p>
 								<Button
 									variant="danger"
 									size="sm"
