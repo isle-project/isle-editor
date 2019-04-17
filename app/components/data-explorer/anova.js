24039861d6fcd1325fa3adfec9377c715142d917
Planeshifter <pburckhardt@outlook.com>
2019-04-17 11:33:22 -0400

    Handle missing values

--- app/components/data-explorer/anova.js
+++ app/components/data-explorer/anova.js
@@ -3,6 +3,9 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import anova1 from '@stdlib/stats/anova1';
+import { isPrimitive as isNumber } from '@stdlib/assert/is-number';
+import isnan from '@stdlib/assert/is-nan';
+import isNull from '@stdlib/assert/is-null';
 import SelectInput from 'components/input/select';
 import Dashboard from 'components/dashboard';
 import { DATA_EXPLORER_TESTS_ANOVA} from 'constants/actions.js';
@@ -23,10 +26,21 @@ class Anova extends Component {
 		this.calculateANOVA = ( variable, grouping ) => {
 			const { data, showDecision } = this.props;
 			const vals = data[ variable ];
-			const groups = data[ grouping ].map( x => `"${x}"` );
+			const groups = data[ grouping ];
+			const groupsFiltered = [];
+			const valsFiltered = [];
+			for ( let i = 0; i < vals.length; i++ ) {
+				if (
+					( isNumber( vals[i] ) && !isnan( vals[i] ) ) &&
+					( !isNull( groups[i] ) && groups[i] !== '' )
+				) {
+					valsFiltered.push( vals[ i ] );
+					groupsFiltered.push( `"${groups[i]}"` );
+				}
+			}
 			const value = <div>
 				<label>ANOVA for {variable} between {grouping}</label>
-				<pre style={{ marginTop: 10 }}>{anova1( vals, groups ).print({
+				<pre style={{ marginTop: 10 }}>{anova1( valsFiltered, groupsFiltered ).print({
 					decision: showDecision
 				})}</pre>
 			</div>;
--- app/components/data-explorer/corrtest.js
+++ app/components/data-explorer/corrtest.js
@@ -7,6 +7,8 @@ import SelectInput from 'components/input/select';
 import Dashboard from 'components/dashboard';
 import TeX from 'components/tex';
 import pcorrtest from '@stdlib/stats/pcorrtest';
+import { isPrimitive as isNumber } from '@stdlib/assert/is-number';
+import isnan from '@stdlib/assert/is-nan';
 import { DATA_EXPLORER_TESTS_CORRTEST } from 'constants/actions.js';
 import QuestionButton from './question_button.js';
 
@@ -26,7 +28,18 @@ class CorrTest extends Component {
 			const { data, showDecision } = this.props;
 			const x = data[ var1 ];
 			const y = data[ var2 ];
-			const result = pcorrtest( x, y, {
+			const xFiltered = [];
+			const yFiltered = [];
+			for ( let i = 0; i < x.length; i++ ) {
+				if (
+					( isNumber( x[i] ) && !isnan( x[i] ) ) &&
+					( isNumber( y[i] ) && !isnan( y[i] ) )
+				) {
+					xFiltered.push( x[i] );
+					yFiltered.push( y[i] );
+				}
+			}
+			const result = pcorrtest( xFiltered, yFiltered, {
 				'alpha': alpha,
 				'alternative': direction,
 				'rho': rho0
@@ -40,7 +53,7 @@ class CorrTest extends Component {
 			const output = {
 				variable: `Test for correlation between ${var1} and ${var2}`,
 				type: 'Test',
-				value: <div>
+				value: <div style={{ overflowX: 'auto', width: '100%' }}>
 					<label>Correlation test between {var1} and {var2}:</label>
 					<TeX displayMode raw={`H_0: \\rho = ${rho0} \\; vs. \\; H_1: \\rho ${arrow} ${rho0}`} tag="" />
 					<pre>
