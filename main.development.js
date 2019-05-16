ba659d1a13da88470c537c853af0eecfd353c232
Planeshifter <pburckhardt@outlook.com>
2019-05-16 10:13:41 -0400

    Set to empty array if not present

--- main.development.js
+++ main.development.js
@@ -24,7 +24,7 @@ else if ( config.has( 'mostRecentPath' ) ) {
 	pathToOpen = config.get( 'mostRecentPath' );
 }
 
-const recentFiles = config.get( 'recentFiles' );
+const recentFiles = config.get( 'recentFiles' ) || [];
 
 
 // FUNCTIONS //
