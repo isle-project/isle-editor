eaeac6516e59a0e2b5a27fd4b4a1c24923fe3a9e
Planeshifter <pburckhardt@outlook.com>
2019-06-27 01:52:45 +0200

    Only show component snippets under conditions

--- app/editor-components/editor/provide_snippet_factory.js
+++ app/editor-components/editor/provide_snippet_factory.js
@@ -1,5 +1,7 @@
 // MODULES //
 
+import isAlphanumeric from '@stdlib/assert/is-alphanumeric';
+import isWhitespace from '@stdlib/assert/is-whitespace';
 import removeFirst from '@stdlib/string/remove-first';
 import COMPONENT_DOCS from './components_documentation.json';
 import { componentSnippets } from 'snippets';
@@ -17,21 +19,29 @@ function factory( monaco ) {
 			endLineNumber: position.lineNumber,
 			endColumn: position.column
 		});
-		const suggestions = componentSnippets.map( x => {
-			const docs = COMPONENT_DOCS[ x.name ];
-			let description = 'No documentation available';
-			if ( docs ) {
-				description = docs.description;
-			}
-			return {
-				label: x.name,
-				kind: monaco.languages.CompletionItemKind.Snippet,
-				documentation: description,
-				insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
-				insertText: lastChar === '<' ? removeFirst( x.value ) : x.value,
-				sortText: 'a'+x.value
-			};
-		});
+		let suggestions = [];
+		if (
+			isWhitespace( lastChar ) ||
+			lastChar === '<' ||
+			!lastChar ||
+			isAlphanumeric( lastChar )
+		) {
+			suggestions = componentSnippets.map( x => {
+				const docs = COMPONENT_DOCS[ x.name ];
+				let description = 'No documentation available';
+				if ( docs ) {
+					description = docs.description;
+				}
+				return {
+					label: x.name,
+					kind: monaco.languages.CompletionItemKind.Snippet,
+					documentation: description,
+					insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+					insertText: lastChar === '<' ? removeFirst( x.value ) : x.value,
+					sortText: 'a'+x.value
+				};
+			});
+		}
 		return {
 			suggestions: suggestions,
 			incomplete: false
