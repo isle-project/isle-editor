7a28ece70fdf4bed962ba3a2d9eece649ced0371
Planeshifter <pburckhardt@outlook.com>
2019-07-13 17:28:05 -0400

    Remove unused imports

--- app/components/data-explorer/create_output_element.js
+++ app/components/data-explorer/create_output_element.js
@@ -7,8 +7,6 @@ import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import Table from 'react-bootstrap/Table';
 import logger from 'debug';
 import isArray from '@stdlib/assert/is-array';
-import isObject from '@stdlib/assert/is-object';
-import { isPrimitive as isNumber } from '@stdlib/assert/is-number';
 import entries from '@stdlib/utils/entries';
 import replace from '@stdlib/string/replace';
 import ClearButton from './clear_button.js';
