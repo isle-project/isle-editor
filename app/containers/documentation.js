24cc1beee0556463bca675585983e5f3c13a1752
Planeshifter <pburckhardt@outlook.com>
2019-07-16 01:33:41 -0400

    Change attribute value to string

--- app/containers/documentation.js
+++ app/containers/documentation.js
@@ -61,7 +61,7 @@ class Documentation extends Component {
 					}}
 					ref={( div ) => { this.webview = div; }}
 					autosize="on"
-					disablewebsecurity
+					disablewebsecurity="true"
 				/>
 			</div>
 		);
