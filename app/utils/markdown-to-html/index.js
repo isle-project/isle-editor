504569abbb87d51a2e6bdd4441b8e07272440804
Planeshifter <pburckhardt@outlook.com>
2019-07-17 17:20:58 -0400

    Only escape in equations

--- app/utils/markdown-to-html/index.js
+++ app/utils/markdown-to-html/index.js
@@ -8,12 +8,12 @@ import Tokenizer from './tokenizer.js';
 // VARIABLES //
 
 const debug = logger( 'isle:markdown-to-html' );
-const RE_RAW_ATTRIBUTE = /raw *= *("[^"]*"|{`[^`]*`})/g;
+const RE_RAW_ATTRIBUTE = /<TeX([^>]*?)raw *= *("[^"]*"|{`[^`]*`})/g;
 const RE_BACKSLASH = /(^|[^\\])\\($|[^\\])/g;
 
 // Escape backslashes in raw attributes tags:
-const escaper = ( match, p1 ) => {
-	return 'raw='+replace( p1, RE_BACKSLASH, '$1\\\\$2' );
+const escaper = ( match, p1, p2 ) => {
+	return '<TeX '+p1+' raw='+replace( p2, RE_BACKSLASH, '$1\\\\$2' );
 };
 
 
