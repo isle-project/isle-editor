8fb124676a63253885fb128296133bec4194cc23
Planeshifter <pburckhardt@outlook.com>
2019-08-11 19:16:59 -0400

    Update equation parsing

--- app/utils/markdown-to-html/tokenizer.js
+++ app/utils/markdown-to-html/tokenizer.js
@@ -2,10 +2,10 @@
 
 import logger from 'debug';
 import markdownit from 'markdown-it';
-import trim from '@stdlib/string/trim';
 import replace from '@stdlib/string/replace';
 import startsWith from '@stdlib/string/starts-with';
 import removeFirst from '@stdlib/string/remove-first';
+import removeLast from '@stdlib/string/remove-last';
 import hasOwnProp from '@stdlib/assert/has-own-property';
 import { replaceEquations } from './replace_equations.js';
 
@@ -14,18 +14,20 @@ import { replaceEquations } from './replace_equations.js';
 
 const debug = logger( 'isle:tokenizer' );
 const IN_BASE = 0;
-const IN_OPENING_TAG = 1;
-const IN_OPENING_TAG_NAME = 2;
-const IN_CLOSING_TAG = 3;
-const IN_STRING_ATTRIBUTE = 4;
-const IN_JSX_ATTRIBUTE = 5;
-const IN_JSX_STRING = 6;
-const IN_JSX_OBJECT = 7;
-const IN_JSX_ARRAY = 8;
-const IN_JSX_EXPRESSION = 9;
-const IN_JSX_OTHER = 10;
-const IN_BETWEEN_TAGS = 11;
-const IN_ANGLE_LINK = 12;
+const IN_EQUATION = 1;
+const IN_DISPLAY_EQUATION = 2;
+const IN_OPENING_TAG = 3;
+const IN_OPENING_TAG_NAME = 4;
+const IN_CLOSING_TAG = 5;
+const IN_STRING_ATTRIBUTE = 6;
+const IN_JSX_ATTRIBUTE = 7;
+const IN_JSX_STRING = 8;
+const IN_JSX_OBJECT = 9;
+const IN_JSX_ARRAY = 10;
+const IN_JSX_EXPRESSION = 11;
+const IN_JSX_OTHER = 12;
+const IN_BETWEEN_TAGS = 13;
+const IN_ANGLE_LINK = 14;
 
 const md = markdownit({
 	html: true,
@@ -75,6 +77,18 @@ function trimLineStarts( str ) {
 	return replace( str, RE_LINE_BEGINNING, '\n' );
 }
 
+function renderInlineMarkdown( text ) {
+	text = replace( text, /\\([()\\[\]])/g, '\\\\$1' );
+	console.log( md.renderInline( text ) );
+	return replaceEquations( md.renderInline( text ) );
+}
+
+function renderMarkdown( text ) {
+	text = replace( text, /\\([()\\[\]])/g, '\\\\$1' );
+	console.log( md.render( text ) );
+	return replaceEquations( md.render( text ) );
+}
+
 /**
 * Tests whether character is whitespace.
 *
@@ -120,7 +134,41 @@ class Tokenizer {
 			this._level += 1;
 			this._startTagNamePos = 0;
 		}
-		if ( char === '{' && this._buffer.charAt( pos-1 ) !== '\\' ) {
+		else if ( char === '$' ) {
+			this._eqnChar = char;
+			const prevChar = this._buffer.charAt( pos-1 );
+			const nextChar = this._buffer.charAt( pos+1 );
+			if (
+				nextChar === '$' &&
+				prevChar !== '\\'
+			) {
+				debug( 'IN_BASE -> IN_DISPLAY_EQUATION' );
+				this._state = IN_DISPLAY_EQUATION;
+				this.pos += 1; // skip the next opening `$` character
+			}
+			else if (
+				( prevChar !== '\\' && nextChar !== '$' ) ||
+				( prevChar === '\\' && nextChar === '(' )
+			) {
+				debug( 'IN_BASE -> IN_EQUATION' );
+				this._state = IN_EQUATION;
+			}
+		}
+		else if ( char === '\\' ) {
+			this._eqnChar = char;
+			const nextChar = this._buffer.charAt( pos+1 );
+			if ( nextChar === '[' ) {
+				debug( 'IN_BASE -> IN_DISPLAY_EQUATION' );
+				this._state = IN_DISPLAY_EQUATION;
+				this.pos += 1; // skip the next opening `[` character
+			}
+			else if ( nextChar === '(' ) {
+				debug( 'IN_BASE -> IN_EQUATION' );
+				this._state = IN_EQUATION;
+				this.pos += 1; // skip the next opening `(` character
+			}
+		}
+		else if ( char === '{' && this._buffer.charAt( pos-1 ) !== '\\' ) {
 			this._JSX_ATTRIBUTE_START = this._current.length;
 			this._braceLevel = 1;
 			debug( 'IN_BASE -> IN_JSX_ATTRIBUTE' );
@@ -135,24 +183,58 @@ class Tokenizer {
 		}
 	}
 
+	_inEquation( char ) {
+		if (
+			( this._eqnChar === '$' && char === '$' ) ||
+			( this._eqnChar === '\\' && char === ')' && this._buffer.charAt( this.pos-1 ) === '\\' )
+		) {
+			let eqn = removeFirst( this._current );
+			if ( this._eqnChar === '\\' ) {
+				eqn = removeLast( eqn );
+			}
+			const str = '<TeX raw={String.raw`' + eqn + '`} />';
+			this.divHash[ '<div id="placeholder_'+this.pos+'"/>' ] = str;
+			this.tokens.push( '<div id="placeholder_'+this.pos+'"/>' );
+			debug( 'IN_EQUATION -> IN_BASE' );
+			this._current = '';
+			this._state = IN_BASE;
+		} else {
+			this._current += char;
+		}
+	}
+
+	_inDisplayEquation( char ) {
+		if (
+			( char === '$' && this._buffer.charAt( this.pos-1 ) === '$' ) ||
+			( this._eqnChar === '\\' && char === ']' && this._buffer.charAt( this.pos-1 ) === '\\' )
+		) {
+			const eqn = this._current.substring( 1, this._current.length-1 );
+			const str = '<TeX raw={String.raw`' + eqn + '`} displayMode />';
+			this.divHash[ '<div id="placeholder_'+this.pos+'"/>' ] = str;
+			this.tokens.push( '<div id="placeholder_'+this.pos+'"/>' );
+			debug( 'IN_DISPLAY_EQUATION -> IN_BASE' );
+			this._current = '';
+			this._state = IN_BASE;
+		} else {
+			this._current += char;
+		}
+	}
+
 	_inAngleLink( char ) {
 		this._current += char;
 		if ( char === '>' ) {
 			this._level -= 1;
+			const url = this._current.substring( this._startTagNamePos+1, this._current.length-1 );
 			if ( this._level === 0 ) {
 				debug( 'IN_ANGLE_LINK -> IN_BASE' );
 				this._state = IN_BASE;
-				const url = this._current.substring( this._startTagNamePos+1, this._current.length-1 );
-				const before = this._current.substring( 0, this._startTagNamePos );
-				const replacement = replaceEquations( md.renderInline( trim( before ) ) ) +
-					' <a href="'+url+'">'+url+'</a>';
+				const replacement = ' <a href="'+url+'">'+url+'</a>';
 				this.divHash[ '<div id="placeholder_'+this.pos+'"/>' ] = replacement;
-				this.tokens.push( '<div id="placeholder_'+this.pos+'"/>' );
+				this.tokens.push( replacement );
 				this._current = '';
 			} else {
 				debug( 'IN_ANGLE_LINK -> IN_BETWEEN_TAGS' );
 				this._state = IN_BETWEEN_TAGS;
-				const url = this._current.substring( this._startTagNamePos+1, this._current.length-1 );
 				this._current = this._current.substring( 0, this._startTagNamePos ) +
 				' ['+url+']('+url+')';
 			}
@@ -172,30 +254,29 @@ class Tokenizer {
 				) {
 					debug( `Render inline markdown for <${this._openingTagName}/>...` );
 					if ( !RE_JSX_OPEN.test( text ) && !RE_JSX_CLOSE.test( text ) ) {
-						text = md.renderInline( text );
+						text = renderInlineMarkdown( text );
 					} else {
 						text = replace( text, RE_JSX_OPEN, ( match, p1, p2, p3 ) => {
-							return md.renderInline( p1 ) + p2 + md.renderInline( p3 );
+							return renderInlineMarkdown( p1 ) + p2 + renderInlineMarkdown( p3 );
 						});
 						text = replace( text, RE_JSX_CLOSE, ( match, p1, p2, p3, p4 ) => {
-							return p1 + p2 + p3 + md.renderInline( p4 );
+							return p1 + p2 + p3 + renderInlineMarkdown( p4 );
 						});
 					}
 				} else {
 					debug( `Render block markdown for <${this._openingTagName}/>...` );
 					if ( !RE_JSX_OPEN.test( text ) && !RE_JSX_CLOSE.test( text ) ) {
-						text = md.render( text );
+						text = renderMarkdown( text );
 					} else {
 						text = replace( text, RE_JSX_OPEN, ( match, p1, p2, p3 ) => {
-							return md.render( p1 ) + p2 + md.render( p3 );
+							return renderMarkdown( p1 ) + p2 + renderMarkdown( p3 );
 						});
 						text = replace( text, RE_JSX_CLOSE, ( match, p1, p2, p3, p4 ) => {
-							return p1 + p2 + p3 + md.render( p4 );
+							return p1 + p2 + p3 + renderMarkdown( p4 );
 						});
 					}
 				}
 			}
-			text = replaceEquations( text );
 			this._current = this._current.substring( 0, this._openTagEnd ) +
 			text + '<';
 			if ( this._buffer.charAt( this.pos+1 ) !== '/' ) {
@@ -451,13 +532,18 @@ class Tokenizer {
 		debug( str );
 		debug( '---' );
 		str = replace( str, RE_RAW_ATTRIBUTE, rawEscaper );
-		str = replaceEquations( str );
 		this.setup( str );
 		for ( this.pos = 0; this.pos < str.length; this.pos++ ) {
 			let char = str.charAt( this.pos );
 			switch ( this._state ) {
 			case IN_BASE:
 				this._inBase( char );
+				break;
+			case IN_EQUATION:
+				this._inEquation( char );
+				break;
+			case IN_DISPLAY_EQUATION:
+				this._inDisplayEquation( char );
 				break;
 			case IN_OPENING_TAG_NAME:
 				this._inOpeningTagName( char );
@@ -502,7 +588,6 @@ class Tokenizer {
 			}
 		}
 		let out = this.tokens.join( '' );
-		console.log( out );
 		out = md.render( out );
 		console.log( out );
 		for ( let key in this.divHash ) {
