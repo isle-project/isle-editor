022db6dba9bed575840fdc0b3afb7ae58d78cc36
Planeshifter <pburckhardt@outlook.com>
2019-08-15 01:46:19 -0400

    Add menu item to check for updates

--- app/main/menus/help.js
+++ app/main/menus/help.js
@@ -1,6 +1,7 @@
 // MODULES //
 
-import { BrowserWindow } from 'electron';
+import { BrowserWindow, net } from 'electron';
+import semver from 'semver';
 import os from 'os';
 import * as actions from './../actions';
 import { version as currentVersion } from './../../../package.json';
@@ -23,6 +24,51 @@ export default {
 			accelerator: '',
 			click: () => {
 				actions.openBrowser( 'https://github.com/isle-project/isle-editor/issues' );
+			}
+		},
+		{
+			type: 'separator'
+		},
+		{
+			label: 'Check for updates...',
+			click: () => {
+				const request = net.request({
+					method: 'GET',
+					url: 'https://raw.githubusercontent.com/planeshifter/isle-editor/master/package.json'
+				});
+				request.on( 'response', response => {
+					let body = '';
+					response.on( 'data', ( chunk ) => {
+						body += chunk;
+					});
+					request.on( 'error', error => console.error( error ) ); // eslint-disable-line
+					response.on( 'end', () => {
+						const json = JSON.parse( body );
+						const newVersion = json.version;
+						const updateAvailable = semver.gt( newVersion, currentVersion );
+						const win = new BrowserWindow({
+							modal: true,
+							resizable: false,
+							center: true,
+							title: 'ISLE Editor',
+							height: 200,
+							width: 300
+						});
+						win.setMenuBarVisibility( false ); // account for Linux bug, see: https://github.com/electron/electron/issues/15901#issuecomment-473773527
+						win.removeMenu();
+						const msg = updateAvailable ?
+							`A new version (${newVersion}) is available for <a href="https://isledocs.com/docs/overview/install" target="_blank" >download</a>.` :
+							'There is currently no update available...';
+						const html = [
+							'<body>',
+								'<h1 style="font-family: Open Sans; color: rgb(46,68,104);">ISLE Editor Update</h1>',
+								`<p>${msg}</p>`,
+							'</body>'
+						].join( '' );
+						win.loadURL( 'data:text/html;charset=utf-8,' + encodeURI( html ) );
+					});
+				});
+				request.end();
 			}
 		},
 		{
