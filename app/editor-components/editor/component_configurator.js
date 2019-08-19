b756b0c163c4697d5b592bc84edf5374ed8e6b3b
Planeshifter <pburckhardt@outlook.com>
2019-08-19 12:55:50 -0400

    Avoid use of label without control input

--- app/editor-components/editor/component_configurator.js
+++ app/editor-components/editor/component_configurator.js
@@ -239,7 +239,7 @@ class ComponentConfigurator extends Component {
 		};
 		/* eslint-disable react/no-danger */
 		const componentDescription = <div>
-			<label style={{ marginBottom: 0 }}dangerouslySetInnerHTML={innerHTML}></label>
+			<span className="configurator-title" dangerouslySetInnerHTML={innerHTML}></span>
 		</div>;
 		/* eslint-enable react/no-danger */
 		return (
