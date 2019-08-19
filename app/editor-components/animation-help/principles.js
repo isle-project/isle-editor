19af4c3d5f6ddb88bdca4dcf2236824d6d6f8ec0
Planeshifter <pburckhardt@outlook.com>
2019-08-19 12:55:31 -0400

    Fix typos

--- app/editor-components/animation-help/principles.js
+++ app/editor-components/animation-help/principles.js
@@ -21,14 +21,14 @@ const AnimationPrinciples = () => {
 			<li>animationFillMode:  <span className="param">'forwards'</span></li>
 			<li>animationPlayState:  <span className="param">'paused'</span></li>
 			<br />
-			<p>The <a hef="https://www.w3schools.com/cssref/css3_pr_animation-name.asp">animation-name</a> CSS property specifies a name for the @keyframes animation.</p>
-			<p>The <a hef="https://www.w3schools.com/cssref/css3_pr_animation-delay.asp" target="_blank">animation-delay</a> CSS property specifies a delay for the start of an animation.</p>
-			<p>The <a hef="https://www.w3schools.com/cssref/css3_pr_animation-direction.asp" target="_blank">animation-diretion</a> CSS property defines whether an animation should be played forwards, backwards or in alternate cycles.</p>
-			<p>The <a hef="https://www.w3schools.com/cssref/css3_pr_animation-duration.asp" target="_blank">animation-duration</a> CSS property defines how long an animation should take to complete one cycle.</p>
-			<p>The <a hef="https://www.w3schools.com/cssref/css3_pr_animation-iteration-count.asp" target="_blank">animation-iteration-count</a> CSS property  specifies the number of times an animation should be played.</p>
-			<p>The <a hef="https://www.w3schools.com/cssref/css3_pr_animation-fill-mode.asp" target="_blank">animation-fill-mode</a> CSS property sets how a CSS animation applies styles to its target before and after its execution.</p>
-			<p>The <a hef="https://www.w3schools.com/cssref/css3_pr_animation-play-state.asp" target="_blank">animation-play-state</a> CSS property specifies whether the animation is running or paused.</p>
-			<p>The <a hef="https://www.w3schools.com/cssref/css3_pr_animation-timing-function.asp" target="_blank">animation-timing-function</a> CSS property specifies the speed curve of an animation .</p>
+			<p>The <a href="https://www.w3schools.com/cssref/css3_pr_animation-name.asp">animation-name</a> CSS property specifies a name for the @keyframes animation.</p>
+			<p>The <a href="https://www.w3schools.com/cssref/css3_pr_animation-delay.asp" target="_blank">animation-delay</a> CSS property specifies a delay for the start of an animation.</p>
+			<p>The <a href="https://www.w3schools.com/cssref/css3_pr_animation-direction.asp" target="_blank">animation-diretion</a> CSS property defines whether an animation should be played forwards, backwards or in alternate cycles.</p>
+			<p>The <a href="https://www.w3schools.com/cssref/css3_pr_animation-duration.asp" target="_blank">animation-duration</a> CSS property defines how long an animation should take to complete one cycle.</p>
+			<p>The <a href="https://www.w3schools.com/cssref/css3_pr_animation-iteration-count.asp" target="_blank">animation-iteration-count</a> CSS property  specifies the number of times an animation should be played.</p>
+			<p>The <a href="https://www.w3schools.com/cssref/css3_pr_animation-fill-mode.asp" target="_blank">animation-fill-mode</a> CSS property sets how a CSS animation applies styles to its target before and after its execution.</p>
+			<p>The <a href="https://www.w3schools.com/cssref/css3_pr_animation-play-state.asp" target="_blank">animation-play-state</a> CSS property specifies whether the animation is running or paused.</p>
+			<p>The <a href="https://www.w3schools.com/cssref/css3_pr_animation-timing-function.asp" target="_blank">animation-timing-function</a> CSS property specifies the speed curve of an animation .</p>
 		</div>
 	);
 };
