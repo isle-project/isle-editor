551b3de522aaeda8e9b5108ebe1aede9c21d72f2
Planeshifter <pburckhardt@outlook.com>
2019-09-29 01:05:09 -0400

    Add options to zscore function

--- app/utils/zscore/index.js
+++ app/utils/zscore/index.js
@@ -10,17 +10,35 @@ import setNonEnumerableProperty from '@stdlib/utils/define-nonenumerable-propert
 /**
 * Standardizes elements in input array.
 *
-* @param {Array} arr - input array
+* @param {Array} x - input array
+* @param {boolean} demean - controls whether the mean should be subtracted from the values
+* @param {boolean} scale - controls whether values should be divided by the standard deviation
 * @returns {Array} output array with added `mu` and `sigma` properties
 */
-function zScore( x ) {
+function zScore( x, demean = true, scale = true ) {
 	const avg = mean( x );
 	const sd = stdev( x );
 	const len = x.length;
 
 	const out = new Array( len );
-	for ( let i = 0; i < len; i++ ) {
-		out[ i ] = ( x[ i ] - avg ) / sd;
+	if ( demean ) {
+		if ( scale ) {
+			for ( let i = 0; i < len; i++ ) {
+				out[ i ] = ( x[ i ] - avg ) / sd;
+			}
+		} else {
+			for ( let i = 0; i < len; i++ ) {
+				out[ i ] = ( x[ i ] - avg );
+			}
+		}
+	}
+	else if ( scale ) {
+		for ( let i = 0; i < len; i++ ) {
+			out[ i ] = x[ i ] / sd;
+		}
+	}
+	else {
+		out = x.slice();
 	}
 	setNonEnumerableProperty( out, 'mu', avg );
 	setNonEnumerableProperty( out, 'sigma', sd );
