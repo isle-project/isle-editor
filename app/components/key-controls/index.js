32ee99a84ff4825f7a147d4a9454abcdb3534e07
Planeshifter <pburckhardt@outlook.com>
2019-10-02 14:44:05 -0400

    Refactor key controls

--- app/components/key-controls/index.js
+++ app/components/key-controls/index.js
@@ -5,6 +5,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import logger from 'debug';
+import objectKeys from '@stdlib/utils/keys';
 import isFunction from '@stdlib/assert/is-function';
 import SessionContext from 'session/context.js';
 
@@ -28,46 +29,62 @@ class KeyControls extends Component {
 	}
 
 	componentDidMount() {
+		debug( 'Component has mounted...' );
 		this.addListeners();
 	}
 
 	componentDidUpdate( prevProps ) {
 		if ( this.props.container !== prevProps.container ) {
+			debug( 'Container has changed, re-attach event listeners...' );
 			this.removeListeners( prevProps.container );
 			this.addListeners();
 		}
 	}
 
 	componentWillUnmount() {
+		debug( 'Component has unmounted...' );
 		this.removeListeners();
 	}
 
 	addListeners = () => {
-		debug( `Add keydown event listeners for ${this.props.container ? 'container' : 'document'}...` );
+		const keys = objectKeys( this.props.actions );
+		debug( `Add keydown event listeners for ${this.props.container ? 'container' : 'document'}: ${keys.join( ', ')}` );
 		if ( this.props.container ) {
 			const node = ReactDOM.findDOMNode( this.props.container );
 			node.addEventListener( 'keydown', this.triggerEvent );
 		} else {
-			document.addEventListener( 'keydown', this.triggerEvent );
+			document.addEventListener( 'keydown', this.triggerDocEvent );
 		}
 	}
 
 	removeListeners = ( container ) => {
-		debug( `Remove keydown event listeners for ${container ? 'container' : 'document'}...` );
+		const keys = objectKeys( this.props.actions );
+		debug( `Remove keydown event listeners for ${container ? 'container' : 'document'}: ${keys.join( ', ')}` );
 		if ( container ) {
 			const node = ReactDOM.findDOMNode( container );
 			node.removeEventListener( 'keydown', this.triggerEvent );
-		} else {
-			document.removeEventListener( 'keydown', this.triggerEvent );
 		}
+		document.removeEventListener( 'keydown', this.triggerDocEvent );
 	}
 
 	triggerEvent = ( event ) => {
 		const keyName = event.key;
-		debug( `Received key press: ${keyName}` );
+		debug( `Received key press for container: ${keyName}` );
+		const fn = this.props.actions[ keyName ];
+		if ( document.activeElement === this.props.container && isFunction( fn ) ) {
+			event.preventDefault();
+			event.stopPropagation();
+			fn();
+		}
+	}
+
+	triggerDocEvent = ( event ) => {
+		const keyName = event.key;
+		debug( `Received key press for document: ${keyName}` );
 		const fn = this.props.actions[ keyName ];
 		if ( isFunction( fn ) ) {
 			event.preventDefault();
+			event.stopPropagation();
 			fn();
 		}
 	}
