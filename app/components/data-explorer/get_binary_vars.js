763dc2bc5ab91686673f84348c8bf79d442b8fed
Planeshifter <pburckhardt@outlook.com>
2019-10-19 22:31:36 -0400

    Ensure variable exists

--- app/components/data-explorer/get_binary_vars.js
+++ app/components/data-explorer/get_binary_vars.js
@@ -9,15 +9,18 @@ import isNull from '@stdlib/assert/is-null';
 function getBinaryVars( vars, data ) {
 	const out = [];
 	for ( let i = 0; i < vars.length; i++ ) {
-		const vals = data[ vars[ i ] ].filter( x => !isNull( x ) && !isnan( x ) );
-		const encountered = new Set();
-		for ( let j = 0; j < vals.length; j++ ) {
-			encountered.add( vals[ j ] );
-			if ( encountered.size > 2 ) {
-				break;
-			}
-			if ( j === vals.length - 1 ) {
-				out.push( vars[ i ] );
+		let vals = data[ vars[ i ] ];
+		if ( vals ) {
+			vals = vals.filter( x => !isNull( x ) && !isnan( x ) );
+			const encountered = new Set();
+			for ( let j = 0; j < vals.length; j++ ) {
+				encountered.add( vals[ j ] );
+				if ( encountered.size > 2 ) {
+					break;
+				}
+				if ( j === vals.length - 1 ) {
+					out.push( vars[ i ] );
+				}
 			}
 		}
 	}
