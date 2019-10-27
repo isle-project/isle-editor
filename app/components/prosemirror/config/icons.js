340c7c17dfb7dc6e67e27b162523000e9a061d6f
Planeshifter <pburckhardt@outlook.com>
2019-10-27 00:27:10 -0400

    Add icons

---
+++ app/components/prosemirror/config/icons.js
@@ -0,0 +1,37 @@
+// MODULES //
+
+import React from 'react';
+
+
+// MAIN //
+
+const icons = {
+	blockquote: <i className="fas fa-quote-left"></i>,
+	bold: <i className="fas fa-bold"></i>,
+	bullet_list: <i className="fas fa-list-ul"></i>,
+	code_block: <i className="fas fa-file-code"></i>,
+	code: <i className="fas fa-code"></i>,
+	editTable: <i className="fas fa-table"></i>,
+	em: <i className="fas fa-italic"></i>,
+	guides: <i className="fa fa-question-circle"></i>,
+	join_up: <i className="fas fa-object-group"></i>,
+	lift: <i className="fas fa-angle-left"></i>,
+	open: <i className="fa fa-folder-open"></i>,
+	ordered_list: <i className="fas fa-list-ol"></i>,
+	paragraph: <i className="fas fa-paragraph"></i>,
+	redo: <i className="fas fa-redo"></i>,
+	save: <i className="fas fa-save"></i>,
+	selectParent: <i className="far fa-object-group"></i>,
+	strikethrough: <i className="fas fa-strikethrough"></i>,
+	strong: <i className="fas fa-bold"></i>,
+	submit: <i className="fa fa-share-square"></i>,
+	subscript: <i className="fas fa-subscript"></i>,
+	superscript: <i className="fas fa-superscript"></i>,
+	underline: <i className="fas fa-underline"></i>,
+	undo: <i className="fas fa-undo"></i>
+};
+
+
+// EXPORTS //
+
+export default icons;
