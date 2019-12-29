b9b756849eeb8dcd3870bfc9a43aefd1c2dc5c21
Planeshifter <pburckhardt@outlook.com>
2019-12-29 12:45:41 +0100

    Add UI classes

---
+++ app/components/text-editor/config/ui/custom_node_view.js
@@ -0,0 +1,213 @@
+/*
+* Adapted from: https://github.com/chanzuckerberg/czi-prosemirror/blob/master/src/ui/CustomNodeView.js
+*
+* MIT License
+*
+* Copyright (c) 2019 Chan Zuckerberg Initiative
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*/
+
+/* eslint-disable class-methods-use-this */
+
+// MODULES //
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SelectionObserver from './selection_observer.js';
+
+
+// VARIABLES //
+
+// Standard className for selected node.
+const SELECTED_NODE_CLASS_NAME = 'ProseMirror-selectednode';
+
+const mountedViews = new Set();
+const pendingViews = new Set();
+
+function onMutation(mutations, observer) {
+	const root = document.body;
+	if (!root) {
+		return;
+	}
+
+	const mountingViews = [];
+	for (const view of pendingViews) {
+		const el = view.dom;
+		if (root.contains(el)) {
+			pendingViews.delete(view);
+			mountingViews.push(view);
+			view.__renderReactComponent();
+		}
+	}
+
+	for (const view of mountedViews) {
+		const el = view.dom;
+		if (!root.contains(el)) {
+			mountedViews.delete(el);
+			ReactDOM.unmountComponentAtNode(el);
+		}
+	}
+
+	mountingViews.forEach(view => mountedViews.add(view));
+
+	if (mountedViews.size === 0) {
+		observer.disconnect();
+	}
+}
+
+// Workaround to get in-selection views selected.
+// See https://discuss.prosemirror.net/t/copy-selection-issue-with-the-image-node/1673/2;
+function onSelection(entries, observer) {
+	if (!window.getSelection) {
+		console.warn('window.getSelection() is not supported');
+		observer.disconnect();
+		return;
+	}
+
+	const selection = window.getSelection();
+	if (!selection.containsNode) {
+		console.warn('selection.containsNode() is not supported');
+		observer.disconnect();
+		return;
+	}
+
+	for (const view of mountedViews) {
+		const el = view.dom;
+		if (selection.containsNode(el)) {
+			view.selectNode();
+		} else {
+			view.deselectNode();
+		}
+	}
+
+	if (mountedViews.size === 0) {
+		observer.disconnect();
+	}
+}
+
+const selectionObserver = new SelectionObserver(onSelection);
+const mutationObserver = new MutationObserver(onMutation);
+
+
+// MAIN //
+
+// This implements the `NodeView` interface and renders a Node with a react
+// Component.
+// https://prosemirror.net/docs/ref/#view.NodeView
+// https://github.com/ProseMirror/prosemirror-view/blob/master/src/viewdesc.js#L429
+class CustomNodeView {
+	constructor(
+		node,
+		editorView,
+		getPos,
+		decorations
+	) {
+		this.props = {
+			decorations,
+			editorView,
+			getPos,
+			node,
+			selected: false,
+			focused: false
+		};
+
+		pendingViews.add(this);
+
+		// The editor will use this as the node's DOM representation
+		const dom = this.createDOMElement();
+		this.dom = dom;
+		this._selected = null;
+		dom.onClick = this._onClick;
+
+		if (pendingViews.size === 1) {
+			mutationObserver.observe(document, {
+				childList: true, subtree: true
+			});
+			selectionObserver.observe(document);
+		}
+	}
+
+	update(node, decorations) {
+		this.props = {
+			...this.props,
+			node
+		};
+		this.__renderReactComponent();
+		return true;
+	}
+
+	stopEvent() {
+		return false;
+	}
+
+	// Mark this node as being the selected node.
+	selectNode() {
+		this._selected = true;
+		this.dom.classList.add(SELECTED_NODE_CLASS_NAME);
+		this.__renderReactComponent();
+	}
+
+	// Remove selected node marking from this node.
+	deselectNode() {
+		this._selected = false;
+		this.dom.classList.remove(SELECTED_NODE_CLASS_NAME);
+		this.__renderReactComponent();
+	}
+
+	// This should be overwrite by subclass.
+	createDOMElement() {
+		// The editor will use this as the node's DOM representation.
+		// return document.createElement('span');
+		throw new Error('not implemented');
+	}
+
+	// This should be overwrite by subclass.
+	renderReactComponent() {
+		// return <CustomNodeViewComponent {...this.props} />;
+		throw new Error('not implemented');
+	}
+
+	destroy() {
+		// Called when the node view is removed from the editor or the whole
+		// editor is destroyed.
+		// sub-class may override this method.
+	}
+
+	__renderReactComponent() {
+		const { editorView, getPos } = this.props;
+
+		if (editorView.state && editorView.state.selection) {
+			const { from } = editorView.state.selection;
+			const pos = getPos();
+			this.props.selected = this._selected;
+			this.props.focused = editorView.focused && pos === from;
+		} else {
+			this.props.selected = false;
+			this.props.focused = false;
+		}
+
+		ReactDOM.render(this.renderReactComponent(), this.dom);
+	}
+}
+
+
+// EXPORTS //
+
+export default CustomNodeView;
---
+++ app/components/text-editor/config/ui/editor_frameset.js
@@ -0,0 +1,107 @@
+/*
+* Adapted from: https://github.com/chanzuckerberg/czi-prosemirror/blob/master/src/ui/EditorFrameset.js
+*
+* MIT License
+*
+* Copyright (c) 2019 Chan Zuckerberg Initiative
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*/
+
+// MODULES //
+
+import React from 'react';
+import cx from 'classnames';
+import isUndefinedOrNull from '@stdlib/assert/is-undefined-or-null';
+import isUndefined from '@stdlib/assert/is-undefined';
+import './editor_frameset.css';
+
+
+// VARIABLES //
+
+export const FRAMESET_BODY_CLASSNAME = 'text-editor-frame-body';
+
+
+// FUNCTIONS //
+
+function toCSS( val ) {
+	if (typeof val === 'number') {
+		return val + 'px';
+	}
+	if ( isUndefinedOrNull( val ) ) {
+		return 'auto';
+	}
+	return String(val);
+}
+
+
+// MAIN //
+
+class EditorFrameset extends React.PureComponent {
+	render() {
+		const {
+			body,
+			className,
+			embedded,
+			header,
+			height,
+			toolbarPlacement,
+			toolbar,
+			width
+		} = this.props;
+
+		const useFixedLayout = !isUndefined( width ) || !isUndefined( height);
+
+		const mainClassName = cx(className, {
+			'text-editor-frameset': true,
+			'with-fixed-layout': useFixedLayout,
+			embedded: embedded
+		});
+
+		const mainStyle = {
+			width: toCSS( isUndefined( width ) && useFixedLayout ? 'auto' : width ),
+			height: toCSS( isUndefined( height ) && useFixedLayout ? 'auto' : height )
+		};
+
+		const toolbarHeader =
+			toolbarPlacement === 'header' || !toolbarPlacement ? toolbar : null;
+		const toolbarBody = toolbarPlacement === 'body' && toolbar;
+
+		return (
+			<div className={mainClassName} style={mainStyle}>
+				<div className="text-editor-frame-main">
+					<div className="text-editor-frame-head">
+						{header}
+						{toolbarHeader}
+					</div>
+					<div className={FRAMESET_BODY_CLASSNAME}>
+						{toolbarBody}
+						<div className="text-editor-frame-body-scroll">{body}</div>
+					</div>
+					<div className="text-editor-frame-footer" />
+				</div>
+			</div>
+		);
+	}
+}
+
+
+// EXPORTS //
+
+export default EditorFrameset;
