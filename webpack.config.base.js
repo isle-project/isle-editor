1a7b4e15a5577c8eb525e99c492eb2f31b82ac5f
Planeshifter <pburckhardt@outlook.com>
2020-01-08 20:05:46 +0100

    Update deps and external resources

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -130,8 +130,7 @@ export default {
 	resolve: {
 		alias: {
 			'victory': resolve( './node_modules/victory/dist/victory.min.js' ),
-			'form-data': resolve( './node_modules/form-data/lib/form_data.js' ),
-			'semver': resolve( './node_modules/make-dir/node_modules/semver/semver.js' )
+			'form-data': resolve( './node_modules/form-data/lib/form_data.js' )
 		},
 		modules: [
 			resolve( './app' ),
