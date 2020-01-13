e8a6fdd8b6fb62b09117ff8527dc7bb4d903481f
Planeshifter <pburckhardt@outlook.com>
2020-01-12 20:15:05 -0500

    Change default server address

--- app/constants/preamble.js
+++ app/constants/preamble.js
@@ -5,7 +5,7 @@ author: John Doe
 date: <today>
 language: en-US
 state:
-server: ${localStorage.getItem( 'server' ) || 'https://isle.heinz.cmu.edu'}
+server: ${localStorage.getItem( 'server' ) || 'https://isle.stat.cmu.edu'}
 license: CC BY 4.0 [https://creativecommons.org/licenses/by/4.0]`;
 
 
