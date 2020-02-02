9c825eeb9af858c334bc30431a0b25a4b1349a99
Planeshifter <pburckhardt@outlook.com>
2020-02-02 00:11:28 -0500

    Resize terminal upon changes

--- app/editor-components/terminal/index.js
+++ app/editor-components/terminal/index.js
@@ -3,9 +3,13 @@
 import React, { Component } from 'react';
 import os from 'os';
 import { dirname } from 'path';
+import PropTypes from 'prop-types';
 import logger from 'debug';
 import { Terminal } from 'xterm';
 import { FitAddon } from 'xterm-addon-fit';
+import { WebLinksAddon } from 'xterm-addon-web-links';
+import { SearchAddon } from 'xterm-addon-search';
+import IS_WINDOWS from '@stdlib/assert/is-windows';
 const pty = require( 'node-pty' );
 import './terminal.css';
 
@@ -15,18 +19,61 @@ import './terminal.css';
 const debug = logger( 'isle-editor:terminal' );
 
 
+// FUNCTIONS //
+
+function calculateColumns( width, fontSize ) {
+	return width / ( 0.79 * fontSize );
+}
+
+function calculateRows( height, fontSize ) {
+	if ( height < 17 ) {
+		height = 17;
+	}
+	return height / ( 1.1 * fontSize );
+}
+
+
 // MAIN //
 
 class TerminalWrapper extends Component {
+	constructor( props ) {
+		super( props );
+		this.searchAddon = new SearchAddon();
+		this.linksAddon = new WebLinksAddon();
+		this.fitAddon = new FitAddon();
+	}
+
 	componentDidMount() {
 		this.xterm = new Terminal({
-			'theme': {
+			fontSize: this.props.fontSize,
+			lineHeight: 1,
+			scrollback: 500,
+			cursorStyle: 'block',
+			windowsMode: IS_WINDOWS,
+			theme: {
 				background: 'gainsboro',
-				foreground: 'darkslategrey'
+				foreground: 'darkslategrey',
+				black: '#000000',
+				blue: '#0a2fc4',
+				brightBlack: '#686868',
+				brightBlue: '#6a76fb',
+				brightCoral: '#f08080',
+				brightCyan: '#68fdfe',
+				brightGreen: '#5acc60',
+				brightMagenta: '#Fd7cfc',
+				brightRed: '#fd6f6b',
+				brightWhite: '#ffffff',
+				brightYellow: '#fffa72',
+				cyan: '#20c5c6',
+				green: '#169e19',
+				magenta: '#c839c5',
+				red: '#c51e14',
+				white: '#c7c7c7',
+				yellow: '#c7c329'
 			}
 		});
-		this.fitAddon = new FitAddon();
 		this.xterm.loadAddon( this.fitAddon );
+		this.xterm.loadAddon( this.linksAddon );
 		this.xterm.open( this.terminal );
 		this.fitAddon.fit();
 		this.createShell();
@@ -36,6 +83,20 @@ class TerminalWrapper extends Component {
 	}
 
 	componentDidUpdate( prevProps ) {
+		if ( this.props.fontSize !== prevProps.fontSize ) {
+			this.xterm.setOption( 'fontSize', this.props.fontSize );
+		}
+		if (
+			this.props.width !== prevProps.width ||
+			this.props.height !== prevProps.height ||
+			this.props.fontSize !== prevProps.fontSize
+		) {
+			this.fitAddon.fit();
+			this.ptyProcess.resize(
+				calculateColumns( this.props.width, this.props.fontSize ),
+				calculateRows( this.props.height, this.props.fontSize )
+			);
+		}
 		if ( this.props.height !== prevProps.height ) {
 			this.fitAddon.fit();
 		}
@@ -55,8 +116,8 @@ class TerminalWrapper extends Component {
 		debug( `Open ${dir} directory in shell...` );
 		this.ptyProcess = pty.spawn( shell, [], {
 			name: 'xterm-color',
-			cols: 80,
-			rows: 30,
+			cols: calculateColumns( this.props.width, this.props.fontSize ),
+			rows: calculateRows( this.props.height, this.props.fontSize ),
 			cwd: dir,
 			env: process.env // eslint-disable-line no-process-env
 		});
@@ -87,6 +148,15 @@ class TerminalWrapper extends Component {
 		);
 	}
 }
+
+
+// PROPERTIES //
+
+TerminalWrapper.propTypes = {
+	fontSize: PropTypes.number.isRequired,
+	height: PropTypes.number.isRequired,
+	width: PropTypes.number.isRequired
+};
 
 
 // EXPORTS //
