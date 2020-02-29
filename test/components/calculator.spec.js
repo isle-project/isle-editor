79b64d69d8987cf7b23786b7bb2baa7d2c8a2be0
Planeshifter <pburckhardt@outlook.com>
2020-02-28 21:37:30 -0500

    Add tests

---
+++ test/components/calculator.spec.js
@@ -0,0 +1,29 @@
+// MODULES //
+
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Calculator from 'components/calculator';
+
+
+// TESTS //
+
+it( 'renders a basic calculator', () => {
+	const calculator = renderer.create(
+		<Calculator expandable={false} />
+	).toJSON();
+	expect( calculator ).toMatchSnapshot();
+});
+
+it( 'renders an expandable calculator', () => {
+	const calculator = renderer.create(
+		<Calculator expandable={true} />
+	).toJSON();
+	expect( calculator ).toMatchSnapshot();
+});
+
+it( 'renders an inactive calculator', () => {
+	const calculator = renderer.create(
+		<Calculator show={false} />
+	).toJSON();
+	expect( calculator ).toMatchSnapshot();
+});
---
+++ test/components/sticky_note.spec.js
@@ -0,0 +1,50 @@
+// MODULES //
+
+import React from 'react';
+import renderer from 'react-test-renderer';
+import StickyNote from 'components/sticky-note';
+
+
+// TESTS //
+
+it( 'renders a sticky note', () => {
+	const calculator = renderer.create(
+		<StickyNote title="My note" body="Text body of note" />
+	).toJSON();
+	expect( calculator ).toMatchSnapshot();
+});
+
+it( 'renders an editable sticky note', () => {
+	const calculator = renderer.create(
+		<StickyNote title="My note" body="Text body of note" editable />
+	).toJSON();
+	expect( calculator ).toMatchSnapshot();
+});
+
+it( 'renders a minimizable sticky note', () => {
+	const calculator = renderer.create(
+		<StickyNote title="My note" body="Text body of note" minimizable />
+	).toJSON();
+	expect( calculator ).toMatchSnapshot();
+});
+
+it( 'renders a colored sticky note', () => {
+	const calculator = renderer.create(
+		<StickyNote title="My note" body="Text body of note" color="orange" />
+	).toJSON();
+	expect( calculator ).toMatchSnapshot();
+});
+
+it( 'renders a sticky note with a date', () => {
+	const calculator = renderer.create(
+		<StickyNote title="My note" body="Text body of note" date="01/20/2020" />
+	).toJSON();
+	expect( calculator ).toMatchSnapshot();
+});
+
+it( 'renders a sticky note with a stain', () => {
+	const calculator = renderer.create(
+		<StickyNote title="My note" body="Text body of note" stain />
+	).toJSON();
+	expect( calculator ).toMatchSnapshot();
+});
