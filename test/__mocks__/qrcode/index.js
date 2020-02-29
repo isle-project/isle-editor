8f86630f7a51f45b84b420f3c06426344138e32b
Planeshifter <pburckhardt@outlook.com>
2020-02-29 12:05:39 -0500

    Add snapshot tests

---
+++ test/__mocks__/qrcode/index.js
@@ -0,0 +1,14 @@
+// MODULES //
+
+const noop = require( '@stdlib/utils/noop' );
+
+
+// MAIN //
+
+function QRCode() {}
+QRCode.toCanvas = noop;
+
+
+// EXPORTS //
+
+module.exports = QRCode;
---
+++ test/components/qrcode.spec.js
@@ -0,0 +1,36 @@
+// MODULES //
+
+import React from 'react';
+import renderer from 'react-test-renderer';
+import QRCode from 'components/qrcode';
+
+
+// TESTS //
+
+it( 'renders a QRCode', () => {
+	const qrcode = renderer.create(
+		<QRCode expandable={false} />
+	).toJSON();
+	expect( qrcode ).toMatchSnapshot();
+});
+
+it( 'renders a QRCode with custom text', () => {
+	const qrcode = renderer.create(
+		<QRCode text="Lorem ipsum" />
+	).toJSON();
+	expect( qrcode ).toMatchSnapshot();
+});
+
+it( 'renders a centered QRCode', () => {
+	const qrcode = renderer.create(
+		<QRCode center />
+	).toJSON();
+	expect( qrcode ).toMatchSnapshot();
+});
+
+it( 'renders a QRCode with a custom scale', () => {
+	const qrcode = renderer.create(
+		<QRCode scale={3} />
+	).toJSON();
+	expect( qrcode ).toMatchSnapshot();
+});
