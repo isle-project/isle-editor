8dd1967f6176da3ee4d462fd4d05330d589456f2
Planeshifter <pburckhardt@outlook.com>
2020-03-09 15:42:04 -0400

    Fix lint warning and disable rule for editor file

--- app/components/text-editor/config/image_upload_placeholder_plugin.js
+++ app/components/text-editor/config/image_upload_placeholder_plugin.js
@@ -79,21 +79,21 @@ function defer(fn) {
 }
 
 function uploadImage( file ) {
-	return new Promise((resolve, reject) => {
+	return new Promise( (resolve, reject) => {
 		const { FileReader } = window;
 		if ( FileReader ) {
 			const reader = new FileReader();
 			reader.onload = event => {
-			// base64 encoded url.
-			const src = event.target.result;
-			resolve({src, height: 0, width: 0, id: ''});
+				// base64 encoded url.
+				const src = event.target.result;
+				resolve({ src, height: 0, width: 0, id: '' });
 			};
 			reader.onerror = () => {
-			reject(new Error('FileReader failed'));
+				reject( new Error( 'FileReader failed' ) );
 			};
-			reader.readAsDataURL(file);
+			reader.readAsDataURL( file );
 		} else {
-			reject(new Error('FileReader is not available'));
+			reject( new Error( 'FileReader is not available' ) );
 		}
 	});
 }
--- app/editor-components/editor/index.js
+++ app/editor-components/editor/index.js
@@ -1,3 +1,5 @@
+/* eslint-disable max-lines */
+
 // MODULES //
 
 import React, { Component } from 'react';
