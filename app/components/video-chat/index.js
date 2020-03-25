24198ce4052d0b4f6dd43be879ca2eb8aeb54e8e
Planeshifter <pburckhardt@outlook.com>
2020-03-25 00:02:15 -0400

    Set avatar URL and mute students by default

--- app/components/video-chat/index.js
+++ app/components/video-chat/index.js
@@ -75,13 +75,12 @@ class VideoChat extends Component {
 			width: max( 0.3 * window.innerWidth, 400 ),
 			height: max( 0.3 * window.innerHeight, 400 ),
 			parentNode: this.videoChatContainer,
-			avatarURL: session.user.picture,
 			userInfo: {
 				email: session.user.email
 			},
 			configOverwrite: {
-				startVideoMuted: !session.isOwner(),
-				startAudioMuted: !session.isOwner()
+				startWithVideoMuted: !isOwner,
+				startWithAudioMuted: !isOwner
 			},
 			interfaceConfigOverwrite: INTERFACE_CONFIG,
 			noSSL: false
@@ -89,6 +88,7 @@ class VideoChat extends Component {
 		this.api = new JitsiMeetExternalAPI( DOMAIN, options );
 		this.api.executeCommand( 'displayName', session.user.name );
 		this.api.executeCommand( 'subject', this.props.roomSubject );
+		this.api.executeCommand( 'avatarUrl', session.user.picture );
 
 		this.api.addEventListener( 'screenSharingStatusChanged', ( status ) => {
 			this.setState({
@@ -96,6 +96,13 @@ class VideoChat extends Component {
 				minimized: status.on
 			});
 		});
+	}
+
+	handleClose = () => {
+		if ( this.context.isOwner() ) {
+			this.api.executeCommand( 'hangup' );
+		}
+		this.props.onHide();
 	}
 
 	handleCompress = () => {
@@ -136,7 +143,7 @@ class VideoChat extends Component {
 				<Tooltip tooltip="Leave video" >
 					<Button
 						size="sm"
-						onClick={this.props.onHide}
+						onClick={this.handleClose}
 						className="video-chat-button"
 					>
 						<i className="fas fa-video-slash"></i>
