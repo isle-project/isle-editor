232f5e8af0302f5c36492aa35421c66124bf3644
Planeshifter <pburckhardt@outlook.com>
2020-04-11 19:32:07 -0400

    Add user list mock

---
+++ test/mocks/user_list.js
@@ -0,0 +1,201 @@
+// MAIN //
+
+const userList = [
+	{
+		email: 'isle@domain.com',
+		exitTime: null,
+		joinTime: '8:57:51 PM',
+		name: 'Instructor',
+		owner: true,
+		picture: '5e8a3391a5e50652801a32ec_0.95_0.png'
+	},
+	{
+		email: 'alpha@domain.com',
+		exitTime: null,
+		joinTime: '8:57:51 PM',
+		name: 'Alpha',
+		owner: false,
+		picture: '5e8a3391a5e50652801a32ec_0.95_0.png'
+	},
+	{
+		email: 'beta@domain.com',
+		exitTime: null,
+		joinTime: '8:57:51 PM',
+		name: 'Beta',
+		owner: false,
+		picture: '5e8a3391a5e50652801a32ec_0.95_0.png'
+	},
+	{
+		email: 'gamma@domain.com',
+		exitTime: null,
+		joinTime: '8:57:51 PM',
+		name: 'Gamma',
+		owner: false,
+		picture: '5e8a3391a5e50652801a32ec_0.95_0.png'
+	},
+	{
+		email: 'Delta@domain.com',
+		exitTime: null,
+		joinTime: '8:57:51 PM',
+		name: 'Delta',
+		owner: false,
+		picture: '5e8a3391a5e50652801a32ec_0.95_0.png'
+	},
+	{
+		email: 'Epsilon@domain.com',
+		exitTime: null,
+		joinTime: '8:57:51 PM',
+		name: 'Epsilon',
+		owner: false,
+		picture: '5e8a3391a5e50652801a32ec_0.95_0.png'
+	},
+	{
+		email: 'Zeta@domain.com',
+		exitTime: null,
+		joinTime: '8:57:51 PM',
+		name: 'Zeta',
+		owner: false,
+		picture: '5e8a3391a5e50652801a32ec_0.95_0.png'
+	},
+	{
+		email: 'Eta@domain.com',
+		exitTime: null,
+		joinTime: '8:57:51 PM',
+		name: 'Eta',
+		owner: false,
+		picture: '5e8a3391a5e50652801a32ec_0.95_0.png'
+	},
+	{
+		email: 'Theta@domain.com',
+		exitTime: null,
+		joinTime: '8:57:51 PM',
+		name: 'Theta',
+		owner: false,
+		picture: '5e8a3391a5e50652801a32ec_0.95_0.png'
+	},
+	{
+		email: 'Iota@domain.com',
+		exitTime: null,
+		joinTime: '8:57:51 PM',
+		name: 'Iota',
+		owner: false,
+		picture: '5e8a3391a5e50652801a32ec_0.95_0.png'
+	},
+	{
+		email: 'Kappa@domain.com',
+		exitTime: null,
+		joinTime: '8:57:51 PM',
+		name: 'Kappa',
+		owner: false,
+		picture: '5e8a3391a5e50652801a32ec_0.95_0.png'
+	},
+	{
+		email: 'Lambda@domain.com',
+		exitTime: null,
+		joinTime: '8:57:51 PM',
+		name: 'Lambda',
+		owner: false,
+		picture: '5e8a3391a5e50652801a32ec_0.95_0.png'
+	},
+	{
+		email: 'My@domain.com',
+		exitTime: null,
+		joinTime: '8:57:51 PM',
+		name: 'My',
+		owner: false,
+		picture: '5e8a3391a5e50652801a32ec_0.95_0.png'
+	},
+	{
+		email: 'Ny@domain.com',
+		exitTime: null,
+		joinTime: '8:57:51 PM',
+		name: 'Ny',
+		owner: false,
+		picture: '5e8a3391a5e50652801a32ec_0.95_0.png'
+	},
+	{
+		email: 'Omikron@domain.com',
+		exitTime: null,
+		joinTime: '8:57:51 PM',
+		name: 'Omikron',
+		owner: false,
+		picture: '5e8a3391a5e50652801a32ec_0.95_0.png'
+	},
+	{
+		email: 'Pi@domain.com',
+		exitTime: null,
+		joinTime: '8:57:51 PM',
+		name: 'Pi',
+		owner: false,
+		picture: '5e8a3391a5e50652801a32ec_0.95_0.png'
+	},
+	{
+		email: 'Rho@domain.com',
+		exitTime: null,
+		joinTime: '8:57:51 PM',
+		name: 'Rho',
+		owner: false,
+		picture: '5e8a3391a5e50652801a32ec_0.95_0.png'
+	},
+	{
+		email: 'Sigma@domain.com',
+		exitTime: null,
+		joinTime: '8:57:51 PM',
+		name: 'Sigma',
+		owner: false,
+		picture: '5e8a3391a5e50652801a32ec_0.95_0.png'
+	},
+	{
+		email: 'Tau@domain.com',
+		exitTime: null,
+		joinTime: '8:57:51 PM',
+		name: 'Tau',
+		owner: false,
+		picture: '5e8a3391a5e50652801a32ec_0.95_0.png'
+	},
+	{
+		email: 'Ypsilon@domain.com',
+		exitTime: null,
+		joinTime: '8:57:51 PM',
+		name: 'Ypsilon',
+		owner: false,
+		picture: '5e8a3391a5e50652801a32ec_0.95_0.png'
+	},
+	{
+		email: 'Phi@domain.com',
+		exitTime: null,
+		joinTime: '8:57:51 PM',
+		name: 'Phi',
+		owner: false,
+		picture: '5e8a3391a5e50652801a32ec_0.95_0.png'
+	},
+	{
+		email: 'Psi@domain.com',
+		exitTime: null,
+		joinTime: '8:57:51 PM',
+		name: 'Psi',
+		owner: false,
+		picture: '5e8a3391a5e50652801a32ec_0.95_0.png'
+	},
+	{
+		email: 'Chi@domain.com',
+		exitTime: null,
+		joinTime: '8:57:51 PM',
+		name: 'Chi',
+		owner: false,
+		picture: '5e8a3391a5e50652801a32ec_0.95_0.png'
+	},
+	{
+		email: 'Omega@domain.com',
+		exitTime: null,
+		joinTime: '8:57:51 PM',
+		name: 'Omega',
+		owner: false,
+		picture: '5e8a3391a5e50652801a32ec_0.95_0.png'
+	}
+];
+
+
+// EXPORTS //
+
+module.exports = userList;
