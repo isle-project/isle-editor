f91065d1d26e3511e26f2866aa44988ec4dd6193
Planeshifter <pburckhardt@outlook.com>
2020-05-31 18:08:16 -0400

    Manually load used react-bootstrap components

--- app/editor-components/preview/create_scope.js
+++ app/editor-components/preview/create_scope.js
@@ -3,7 +3,25 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import Draggable from 'react-draggable';
-import * as ReactBootstrap from 'react-bootstrap';
+
+import Card from 'react-bootstrap/Card';
+import Form from 'react-bootstrap/Form';
+import Modal from 'react-bootstrap/Modal';
+import Alert from 'react-bootstrap/Alert';
+import Badge from 'react-bootstrap/Badge';
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import FormControl from 'react-bootstrap/FormControl';
+import ListGroup from 'react-bootstrap/ListGroup';
+import ListGroupItem from 'react-bootstrap/ListGroupItem';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavItem from 'react-bootstrap/NavItem';
+import NavLink from 'react-bootstrap/NavLink';
+import Row from 'react-bootstrap/Row';
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
+
 import copy from '@stdlib/utils/copy';
 import Components from 'components';
 import factor from 'utils/factor-variable';
@@ -13,7 +31,23 @@ import factor from 'utils/factor-variable';
 
 export const SCOPE = {
 	...Components,
-	...ReactBootstrap
+	Card,
+	Form,
+	Modal,
+	Alert,
+	Badge,
+	Button,
+	Col,
+	FormControl,
+	ListGroup,
+	ListGroupItem,
+	Nav,
+	Navbar,
+	NavItem,
+	NavLink,
+	Row,
+	Tab,
+	Tabs
 };
 
 
