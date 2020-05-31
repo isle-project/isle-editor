4f1852c2ec205f4f43804d0eb22b7eaa665f2859
Planeshifter <pburckhardt@outlook.com>
2020-05-31 14:34:25 -0400

    Load internal components asynchronously

---
+++ app/internal-components/lesson/main.js
@@ -0,0 +1,211 @@
+// MODULES //
+
+import React, { Component, Fragment } from 'react';
+import PropTypes from 'prop-types';
+import ReactNotificationSystem from 'react-notification-system';
+import { ContextMenuTrigger } from 'react-contextmenu';
+import isObjectArray from '@stdlib/assert/is-object-array';
+import StickyNote from 'components/sticky-note';
+import randomstring from 'utils/randomstring/alphanumeric';
+import { SessionContext } from '@isle-project/session';
+import { DELETE_STICKY_NOTE, INSERT_STICKY_NOTE, STICKY_NOTE_TITLE, STICKY_NOTE_BODY, STICKY_NOTE_MOVE } from 'constants/actions.js';
+import { RECEIVED_LESSON_INFO, RECEIVED_USER_RIGHTS, RETRIEVED_CURRENT_USER_ACTIONS } from 'constants/events.js';
+import LessonContextMenu from './contextmenu.js';
+import Forbidden from './forbidden.js';
+
+
+// VARIABLES //
+
+const NOTIFICATION_STYLE = {
+	Containers: {
+		tc: {
+			margin: '30px auto 0px auto'
+		}
+	},
+	NotificationItem: { // Override the notification item
+		info: {
+			border: 'solid 8px #007bff',
+			borderRadius: 6,
+			padding: 20
+		},
+		success: {
+			boxShadow: '1px 1px 3px 1px darkslategrey'
+		}
+	},
+	Title: {
+		success: {
+			color: '#3c763d'
+		}
+	}
+};
+
+
+// MAIN //
+
+/**
+* The main lesson component holding all other components.
+*/
+class Lesson extends Component {
+	constructor( props ) {
+		super( props );
+
+		this.state = {
+			visible: true,
+			notes: []
+		};
+	}
+
+	componentDidMount() {
+		const session = this.context;
+		session.onLessonMount();
+		this.unsubscribe = session.subscribe( ( type, value ) => {
+			if ( type === RETRIEVED_CURRENT_USER_ACTIONS ) {
+				const currentUserActions = value;
+				if ( isObjectArray( currentUserActions[ 'lesson' ] ) ) {
+					this.restoreNotes( currentUserActions );
+				}
+			}
+			else if (
+				type === RECEIVED_LESSON_INFO ||
+				type === RECEIVED_USER_RIGHTS
+			) {
+				this.setState({
+					visible: session.isActive() || session.isOwner()
+				});
+			}
+		});
+	}
+
+	restoreNotes = ( currentUserActions ) => {
+		const actions = currentUserActions[ 'lesson' ];
+		for ( let i = 0; i < actions.length; i++ ) {
+			const action = actions[ i ];
+			if ( action.type === INSERT_STICKY_NOTE ) {
+				let { left, top, noteID } = action.value;
+				let isDeleted = false;
+				let deltaX = 0;
+				let deltaY = 0;
+				let body = null;
+				let title = null;
+				const manipulations = currentUserActions[ noteID ];
+				if ( manipulations ) {
+					for ( let j = manipulations.length - 1; j >= 0; j-- ) {
+						switch ( manipulations[ j ].type ) {
+							case DELETE_STICKY_NOTE:
+								isDeleted = true;
+								break;
+							case STICKY_NOTE_TITLE:
+								title = manipulations[ j ].value;
+								break;
+							case STICKY_NOTE_BODY:
+								body = manipulations[ j ].value;
+								break;
+							case STICKY_NOTE_MOVE:
+								deltaX += manipulations[ j ].value.x;
+								deltaY += manipulations[ j ].value.y;
+								break;
+						}
+					}
+				}
+				left += deltaX;
+				top += deltaY;
+				if ( !isDeleted ) {
+					this.addNote({ left, top, noteID, title, body });
+				}
+			}
+		}
+	}
+
+	addNote = ({ left, top, noteID, body, title }) => {
+		if ( !noteID ) {
+			noteID = randomstring( 3 );
+			const session = this.context;
+			session.log({
+				id: 'lesson',
+				type: INSERT_STICKY_NOTE,
+				value: {
+					top,
+					left,
+					noteID
+				}
+			});
+		}
+		const note = <StickyNote
+			id={noteID}
+			color="green"
+			editable minimizable removable
+			key={this.state.notes.length}
+			draggable={{
+				default: {
+					x: left,
+					y: top
+				}
+			}}
+			title={title}
+			body={body}
+		/>;
+		const notes = this.state.notes.slice();
+		notes.push( note );
+		this.setState({
+			notes
+		});
+	}
+
+	render() {
+		if ( !this.state.visible ) {
+			return <Forbidden />;
+		}
+		return (
+			<Fragment>
+				<ContextMenuTrigger
+					attributes={{
+						className: 'contextmenu-fullscreen'
+					}}
+					holdToDisplay={-1}
+					disableIfShiftIsPressed
+					id="lessonWindow"
+				>
+					<div
+						id="Lesson"
+						className={this.props.className}
+						style={this.props.style}
+					>
+						{this.props.children}
+						{this.state.notes}
+					</div>
+				</ContextMenuTrigger>
+				<LessonContextMenu
+					addNote={this.addNote}
+					session={this.context}
+				/>
+				<ReactNotificationSystem
+					ref={( div ) => {
+						global.notificationSystemISLE = div;
+					}}
+					allowHTML={true}
+					style={NOTIFICATION_STYLE}
+				/>
+			</Fragment>
+		);
+	}
+}
+
+
+// PROPERTIES //
+
+Lesson.propTypes = {
+	className: PropTypes.string,
+	style: PropTypes.object
+};
+
+Lesson.defaultProps = {
+	className: '',
+	style: {}
+};
+
+Lesson.contextType = SessionContext;
+
+
+// EXPORTS //
+
+export default Lesson;
---
+++ app/internal-components/statusbar/main.js
@@ -0,0 +1,589 @@
+// MODULES //
+
+import React, { Component, Fragment, lazy, Suspense } from 'react';
+import logger from 'debug';
+import Button from 'react-bootstrap/Button';
+import ProgressBar from 'react-bootstrap/ProgressBar';
+import round from '@stdlib/math/base/special/round';
+import isArray from '@stdlib/assert/is-array';
+import isEmptyObject from '@stdlib/assert/is-empty-object';
+import Signup from 'internal-components/signup';
+import Login from 'internal-components/login';
+import Gate from 'components/gate';
+import Tooltip from 'components/tooltip';
+import KeyControls from 'components/key-controls';
+import Seal from 'components/seal';
+import isElectron from 'utils/is-electron';
+import VideoChatButton from 'components/video-chat-button';
+import ChatButton from 'components/chat-button';
+import animatePosition from 'utils/animate-position';
+import { SessionContext } from '@isle-project/session';
+import VideoChats from './video-chats';
+import Chats from './chats';
+import ConfirmModal from './confirm_modal.js';
+import { TOGGLE_BLACKSCREEN } from 'constants/actions.js';
+import { CREATED_GROUPS, DISCONNECTED_FROM_SERVER, MEMBER_ACTION, SELF_INITIAL_PROGRESS, SELF_UPDATED_PROGRESS, SELF_UPDATED_SCORE,
+	SERVER_IS_LIVE, LOGGED_OUT, LOGGED_IN, RECEIVED_USER_RIGHTS } from 'constants/events.js';
+import VoiceControl from './voice-control';
+import GroupManager from './group-manager';
+import GroupClient from './group-client';
+import Score from './score';
+import './statusbar.css';
+const InstructorView = lazy( () => import( 'internal-components/statusbar/instructor-view' ) );
+
+
+// VARIABLES //
+
+const debug = logger( 'isle:statusbar' );
+const SERVER_ACTIVE_COLOR = 'rgb(130, 224, 160)';
+const NO_RESPONSE_FROM_SERVER_COLOR = 'rgb(209, 107, 71)';
+
+
+// FUNCTIONS //
+
+function getDuration( session ) {
+	let dur = new Date().getTime() - session.startTime;
+	dur = round( dur / (1000 * 60) );
+	return dur;
+}
+
+function isHidden( el ) {
+	return el.offsetParent === null;
+}
+
+function preventPropagation( event ) {
+	event.stopPropagation();
+}
+
+
+// MAIN //
+
+class StatusBar extends Component {
+	constructor( props, context ) {
+		super( props );
+		this.state = {
+			visibleSignup: false,
+			visibleLogin: false,
+			visibleLogout: false,
+			showGroupManager: false,
+			showStatusBar: !context.config.hideStatusBar,
+			showProgressBar: false,
+			isProgressLeaving: false,
+			progress: 0,
+			duration: '0'
+		};
+		this.hidden = true;
+	}
+
+	componentDidMount() {
+		let sentNotification = false;
+		const promptLogin = () => {
+			const session = this.context;
+			if ( !isElectron && this.state.showStatusBar ) {
+				session.addNotification({
+					title: 'Login',
+					message: 'Please connect with your ISLE account or create a new one.',
+					level: 'success',
+					autoDismiss: session.config.noLoginDismiss ? 0 : 15,
+					position: 'tl',
+					children: <div style={{ marginBottom: '30px' }}>
+						<Button size="sm" style={{ float: 'right', marginRight: '10px' }} onClick={this.signup}>Sign up</Button>
+						<Button size="sm" variant="primary" style={{ float: 'right', marginRight: '10px' }} onClick={this.login}>Login</Button>
+					</div>
+				});
+			}
+		};
+
+		const session = this.context;
+		this.unsubscribe = session.subscribe( ( type, data ) => {
+			setTimeout( () => {
+				if ( !sentNotification && session.anonymous && session.live ) {
+					promptLogin();
+					sentNotification = true;
+				}
+			}, 3000);
+
+			if ( type === SELF_INITIAL_PROGRESS ) {
+				this.setState({
+					progress: round( Number( data ) * 100 )
+				});
+			}
+			else if (
+				type === SELF_UPDATED_PROGRESS ||
+				type === SELF_UPDATED_SCORE
+			) {
+				const newState = {
+					showProgressBar: true,
+					isProgressLeaving: true
+				};
+				if ( type === SELF_UPDATED_PROGRESS ) {
+					newState.progress = round( Number( data ) * 100 );
+				}
+				this.setState( newState );
+				if ( this.progressTimeout ) {
+					clearTimeout( this.progressTimeout );
+					this.progressTimeout = null;
+				}
+				this.progressTimeout = setTimeout(() => {
+					this.setState({
+						showProgressBar: false,
+						isProgressLeaving: false,
+						duration: getDuration( session )
+					});
+					this.progressTimeout = null;
+				}, 4000 );
+			}
+			else if (
+				type === DISCONNECTED_FROM_SERVER ||
+				type === LOGGED_IN || type === LOGGED_OUT ||
+				type === SERVER_IS_LIVE || type === RECEIVED_USER_RIGHTS
+			) {
+				this.forceUpdate();
+			}
+			else if ( type === MEMBER_ACTION && data.type === TOGGLE_BLACKSCREEN ) {
+				if ( data.value ) {
+					const blackscreen = document.createElement( 'div' );
+					blackscreen.setAttribute( 'id', 'blackscreen' );
+					const lesson = document.getElementById( 'Lesson' );
+					lesson.appendChild( blackscreen );
+				} else {
+					const blackscreen = document.getElementById( 'blackscreen' );
+					if ( blackscreen ) {
+						blackscreen.parentElement.removeChild( blackscreen );
+					}
+				}
+			} else if ( type === CREATED_GROUPS && !this.state.showGroupManager ) {
+				this.toggleGroupManager();
+			}
+		});
+		this.renderTextInterval = setInterval( this.renderRecordedText, 1000 );
+	}
+
+	componentWillUnmount() {
+		this.unsubscribe();
+		if ( this.renderTextInterval ) {
+			clearInterval( this.renderTextInterval );
+		}
+	}
+
+	closeSignup = () => {
+		this.setState({
+			visibleSignup: false
+		});
+	}
+
+	closeLogin = () => {
+		this.setState({
+			visibleLogin: false
+		});
+	}
+
+	closeLogout = () => {
+		this.setState({
+			visibleLogout: false
+		});
+	}
+
+	toggleBarVisibility = ( event ) => {
+		if ( event ) {
+			event.stopPropagation();
+		}
+		const session = this.context;
+		session.togglePresentationView();
+		debug( 'Toggle visibility of statusbar...' );
+		if ( !session.config.hideStatusBar ) {
+			this.setState({
+				showStatusBar: !this.state.showStatusBar
+			});
+		}
+	}
+
+	toggleGroupManager = ( event ) => {
+		if ( event ) {
+			event.stopPropagation();
+		}
+		this.setState({
+			showGroupManager: !this.state.showGroupManager
+		});
+	}
+
+	toggleBlackScreen = ( event ) => {
+		if ( event ) {
+			event.stopPropagation();
+		}
+		let blackscreen = document.getElementById( 'blackscreen' );
+		const session = this.context;
+		if ( !blackscreen ) {
+			blackscreen = document.createElement( 'div' );
+			blackscreen.setAttribute( 'id', 'blackscreen' );
+			const lesson = document.getElementById( 'Lesson' );
+			lesson.appendChild( blackscreen );
+			session.log({
+				id: 'statusbar',
+				type: TOGGLE_BLACKSCREEN,
+				value: true
+			}, 'members' );
+		} else {
+			blackscreen.parentElement.removeChild( blackscreen );
+			session.log({
+				id: 'statusbar',
+				type: TOGGLE_BLACKSCREEN,
+				value: false
+			}, 'members' );
+		}
+	}
+
+	toggleBar = () => {
+		if ( this.hidden ) {
+			animatePosition( this.statusbar, 'top', 0, 300 );
+			this.statusbar.style.opacity = 1.0;
+			this.hidden = false;
+		} else {
+			animatePosition( this.statusbar, 'top', -32, 300 );
+			this.statusbar.style.opacity = 0.95;
+			this.hidden = true;
+		}
+	}
+
+	onMouseOver = () => {
+		if ( this.hidden ) {
+			this.statusbar.style.opacity = 1.0;
+		}
+	}
+
+	onMouseOut = () => {
+		if ( this.hidden ) {
+			this.statusbar.style.opacity = 0.95;
+		}
+	}
+
+	login = ( e ) => {
+		e.stopPropagation();
+		this.setState({
+			visibleLogin: true
+		});
+	}
+
+	signup = ( e ) => {
+		e.stopPropagation();
+		this.setState({
+			visibleSignup: true
+		});
+	}
+
+	logout = ( e ) => {
+		e.stopPropagation();
+		this.setState({
+			visibleLogout: true
+		});
+	}
+
+	handleLogout = () => {
+		const session = this.context;
+		session.logout();
+		this.closeLogout();
+	}
+
+	handleBarClick = ( event ) => {
+		this.toggleBar();
+	}
+
+	handleBarKeyPress = () => {
+		this.toggleBar();
+	}
+
+	toggleProgress = () => {
+		const session = this.context;
+
+		// Initialize progress if not already set:
+		session.setProgress();
+		if ( this.state.isProgressLeaving || session.isOwner() ) {
+			return;
+		}
+		if ( !this.state.showProgressBar ) {
+			this.setState({
+				showProgressBar: true,
+				duration: getDuration( this.context )
+			});
+		} else {
+			this.progressTimeout = setTimeout(() => {
+				this.setState({
+					showProgressBar: false,
+					isProgressLeaving: false,
+					duration: getDuration( this.context )
+				});
+				this.progressTimeout = null;
+			}, 12000 );
+		}
+	}
+
+	jumpToUnfinished = ( event ) => {
+		debug( 'Jump to next unfinished question...' );
+		event.stopPropagation();
+		const session = this.context;
+		const unfinished = session.unfinished;
+		if ( isArray( unfinished ) ) {
+			const first = unfinished[ 0 ];
+			if ( first ) {
+				const elem = document.getElementById( first );
+				if ( isHidden( elem ) ) {
+					const clone = elem.cloneNode( true );
+					const newDiv = document.createElement( 'div' );
+					newDiv.id = 'unfinished-container';
+					clone.id = 'unfinished-elem';
+					clone.classList.add( 'focus-glow' );
+					newDiv.append( clone );
+					document.body.appendChild( newDiv );
+					setTimeout( () => {
+						const element = document.getElementById( 'unfinished-container' );
+						if ( element ) {
+							element.parentNode.removeChild( element );
+						}
+					}, 4000 );
+				} else {
+					elem.classList.add( 'focus-glow' );
+					elem.scrollIntoView();
+					setTimeout( () => {
+						elem.classList.remove( 'focus-glow' );
+					}, 4000 );
+				}
+			}
+		}
+	}
+
+	render() {
+		const session = this.context;
+		const finishedLesson = this.state.progress === 100;
+		const preventPropForUsers = ( evt ) => {
+			if ( !this.context.anonymous ) {
+				evt.stopPropagation();
+			}
+		};
+		const isOwner = session.isOwner() || isElectron;
+		/* eslint-disable jsx-a11y/click-events-have-key-events, jsx-a11y/no-noninteractive-element-interactions, jsx-a11y/no-static-element-interactions */
+		const picture = session.user.picture;
+		const duration = <Fragment>
+			{picture ? <img className="statusbar-profile" alt="User Profile Pic" src={picture} onClick={preventPropagation} /> : null}
+			<Tooltip placement="bottom" tooltip="Time spent in current session">
+				<div className="progress-time" onClick={preventPropagation}>
+					DUR: {this.state.duration} MIN
+				</div>
+			</Tooltip>
+		</Fragment>;
+		const roomName = session.namespaceName + '-' + session.lessonName;
+		/* eslint-enable jsx-a11y/click-events-have-key-events, jsx-a11y/no-noninteractive-element-interactions, jsx-a11y/no-static-element-interactions */
+		return (
+			<Fragment>
+				<Chats />
+				<VideoChats />
+				{isOwner ? <GroupManager active={this.state.showGroupManager} onHide={this.toggleGroupManager} /> : <GroupClient />}
+				<div>
+					<div
+						className="statusbar unselectable"
+						role="button" tabIndex={0}
+						ref={( statusbar ) => { this.statusbar = statusbar; }}
+						onClick={this.handleBarClick} onKeyPress={this.handleBarKeyPress}
+						onMouseOver={this.onMouseOver} onFocus={this.onMouseOver}
+						onMouseOut={this.onMouseOut} onBlur={this.onMouseOut}
+						style={{
+							top: '-32px',
+							display: !this.state.showStatusBar ? 'none' : null
+						}}
+					>
+						<div className="statusbar-left"></div>
+						<div className="statusbar-middle">
+							<VoiceControl session={this.context} />
+							{isOwner ?
+								<Tooltip placement="bottom" tooltip="Enter presentation mode (F7)" >
+									<span role="button" tabIndex={0}
+										onClick={this.toggleBarVisibility} onKeyPress={this.toggleBarVisibility}
+										className="statusbar-presentation-mode statusbar-icon"
+										aria-label="Toggle presentation mode"
+									>
+										<span className="fa fa-xs fa-eye-slash" />
+									</span>
+								</Tooltip> : null }
+							{isOwner ?
+								<Tooltip placement="bottom" tooltip="Black out screen for everyone" >
+									<span role="button" tabIndex={0} aria-label="Black out screen"
+										onClick={this.toggleBlackScreen} onKeyPress={this.toggleBlackScreen}
+										className="statusbar-blackscreen-mode statusbar-icon"
+									>
+										<span className="fa fa-xs fa-stop" />
+									</span>
+								</Tooltip> : null }
+							{isOwner ?
+								<Tooltip placement="bottom" tooltip="Group Manager" >
+									<span role="button" tabIndex={0} aria-label="Group Manager"
+										onClick={this.toggleGroupManager} onKeyPress={this.toggleGroupManager}
+										className="statusbar-group-manager statusbar-icon"
+									>
+										<span className="fa fa-xs fa-user-friends" />
+									</span>
+								</Tooltip> : null }
+							{ session.cohort ? <div className="statusbar-cohort" >{session.cohort}</div> : null }
+							<Tooltip placement="bottom" tooltip={session.live ? 'The connection to the ISLE server is active' : 'The connection to the ISLE server is broken'} >
+								<div className="statusbar-presence" style={{
+									backgroundColor: session.live ? SERVER_ACTIVE_COLOR : NO_RESPONSE_FROM_SERVER_COLOR
+								}}>
+									<div className="statusbar-inner-presence"></div>
+								</div>
+							</Tooltip>
+							<div
+								role="button" tabIndex={0}
+								style={{ cursor: this.context.anonymous ? 'pointer' : 'help' }}
+								className="statusbar-username"
+								onClick={preventPropForUsers} onKeyPress={preventPropForUsers}
+								onMouseEnter={this.toggleProgress} onFocus={this.toggleProgress}
+								onMouseOut={this.toggleProgress} onBlur={this.toggleProgress}
+								aria-label="User Progress"
+							>
+								{ session.anonymous ? `Anonymous ${session.anonymousIdentifier}` : session.user.name }
+							</div>
+							{ !session.config.hideVideoChat ? <VideoChatButton
+								for={roomName}
+								buttonVariant="link"
+								buttonLabel={<i className="fa fa-xs fa-video"></i>}
+								tooltipPlacement="bottom"
+								tooltip="Video Chat"
+								className="statusbar-icon statusbar-video-chat"
+								onClick={preventPropagation}
+							/> : null }
+							{ !session.config.hideTextChat ? <ChatButton
+								for={roomName}
+								buttonVariant="link"
+								anonymousSubmissions
+								buttonLabel={<i className="fas fa-comments"></i>}
+								tooltipPlacement="bottom"
+								tooltip="Text Chat"
+								className="statusbar-icon statusbar-text-chat"
+								onClick={preventPropagation}
+							/> : null }
+							{ session.anonymous ?
+								<div>
+									<Button
+										size="sm"
+										className="statusbar-button"
+										variant="outline-secondary"
+										style={{ float: 'right', marginRight: '-20px' }}
+										onClick={this.signup}
+										disabled={!session.live}
+									>Sign up</Button>
+									<Button
+										size="sm"
+										className="statusbar-button"
+										variant="outline-secondary"
+										style={{ float: 'right', marginRight: '10px' }}
+										onClick={this.login}
+										disabled={!session.live}
+									>Login</Button>
+								</div> :
+								<Fragment>
+									<Button size="sm" className="statusbar-button" variant="outline-secondary" style={{ float: 'right', marginRight: '10px' }} onClick={this.logout}>Log Out</Button>
+									<a href={session.server} target="_blank"><Button size="sm" className="statusbar-button" variant="outline-secondary" style={{ float: 'right', marginRight: '10px' }}>Go to Dashboard</Button></a>
+								</Fragment> }
+							<div className="statusbar-text">
+								ISLE
+							</div>
+						</div>
+						<div className="statusbar-right"></div>
+						<div
+							className={`statusbar-progress ${this.state.isProgressLeaving ? 'progress-fade-out' : ''} `}
+							style={{
+								display: !session.config.hideProgressBar && this.state.showProgressBar ? 'inherit' : 'none'
+							}}
+						>
+							<Gate user>
+								{duration}
+								{ !isEmptyObject( session.responseVisualizers ) ?
+									<Tooltip
+										placement="bottom"
+										tooltip={!finishedLesson ? 'Click to preview unfinished' : 'Completed!'}
+										show={isArray( session.unfinished )}
+									>
+										<div className="outer-statusbar-progress-bar">
+											<ProgressBar
+												label={`COMPLETION RATE: ${this.state.progress}%`}
+												variant="success"
+												now={this.state.progress} style={{
+													animation: 'anim-fade-in 0.7s',
+													border: 'solid 1px darkgrey'
+												}}
+												onClick={this.jumpToUnfinished}
+											/>
+										</div>
+									</Tooltip> :
+									<div className="outer-statusbar-progress-bar"></div>
+								}
+								<Score />
+							</Gate>
+						</div>
+					</div>
+					<Suspense fallback={null} >
+						<Gate owner>
+							<InstructorView />
+						</Gate>
+					</Suspense>
+					{!this.state.showStatusBar && ( isOwner || isElectron ) ?
+						<Tooltip placement="bottom" tooltip="Exit presentation mode (F7)" >
+							<span className="statusbar-presentation-mode-lone-icon" role="button" tabIndex={0}
+								onClick={this.toggleBarVisibility} onKeyPress={this.toggleBarVisibility}
+							>
+								<span className="fa fa-xs fa-eye-slash" />
+							</span>
+						</Tooltip> :
+						null
+					}
+				</div>
+				<Login show={this.state.visibleLogin} onClose={this.closeLogin} />
+				<Signup show={this.state.visibleSignup} onClose={this.closeSignup} />
+				<ConfirmModal
+					show={this.state.visibleLogout}
+					close={this.closeLogout}
+					title="Logout"
+					message="Do you really want to log out? To log in again, you will need your password."
+					onConfirm={this.handleLogout}
+				/>
+				<KeyControls
+					actions={{
+						'F7': this.toggleBarVisibility
+					}}
+				/>
+				{ finishedLesson && !session.config.hideProgressBar ?
+					<Seal
+						title="100%"
+						upper="Congratulations!"
+						lower="Lesson Completed!"
+						upperArc={120}
+						lowerArc={120}
+						noOrnaments
+						removable
+						style={{
+							position: 'fixed',
+							right: '0px',
+							bottom: '5%',
+							transform: `rotate(-17deg) scale(${window.innerWidth / 1920})`,
+							transformOrigin: '100% 100%',
+							animation: 'anim-fade-in 1s forwards',
+							background: 'linear-gradient(to right, white, silver, white)',
+							fontFamily: 'Courier',
+							zIndex: 1002
+						}}
+						innerStyle={{
+							background: 'linear-gradient(to right, ghostwhite, #ffc107, ghostwhite, #ffc107, silver)'
+						}}
+					/> : null }
+			</Fragment>
+		);
+	}
+}
+
+
+// TYPES //
+
+StatusBar.contextType = SessionContext;
+
+
+// EXPORTS //
+
+export default StatusBar;
