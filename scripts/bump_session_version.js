0a8ed5c1eb3fcd668dd33bea658faf0e371d0e00
Planeshifter <pburckhardt@outlook.com>
2020-06-03 02:46:18 -0400

    Update scripts

--- scripts/bump_session_version.js
+++ scripts/bump_session_version.js
@@ -3,7 +3,9 @@
 // MODULES //
 
 const { join } = require( 'path' );
+const contains = require( '@stdlib/assert/contains' );
 const replace = require( '@stdlib/string/replace' );
+const ENV = require( '@stdlib/process/env' );
 const fs = require( 'fs' );
 
 
@@ -11,8 +13,13 @@ const fs = require( 'fs' );
 
 const RE_VERSION = /([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})/g;
 const RE_DLL_FILE = /(\.js|\.js\.LICENSE\.txt)$/;
+const SEMVER = [ 'major', 'minor', 'patch' ];
 let newVersion;
 let oldVersion;
+let type = ENV.VERSION;
+if ( !contains( SEMVER, type ) ) {
+	throw new Error( '`VERSION` environment variable must be set to either `major`, `minor`, or `patch`. Value: '+type );
+}
 
 
 // MAIN //
@@ -23,22 +30,32 @@ const pkg = require( pkgPath );
 
 pkg.version = replace( pkg.version, RE_VERSION, ( match, p1, p2, p3 ) => {
 	oldVersion = replace( match, RE_VERSION, '$1.$2' );
-	newVersion = `${p1}.${Number( p2 ) + 1}`;
-	return newVersion;
+	if ( type === 'major' ) {
+		newVersion = `${Number( p1 ) + 1}.${p2}`;
+		return `${newVersion}.0`;
+	}
+	if ( type === 'minor' ) {
+		newVersion = `${p1}.${Number( p2 ) + 1}`;
+		return `${newVersion}.0`;
+	}
+	return `${p1}.${p2}.${Number( p3 ) + 1}`;
 });
 fs.writeFileSync( pkgPath, JSON.stringify( pkg, null, '\t' ) );
 
-const configPath = join( __dirname, '..', 'webpack.config.session.js' );
-const config = fs.readFileSync( configPath, 'utf8' );
+// Only update `index.html` and `webpack` configuration when major or minor version is increased:
+if ( type === 'major' || type === 'minor' ) {
+	const configPath = join( __dirname, '..', 'webpack.config.session.js' );
+	const config = fs.readFileSync( configPath, 'utf8' );
 
-let out = replace( config, oldVersion, newVersion );
-fs.writeFileSync( configPath, out );
+	let out = replace( config, oldVersion, newVersion );
+	fs.writeFileSync( configPath, out );
 
-const htmlPath = join( __dirname, '..', 'app', 'bundler', 'index.html' );
-const html = fs.readFileSync( htmlPath, 'utf8' );
+	const htmlPath = join( __dirname, '..', 'app', 'bundler', 'index.html' );
+	const html = fs.readFileSync( htmlPath, 'utf8' );
 
-out = replace( html, oldVersion, newVersion );
-fs.writeFileSync( htmlPath, out );
+	out = replace( html, oldVersion, newVersion );
+	fs.writeFileSync( htmlPath, out );
+}
 
 // Delete current DLL files:
 fs.readdir( join( __dirname, '..', 'dll', 'session' ), ( error, files ) => {
