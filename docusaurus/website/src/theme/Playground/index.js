7421637f921e2f55887592455302ef0725fcdfb2
Planeshifter <pburckhardt@outlook.com>
2020-07-03 02:15:53 -0400

    Update docusaurus playground and formatting

---
+++ docusaurus/website/src/theme/Playground/index.js
@@ -0,0 +1,53 @@
+/**
+* Copyright (c) Facebook, Inc. and its affiliates.
+*
+* This source code is licensed under the MIT license found in the
+* LICENSE file in the root directory of this source tree.
+*/
+
+// MODULES //
+
+import * as React from 'react';
+import { LiveProvider, LiveEditor, LiveError, LivePreview } from 'react-live';
+import clsx from 'clsx';
+import styles from './styles.module.css';
+
+
+// MAIN //
+
+function Playground({ children, theme, transformCode, ...props }) {
+	return (
+		<LiveProvider
+			code={children.replace(/\n$/, '')}
+			theme={theme}
+			transformCode={( code ) => {
+				return '<Provider session={session}><Lesson>'+code+'</Lesson></Provider>;';
+			}}
+			{...props}>
+			<div
+				className={clsx(
+					styles.playgroundHeader,
+					styles.playgroundEditorHeader,
+				)}>
+				Live Editor
+			</div>
+			<LiveEditor className={styles.playgroundEditor} />
+			<div
+				className={clsx(
+					styles.playgroundHeader,
+					styles.playgroundPreviewHeader,
+				)}>
+				Result
+			</div>
+			<div className={styles.playgroundPreview}>
+				<LivePreview />
+				<LiveError />
+			</div>
+		</LiveProvider>
+	);
+}
+
+
+// EXPORTS //
+
+export default Playground;
