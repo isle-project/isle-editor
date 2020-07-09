9f3bd2c7f8b4c17e259e3e7893a4b18d3f0edf9c
Planeshifter <pburckhardt@outlook.com>
2020-07-09 09:46:02 -0400

    Fix semicolon

--- app/editor-components/header/index.js
+++ app/editor-components/header/index.js
@@ -9,8 +9,8 @@ import DropdownButton from 'react-bootstrap/DropdownButton';
 import DropdownItem from 'react-bootstrap/DropdownItem';
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 import HeaderUpperBar from 'editor-components/header-upper-bar';
-import CheckboxInput from 'components/input/checkbox'
-import KeyControls from 'components/key-controls';;
+import CheckboxInput from 'components/input/checkbox';
+import KeyControls from 'components/key-controls';
 import './header.css';
 
 
