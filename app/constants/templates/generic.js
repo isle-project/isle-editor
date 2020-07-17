d2f0f6be5728b7a396fa2027968533a31c4f4285
Planeshifter <pburckhardt@outlook.com>
2020-07-16 22:14:30 -0400

    Add empty lines to template

--- app/constants/templates/generic.js
+++ app/constants/templates/generic.js
@@ -7,16 +7,19 @@ const template = `---
 # This is an ISLE lesson.
 
 ## RShell
+
 Here is an interactive R Shell to play with
 
 <RShell code="mean( c(10, 5, 8, 2, 13) )" lines={5} />
 
 ## LaTeX
+
 You can include LaTeX equations:
 
 <TeX raw="\\int f(x) dx" displayMode={true} id={1}/>
 
 ## Widgets
+
 You can include a variety of *ISLE components*, for example plotting widgets and feedback buttons:
 
 ### Question & Feedback
@@ -24,6 +27,7 @@ You can include a variety of *ISLE components*, for example plotting widgets and
 <FreeTextQuestion id="q1" question="Where can I find help on ISLE?" solution="Browse the docs at https://isledocs.com or ask for help on the discussion forum at https://discourse.isledocs.com" />
 
 ### Videos
+
 You can easily embed media content:
 
 <VideoPlayer url="https://www.youtube.com/watch?v=wPqtzj5VZus" />`;
