f01595265dcb92e7507d7526d8c44d6fce0fdd27
Planeshifter <pburckhardt@outlook.com>
2020-07-17 21:34:36 -0400

    Avoid transform

--- app/utils/transform-to-presentation/index.js
+++ app/utils/transform-to-presentation/index.js
@@ -9,18 +9,6 @@ import objectKeys from '@stdlib/utils/keys';
 // VARIABLES //
 
 const debug = logger( 'isle:transform-to-presentation' );
-const RE_HEADING = /<h([0-5])([^>]*)>(.*?)<\/h[0-5]>/g;
-const RE_PARAGRAPH = /<p([^>]*)>([\s\S]*?)<\/p>/g;
-const RE_TABLE = /<table([^>]*)>([\s\S]*?)<\/table>/g;
-const RE_OPEN_LIST = /<ul([^>]*)>/g;
-const RE_CLOSE_LIST = /<\/ul>/g;
-const RE_OPEN_LIST_ITEM = /<li([^>]*)>/g;
-const RE_CLOSE_LIST_ITEM = /<\/li>/g;
-const RE_THEAD = /<thead([^>]*)>([\s\S]*?)<\/thead>/g;
-const RE_TBODY = /<tbody([^>]*)>([\s\S]*?)<\/tbody>/g;
-const RE_TABLE_ROW = /<tr([^>]*)>([\s\S]*?)<\/tr>/g;
-const RE_TABLE_HEADER_ITEM = /<th([^>]*)>([\s\S]*?)<\/th>/g;
-const RE_TABLE_ITEM = /<td([^>]*)>([\s\S]*?)<\/td>/g;
 
 
 // MAIN
@@ -54,18 +42,6 @@ function transformToPresentation( code, preamble ) {
 		pres += arr.join( '</Slide><Slide>' );
 		pres += '</Slide>';
 	}
-	pres = pres.replace( RE_HEADING, '<Heading size={$1}$2>$3</Heading>' );
-	pres = pres.replace( RE_PARAGRAPH, '<SText$1>$2</SText>' );
-	pres = pres.replace( RE_OPEN_LIST, '<List$1>' );
-	pres = pres.replace( RE_OPEN_LIST_ITEM, '<ListItem$1>' );
-	pres = pres.replace( RE_CLOSE_LIST, '</List>' );
-	pres = pres.replace( RE_CLOSE_LIST_ITEM, '</ListItem>' );
-	pres = pres.replace( RE_TABLE, '<Table$1>$2</Table>' );
-	pres = pres.replace( RE_THEAD, '<TableHeader$1>$2</TableHeader>' );
-	pres = pres.replace( RE_TBODY, '<TableBody$1>$2</TableBody>' );
-	pres = pres.replace( RE_TABLE_ROW, '<TableRow$1>$2</TableRow>' );
-	pres = pres.replace( RE_TABLE_HEADER_ITEM, '<TableHeaderItem$1>$2</TableHeaderItem>' );
-	pres = pres.replace( RE_TABLE_ITEM, '<TableItem$1>$2</TableItem>' );
 
 	// Add opening <Deck> tag in front of first slide:
 	pres = pres.replace( '<Slide', `<Deck
