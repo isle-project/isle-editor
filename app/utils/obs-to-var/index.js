37c67b44ea83a375edc55d414cd73f22e06f0db2
Planeshifter <pburckhardt@outlook.com>
2020-08-12 20:47:13 -0400

    Trim whitespace from column names

--- app/utils/obs-to-var/index.js
+++ app/utils/obs-to-var/index.js
@@ -1,3 +1,8 @@
+// MODULES //
+
+import trim from '@stdlib/string/trim';
+
+
 // VARIABLES //
 
 const RE_NUMBER = /^[+-]?[\d.]+e?[+-]?\d*$/;
@@ -5,21 +10,33 @@ const RE_NUMBER = /^[+-]?[\d.]+e?[+-]?\d*$/;
 
 // MAIN //
 
-function obsToVar( output ) {
+/**
+* Returns a data object representation from an object array.
+*
+* ## Notes
+*
+* -   The function trims any whitespace from the variable names.
+*
+* @param {Array} arr - object array with each element corresponding to one observation
+* @returns {Object} object with each key corresponding to one variable containing all its values
+*/
+function obsToVar( arr ) {
 	const data = {};
-	const columnNames = Object.keys( output[ 0 ] ).filter( x => x !== '' );
-	for ( let j = 0; j < columnNames.length; j++ ) {
-		const col = columnNames[ j ];
-		data[ col ] = new Array( output.length );
+	const keymap = {};
+	const columnNames = Object.keys( arr[ 0 ] ).filter( x => x !== '' );
+	for ( let i = 0; i < columnNames.length; i++ ) {
+		const col = trim( columnNames[ i ] );
+		keymap[ columnNames[ i ] ] = col;
+		data[ col ] = new Array( arr.length );
 	}
-	for ( let i = 0; i < output.length; i++ ) {
+	for ( let i = 0; i < arr.length; i++ ) {
 		for ( let j = 0; j < columnNames.length; j++ ) {
 			const col = columnNames[ j ];
-			let val = output[ i ][ col ];
+			let val = arr[ i ][ col ];
 			if ( RE_NUMBER.test( val ) ) {
 				val = Number( val );
 			}
-			data[ col ][ i ] = val;
+			data[ keymap[ col ] ][ i ] = val;
 		}
 	}
 	return data;
