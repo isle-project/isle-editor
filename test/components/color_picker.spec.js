53b0f1df354df22cbcd459765474c6a827cccafe
Planeshifter <pburckhardt@outlook.com>
2020-08-30 01:21:25 -0400

    Update tests

--- test/components/color_picker.spec.js
+++ test/components/color_picker.spec.js
@@ -4,7 +4,6 @@ import React from 'react';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import ColorPicker from 'components/color-picker/main.js';
-import { SketchPicker } from 'react-color';
 
 
 // VARIABLES //
@@ -15,16 +14,16 @@ Enzyme.configure({ adapter: new Adapter() });
 // TESTS //
 
 describe( '<ColorPicker />', function test() {
-	it( 'renders a <SketchPicker /> from react-color', () => {
+	it( 'renders a container div wrapping a color picker from react-color', () => {
 		const wrapper = shallow( <ColorPicker /> );
-		const picker = wrapper.find( SketchPicker );
-		expect( picker.length ).toBe( 1 );
+		const outer = wrapper.find( 'div' );
+		expect( outer.length ).toBe( 1 );
 	});
 
-	it( 'passes down props to <SketchPicker />', () => {
+	it( 'passes down props to color picker', () => {
 		const wrapper = shallow( <ColorPicker color="#fff" /> );
-		const picker = wrapper.find( SketchPicker );
-		const props = picker.props();
+		const outer = wrapper.find( 'div' );
+		const props = outer.childAt( 0 ).props();
 		expect( props.color ).toBe( '#fff' );
 	});
 });
