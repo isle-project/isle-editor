ee69b8b444166162924aaa4904ef50ed24d9fae2
Planeshifter <pburckhardt@outlook.com>
2020-08-30 23:05:28 -0400

    Add until and after properties to gate

--- app/components/gate/main.js
+++ app/components/gate/main.js
@@ -19,6 +19,8 @@ import RoleContext from 'session/role_context.js';
 * @property {boolean} notEnrolled - when set the gated content is **not** displayed to the students enrolled in the course
 * @property {boolean} owner - when set the gated content is displayed to the owner of the course (usually the instructor)
 * @property {boolean} notOwner - when set the gated content is **not** displayed to the owner of the course (usually the instructor)
+* @property {Date} until - time until the contents of the gate should remain visible
+* @property {Date} after - time after which the contents of the gate should become visible
 * @property {Node} banner - a message which is visible to the visitors lacking the gate privilege
 * @property {boolean} disabled - if a gate is disabled, the banner will be displayed no matter what
 * @property {Function} check - callback function returning a `boolean` indicating whether gate should display child components; the function is invoked whenever session actions arrive
@@ -97,6 +99,20 @@ class Gate extends Component {
 		}
 	}
 
+	isTimeActive() {
+		if ( !this.props.after && !this.props.until ) {
+			return true;
+		}
+		const session = this.context;
+		if ( this.props.after && this.props.until ) {
+			return this.props.after < session.startTime && session.startTime <= this.props.until;
+		} else if ( this.props.until ) {
+			return session.startTime <= this.props.until;
+		}
+		// Case: `after` property assigned
+		return this.props.after < session.startTime;
+	}
+
 	render() {
 		const session = this.context;
 		return ( <RoleContext.Consumer>
@@ -112,6 +128,9 @@ class Gate extends Component {
 					notOwner && isOwner ||
 					notEnrolled && isEnrolled
 				) {
+					return this.props.banner;
+				}
+				if ( !this.isTimeActive() ) {
 					return this.props.banner;
 				}
 				if ( user && isUser ) {
@@ -145,6 +164,8 @@ Gate.defaultProps = {
 	notEnrolled: false,
 	owner: false,
 	notOwner: false,
+	after: null,
+	until: null,
 	banner: null,
 	disabled: false,
 	check: null
@@ -157,6 +178,8 @@ Gate.propTypes = {
 	notEnrolled: PropTypes.bool,
 	owner: PropTypes.bool,
 	notOwner: PropTypes.bool,
+	after: PropTypes.instanceOf( Date ),
+	until: PropTypes.instanceOf( Date ),
 	banner: PropTypes.node,
 	disabled: PropTypes.bool,
 	check: PropTypes.func
