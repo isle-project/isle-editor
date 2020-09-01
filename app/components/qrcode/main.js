fe088d99e52615ed3f2a5ab902f5518f51c70cf8
Planeshifter <pburckhardt@outlook.com>
2020-09-01 02:27:00 -0400

    Add props and handle hash changes

--- app/components/qrcode/main.js
+++ app/components/qrcode/main.js
@@ -1,9 +1,10 @@
 // MODULES //
 
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import logger from 'debug';
 import PropTypes from 'prop-types';
 import QRCode from 'qrcode';
+import isElectron from 'utils/is-electron';
 
 
 // VARIABLES //
@@ -19,6 +20,7 @@ const debug = logger( 'isle:qrcode' );
 * @property {string} text - text to encode in QR code (defaults to lesson URL if not set)
 * @property {number} scale - number of pixels per modules (black dots)
 * @property {boolean} center - boolean controlling whether to center the QR code
+* @property {boolean} showText - boolean determining whether to show the text encoded in the QR code
 * @property {Object} style - CSS inline styles
 */
 class Qrcode extends Component {
@@ -28,6 +30,9 @@ class Qrcode extends Component {
 
 	componentDidMount() {
 		this.renderCode();
+		if ( !isElectron ) {
+			window.addEventListener( 'hashchange', this.handleHashChange );
+		}
 	}
 
 	componentDidUpdate( prevProps ) {
@@ -39,15 +44,28 @@ class Qrcode extends Component {
 		}
 	}
 
+	componentWillUnmount() {
+		if ( !isElectron ) {
+			window.removeEventListener( 'hashchange', this.handleHashChange );
+		}
+	}
+
+	handleHashChange = () => {
+		if ( !this.props.text ) {
+			this.renderCode();
+		}
+	}
+
 	renderCode() {
 		const text = this.props.text || window.location.href;
+		debug( `Display '${text}' as QR code...` );
 		QRCode.toCanvas( this.canvas, text, {
 			scale: this.props.scale
 		}, debug );
 	}
 
 	render() {
-		return ( <canvas
+		const canvas = <canvas
 			className={`qrcode-canvas ${this.props.center ? 'center' : ''}`}
 			width={this.props.width}
 			ref={( canvas ) => {
@@ -56,7 +74,15 @@ class Qrcode extends Component {
 				}
 			}}
 			style={this.props.style}
-		/> );
+		/>;
+		if ( this.props.showText ) {
+			const text = this.props.text || window.location.href;
+			return ( <Fragment>
+				{canvas}
+				<span className="title">{text}</span>
+			</Fragment> );
+		}
+		return canvas;
 	}
 }
 
@@ -66,6 +92,7 @@ class Qrcode extends Component {
 Qrcode.defaultProps = {
 	text: null,
 	scale: 8,
+	showText: false,
 	center: false,
 	style: {}
 };
@@ -73,6 +100,7 @@ Qrcode.defaultProps = {
 Qrcode.propTypes = {
 	text: PropTypes.string,
 	scale: PropTypes.number,
+	showText: PropTypes.bool,
 	center: PropTypes.bool,
 	style: PropTypes.object
 };
