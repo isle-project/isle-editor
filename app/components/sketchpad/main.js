0788ade59d582d51688d47796dcc94157a757924
Planeshifter <pburckhardt@outlook.com>
2020-09-29 01:05:49 -0400

    Remove unneeded escape characters

--- app/components/sketchpad/main.js
+++ app/components/sketchpad/main.js
@@ -84,7 +84,7 @@ const OMITTED_KEYS = [
 	'isExporting', 'showColorPicker', 'showUploadModal', 'showNavigationModal', 'showResetModal', 'showFeedbackModal', 'showSaveModal',
 	'hideInputButtons', 'hideNavigationButtons', 'hideSaveButtons', 'hideTransmitButtons'
 ];
-const RE_URL = /(https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&//=]*))/g;
+const RE_URL = /(https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_+.~#?&//=]*))/g;
 const RE_EMAIL = /([a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,})/gi;
 const MIN_SWIPE_X = 45;
 const MAX_SWIPE_Y = 40;
