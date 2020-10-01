f1953bc3cd43534016eb792cda77325b07d9fc60
Planeshifter <pburckhardt@outlook.com>
2020-10-01 12:20:27 -0400

    Check whether to increment revision

--- app/reducers/linting.js
+++ app/reducers/linting.js
@@ -17,7 +17,14 @@ export default function linting( state = initialState, action ) {
 	switch ( action.type ) {
 	case types.LINT_ERRORS: {
 		const lintErrors = action.payload.errors;
-		lintErrors.revision = ( state.lintErrors.revision || 0 ) + 1;
+		let increment = 0;
+		if (
+			lintErrors.length !== state.lintErrors.length ||
+			!lintErrors.every( ( v, i ) => v.startLineNumber === state.lintErrors[ i ].startLineNumber )
+		) {
+			increment += 1;
+		}
+		lintErrors.revision = ( state.lintErrors.revision || 0 ) + increment;
 		return {
 			...state,
 			lintErrors
@@ -25,7 +32,14 @@ export default function linting( state = initialState, action ) {
 	}
 	case types.SPELLING_ERRORS: {
 		const spellingErrors = action.payload.errors;
-		spellingErrors.revision = ( state.spellingErrors.revision || 0 ) + 1;
+		let increment = 0;
+		if (
+			spellingErrors.length !== state.spellingErrors.length ||
+			!spellingErrors.every( ( v, i ) => v.startLineNumber === state.spellingErrors[ i ].startLineNumber )
+		) {
+			increment += 1;
+		}
+		spellingErrors.revision = ( state.spellingErrors.revision || 0 ) + increment;
 		return {
 			...state,
 			spellingErrors
