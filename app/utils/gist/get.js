62f65b9327e9b619346ac701228baeca94f58230
Planeshifter <pburckhardt@outlook.com>
2020-10-04 04:10:03 -0400

    Get specified gist

---
+++ app/utils/gist/get.js
@@ -0,0 +1,30 @@
+// MODULES //
+
+import axios from 'axios';
+import electronStore from 'store/electron.js';
+
+
+// MAIN //
+
+/**
+* Get a specified Gist.
+*
+* @param {string} id - gist identifier
+* @returns {Object} GET request result
+*/
+async function getGist( id ) {
+	const accessToken = electronStore.get( 'githubAccessToken' );
+	const res = await axios.get( `https://api.github.com/gists/${id}`,
+	{
+		headers: {
+			'Accept': 'application/vnd.github.v3+json',
+			'Authorization': `token ${accessToken}`
+		}
+	});
+	return res;
+}
+
+
+// EXPORTS //
+
+export default getGist;
