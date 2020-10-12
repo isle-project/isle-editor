0165091c61c30403c58b1e7f25e067df0bc23fc5
Planeshifter <pburckhardt@outlook.com>
2020-10-12 17:30:56 -0400

    Add placeholder

---
+++ app/components/models/multiple-linear-regression/index.js
@@ -0,0 +1,18 @@
+// MODULES //
+
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+
+// MAIN //
+
+class MultipleLinearRegression extends Component {
+	render() {
+		return null;
+	}
+}
+
+
+// EXPORTS //
+
+export default MultipleLinearRegression;
