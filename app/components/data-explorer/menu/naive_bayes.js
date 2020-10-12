7a5b6177dbba33b242372f6c4b093ba7831cef61
Planeshifter <pburckhardt@outlook.com>
2020-10-12 16:20:13 -0400

    Use separate component and move to menu sub-folder

---
+++ app/components/data-explorer/menu/naive_bayes.js
@@ -0,0 +1,125 @@
+// MODULES //
+
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import uniq from 'uniq';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import contains from '@stdlib/assert/contains';
+import copy from '@stdlib/utils/copy';
+import isArray from '@stdlib/assert/is-array';
+import SelectInput from 'components/input/select';
+import CheckboxInput from 'components/input/checkbox';
+import { DATA_EXPLORER_NAIVE_BAYES } from 'constants/actions.js';
+import NaiveBayes, { designMatrix } from 'components/models/naive-bayes';
+import QuestionButton from './../question_button.js';
+
+
+// VARIABLES //
+
+const DESCRIPTION = '';
+
+
+// MAIN //
+
+class NaiveBayesMenu extends Component {
+	constructor( props ) {
+		super( props );
+
+		this.state = {
+			y: props.categorical[ 0 ],
+			x: props.quantitative[ 0 ],
+			omitMissing: false
+		};
+	}
+
+	compute = () => {
+		let { y, x, omitMissing } = this.state;
+		if ( !isArray( x ) ) {
+			x = [ x ];
+		}
+		const output = <NaiveBayes
+			onPredict={( results, counter ) => {
+				const newData = copy( this.props.data, 1 );
+				const newQuantitative = this.props.quantitative.slice();
+				const { matrix } = designMatrix( x, this.props.data, this.props.quantitative );
+				const probs = results.predictProbs( matrix );
+				for ( let i = 0; i < results.classes.length; i++ ) {
+					const name = 'probs_' + results.classes[ i ] + '_bayes' + counter;
+					const classProbs = probs.map( x => x[ i ] );
+					newData[ name ] = classProbs;
+					if ( !contains( newQuantitative, name ) ) {
+						newQuantitative.push( name );
+					}
+				}
+				const pred = results.predict( matrix );
+				const name = 'pred_bayes'+ counter;
+				newData[ name ] = pred;
+				const newCategorical = this.props.categorical.slice();
+				if ( !contains( newCategorical, name ) ) {
+					newCategorical.push( name );
+				}
+				this.props.onGenerate( newQuantitative, newCategorical, newData );
+			}}
+		/>;
+		this.props.logAction( DATA_EXPLORER_NAIVE_BAYES, {
+			y, x, omitMissing
+		});
+		this.props.onCreated( output );
+	}
+
+	render() {
+		const { categorical, quantitative } = this.props;
+		const { x, y } = this.state;
+		return (
+			<Card
+				style={{ fontSize: '14px' }}
+			>
+				<Card.Header as="h4">
+					Naive Bayes<QuestionButton title="Naive Bayes" content={DESCRIPTION} />
+				</Card.Header>
+				<Card.Body>
+					<SelectInput
+						legend="Outcome (Y):"
+						options={categorical}
+						defaultValue={y}
+						onChange={( y ) => this.setState({ y })}
+					/>
+					<SelectInput
+						legend="Predictors (X):" multi
+						options={uniq( quantitative.concat( categorical ) )}
+						defaultValue={x || ''}
+						onChange={( x ) => this.setState({ x })}
+					/>
+					<CheckboxInput
+						legend="Omit missing values"
+						defaultValue={false}
+						onChange={( omitMissing ) => this.setState({ omitMissing })}
+					/>
+					<Button disabled={!x || x.length === 0} variant="primary" block onClick={this.compute}>Calculate</Button>
+				</Card.Body>
+			</Card>
+		);
+	}
+}
+
+
+// PROPERTIES //
+
+NaiveBayesMenu.defaultProps = {
+	logAction() {}
+};
+
+NaiveBayesMenu.propTypes = {
+	categorical: PropTypes.array.isRequired,
+	quantitative: PropTypes.array.isRequired,
+	data: PropTypes.object.isRequired,
+	logAction: PropTypes.func,
+	onGenerate: PropTypes.func.isRequired,
+	onCreated: PropTypes.func.isRequired
+};
+
+
+// EXPORTS //
+
+export default NaiveBayesMenu;
