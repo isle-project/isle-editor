f554ffde85263ecd7b2b21f61d297d061e2b5119
Planeshifter <pburckhardt@outlook.com>
2020-10-15 19:47:55 -0400

    Duplicate element instead of showing alert

--- app/editor-components/editor/scroll_into_view.js
+++ app/editor-components/editor/scroll_into_view.js
@@ -10,17 +10,33 @@ function scrollIntoView( lineNumber, startColumn ) {
 	const elem = document.getElementById( 'line-'+lineNumber+'-'+startColumn );
 	if ( elem ) {
 		if ( isHidden( elem ) ) {
-			vex.dialog.alert( 'The component is not displayed.' );
+			const clone = elem.cloneNode( true );
+			clone.addEventListener( 'click', ( event ) => {
+				// Returns original element with the given ID since it was first inserted into the DOM:
+				const button = document.getElementById( event.target.id );
+				button.click();
+			});
+			const newDiv = document.createElement( 'div' );
+			newDiv.id = 'revealed-hidden-container';
+			clone.id = 'revealed-hidden-elem';
+			clone.classList.add( 'focus-glow' );
+			newDiv.append( clone );
+			const lesson = document.getElementById( 'Lesson' );
+			lesson.appendChild( newDiv );
+			setTimeout( () => {
+				newDiv.remove();
+			}, 2000 );
+		} else {
+			elem.scrollIntoView({
+				behavior: 'smooth',
+				block: 'nearest',
+				inline: 'end'
+			});
+			elem.classList.add( 'focus-glow' );
+			setTimeout( () => {
+				elem.classList.remove( 'focus-glow' );
+			}, 2000 );
 		}
-		elem.scrollIntoView({
-			behavior: 'smooth',
-			block: 'nearest',
-			inline: 'end'
-		});
-		elem.classList.add( 'focus-glow' );
-		setTimeout( () => {
-			elem.classList.remove( 'focus-glow' );
-		}, 2000 );
 	}
 }
 
