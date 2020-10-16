e5d1304b6d1bb54bd3553645ef13c042c4de58a4
Planeshifter <pburckhardt@outlook.com>
2020-10-16 03:08:06 -0400

    Use loadable and handle prop changes

--- app/components/models/decision-tree/index.js
+++ app/components/models/decision-tree/index.js
@@ -1,111 +1,11 @@
 // MODULES //
 
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import Button from 'react-bootstrap/Button';
-import isArray from '@stdlib/assert/is-array';
-import Tooltip from 'components/tooltip';
-import { RegressionTree, ClassificationTree, TreePlot } from './tree.js';
-
-
-// VARIABLES //
-
-let COUNTER = 0;
+import Loadable from 'components/internal/loadable';
 
 
 // MAIN //
 
-/**
-* Regression and classification trees.
-*
-* @property {Object} data - object of value arrays
-* @property {string} type - `Classification` for categorical or `Regression` for quantitative outcomes
-* @property {Array<string>} quantitative - array of variables in `data` that are `quantitative`
-* @property {string} y - outcome variable
-* @property {Array<string>} x - one or more predictor variables
-* @property {string} impurityMeasure - impurity measure (`gini` or `entropy`)
-* @property {number} scoreThreshold - score threshold for split
-* @property {number} maxTreeDepth - maximum tree depth
-* @property {number} minItemsCount - minimum # of observations in leaf nodes
-* @property {Function} onResult - callback invoked with model object
-*/
-class DecisionTree extends Component {
-	constructor( props ) {
-		super( props );
-
-		COUNTER += 1;
-		let predictors;
-		if ( isArray( props.x ) ) {
-			predictors = props.x;
-		} else {
-			predictors = [ props.x ];
-		}
-		let tree;
-		if ( props.type === 'Classification' ) {
-			tree = new ClassificationTree({
-				response: props.y,
-				predictors,
-				data: props.data,
-				quantitative: props.quantitative,
-				criterion: props.impurityMeasure,
-				scoreThreshold: props.scoreThreshold,
-				maxTreeDepth: props.maxTreeDepth,
-				minItemsCount: props.minItemsCount
-			});
-		} else {
-			tree = new RegressionTree({
-				response: props.y,
-				predictors,
-				data: props.data,
-				quantitative: props.quantitative,
-				scoreThreshold: props.scoreThreshold,
-				maxTreeDepth: props.maxTreeDepth,
-				minItemsCount: props.minItemsCount
-			});
-		}
-		this.tree = tree;
-	}
-
-	handlePredict = () => {
-		this.props.onResult( this.tree, COUNTER );
-	}
-
-	render() {
-		return (
-			<div style={{ overflowX: 'auto', width: '100%' }}>
-				<span className="title" >Decision Tree for {this.props.y} (model id: tree{COUNTER})</span>
-				<TreePlot tree={this.tree.root} />
-				{this.props.onResult ? <Tooltip tooltip="Predictions will be attached to data table">
-					<Button variant="secondary" size="sm" style={{ marginTop: 10 }} onClick={this.handlePredict}>Use this model to predict for currently selected data</Button>
-				</Tooltip> : null}
-			</div>
-		);
-	}
-}
-
-
-// PROPERTIES //
-
-DecisionTree.defaultProps = {
-	impurityMeasure: 'gini',
-	scoreThreshold: 0.0075,
-	maxTreeDepth: 5,
-	minItemsCount: 50,
-	onResult: null
-};
-
-DecisionTree.propTypes = {
-	data: PropTypes.object.isRequired,
-	y: PropTypes.string.isRequired,
-	x: PropTypes.arrayOf( PropTypes.string ).isRequired,
-	type: PropTypes.oneOf([ 'Classification', 'Regression' ]).isRequired,
-	quantitative: PropTypes.arrayOf( PropTypes.string ).isRequired,
-	impurityMeasure: PropTypes.oneOf([ 'gini', 'entropy' ]),
-	scoreThreshold: PropTypes.number,
-	maxTreeDepth: PropTypes.number,
-	minItemsCount: PropTypes.number,
-	onResult: PropTypes.func
-};
+const DecisionTree = Loadable( () => import( /* webpackChunkName: "DecisionTree" */ './main.js' ) );
 
 
 // EXPORTS //
---
+++ app/components/models/decision-tree/main.js
@@ -0,0 +1,155 @@
+// MODULES //
+
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withTranslation } from 'react-i18next';
+import Alert from 'react-bootstrap/Alert';
+import Button from 'react-bootstrap/Button';
+import isArray from '@stdlib/assert/is-array';
+import Tooltip from 'components/tooltip';
+import { RegressionTree, ClassificationTree, TreePlot } from './tree.js';
+import './load_translations.js';
+
+
+// VARIABLES //
+
+let COUNTER = 0;
+
+
+// FUNCTIONS //
+
+const fitModel = ({ x, y, data, type, quantitative, impurityMeasure, scoreThreshold, maxTreeDepth, minItemsCount }) => {
+	let predictors;
+	if ( isArray( x ) ) {
+		predictors = x;
+	} else {
+		predictors = [ x ];
+	}
+	try {
+		let tree;
+		if ( type === 'Classification' ) {
+			tree = new ClassificationTree({
+				response: y,
+				predictors,
+				data: data,
+				quantitative: quantitative,
+				criterion: impurityMeasure,
+				scoreThreshold: scoreThreshold,
+				maxTreeDepth: maxTreeDepth,
+				minItemsCount: minItemsCount
+			});
+		} else {
+			tree = new RegressionTree({
+				response: y,
+				predictors,
+				data: data,
+				quantitative: quantitative,
+				scoreThreshold: scoreThreshold,
+				maxTreeDepth: maxTreeDepth,
+				minItemsCount: minItemsCount
+			});
+		}
+		return tree;
+	} catch ( _ ) {
+		return null;
+	}
+};
+
+
+// MAIN //
+
+/**
+* Regression and classification trees.
+*
+* @property {Object} data - object of value arrays
+* @property {string} type - `Classification` for categorical or `Regression` for quantitative outcomes
+* @property {Array<string>} quantitative - array of variables in `data` that are `quantitative`
+* @property {string} y - outcome variable
+* @property {Array<string>} x - one or more predictor variables
+* @property {string} impurityMeasure - impurity measure (`gini` or `entropy`)
+* @property {number} scoreThreshold - score threshold for split
+* @property {number} maxTreeDepth - maximum tree depth
+* @property {number} minItemsCount - minimum # of observations in leaf nodes
+* @property {Function} onPredict - callback invoked with model object when clicking on the predict button
+*/
+class DecisionTree extends Component {
+	constructor( props ) {
+		super( props );
+
+		COUNTER += 1;
+
+		const { x, y, data, type, quantitative, impurityMeasure, scoreThreshold, maxTreeDepth, minItemsCount } = this.props;
+		this.state = {
+			tree: fitModel({ x, y, data, type, quantitative, impurityMeasure, scoreThreshold, maxTreeDepth, minItemsCount })
+		};
+	}
+
+	static getDerivedStateFromProps( nextProps, prevState ) {
+		if (
+			nextProps.y !== prevState.y ||
+			nextProps.x !== prevState.x ||
+			nextProps.type !== prevState.type ||
+			nextProps.impurityMeasure !== prevState.impurityMeasure ||
+			nextProps.data !== prevState.data ||
+			nextProps.quantitative !== prevState.quantitative ||
+			nextProps.scoreThreshold !== prevState.scoreThreshold ||
+			nextProps.maxTreeDepth !== prevState.maxTreeDepth ||
+			nextProps.minItemsCount !== prevState.minItemsCount
+		) {
+			const { x, y, data, type, quantitative, impurityMeasure, scoreThreshold, maxTreeDepth, minItemsCount } = nextProps;
+			return {
+				tree: fitModel({ x, y, data, type, quantitative, impurityMeasure, scoreThreshold, maxTreeDepth, minItemsCount })
+			};
+		}
+		return null;
+	}
+
+	handlePredict = () => {
+		this.props.onPredict( this.state.tree, COUNTER );
+	}
+
+	render() {
+		const { tree } = this.state;
+		if ( !tree ) {
+			return <Alert variant="danger">{this.props.t('missing-attributes')}</Alert>;
+		}
+		return (
+			<div style={{ overflowX: 'auto', width: '100%' }}>
+				<span className="title" >Decision Tree for {this.props.y} (model id: tree{COUNTER})</span>
+				<TreePlot tree={tree.root} />
+				{this.props.onPredict ? <Tooltip tooltip="Predictions will be attached to data table">
+					<Button variant="secondary" size="sm" style={{ marginTop: 10 }} onClick={this.handlePredict}>Use this model to predict for currently selected data</Button>
+				</Tooltip> : null}
+			</div>
+		);
+	}
+}
+
+
+// PROPERTIES //
+
+DecisionTree.defaultProps = {
+	impurityMeasure: 'gini',
+	scoreThreshold: 0.0075,
+	maxTreeDepth: 5,
+	minItemsCount: 50,
+	onPredict: null
+};
+
+DecisionTree.propTypes = {
+	data: PropTypes.object.isRequired,
+	y: PropTypes.string.isRequired,
+	x: PropTypes.arrayOf( PropTypes.string ).isRequired,
+	type: PropTypes.oneOf([ 'Classification', 'Regression' ]).isRequired,
+	quantitative: PropTypes.arrayOf( PropTypes.string ).isRequired,
+	impurityMeasure: PropTypes.oneOf([ 'gini', 'entropy' ]),
+	scoreThreshold: PropTypes.number,
+	maxTreeDepth: PropTypes.number,
+	minItemsCount: PropTypes.number,
+	onPredict: PropTypes.func
+};
+
+
+// EXPORTS //
+
+export default withTranslation( 'decision-tree' )( DecisionTree );
