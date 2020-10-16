01d61d36820838815a88a6147f9894e15a8734ff
Planeshifter <pburckhardt@outlook.com>
2020-10-16 01:46:05 -0400

    Use loadable and handle property changes

---
+++ app/components/models/naive-bayes/load_translations.js
@@ -0,0 +1,12 @@
+// MODULES //
+
+import i18next from 'i18next';
+import TRANSLATION from './translation.json';
+
+
+// MAIN //
+
+i18next.addResources( 'de', 'naive-bayes', TRANSLATION.DE );
+i18next.addResources( 'en', 'naive-bayes', TRANSLATION.EN );
+i18next.addResources( 'es', 'naive-bayes', TRANSLATION.ES );
+
