5bd99d0fc7689a7c0200e427ac75f571d834e171
Planeshifter <pburckhardt@outlook.com>
2020-10-16 01:05:55 -0400

    Add translations and use loadable component

---
+++ app/components/models/logistic-regression/load_translations.js
@@ -0,0 +1,12 @@
+// MODULES //
+
+import i18next from 'i18next';
+import TRANSLATION from './translation.json';
+
+
+// MAIN //
+
+i18next.addResources( 'de', 'logistic-regression', TRANSLATION.DE );
+i18next.addResources( 'en', 'logistic-regression', TRANSLATION.EN );
+i18next.addResources( 'es', 'logistic-regression', TRANSLATION.ES );
+
