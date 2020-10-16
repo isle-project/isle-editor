6cf85eb9e039126a9a9c1cd941a6d68751b829c6
Planeshifter <pburckhardt@outlook.com>
2020-10-16 10:24:02 -0400

    Update dll versions

--- webpack.config.session.js
+++ webpack.config.session.js
@@ -49,7 +49,7 @@ const config = {
 		path: join( __dirname, 'dll', 'session' ),
 		filename: 'dll.[name].js',
 		library: '[name]_dll',
-		publicPath: 'https://cdn.jsdelivr.net/npm/@isle-project/session@0.32.1/'
+		publicPath: 'https://cdn.jsdelivr.net/npm/@isle-project/session@0.32.2/'
 	},
 	optimization: {
 		minimize: true,
