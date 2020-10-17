a62fbb21203ad692430bc41a694cf5eddcba86b7
Planeshifter <pburckhardt@outlook.com>
2020-10-17 18:24:23 -0400

    Add alert modal

---
+++ app/components/internal/alert-modal/load_translations.js
@@ -0,0 +1,12 @@
+// MODULES //
+
+import i18next from 'i18next';
+import TRANSLATION from './translation.json';
+
+
+// MAIN //
+
+i18next.addResources( 'de', 'alert-modal', TRANSLATION.DE );
+i18next.addResources( 'en', 'alert-modal', TRANSLATION.EN );
+i18next.addResources( 'es', 'alert-modal', TRANSLATION.ES );
+
