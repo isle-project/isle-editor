9058cb74aec841755a7a210b447763099d1bfb09
Planeshifter <pburckhardt@outlook.com>
2020-10-18 14:52:30 -0400

    Cast value to string

--- app/utils/escape-latex/index.js
+++ app/utils/escape-latex/index.js
@@ -10,7 +10,14 @@ const RE_SPECIAL_CHARS = /([$#&%_{}])/;
 
 // MAIN //
 
+/**
+* Escapes special characters in a string to be included in a LaTeX equation.
+*
+* @param {any} str - input value (non-string values will be cast to a string)
+* @param {string} escaped string
+*/
 function escapeLaTeX( str ) {
+	str = String( str );
 	str = replace( str, RE_SPECIAL_CHARS, '\\$1' );
 	str = replace( str, '~', '\\texttt{\\~{}}' );
 	str = replace( str, '^', '\\^{}' );
