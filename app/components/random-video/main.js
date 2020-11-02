97c936b8b55b9e86b8a6759ebd289e0636438fc7
Planeshifter <pburckhardt@outlook.com>
2020-11-01 23:57:25 -0500

    Refactor as hook and display alert if invalid

--- app/components/random-video/main.js
+++ app/components/random-video/main.js
@@ -1,7 +1,10 @@
 // MODULES //
 
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+import Alert from 'react-bootstrap/Alert';
+import isArray from '@stdlib/assert/is-array';
+import isNull from '@stdlib/assert/is-null';
 import runif from '@stdlib/random/base/discrete-uniform';
 import VideoPlayer from 'components/video-player';
 
@@ -13,22 +16,24 @@ import VideoPlayer from 'components/video-player';
 *
 * @property {Array<string>} videos - video URLs
 */
-class RandomVideo extends Component {
-	constructor( props ) {
-		super( props );
-		this.choice = runif( 0, this.props.videos.length-1 );
+const RandomVideo = ( props ) => {
+	const [ choice, setChoice ] = useState( null );
+	useEffect( () => {
+		setChoice( runif( 0, props.videos.length-1 ) );
+	}, [ props.videos ]);
+	if ( !isArray( props.videos ) || props.videos.length === 0 ) {
+		return <Alert variant="danger" >Please supply at least two URLs to the <code>videos</code> attribute.</Alert>;
 	}
-
-	render() {
-		const elem = this.props.videos[ this.choice ];
-		return (
-			<VideoPlayer
-				url={elem}
-				{...this.props}
-			/>
-		);
+	if ( isNull( choice ) ) {
+		return null;
 	}
-}
+	return (
+		<VideoPlayer
+			url={props.videos[ choice ]}
+			{...props}
+		/>
+	);
+};
 
 
 // PROPERTIES //
