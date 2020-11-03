2de5220c636711f833d82f8326ffe828501e9878
Planeshifter <pburckhardt@outlook.com>
2020-11-03 12:39:19 -0500

    Rewrite using hooks

--- app/components/bibliography/main.js
+++ app/components/bibliography/main.js
@@ -1,6 +1,6 @@
 // MODULES //
 
-import React, { Fragment, Component } from 'react';
+import React, { Fragment, useContext } from 'react';
 import PropTypes from 'prop-types';
 import { withTranslation } from 'react-i18next';
 import endsWith from '@stdlib/string/ends-with';
@@ -26,64 +26,62 @@ function generateAuthorString( author ) {
 *
 * @prop {string} title - heading to be displayed in front of references
 */
-class Bibliography extends Component {
-	render() {
-		const session = this.context;
-		const references = session.config.references;
+const Bibliography = ({ title, t }) => {
+	const session = useContext( SessionContext );
+	const references = session.config.references;
 
-		let keys = objectKeys( references );
-		keys = keys.sort( ( a, b ) => {
-			const itemA = references[ a ];
-			const itemB = references[ b ];
-			if ( !itemA || !itemA.author ) {
-				return 1;
-			}
-			if ( !itemB || !itemB.author ) {
-				return -1;
-			}
-			return itemA.author[ 0 ] > itemB.author[ 0 ] ? 1 : -1;
-		});
-		const out = [];
-		for ( let i = 0; i < keys.length; i++ ) {
-			const item = references[ keys[ i ] ];
-			if ( !item ) {
-				continue;
-			}
-			let li;
-			if ( item.type === 'article' ) {
-				const author = generateAuthorString( item.author );
-				const title = item.title ? `${item.title}. ` : '';
-				const journal = item.journal ? <i>{`${item.journal}, `}</i> : '';
-				const year = item.year ? `${item.year}.` : '';
-				const number = item.number ? `(${item.number})` : '';
-				const pages = item.pages ? `${item.volume || number ? ':' : ''}${item.pages}, ` : '';
-				const month = item.month ? ` ${item.month} ` : '';
-				let val = <span>{author} {title}{journal}{item.volume}{number}{pages}{month}{year}</span>;
-				li = <li key={`ref-${i}`}>{val}</li>;
-			}
-			else if ( item.type === 'book' ) {
-				const author = generateAuthorString( item.author );
-				const title = item.title ? <i>{item.title}. </i> : '';
-				const publisher = item.publisher ? `${item.publisher}, ` : '';
-				const address = item.address ? `${item.address}, ` : '';
-				const edition = item.edition ? `${item.edition} ${this.props.t('edition')}, ` : '';
-				const month = item.month ? `${item.month} ` : '';
-				const year = item.year ? `${item.year}.` : '';
-				let val = <span>{author} {title}{publisher}{address}{edition}{month}{year}</span>;
-				li = <li key={`ref-${i}`}>{val}</li>;
-			}
-			out.push( li );
+	let keys = objectKeys( references );
+	keys = keys.sort( ( a, b ) => {
+		const itemA = references[ a ];
+		const itemB = references[ b ];
+		if ( !itemA || !itemA.author ) {
+			return 1;
 		}
-		return (
-			<Fragment>
-				{this.props.title ? <h2>{this.props.title}</h2> : <h2>{this.props.t('references')}</h2>}
-				<ol>
-					{out}
-				</ol>
-			</Fragment>
-		);
+		if ( !itemB || !itemB.author ) {
+			return -1;
+		}
+		return itemA.author[ 0 ] > itemB.author[ 0 ] ? 1 : -1;
+	});
+	const out = [];
+	for ( let i = 0; i < keys.length; i++ ) {
+		const item = references[ keys[ i ] ];
+		if ( !item ) {
+			continue;
+		}
+		let li;
+		if ( item.type === 'article' ) {
+			const author = generateAuthorString( item.author );
+			const title = item.title ? `${item.title}. ` : '';
+			const journal = item.journal ? <i>{`${item.journal}, `}</i> : '';
+			const year = item.year ? `${item.year}.` : '';
+			const number = item.number ? `(${item.number})` : '';
+			const pages = item.pages ? `${item.volume || number ? ':' : ''}${item.pages}, ` : '';
+			const month = item.month ? ` ${item.month} ` : '';
+			let val = <span>{author} {title}{journal}{item.volume}{number}{pages}{month}{year}</span>;
+			li = <li key={`ref-${i}`}>{val}</li>;
+		}
+		else if ( item.type === 'book' ) {
+			const author = generateAuthorString( item.author );
+			const title = item.title ? <i>{item.title}. </i> : '';
+			const publisher = item.publisher ? `${item.publisher}, ` : '';
+			const address = item.address ? `${item.address}, ` : '';
+			const edition = item.edition ? `${item.edition} ${t('edition')}, ` : '';
+			const month = item.month ? `${item.month} ` : '';
+			const year = item.year ? `${item.year}.` : '';
+			let val = <span>{author} {title}{publisher}{address}{edition}{month}{year}</span>;
+			li = <li key={`ref-${i}`}>{val}</li>;
+		}
+		out.push( li );
 	}
-}
+	return (
+		<Fragment>
+			{title ? <h2>{title}</h2> : <h2>{t('references')}</h2>}
+			<ol>
+				{out}
+			</ol>
+		</Fragment>
+	);
+};
 
 
 // PROPERTIES //
@@ -96,7 +94,6 @@ Bibliography.defaultProps = {
 	title: null
 };
 
-Bibliography.contextType = SessionContext;
 
 
 // EXPORTS //
