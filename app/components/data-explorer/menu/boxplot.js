59be21a6c5faff28359b913167236f4cc943c54d
Planeshifter <pburckhardt@outlook.com>
2020-11-03 02:03:21 -0500

    Convert to hook

--- app/components/data-explorer/menu/boxplot.js
+++ app/components/data-explorer/menu/boxplot.js
@@ -1,6 +1,6 @@
 // MODULES //
 
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Select from 'react-select';
 import Card from 'react-bootstrap/Card';
@@ -23,100 +23,83 @@ const DESCRIPTION = 'A box plot (full name: box and whisker plot, coined by famo
 
 // MAIN //
 
-class BoxplotMenu extends Component {
-	constructor( props ) {
-		super( props );
+const BoxplotMenu = ({ data, variables, defaultValue, groupingVariables, t, session, logAction, onCreated }) => {
+	const [ variable, setVariable ] = useState( defaultValue || variables[ 0 ] );
+	const [ group, setGroup ] = useState([]);
+	const [ orientation, setOrientation ] = useState( 'vertical' );
+	const [ overlayPoints, setOverlayPoints ] = useState( false );
 
-		this.state = {
-			variable: props.defaultValue || props.variables[ 0 ],
-			group: [],
-			orientation: 'vertical',
-			overlayPoints: false
-		};
-	}
-
-	generateBoxplot = () => {
-		let group = ( this.state.group || [] ).map( e => e.value );
-		let { variable } = this.state;
+	const generateBoxplot = () => {
 		const plotId = randomstring( 6 );
 		const action = {
 			variable,
-			group,
+			group: ( group || [] ).map( e => e.value ),
 			plotId
 		};
 		const onShare = () => {
-			this.props.session.addNotification({
-				title: this.props.t('plot-shared'),
-				message: this.props.t('plot-shared-message'),
+			session.addNotification({
+				title: t('plot-shared'),
+				message: t('plot-shared-message'),
 				level: 'success',
 				position: 'tr'
 			});
-			this.props.logAction( DATA_EXPLORER_SHARE_BOXPLOT, action );
+			logAction( DATA_EXPLORER_SHARE_BOXPLOT, action );
 		};
-		const output = <BoxPlot id={plotId} onShare={onShare} action={action} {...this.props} {...this.state} />;
-		this.props.logAction( DATA_EXPLORER_BOXPLOT, action );
-		this.props.onCreated( output );
-	}
-
-	render() {
-		const { variables, groupingVariables, t } = this.props;
-		return (
-			<Card>
-				<Card.Header as="h4">
-					{t('Box Plot')}
-					<QuestionButton title={t('Box Plot')} content={DESCRIPTION} />
-				</Card.Header>
-				<Card.Body>
-					<SelectInput
-						legend={t('variable')}
-						defaultValue={this.state.variable}
-						options={variables}
-						onChange={( variable ) => {
-							this.setState({ variable });
+		const output = <BoxPlot id={plotId} onShare={onShare} action={action}
+			data={data} variable={variable} group={group} orientation={orientation}
+			overlayPoints={overlayPoints} variables={variables}
+		/>;
+		logAction( DATA_EXPLORER_BOXPLOT, action );
+		onCreated( output );
+	};
+	return (
+		<Card>
+			<Card.Header as="h4">
+				{t('Box Plot')}
+				<QuestionButton title={t('Box Plot')} content={DESCRIPTION} />
+			</Card.Header>
+			<Card.Body>
+				<SelectInput
+					legend={t('variable')}
+					defaultValue={variable}
+					options={variables}
+					onChange={setVariable}
+				/>
+				<FormGroup controlId="boxplot-form-select">
+					<FormLabel>{t('group-by')}:</FormLabel>
+					<Select
+						value={group}
+						options={groupingVariables.map( e => ( { 'label': e, 'value': e } ))}
+						isClearable
+						isMulti
+						onChange={( value ) => {
+							if ( !value || value.length <= 2 ) {
+								setGroup( value );
+							}
 						}}
+						styles={selectStyles}
+						menuPortalTarget={document.body}
 					/>
-					<FormGroup controlId="boxplot-form-select">
-						<FormLabel>{t('group-by')}:</FormLabel>
-						<Select
-							value={this.state.group}
-							options={groupingVariables.map( e => ( { 'label': e, 'value': e } ))}
-							isClearable
-							isMulti
-							onChange={( value ) => {
-								if ( !value || value.length <= 2 ) {
-									this.setState({
-										group: value
-									});
-								}
-							}}
-							styles={selectStyles}
-							menuPortalTarget={document.body}
-						/>
-					</FormGroup>
-					<SelectInput
-						legend={t('orientation')}
-						options={[ 'vertical', 'horizontal' ]}
-						defaultValue="vertical"
-						menuPlacement="top"
-						onChange={( orientation ) => {
-							this.setState({ orientation });
-						}}
-					/>
-					<CheckboxInput
-						legend={t('overlay-points')}
-						defaultValue={false}
-						onChange={( overlayPoints ) => {
-							this.setState({ overlayPoints });
-						}}
-					/>
-					<Button variant="primary" block onClick={this.generateBoxplot}>
-						{t('generate')}
-					</Button>
-				</Card.Body>
-			</Card>
-		);
-	}
-}
+				</FormGroup>
+				<SelectInput
+					legend={t('orientation')}
+					options={[ 'vertical', 'horizontal' ]}
+					defaultValue="vertical"
+					menuPlacement="top"
+					onChange={setOrientation}
+				/>
+				<CheckboxInput
+					legend={t('overlay-points')}
+					defaultValue={false}
+					onChange={setOverlayPoints}
+				/>
+				<Button variant="primary" block onClick={generateBoxplot}>
+					{t('generate')}
+				</Button>
+			</Card.Body>
+		</Card>
+	);
+};
 
 
 // PROPERTIES //
