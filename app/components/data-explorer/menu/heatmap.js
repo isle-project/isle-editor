25095cc15a24239b3b8dd4531cc7f23757a17662
Planeshifter <pburckhardt@outlook.com>
2020-11-04 23:27:12 -0500

    Refactor using hooks and add translation

--- app/components/data-explorer/menu/heatmap.js
+++ app/components/data-explorer/menu/heatmap.js
@@ -1,6 +1,6 @@
 // MODULES //
 
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
@@ -17,197 +17,151 @@ import { DATA_EXPLORER_SHARE_HEATMAP, DATA_EXPLORER_HEATMAP } from 'constants/ac
 import QuestionButton from './../question_button.js';
 
 
-// VARIABLES //
-
-const DESCRIPTION = 'A data display for quantitative variables in which data values are represented as colors. Regions with many observations are colored red, whereas regions without observations are colored in dark blue.';
-
-
 // MAIN //
 
-class HeatMapMenu extends Component {
-	constructor( props ) {
-		super( props );
+const HeatMapMenu = ( props ) => {
+	const { variables, groupingVariables, defaultX, defaultY, t } = props;
 
-		const { variables, defaultX, defaultY } = props;
-		this.state = {
-			x: defaultX || variables[ 0 ],
-			y: defaultY || variables[ 1 ],
-			group: null,
-			overlayPoints: false,
-			commonXAxis: false,
-			commonYAxis: false,
-			alternateColor: false,
-			regressionMethod: [],
-			smoothSpan: 0.66
-		};
-	}
+	const [ x, setX ] = useState( defaultX || variables[ 0 ] );
+	const [ y, setY ] = useState( defaultY || variables[ 1 ] );
+	const [ group, setGroup ] = useState( null );
+	const [ overlayPoints, setOverlayPoints ] = useState( false );
+	const [ commonXAxis, setCommonXAxis ] = useState( false );
+	const [ commonYAxis, setCommonYAxis ] = useState( false );
+	const [ alternateColor, setAlternateColor ] = useState( false );
+	const [ regressionMethod, setRegressionMethod ] = useState( [] );
+	const [ smoothSpan, setSmoothSpan ] = useState( 0.66 );
 
-	generateHeatmap() {
-		const { x, y, overlayPoints, regressionMethod } = this.state;
+	const generateHeatmap = () => {
 		const plotId = randomstring( 6 );
 		const action = {
 			x, y, overlayPoints, regressionMethod, plotId
 		};
 		const onShare = () => {
-			this.props.session.addNotification({
-				title: this.props.t('plot-shared'),
-				message: this.props.t('plot-shared-message'),
+			props.session.addNotification({
+				title: t('plot-shared'),
+				message: t('plot-shared-message'),
 				level: 'success',
 				position: 'tr'
 			});
-			this.props.logAction( DATA_EXPLORER_SHARE_HEATMAP, action );
+			props.logAction( DATA_EXPLORER_SHARE_HEATMAP, action );
 		};
-		const output = <HeatMap id={plotId} data={this.props.data} {...this.state} action={action} onShare={onShare} />;
-		this.props.logAction( DATA_EXPLORER_HEATMAP, action );
-		this.props.onCreated( output );
-	}
-
-	render() {
-		const { variables, groupingVariables, t } = this.props;
-		return (
-			<Card>
-				<Card.Header as="h4">
-					{t('Heat Map')}
-					<QuestionButton title={t('Heat Map')} content={DESCRIPTION} />
-				</Card.Header>
-				<Card.Body>
-					<Row>
-						<Col><SelectInput
-							legend={t('x-axis-variable')}
-							defaultValue={this.state.x}
-							options={variables}
-							onChange={( value )=>{
-								this.setState({
-									x: value
-								});
+		const output = <HeatMap id={plotId} data={props.data} x={x} y={y} group={group}
+			overlayPoints={overlayPoints} commonXAxis={commonXAxis} commonYAxis={commonYAxis}
+			alternateColor={alternateColor} regressionMethod={regressionMethod}
+			smoothSpan={smoothSpan} action={action} onShare={onShare} onSelected={props.onSelected} />;
+		props.logAction( DATA_EXPLORER_HEATMAP, action );
+		props.onCreated( output );
+	};
+	return (
+		<Card>
+			<Card.Header as="h4">
+				{t('Heat Map')}
+				<QuestionButton title={t('Heat Map')} content={t('Heat Map-description')} />
+			</Card.Header>
+			<Card.Body>
+				<Row>
+					<Col><SelectInput
+						legend={t('x-axis-variable')}
+						defaultValue={x}
+						options={variables}
+						onChange={setX}
+					/>
+					</Col>
+					<Col><SelectInput
+						legend={t('y-axis-variable')}
+						defaultValue={y}
+						options={variables}
+						onChange={setY}
+					/></Col>
+				</Row>
+				<Row>
+					<Col>
+						<SelectInput
+							legend={t('group-by')}
+							options={groupingVariables}
+							clearable={true}
+							onChange={setGroup}
+							menuPlacement="top"
+						/>
+					</Col>
+				</Row>
+				<Row>
+					<Col>
+						<CheckboxInput
+							legend={t('common-x-axis')}
+							defaultValue={commonXAxis}
+							onChange={setCommonXAxis}
+							disabled={!group}
+							style={{
+								opacity: group ? 1.0 : 0.0,
+								float: 'left'
 							}}
 						/>
-						</Col>
-						<Col><SelectInput
-							legend={t('y-axis-variable')}
-							defaultValue={this.state.y}
-							options={variables}
-							onChange={( value )=>{
-								this.setState({
-									y: value
-								});
+					</Col>
+					<Col>
+						<CheckboxInput
+							legend={t('common-y-axis')}
+							defaultValue={commonYAxis}
+							onChange={setCommonYAxis}
+							disabled={!group}
+							style={{
+								opacity: group ? 1.0 : 0.0,
+								float: 'left'
 							}}
-						/></Col>
-					</Row>
-					<Row>
-						<Col>
-							<SelectInput
-								legend={t('group-by')}
-								options={groupingVariables}
-								clearable={true}
-								onChange={( value )=>{
-									this.setState({
-										group: value
-									});
-								}}
-								menuPlacement="top"
-							/>
-						</Col>
-					</Row>
-					<Row>
-						<Col>
-							<CheckboxInput
-								legend={t('common-x-axis')}
-								defaultValue={this.state.commonXAxis}
-								onChange={( value )=>{
-									this.setState({
-										commonXAxis: value
-									});
-								}}
-								disabled={!this.state.group}
-								style={{
-									opacity: this.state.group ? 1.0 : 0.0,
-									float: 'left'
-								}}
-							/>
-						</Col>
-						<Col>
-							<CheckboxInput
-								legend={t('common-y-axis')}
-								defaultValue={this.state.commonYAxis}
-								onChange={( value )=>{
-									this.setState({
-										commonYAxis: value
-									});
-								}}
-								disabled={!this.state.group}
-								style={{
-									opacity: this.state.group ? 1.0 : 0.0,
-									float: 'left'
-								}}
-							/>
-						</Col>
-					</Row>
-					<Row>
-						<Col>
-							<CheckboxInput
-								legend={t('overlay-observations')}
-								defaultValue={this.state.overlayPoints}
-								onChange={( value )=>{
-									this.setState({
-										overlayPoints: value
-									});
-								}}
-							/>
-						</Col>
-						<Col>
-							<CheckboxInput
-								legend={t('alternate-color-scheme')}
-								defaultValue={this.state.alternateColor}
-								onChange={( value )=>{
-									this.setState({
-										alternateColor: value
-									});
-								}}
-							/>
-						</Col>
-					</Row>
-					<Row>
-						<Col>
-							<SelectInput
-								legend={t('overlay-regression-line')}
-								defaultValue={this.state.regressionMethod}
-								multi={true}
-								options={[ 'linear', 'smooth' ]}
-								onChange={( value ) => {
-									if ( !isArray( value ) ) {
-										value = [ value ];
-									}
-									this.setState({
-										regressionMethod: value
-									});
-								}}
-							/>
-						</Col>
-						<Col>
-							<SliderInput
-								legend={t('smoothing-parameter')}
-								disabled={!contains(this.state.regressionMethod, 'smooth')}
-								min={0.01}
-								max={1}
-								step={0.01}
-								defaultValue={this.state.smoothSpan}
-								onChange={( value ) => {
-									this.setState({
-										smoothSpan: value
-									});
-								}}
-							/>
-						</Col>
-					</Row>
-					<Button variant="primary" block onClick={this.generateHeatmap.bind( this )}>
-						{t('generate')}
-					</Button>
-				</Card.Body>
-			</Card>
-		);
-	}
-}
+						/>
+					</Col>
+				</Row>
+				<Row>
+					<Col>
+						<CheckboxInput
+							legend={t('overlay-observations')}
+							defaultValue={overlayPoints}
+							onChange={setOverlayPoints}
+						/>
+					</Col>
+					<Col>
+						<CheckboxInput
+							legend={t('alternate-color-scheme')}
+							defaultValue={alternateColor}
+							onChange={setAlternateColor}
+						/>
+					</Col>
+				</Row>
+				<Row>
+					<Col>
+						<SelectInput
+							legend={t('overlay-regression-line')}
+							defaultValue={regressionMethod}
+							multi={true}
+							options={[ 'linear', 'smooth' ]}
+							onChange={( value ) => {
+								if ( !isArray( value ) ) {
+									value = [ value ];
+								}
+								setRegressionMethod( value );
+							}}
+						/>
+					</Col>
+					<Col>
+						<SliderInput
+							legend={t('smoothing-parameter')}
+							disabled={!contains(regressionMethod, 'smooth')}
+							min={0.01}
+							max={1}
+							step={0.01}
+							defaultValue={smoothSpan}
+							onChange={setSmoothSpan}
+						/>
+					</Col>
+				</Row>
+				<Button variant="primary" block onClick={generateHeatmap}>
+					{t('generate')}
+				</Button>
+			</Card.Body>
+		</Card>
+	);
+};
 
 
 // PROPERTIES //
