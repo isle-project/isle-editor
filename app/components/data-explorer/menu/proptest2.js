b8df65bb7f3adca6c951b29e0562d7dd29a04502
Planeshifter <pburckhardt@outlook.com>
2020-11-05 00:25:46 -0500

    Rewrite using hooks and add translation

--- app/components/data-explorer/menu/proptest2.js
+++ app/components/data-explorer/menu/proptest2.js
@@ -1,6 +1,6 @@
 // MODULES //
 
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
@@ -19,46 +19,35 @@ import QuestionButton from './../question_button.js';
 import getBinaryVars from './../get_binary_vars.js';
 
 
-// VARIABLES //
-
-const DESCRIPTION = 'A test for equality of proportions for a selected category of a qualitative variable across two groups.';
-
-
 // MAIN //
 
-class PropTest2Menu extends Component {
-	constructor( props ) {
-		super( props );
+const PropTest2Menu = ( props ) => {
+	const { categorical, data, showDecision, t } = props;
 
-		const var1 = props.categorical[ 0 ];
-		let categories;
-		if ( isArray( props.categorical ) && props.categorical.length > 0 ) {
-			const vals = props.data[ var1 ];
-			if ( vals ) {
-				categories = extractCategoriesFromValues( vals, var1 );
-			} else {
-				categories = [];
-			}
+	const defaultVar1 = categorical[ 0 ];
+	let defaultCategories;
+	if ( isArray( categorical ) && categorical.length > 0 ) {
+		const values = data[ defaultVar1 ];
+		if ( values ) {
+			defaultCategories = extractCategoriesFromValues( values, defaultVar1 );
 		} else {
-			categories = [];
+			defaultCategories = [];
 		}
-		this.state = {
-			categories,
-			var1,
-			success: categories[ 0 ],
-			group: null,
-			var2: null,
-			diff: 0,
-			direction: 'two-sided',
-			alpha: 0.05
-		};
+	} else {
+		defaultCategories = [];
 	}
+	const [ categories, setCategories ] = useState( defaultCategories );
+	const [ var1, setVar1 ] = useState( defaultVar1 );
+	const [ success, setSuccess ] = useState( categories[ 0 ] );
+	const [ group, setGroup ] = useState( null );
+	const [ var2, setVar2 ] = useState( null );
+	const [ diff, setDiff ] = useState( 0 );
+	const [ direction, setDirection ] = useState( 'two-sided' );
+	const [ alpha, setAlpha ] = useState( 0.05 );
 
-	calculateTwoSamplePropTest = () => {
-		const { var1, success, group, var2, diff, direction, alpha } = this.state;
-		const { data, showDecision } = this.props;
+	const calculateTwoSamplePropTest = () => {
 		if ( var1 === group || var1 === var2 ) {
-			return this.props.session.addNotification({
+			return props.session.addNotification({
 				title: 'Action required',
 				message: 'Please make sure that the group variable or second variable is not equal to your variable of interest.',
 				level: 'warning',
@@ -66,7 +55,7 @@ class PropTest2Menu extends Component {
 			});
 		}
 		if ( !group && !var2 ) {
-			this.props.session.addNotification({
+			props.session.addNotification({
 				title: 'Action required',
 				message: `Please select either a grouping variable or a second variable to compare ${var1} against.`,
 				level: 'warning',
@@ -78,132 +67,106 @@ class PropTest2Menu extends Component {
 			var1={var1} var2={var2} group={group}
 			diff={diff} direction={direction} alpha={alpha} success={success}
 		/>;
-		this.props.logAction( DATA_EXPLORER_TESTS_TWO_SAMPLE_PROPTEST, {
+		props.logAction( DATA_EXPLORER_TESTS_TWO_SAMPLE_PROPTEST, {
 			var1, group, var2, success, diff, direction, alpha, showDecision
 		});
-		this.props.onCreated( output );
-	}
-
-	render() {
-		const { categorical, data, t } = this.props;
-		const binary = getBinaryVars( categorical, data );
-		return (
-			<Card
-				style={{ fontSize: '14px' }}
-			>
-				<Card.Header as="h4">
-					{t('Two-Sample Proportion Test')}
-					<QuestionButton title={t('Two-Sample Proportion Test')} content={DESCRIPTION} />
-				</Card.Header>
-				<Card.Body>
-					<Row>
-						<Col md={6}>
-							<SelectInput
-								legend={t('variable')}
-								defaultValue={categorical[ 0 ]}
-								options={categorical}
-								onChange={( variable ) => {
-									const values = this.props.data[ variable ];
-									const categories = extractCategoriesFromValues( values, variable );
-									unique( categories );
-									this.setState({
-										var1: variable,
-										categories,
-										success: categories[ 0 ]
-									});
-								}}
-							/>
-						</Col>
-						<Col md={6}>
-							<SelectInput
-								legend={t('success')}
-								defaultValue={this.state.categories[ 0 ]}
-								options={this.state.categories}
-								onChange={( value ) => {
-									this.setState({
-										success: value
-									});
-								}}
-							/>
-						</Col>
-					</Row>
-					<Row>
-						<Col md={5}>
-							<SelectInput
-								legend={t('groups')}
-								options={binary}
-								clearable
-								onChange={( value ) => {
-									this.setState({
-										group: value,
-										var2: null
-									});
-								}}
-							/>
-						</Col>
-						<Col md={2}><p>OR</p></Col>
-						<Col md={5}>
-							<SelectInput
-								legend={t('second-variable')}
-								options={categorical.filter( elem =>
-									contains( this.state.categories, elem )
-								)}
-								clearable
-								onChange={( value ) => {
-									this.setState({
-										group: null,
-										var2: value
-									});
-								}}
-							/>
-						</Col>
-					</Row>
-					<NumberInput
-						legend={t('difference-h0')}
-						defaultValue={this.state.diff}
-						min={0}
-						max={1}
-						step="any"
-						onChange={( value ) => {
-							this.setState({
-								diff: value
-							});
-						}}
-					/>
-					<SelectInput
-						legend={t('direction')}
-						defaultValue={this.state.direction}
-						options={[ 'less', 'greater', 'two-sided' ]}
-						onChange={( value ) => {
-							this.setState({
-								direction: value
-							});
-						}}
-					/>
-					<NumberInput
-						legend={<span>{t('significance-level')}<TeX raw="\alpha" /></span>}
-						defaultValue={this.state.alpha}
-						min={0.0}
-						max={1.0}
-						step="any"
-						onChange={( value ) => {
-							this.setState({
-								alpha: value
-							});
-						}}
-					/>
-					<Button
-						variant="primary" block
-						onClick={this.calculateTwoSamplePropTest}
-						disabled={!this.state.var2 && !this.state.group}
-					>
-						{t('calculate')}
-					</Button>
-				</Card.Body>
-			</Card>
-		);
-	}
-}
+		props.onCreated( output );
+	};
+	const binary = getBinaryVars( categorical, data );
+	return (
+		<Card
+			style={{ fontSize: '14px' }}
+		>
+			<Card.Header as="h4">
+				{t('Two-Sample Proportion Test')}
+				<QuestionButton title={t('Two-Sample Proportion Test')} content={t('Two-Sample Proportion Test-description')} />
+			</Card.Header>
+			<Card.Body>
+				<Row>
+					<Col md={6}>
+						<SelectInput
+							legend={t('variable')}
+							defaultValue={categorical[ 0 ]}
+							options={categorical}
+							onChange={( variable ) => {
+								const values = data[ variable ];
+								const newCategories = extractCategoriesFromValues( values, variable );
+								unique( newCategories );
+								setVar1( variable );
+								setCategories( newCategories );
+								setSuccess( newCategories[ 0 ] );
+							}}
+						/>
+					</Col>
+					<Col md={6}>
+						<SelectInput
+							legend={t('success')}
+							defaultValue={categories[ 0 ]}
+							options={categories}
+							onChange={setSuccess}
+						/>
+					</Col>
+				</Row>
+				<Row>
+					<Col md={5}>
+						<SelectInput
+							legend={t('groups')}
+							options={binary}
+							clearable
+							onChange={( value ) => {
+								setGroup( value );
+								setVar2( null );
+							}}
+						/>
+					</Col>
+					<Col md={2}><p>OR</p></Col>
+					<Col md={5}>
+						<SelectInput
+							legend={t('second-variable')}
+							options={categorical.filter( elem =>
+								contains( categories, elem )
+							)}
+							clearable
+							onChange={( value ) => {
+								setGroup( null );
+								setVar2( value );
+							}}
+						/>
+					</Col>
+				</Row>
+				<NumberInput
+					legend={t('difference-h0')}
+					defaultValue={diff}
+					min={0}
+					max={1}
+					step="any"
+					onChange={setDiff}
+				/>
+				<SelectInput
+					legend={t('direction')}
+					defaultValue={direction}
+					options={[ 'less', 'greater', 'two-sided' ]}
+					onChange={setDirection}
+				/>
+				<NumberInput
+					legend={<span>{t('significance-level')}<TeX raw="\alpha" /></span>}
+					defaultValue={alpha}
+					min={0.0}
+					max={1.0}
+					step="any"
+					onChange={setAlpha}
+				/>
+				<Button
+					variant="primary" block
+					onClick={calculateTwoSamplePropTest}
+					disabled={!var2 && !group}
+				>
+					{t('calculate')}
+				</Button>
+			</Card.Body>
+		</Card>
+	);
+};
 
 
 // PROPERTIES //
