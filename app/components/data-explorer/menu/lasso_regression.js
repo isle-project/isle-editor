f691d83a371af46f79abf4250f55794f6fae47fd
Planeshifter <pburckhardt@outlook.com>
2020-11-08 01:34:50 -0500

    Rewrite using hooks and add translation

--- app/components/data-explorer/menu/lasso_regression.js
+++ app/components/data-explorer/menu/lasso_regression.js
@@ -15,27 +15,19 @@ import { DATA_EXPLORER_LASSO_REGRESSION } from 'constants/actions.js';
 import QuestionButton from './../question_button.js';
 
 
-// VARIABLES //
-
-const DESCRIPTION = <span>Fit a regression model in which non-zero regression coefficients are penalized using the <TeX raw="\lambda" /> regularization parameter. The LASSO performs automatic model selection as it will shrink the regression coefficients of some variables to zero.</span>;
-
-
 // MAIN //
 
-class LassoRegressionMenu extends Component {
-	constructor( props ) {
-		super( props );
-	}
-
-	compute = ( y, x, lambda, intercept ) => {
+const LassoRegressionMenu = ( props ) => {
+	const { data, categorical, quantitative, t, logAction, onCreated, onGenerate } = props;
+	const compute = ( y, x, lambda, intercept ) => {
 		const output = <LassoRegression
 			x={x} y={y}
 			lambda={lambda} intercept={intercept}
-			data={this.props.data}
-			quantitative={this.props.quantitative}
+			data={data}
+			quantitative={quantitative}
 			onPredict={( fitted, residuals, counter ) => {
-				const newData = copy( this.props.data, 1 );
-				const newQuantitative = this.props.quantitative.slice();
+				const newData = copy( data, 1 );
+				const newQuantitative = quantitative.slice();
 				let name = 'pred_lasso' + counter;
 				newData[ name ] = fitted;
 				if ( !contains( newQuantitative, name ) ) {
@@ -46,54 +38,50 @@ class LassoRegressionMenu extends Component {
 				if ( !contains( newQuantitative, name ) ) {
 					newQuantitative.push( name );
 				}
-				this.props.onGenerate( newQuantitative, newData );
+				onGenerate( newQuantitative, newData );
 			}}
 		/>;
-		this.props.logAction( DATA_EXPLORER_LASSO_REGRESSION, {
+		logAction( DATA_EXPLORER_LASSO_REGRESSION, {
 			y, x, intercept, lambda
 		});
-		this.props.onCreated( output );
-	}
-
-	render() {
-		const { categorical, quantitative, t } = this.props;
-		return (
-			<Dashboard
-				title={<span>
-					{t('LASSO Regression')}
-					<QuestionButton title={t('LASSO Regression')} content={DESCRIPTION} />
-				</span>}
-				autoStart={false}
-				onGenerate={this.compute}
-			>
-				<SelectInput
-					legend={t('outcome-y')}
-					options={quantitative}
-					defaultValue={quantitative[ 0 ]}
-				/>
-				<SelectInput
-					legend={t('predictors-x')} multi
-					options={unique( quantitative.concat( categorical ) )}
-					defaultValue={quantitative[ 1 ]}
-					closeMenuOnSelect={false}
-				/>
-				<SliderInput
-					legend={<span>L1 Penalty Term <TeX raw="\lambda" /></span>}
-					defaultValue={1e-3}
-					min={1e-12}
-					max={1}
-					step="any"
-					rangeInputStyle={{ width: 260 }}
-					numberInputStyle={{ width: 120 }}
-				/>
-				<CheckboxInput
-					legend={t('include-intercept')}
-					defaultValue={true}
-				/>
-			</Dashboard>
-		);
-	}
-}
+		onCreated( output );
+	};
+	return (
+		<Dashboard
+			title={<span>
+				{t('LASSO')}
+				<QuestionButton title={t('LASSO')} content={t('LASSO-description')} />
+			</span>}
+			autoStart={false}
+			onGenerate={compute}
+		>
+			<SelectInput
+				legend={t('outcome-y')}
+				options={quantitative}
+				defaultValue={quantitative[ 0 ]}
+			/>
+			<SelectInput
+				legend={t('predictors-x')} multi
+				options={unique( quantitative.concat( categorical ) )}
+				defaultValue={quantitative[ 1 ]}
+				closeMenuOnSelect={false}
+			/>
+			<SliderInput
+				legend={<span>L1 Penalty Term <TeX raw="\lambda" /></span>}
+				defaultValue={1e-3}
+				min={1e-12}
+				max={1}
+				step="any"
+				rangeInputStyle={{ width: 260 }}
+				numberInputStyle={{ width: 120 }}
+			/>
+			<CheckboxInput
+				legend={t('include-intercept')}
+				defaultValue={true}
+			/>
+		</Dashboard>
+	);
+};
 
 
 // PROPERTIES //
