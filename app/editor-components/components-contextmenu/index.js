b02a2a2d03f3037c02307dac77534c3d89f7dfe0
Planeshifter <pburckhardt@outlook.com>
2020-11-13 02:45:02 -0500

    Refactor configurator contextmenu for searching

--- app/editor-components/components-contextmenu/index.js
+++ app/editor-components/components-contextmenu/index.js
@@ -5,6 +5,9 @@ import PropTypes from 'prop-types';
 import logger from 'debug';
 import { ContextMenu, MenuItem, SubMenu } from 'react-contextmenu';
 import objectKeys from '@stdlib/utils/keys';
+import lowercase from '@stdlib/string/lowercase';
+import replace from '@stdlib/string/replace';
+import contains from '@stdlib/assert/contains';
 import { LANGUAGES } from 'constants/deepl';
 import SearchBar from 'editor-components/searchbar';
 import GROUPED_SNIPPETS from 'snippets/grouped_snippets.json';
@@ -16,6 +19,7 @@ import electronStore from 'store/electron.js';
 const debug = logger( 'isle:editor:context-menu' );
 const LANGUAGE_NAMES = objectKeys( LANGUAGES );
 const ISLE_SERVER_TOKEN = electronStore.get( 'token' );
+const RE_WHITESPACE = require( '@stdlib/regexp/whitespace' );
 
 
 // MAIN //
@@ -23,6 +27,37 @@ const ISLE_SERVER_TOKEN = electronStore.get( 'token' );
 class EditorContextMenu extends Component {
 	constructor( props ) {
 		super( props );
+
+		this.state = {
+			searchValue: ''
+		};
+	}
+
+	createMenuEntries = ( arr, title, ...otherMenuEntries ) => {
+		const out = [];
+		const { searchValue } = this.state;
+		for ( let i = 0; i < arr.length; i++ ) {
+			const obj = arr[ i ];
+			if ( searchValue && !contains( lowercase( obj.name ), searchValue ) ) {
+				continue;
+			}
+			out.push( this.renderMenuItem( obj, out.length ) );
+		}
+		if ( !searchValue ) {
+			return ( <SubMenu title={title} >
+				{out}
+				{otherMenuEntries}
+			</SubMenu> );
+		}
+		if ( out.length > 0 ) {
+			return ( <Fragment>
+				<span style={{ marginLeft: 6, fontSize: 12 }} >{title}</span>
+				<div className="react-contextmenu-item react-contextmenu-item--divider"></div>
+				{out}
+				{otherMenuEntries}
+			</Fragment> );
+		}
+		return otherMenuEntries;
 	}
 
 	renderMenuItem = ( obj, idx ) => {
@@ -47,6 +82,12 @@ class EditorContextMenu extends Component {
 		);
 	}
 
+	handleHide = () => {
+		this.setState({
+			searchValue: ''
+		});
+	}
+
 	handleContextMenuClick = ( _, data ) => {
 		this.props.onContextMenuClick( this.customClick, data );
 		this.customClick = false;
@@ -63,78 +104,56 @@ class EditorContextMenu extends Component {
 	}
 
 	render() {
+		const main = this.createMenuEntries( GROUPED_SNIPPETS.main, 'Main' );
+		const layout = this.createMenuEntries( GROUPED_SNIPPETS.layout, 'Layout' );
+		const displayComponents = this.createMenuEntries( GROUPED_SNIPPETS.displayComponents, 'Display' );
+		const inputs = this.createMenuEntries( GROUPED_SNIPPETS.inputs, 'Inputs' );
+		const questions = this.createMenuEntries( GROUPED_SNIPPETS.questions, 'Questions' );
+		const surveys = this.createMenuEntries( GROUPED_SNIPPETS.surveys, 'Surveys' );
+		const rComponents = this.createMenuEntries( GROUPED_SNIPPETS.rComponents, 'R Components' );
+		const programmatic = this.createMenuEntries( GROUPED_SNIPPETS.programmatic, 'Programmatic Components' );
+		const models = this.createMenuEntries( GROUPED_SNIPPETS.models, 'Models' );
+		const tables = this.createMenuEntries( GROUPED_SNIPPETS.tables, 'Tables' );
+		const tests = this.createMenuEntries( GROUPED_SNIPPETS.tests, 'Tests' );
+		const plots = this.createMenuEntries( GROUPED_SNIPPETS.plots, 'Plots', this.createMenuEntries( GROUPED_SNIPPETS.victory, 'Victory Charts' ) );
+		const learning = this.createMenuEntries( GROUPED_SNIPPETS.learning, 'Learning Components', this.createMenuEntries( GROUPED_SNIPPETS.learningDistribution, 'Distributions' ) );
+		const general = this.createMenuEntries( GROUPED_SNIPPETS.general, 'General', this.createMenuEntries( GROUPED_SNIPPETS.services, 'Services' ) );
+		const basic = this.createMenuEntries( GROUPED_SNIPPETS.basic, 'Basic',
+			this.createMenuEntries( GROUPED_SNIPPETS.basicHeadings, 'Headings' ),
+			this.createMenuEntries( GROUPED_SNIPPETS.basicLists, 'Lists' )
+		);
+		const presentation = this.createMenuEntries( GROUPED_SNIPPETS.presentation, 'Presentation' );
+		const data = this.createMenuEntries( [], 'Data', models, tables, tests );
 		return (
 			<Fragment>
-				<ContextMenu id={this.props.id} >
+				<ContextMenu id={this.props.id} onHide={this.handleHide} >
 					<span style={{ marginLeft: 6 }} >Select Component to Insert</span>
 					<div className="react-contextmenu-item react-contextmenu-item--divider"></div>
-					<SubMenu title="Main" >
-						{GROUPED_SNIPPETS.main.map( this.renderMenuItem )}
-					</SubMenu>
-					<SubMenu title="Basic">
-						<SubMenu title="Headings">
-							{GROUPED_SNIPPETS.basicHeadings.map( this.renderMenuItem )}
-						</SubMenu>
-						<SubMenu title="Lists">
-							{GROUPED_SNIPPETS.basicLists.map( this.renderMenuItem )}
-						</SubMenu>
-						{GROUPED_SNIPPETS.basic.map( this.renderMenuItem )}
-					</SubMenu>
-					<SubMenu title="Display">
-						{GROUPED_SNIPPETS.displayComponents.map( this.renderMenuItem )}
-					</SubMenu>
-					<SubMenu title="Layout">
-						{GROUPED_SNIPPETS.layout.map( this.renderMenuItem )}
-					</SubMenu>
-					<SubMenu title="Inputs">
-						{GROUPED_SNIPPETS.inputs.map( this.renderMenuItem )}
-					</SubMenu>
-					<SubMenu title="Questions">
-						{GROUPED_SNIPPETS.questions.map( this.renderMenuItem )}
-					</SubMenu>
-					<SubMenu title="Surveys">
-						{GROUPED_SNIPPETS.surveys.map( this.renderMenuItem )}
-					</SubMenu>
-					<SubMenu title="R Components">
-						{GROUPED_SNIPPETS.rComponents.map( this.renderMenuItem )}
-					</SubMenu>
-					<SubMenu title="Programmatic Components">
-						{GROUPED_SNIPPETS.programmatic.map( this.renderMenuItem )}
-					</SubMenu>
-					<SubMenu title="Learning Components">
-						{GROUPED_SNIPPETS.learning.map( this.renderMenuItem )}
-						<SubMenu title="Distributions">
-							{GROUPED_SNIPPETS.learningDistribution.map( this.renderMenuItem )}
-						</SubMenu>
-					</SubMenu>
-					<SubMenu title="General">
-						{GROUPED_SNIPPETS.general.map( this.renderMenuItem )}
-						<SubMenu title="Services">
-							{GROUPED_SNIPPETS.services.map( this.renderMenuItem )}
-						</SubMenu>
-					</SubMenu>
-					<SubMenu title="Presentation">
-						{GROUPED_SNIPPETS.presentation.map( this.renderMenuItem )}
-					</SubMenu>
-					<SubMenu title="Data">
-						<SubMenu title="Models">
-							{GROUPED_SNIPPETS.models.map( this.renderMenuItem )}
-						</SubMenu>
-						<SubMenu title="Tables">
-							{GROUPED_SNIPPETS.tables.map( this.renderMenuItem )}
-						</SubMenu>
-						<SubMenu title="Tests">
-							{GROUPED_SNIPPETS.tests.map( this.renderMenuItem )}
-						</SubMenu>
-					</SubMenu>
-					<SubMenu title="Plots">
-						{GROUPED_SNIPPETS.plots.map( this.renderMenuItem )}
-						<SubMenu title="Victory Charts">
-							{GROUPED_SNIPPETS.victory.map( this.renderMenuItem )}
-						</SubMenu>
-					</SubMenu>
+					{main}
+					{basic}
+					{displayComponents}
+					{layout}
+					{inputs}
+					{questions}
+					{surveys}
+					{rComponents}
+					{programmatic}
+					{learning}
+					{general}
+					{presentation}
+					{data}
+					{plots}
 					<div className="react-contextmenu-item react-contextmenu-item--divider"></div>
-					<SearchBar buttonSize="sm" style={{ margin: 0, fontSize: 10 }} />
+					<SearchBar
+						placeholder="Search for components..."
+						onChange={( event ) => {
+							this.setState({
+								searchValue: replace( lowercase( event.target.value ), RE_WHITESPACE, '' )
+							});
+						}}
+						buttonSize="sm"
+						style={{ margin: 0, fontSize: 10 }}
+					/>
 					<div className="react-contextmenu-item react-contextmenu-item--divider"></div>
 					<SubMenu title="Translate selection to" disabled={!ISLE_SERVER_TOKEN || !this.props.hasSelection} >
 						{LANGUAGE_NAMES.map( ( name, idx ) => {
