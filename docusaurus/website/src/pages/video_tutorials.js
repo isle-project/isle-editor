6845650ec4e753c929652caf49a741b6e4bf7280
Planeshifter <pburckhardt@outlook.com>
2020-11-24 15:18:31 -0500

    Add video tutorials

--- docusaurus/website/src/pages/video_tutorials.js
+++ docusaurus/website/src/pages/video_tutorials.js
@@ -35,9 +35,20 @@ function VideoTutorials() {
 						<iframe title="Using the Sketchpad" width="100%" height="350px" src="https://www.youtube.com/embed/R8a5iO46lDU" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullscreen></iframe>
 					</div>
 				</div>
-				<div>
+				<div className="row">
 					<div className="col col--6 padding-vert--lg">
+						<h3>Revealer and Blackout</h3>
 						<iframe title="Revealer and Blackout" width="560" height="315" src="https://www.youtube.com/embed/eXmpIfWjoEU" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullscreen></iframe>
+					</div>
+					<div className="col col--6 padding-vert--lg">
+						<h3>Styling Lessons</h3>
+						<iframe title="Styling Lessons" width="560" height="315" src="https://www.youtube.com/embed/oW3-6f1wfQ4" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullscreen></iframe>
+					</div>
+				</div>
+				<div className="row">
+					<div className="col col--6 padding-vert--lg">
+						<h3>Ticketing System</h3>
+						<iframe title="Ticketing System" width="560" height="315" src="https://www.youtube.com/embed/V82sHJc1dpc" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullscreen></iframe>
 					</div>
 				</div>
 			</div>
