53f3f3ff221fe35518a03a98e0c77db8f313d594
Planeshifter <pburckhardt@outlook.com>
2020-11-29 17:36:18 -0500

    Add placeholders for other background options

--- app/editor-components/component-styler/layout.js
+++ app/editor-components/component-styler/layout.js
@@ -1,19 +1,76 @@
 // MODULES //
 
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
+import ToggleButtonGroup from 'react-bootstrap/ToggleButtonGroup';
+import ToggleButton from 'react-bootstrap/ToggleButton';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import ColorPicker from 'components/color-picker';
 import SelectInput from 'components/input/select';
 import DISPLAY_TYPES from './display_types.json';
+import './layout.css';
 
 
 // MAIN //
 
 const Layout = ( props ) => {
+	const [ type, setType ] = useState( 'color' );
 	if ( !props.active ) {
 		return null;
+	}
+	let input;
+	switch ( type ) {
+		case 'color':
+			input = <Form.Group as={Row} >
+				<Form.Label column sm="4">
+					Background Color
+				</Form.Label>
+				<Col sm={1} >
+					<ColorPicker
+						style={{ zIndex: 2000 }}
+						color={props.style.backgroundColor}
+						onChange={({ rgb }) => {
+							const { r, g, b, a } = rgb;
+							const newStyle = { ...props.style };
+							newStyle.backgroundColor = `rgba(${r}, ${g}, ${b}, ${a} )`;
+							props.onChange( newStyle );
+						}}
+						variant="Button"
+					/>
+				</Col>
+			</Form.Group>;
+			break;
+		case 'image':
+			input = <Form.Group as={Row} >
+				<Form.Label column sm="4">
+					Background Image
+				</Form.Label>
+				<Col sm="8">
+
+				</Col>
+			</Form.Group>;
+			break;
+		case 'linear-gradient':
+			input = <Form.Group as={Row} >
+				<Form.Label column sm="4">
+					Linear Gradient
+				</Form.Label>
+				<Col sm="8">
+
+				</Col>
+			</Form.Group>;
+			break;
+		case 'radial-gradient':
+			input = <Form.Group as={Row} >
+				<Form.Label column sm="4">
+					Radial Gradient
+				</Form.Label>
+				<Col sm="8">
+
+				</Col>
+			</Form.Group>;
+			break;
 	}
 	return (
 		<Fragment>
@@ -35,22 +92,43 @@ const Layout = ( props ) => {
 			</Form.Group>
 			<Form.Group as={Row} >
 				<Form.Label column sm="4">
-					Background Color
+					Background Type
 				</Form.Label>
-				<Col sm="8">
-					<ColorPicker
-						style={{ zIndex: 2000 }}
-						color={props.style.backgroundColor}
-						onChange={({ rgb }) => {
-							const { r, g, b, a } = rgb;
-							const newStyle = { ...props.style };
-							newStyle.backgroundColor = `rgba(${r}, ${g}, ${b}, ${a} )`;
-							props.onChange( newStyle );
-						}}
-						variant="Button"
-					/>
+				<Col sm={8} >
+					<ToggleButtonGroup
+						name="type"
+						onChange={setType}
+						type="radio"
+						value={type}
+					>
+						<ToggleButton
+							variant="outline-secondary"
+							value="color"
+						>
+							<i className="fas fa-square fa-lg" style={{ color: 'black' }} ></i>
+						</ToggleButton>
+						<ToggleButton
+							variant="outline-secondary"
+							value="image"
+						>
+							<i className="far fa-image fa-lg"></i>
+						</ToggleButton>
+						<ToggleButton
+							variant="outline-secondary"
+							value="linear-gradient"
+						>
+							<div className="component-styler-box linear-gradient-box" ></div>
+						</ToggleButton>
+						<ToggleButton
+							variant="outline-secondary"
+							value="radial-gradient"
+						>
+							<div className="component-styler-box radial-gradient-box" ></div>
+						</ToggleButton>
+					</ToggleButtonGroup>
 				</Col>
 			</Form.Group>
+			{input}
 		</Fragment>
 	);
 };
