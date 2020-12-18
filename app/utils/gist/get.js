875a4e032ed260956cd10e8c68623967125de0bd
Planeshifter <pburckhardt@outlook.com>
2020-12-18 03:16:47 -0500

    Disable lint rule

--- app/utils/gist/get.js
+++ app/utils/gist/get.js
@@ -14,12 +14,12 @@ import electronStore from 'store/electron.js';
 */
 async function getGist( id ) {
 	const accessToken = electronStore.get( 'githubAccessToken' );
-	const url = `https://api.github.com/gists/${id}`;
+	const url = `https://api.github.com/gists/${id}`; // eslint-disable-line i18next/no-literal-string
 	const res = await axios.get( url,
 	{
 		headers: {
 			'Accept': 'application/vnd.github.v3+json',
-			'Authorization': `token ${accessToken}`
+			'Authorization': `token ${accessToken}` // eslint-disable-line i18next/no-literal-string
 		}
 	});
 	return res;
