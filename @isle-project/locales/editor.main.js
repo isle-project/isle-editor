42e7c7fbcf0c0d77f9279b966c5cd4c5b0b98bee
Planeshifter <pburckhardt@outlook.com>
2020-12-17 20:58:23 -0500

    Split into two files for renderer and main

--- @isle-project/locales/editor.main.js
+++ @isle-project/locales/editor.main.js
@@ -7,7 +7,7 @@ import logger from 'debug';
 
 // VARIABLES //
 
-const debug = logger( 'isle:locales-editor' );
+const debug = logger( 'isle:locales-editor-main' );
 
 
 // MAIN //
@@ -15,37 +15,37 @@ const debug = logger( 'isle:locales-editor' );
 const NAMESPACES = new Set();
 const TRANSLATIONS = {
 	'de': {
-		'Editor': () => import( './editor/de.json' )
+		'EditorMenu': () => import( './editor-menu/de.json' )
 	},
 	'en': {
-		'Editor': () => import( './editor/en.json' )
+		'EditorMenu': () => import( './editor-menu/en.json' )
 	},
 	'es': {
-		'Editor': () => import( './editor/es.json' )
+		'EditorMenu': () => import( './editor-menu/es.json' )
 	},
 	'fr': {
-		'Editor': () => import( './editor/fr.json' )
+		'EditorMenu': () => import( './editor-menu/fr.json' )
 	},
 	'it': {
-		'Editor': () => import( './editor/it.json' )
+		'EditorMenu': () => import( './editor-menu/it.json' )
 	},
 	'ja': {
-		'Editor': () => import( './editor/ja.json' )
+		'EditorMenu': () => import( './editor-menu/ja.json' )
 	},
 	'nl': {
-		'Editor': () => import( './editor/nl.json' )
+		'EditorMenu': () => import( './editor-menu/nl.json' )
 	},
 	'pl': {
-		'Editor': () => import( './editor/pl.json' )
+		'EditorMenu': () => import( './editor-menu/pl.json' )
 	},
 	'pt': {
-		'Editor': () => import( './editor/pt.json' )
+		'EditorMenu': () => import( './editor-menu/pt.json' )
 	},
 	'ru': {
-		'Editor': () => import( './editor/ru.json' )
+		'EditorMenu': () => import( './editor-menu/ru.json' )
 	},
 	'zh': {
-		'Editor': () => import( './editor/zh.json' )
+		'EditorMenu': () => import( './editor-menu/zh.json' )
 	}
 };
 
@@ -71,8 +71,8 @@ i18n.use( LanguageDetector )
 	.init({
 		debug: !!process.env.DEBUG_I18N, // eslint-disable-line no-process-env
 		fallbackLng: 'en',
-		ns: [ 'Editor' ],
-		defaultNS: 'Editor',
+		ns: [ 'EditorMenu' ],
+		defaultNS: 'EditorMenu',
 		resources: RESOURCES,
 		interpolation: {
 			escapeValue: false // Not needed for React!
