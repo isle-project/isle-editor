6916c800a48ee06f91d824498b248b85a8f64621
Planeshifter <pburckhardt@outlook.com>
2020-12-27 20:13:59 -0500

    Update dll version

--- webpack.config.dll.js
+++ webpack.config.dll.js
@@ -179,7 +179,7 @@ const config = {
 		path: join( __dirname, '@isle-project', 'dll' ),
 		filename: 'dll.[name].js',
 		library: '[name]_dll',
-		publicPath: 'https://cdn.jsdelivr.net/npm/@isle-project/dll@0.0.5/'
+		publicPath: 'https://cdn.jsdelivr.net/npm/@isle-project/dll@0.0.6/'
 	},
 	optimization: {
 		minimize: true,
