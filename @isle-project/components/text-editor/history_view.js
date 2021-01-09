0a826fa2462b1ff88581183e03058a66d2e366ca
Planeshifter <pburckhardt@outlook.com>
2021-01-09 17:40:28 -0500

    Add tooltips and fast-forwarding

--- @isle-project/components/text-editor/history_view.js
+++ @isle-project/components/text-editor/history_view.js
@@ -10,6 +10,7 @@ import { EditorView } from 'prosemirror-view';
 import { EditorState } from 'prosemirror-state';
 import isJSON from '@stdlib/assert/is-json';
 import SliderInput from '@isle-project/components/input/slider';
+import Tooltip from '@isle-project/components/tooltip';
 import collaborativeCursorPlugin from './config/collab_cursor.js';
 import plugins from './config/plugins';
 import schema from './config/schema';
@@ -140,6 +141,42 @@ class HistoryView extends Component {
 		});
 	}
 
+	fastForward = () => {
+		let newState = this.state.editorState;
+		const invertedSteps = this.state.invertedSteps.slice();
+		for ( let i = 0; i < this.state.document.steps.length; i++ ) {
+			const tr = newState.tr;
+			const jsonStep = this.state.document.steps[ i ];
+
+			const step = Step.fromJSON( schema, jsonStep );
+			const invertedStep = step.invert( tr.doc );
+			invertedStep.clientID = jsonStep.clientID;
+			tr.maybeStep( step );
+
+			newState = newState.apply( tr );
+			invertedSteps[ i ] = invertedStep;
+		}
+		this.editorView.updateState( newState );
+
+		let nChars = 0;
+		let nWords = 0;
+		newState.doc.forEach( blockNode => {
+			blockNode.forEach( inlineNode => {
+				if ( inlineNode.text ) {
+					nChars += inlineNode.text.length;
+					nWords += countWords( inlineNode.text );
+				}
+			});
+		});
+		this.setState({
+			counter: this.state.document.steps.length,
+			editorState: newState,
+			nWords,
+			nChars,
+			invertedSteps
+		});
+	}
+
 	subtractStep = () => {
 		if ( !this.state.running ) {
 			return;
@@ -227,46 +264,63 @@ class HistoryView extends Component {
 
 	render() {
 		const { document, counter } = this.state;
-		const notSignedIn = this.props.session.anonymous;
+		const { t, session } = this.props;
+		const notSignedIn = session.anonymous;
 		let editorDiv;
 		if ( !notSignedIn ) {
 			editorDiv = <div
 				ref={this._createEditorView}
 			/>;
 		} else {
-			editorDiv = <Jumbotron>{this.props.t('history-only-available-for-users')}</Jumbotron>;
+			editorDiv = <Jumbotron>{t('history-only-available-for-users')}</Jumbotron>;
 		}
 		return (
 			<Fragment>
-				<Button size="sm" variant="light" onClick={this.props.onClose} >
+				<Button variant="light" onClick={this.props.onClose} >
 					<i className="fas fa-arrow-left"></i>
 				</Button>
 				<span className="title text-editor-playback-title" >
-					{this.props.t('document-history')}
+					{t('document-history')}
 				</span>
-				<ButtonGroup size="sm" >
-					<Button variant="light" onClick={this.reset} disabled={notSignedIn} >
-						<i className="far fa-times-circle"></i>
-					</Button>
-					<Button variant="light" onClick={this.playBackward} disabled={notSignedIn || !document || counter === 0} >
-						<i className="fas fa-fast-backward" ></i>
-					</Button>
-					<Button variant="light" onClick={this.stop} disabled={notSignedIn} >
-						<i className="fas fa-pause"></i>
-					</Button>
-					<Button variant="light" onClick={this.playForward} disabled={notSignedIn || !document || ( document.steps.length <= counter )} >
-						<i className="fas fa-fast-forward" ></i>
-					</Button>
+				<ButtonGroup>
+					<Tooltip tooltip={t('jump-to-beginning')} disabled={notSignedIn} >
+						<Button variant="light" onClick={this.reset} disabled={notSignedIn} >
+							<i className="fas fa-fast-backward" ></i>
+						</Button>
+					</Tooltip>
+					<Tooltip tooltip={t('play-in-reverse')} disabled={notSignedIn || !document || counter === 0} >
+						<Button variant="light" onClick={this.playBackward} disabled={notSignedIn || !document || counter === 0} >
+							<i className="fas fa-play fa-rotate-180" ></i>
+						</Button>
+					</Tooltip>
+					<Tooltip tooltip={t('stop')} disabled={notSignedIn} >
+						<Button variant="light" onClick={this.stop} disabled={notSignedIn} >
+							<i className="fas fa-pause" ></i>
+						</Button>
+					</Tooltip>
+					<Tooltip tooltip={t('play')} disabled={notSignedIn || !document || ( document.steps.length <= counter )} >
+						<Button variant="light" onClick={this.playForward} disabled={notSignedIn || !document || ( document.steps.length <= counter )} >
+							<i className="fas fa-play" ></i>
+						</Button>
+					</Tooltip>
+					<Tooltip tooltip={t('jump-to-end')} disabled={notSignedIn || !document || ( document.steps.length <= counter )} >
+						<Button variant="light" onClick={this.fastForward} disabled={notSignedIn || !document || ( document.steps.length <= counter )} >
+							<i className="fas fa-fast-forward" ></i>
+						</Button>
+					</Tooltip>
 				</ButtonGroup>
 				<span style={{ marginLeft: 20, marginRight: 10 }} >
-					{this.props.t('update-time')}
+					{t('update-time')}
 				</span>
 				<SliderInput inline defaultValue={this.state.interval} min={50} max={1000} onChange={( value ) => {
 					this.setState({ interval: value });
 				}} numberInputStyle={{ display: 'none' }} />
 				<span>{`${this.state.interval}${MS}`}</span>
+				<Tooltip tooltip={t('restore-tooltip')} >
+					<Button variant="secondary" style={{ margin: 4, marginLeft: 8 }} >{t('restore')}</Button>
+				</Tooltip>
 				{editorDiv}
-				<StatusBar nWords={this.state.nWords} nChars={this.state.nChars} t={this.props.t} />
+				<StatusBar nWords={this.state.nWords} nChars={this.state.nChars} t={t} />
 			</Fragment>
 		);
 	}
