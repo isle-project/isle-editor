e57eae541150850416885b7b5c3cf2765eceb949
Planeshifter <pburckhardt@outlook.com>
2021-01-28 19:03:29 -0500

    Handle empty line check correctly on Windows

--- app/utils/markdown-to-html/is_empty_line.js
+++ app/utils/markdown-to-html/is_empty_line.js
@@ -13,14 +13,17 @@ import isWhitespace from './is_whitespace.js';
 * @returns {boolean} boolean indicating whether character is whitespace
 */
 function isEmptyLine( buffer, pos ) {
-	if ( buffer[ pos ] !== '\n' ) {
+	if (
+		buffer[ pos ] !== '\n' ||
+		( buffer[ pos ] !== '\r' && buffer[ pos+1 ] !== '\n' )
+	) {
 		return false;
 	}
 	pos += 1;
 	let eol = false;
 	while ( !eol ) {
 		const char = buffer[ pos ];
-		if ( char === '\n' ) {
+		if ( char === '\n' || ( char === '\r' && buffer[ pos+1 ] === '\n' ) ) {
 			eol = true;
 		} else if ( !isWhitespace( char ) ) {
 			return false;
