4cd168d7278f0cea0aa16e5c783aa896ea217067
Planeshifter <pburckhardt@outlook.com>
2021-01-30 17:44:43 -0500

    Refactor as hook and change props

--- @isle-project/components/flippable-card/main.js
+++ @isle-project/components/flippable-card/main.js
@@ -1,9 +1,10 @@
 // MODULES //
 
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
-import { withTranslation } from 'react-i18next';
+import { useTranslation } from 'react-i18next';
 import ReactDraggable from 'react-draggable';
+import noop from '@stdlib/utils/noop';
 import Alert from 'react-bootstrap/Alert';
 import Button from 'react-bootstrap/Button';
 import isArray from '@stdlib/assert/is-array';
@@ -15,149 +16,115 @@ import isArray from '@stdlib/assert/is-array';
 * An ISLE component that allows two render two-sides.
 *
 * @property {(string|node)} button - button label
-* @property {Array<Object>} cardStyles - allows to override the given styles
 * @property {boolean} draggable - controls whether the card may be dragged around
 * @property {number} flipSpeedBackToFront - the speed by which the card turns from background to foreground, in seconds
-* @property {number} flipSpeedFrontToBack {number} the speed by which the card turns from foreground to background, in seconds
+* @property {number} flipSpeedFrontToBack - the speed by which the card turns from foreground to background, in seconds
 * @property {boolean} defaultValue - initial flip state of the card
 * @property {boolean} value - flip state of the card (for controlled component)
 * @property {number} perspective - CSS property value to give 3d-positioned element a perspective
 * @property {boolean} oneTime - indicates whether the flip process may be executed just once
+* @property {Object} containerStyle - CSS style of the container
+* @property {Object} frontStyle - CSS style of the front card
+* @property {Object} backStyle - CSS style of the back card
 * @property {Function} onChange - callback invoked once the card is flipped; receives the current flipped status as its sole argument
 */
-class FlippableCard extends Component {
-	constructor( props ) {
-		super( props );
+const FlippableCard = ( props ) => {
+	const defaultValue = props.defaultValue;
+	const [ isFlipped, setIsFlipped ] = useState( defaultValue );
+	const [ fired, setFired ] = useState( false );
+	const { t } = useTranslation( 'General' );
+	useEffect( () => {
+		setIsFlipped( defaultValue );
+	}, [ defaultValue ] );
 
-		this.state = {
-			isFlipped: props.defaultValue,
-			fired: false
-		};
+	if ( !isArray( props.children ) ) {
+		return <Alert variant="danger" >{t('flippable-requires-two-children')}</Alert>;
 	}
-
-	componentDidUpdate( nextProps ) {
-		if ( nextProps.defaultValue !== this.props.defaultValue ) {
-			this.setState({
-				isFlipped: nextProps.defaultValue
-			});
+	const handleToggle = () => {
+		if ( props.value !== void 0 ) {
+			return props.onChange( !props.value );
 		}
-	}
-
-	oneShot = () => {
-		if ( !this.state.fired ) {
-			this.setState({
-				fired: true,
-				isFlipped: !this.state.isFlipped
-			}, () => {
-				this.props.onChange( this.state.isFlipped );
-			});
+		if ( !props.oneTime ) {
+			setIsFlipped( !isFlipped );
+			props.onChange( !isFlipped );
+		} else if ( !fired ) {
+			// Case: one-shot
+			setFired( true );
+			setIsFlipped( !isFlipped );
+			props.onChange( !isFlipped );
 		}
-	}
-
-	handleToggle = () => {
-		if ( this.props.value !== void 0 ) {
-			return this.props.onChange( !this.props.value );
+	};
+	const flipped = props.value !== void 0 ? props.value : isFlipped;
+	const styles = {
+		container: {
+			perspective: props.perspective,
+			transformStyle: 'preserve-3d',
+			width: props.size,
+			height: props.size,
+			cursor: 'pointer',
+			...props.containerStyle
+		},
+		flipper: {
+			position: 'relative',
+			transformStyle: 'preserve-3d',
+			width: '100%',
+			height: '100%'
+		},
+		front: {
+			WebkitBackfaceVisibility: 'hidden',
+			backfaceVisibility: 'hidden',
+			left: '0',
+			position: 'absolute',
+			top: '0',
+			transform: `rotateY(${flipped ? 180 : 0 }deg)`, // eslint-disable-line i18next/no-literal-string
+			transformStyle: 'preserve-3d',
+			width: '100%',
+			height: '100%',
+			zIndex: '2',
+			transition: `${props.flipSpeedBackToFront}s`, // eslint-disable-line i18next/no-literal-string
+			...props.frontStyle
+		},
+		back: {
+			WebkitBackfaceVisibility: 'hidden',
+			backfaceVisibility: 'hidden',
+			left: '0',
+			position: 'absolute',
+			transform: `rotateY(${flipped ? 0 : -180 }deg)`, // eslint-disable-line i18next/no-literal-string
+			transformStyle: 'preserve-3d',
+			top: '0',
+			width: '100%',
+			height: '100%',
+			zIndex: 2,
+			transition: `${props.flipSpeedFrontToBack}s`, // eslint-disable-line i18next/no-literal-string
+			...props.backStyle
 		}
-		if ( !this.props.oneTime )
-			{
-			this.setState({
-				isFlipped: !this.state.isFlipped
-			}, () => {
-				this.props.onChange( this.state.isFlipped );
-			});
-		} else {
-			this.oneShot();
-		}
-	}
-
-	renderButton() {
-		if ( this.props.button !== null ) {
-			return (
-				<Button onClick={this.handleToggle} block >
-					{this.props.button}
-				</Button>
-			);
-		}
-		return null;
-	}
-
-	interaction = () => {
-		if ( this.props.button === null ) {
-			this.handleToggle();
-		}
-	}
-
-	render() {
-		if ( !isArray( this.props.children ) ) {
-			return <Alert variant="danger" >{this.props.t('flippable-requires-two-children')}</Alert>;
-		}
-		const isFlipped = this.props.value !== void 0? this.props.value : this.state.isFlipped;
-		const styles = {
-			container: {
-				perspective: this.props.perspective,
-				transformStyle: 'preserve-3d',
-				width: 400,
-				height: 400,
-				cursor: 'pointer',
-				...this.props.cardStyles.container
-			},
-			flipper: {
-				position: 'relative',
-				transformStyle: 'preserve-3d',
-				width: '100%',
-				height: '100%'
-			},
-			front: {
-				WebkitBackfaceVisibility: 'hidden',
-				backfaceVisibility: 'hidden',
-				left: '0',
-				position: 'absolute',
-				top: '0',
-				transform: `rotateY(${isFlipped ? 180 : 0 }deg)`, // eslint-disable-line i18next/no-literal-string
-				transformStyle: 'preserve-3d',
-				width: '100%',
-				height: '100%',
-				zIndex: '2',
-				transition: `${this.props.flipSpeedBackToFront}s`, // eslint-disable-line i18next/no-literal-string
-				...this.props.cardStyles.front
-			},
-			back: {
-				WebkitBackfaceVisibility: 'hidden',
-				backfaceVisibility: 'hidden',
-				left: '0',
-				position: 'absolute',
-				transform: `rotateY(${isFlipped ? 0 : -180 }deg)`, // eslint-disable-line i18next/no-literal-string
-				transformStyle: 'preserve-3d',
-				top: '0',
-				width: '100%',
-				height: '100%',
-				zIndex: 2,
-				transition: `${this.props.flipSpeedFrontToBack}s`, // eslint-disable-line i18next/no-literal-string
-				...this.props.cardStyles.back
-			}
-		};
-		const out = <div
-			id={this.props.id} role="button" tabIndex={0}
-			onClick={this.interaction} onKeyPress={this.interaction}
-			className="react-card-flip" style={styles.container}
-		>
-			<div className="react-card-flipper" style={styles.flipper} >
-				<div className="react-card-front" style={styles.front} >
-					{this.props.children[ 0 ]}
-					{this.renderButton()}
-				</div>
-				<div className="react-card-back" style={styles.back} >
-					{this.props.children[ 1 ]}
-					{this.renderButton()}
-				</div>
+	};
+	const button = props.button ?
+		<Button onClick={handleToggle} block >
+			{props.button}
+		</Button> : null;
+	const out = <div
+		id={props.id} role="button" tabIndex={0}
+		onClick={!button ? handleToggle : noop} onKeyPress={!button ? handleToggle : noop}
+		className="react-card-flip" style={styles.container}
+	>
+		<div className="react-card-flipper" style={styles.flipper} >
+			<div className="react-card-front" style={styles.front} >
+				{props.children[ 0 ]}
+				{button}
 			</div>
-		</div>;
-		if ( this.props.draggable ) {
-			return ( <ReactDraggable>{out}</ReactDraggable> );
-		}
-		return out;
+			<div className="react-card-back" style={styles.back} >
+				{props.children[ 1 ]}
+				{button}
+			</div>
+		</div>
+	</div>;
+	if ( props.draggable ) {
+		return ( <ReactDraggable>{out}</ReactDraggable> );
 	}
-}
+	return out;
+};
+
 
 // PROPERTIES //
 
@@ -181,24 +148,30 @@ FlippableCard.propTypes = {
 	onChange: PropTypes.func,
 	oneTime: PropTypes.bool,
 	perspective: PropTypes.number,
+	size: PropTypes.number,
 	value: PropTypes.bool,
 	defaultValue: PropTypes.bool
 };
 
 FlippableCard.defaultProps = {
-	button: null,
-	cardStyles: {
-		container: {},
-		front: {},
-		back: {}
+	backStyle: {
+		background: 'pink'
 	},
+	button: null,
 	children: null,
+	containerStyle: {
+		float: 'left'
+	},
 	draggable: false,
 	flipSpeedBackToFront: 1,
 	flipSpeedFrontToBack: 1,
+	frontStyle: {
+		background: 'gainsboro'
+	},
 	onChange() {},
 	oneTime: false,
 	perspective: 1000,
+	size: 400,
 	value: void 0,
 	defaultValue: false
 };
@@ -206,4 +179,4 @@ FlippableCard.defaultProps = {
 
 // EXPORTS //
 
-export default withTranslation( 'General' )( FlippableCard );
+export default FlippableCard;
