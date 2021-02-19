7586ad7c5105a471f48a96a21ab54db7ebade80b
Planeshifter <pburckhardt@outlook.com>
2021-02-19 15:01:47 -0500

    No fallback for tooltip

--- @isle-project/components/tooltip/index.js
+++ @isle-project/components/tooltip/index.js
@@ -5,7 +5,10 @@ import Loadable from '@isle-project/components/internal/loadable';
 
 // MAIN //
 
-const Tooltip = Loadable( () => import( /* webpackChunkName: "Tooltip" */ './main.js' ) );
+const Tooltip = Loadable(
+	() => import( /* webpackChunkName: "Tooltip" */ './main.js' ),
+	{ fallback: null }
+);
 
 
 // EXPORTS //
