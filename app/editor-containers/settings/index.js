901c8f049e08ba4edd42c6ed7bfd8c058f64ba0f
Planeshifter <pburckhardt@outlook.com>
2021-02-21 16:04:41 -0500

    Make settings card collapsible

--- app/editor-containers/settings/index.js
+++ app/editor-containers/settings/index.js
@@ -1,9 +1,10 @@
 // MODULES //
 
-import React, { Component, Fragment } from 'react';
+import React, { Fragment, useCallback, useState } from 'react';
 import { connect } from 'react-redux';
 import { changeRenderInterval, changeFontSize, changePreambleTemplate, updateDownloading } from 'actions';
 import { withTranslation } from 'react-i18next';
+import Collapse from '@isle-project/components/collapse';
 import NumberInput from '@isle-project/components/input/number';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
@@ -20,79 +21,86 @@ import './settings.css';
 
 // MAIN //
 
-class Settings extends Component {
-	handleFontSizeChange = ( number ) => {
-		this.props.changeFontSize( number );
-	}
-
-	handleRenderIntervalChange = ( number ) => {
-		this.props.changeRenderInterval( number * 1000 );
-	}
-
-	handlePreambleTemplateChange = ( event ) => {
-		this.props.changePreambleTemplate( event.target.value );
-	}
-
-	render() {
-		const { t } = this.props;
-		return (
-			<Fragment>
-				<HeaderUpperBar
-					backToEditor
-					title={t('settings')}
-					updateStatus={this.props.updateStatus}
-					updateInfo={this.props.updateInfo}
-					updateDownloading={this.props.updateDownloading}
-					updateDownloadPercent={this.props.updateDownloadPercent}
-				/>
-				<div className="settings-wrapper">
-					<SettingsLogin />
-					<Card className="settings-card" >
-						<Card.Header as="h5">
-							{t('settings')}
-						</Card.Header>
-						<Card.Body>
-							<Container><Row>
-								<Col md={5}>
-									<NumberInput
-										description={t('font-size-description')}
-										legend={t('font-size')}
-										onChange={this.handleFontSizeChange}
-										min={8}
-										max={56}
-										step={1}
-										defaultValue={this.props.fontSize}
+const Settings = ({
+	changeFontSize, changeRenderInterval, changePreambleTemplate, t,
+	updateStatus, updateInfo, updateDownloading, updateDownloadPercent,
+	fontSize, renderInterval, preambleTemplate
+}) => {
+	const [ showSettings, setShowSettings ] = useState( false );
+	const handleFontSizeChange = useCallback( ( number ) => {
+		changeFontSize( number );
+	}, [ changeFontSize ] );
+	const handleRenderIntervalChange = useCallback( ( number ) => {
+		changeRenderInterval( number * 1000 );
+	}, [ changeRenderInterval ] );
+	const handlePreambleTemplateChange = useCallback( ( event ) => {
+		changePreambleTemplate( event.target.value );
+	}, [ changePreambleTemplate ] );
+	return (
+		<Fragment>
+			<HeaderUpperBar
+				backToEditor
+				title={t('settings')}
+				updateStatus={updateStatus}
+				updateInfo={updateInfo}
+				updateDownloading={updateDownloading}
+				updateDownloadPercent={updateDownloadPercent}
+			/>
+			<div className="settings-wrapper" >
+				<SettingsLogin />
+				<Collapse
+					header={<Fragment>
+						<span style={{ marginRight: 8 }} >{t('settings')}</span>
+						{showSettings ? <i className="fas fa-chevron-up"></i> : <i className="fas fa-chevron-down"></i>}
+					</Fragment>}
+					className="settings-card" style={{ maxWidth: '100%' }}
+					onClick={( visible ) => {
+						setShowSettings( visible );
+					}}
+				>
+					<Card.Body>
+						<Container><Row>
+							<Col md={5}>
+								<NumberInput
+									description={t('font-size-description')}
+									legend={t('font-size')}
+									onChange={handleFontSizeChange}
+									min={8}
+									max={56}
+									step={1}
+									defaultValue={fontSize}
+								/>
+								<NumberInput
+									description={t('rendering-interval-description')}
+									legend={t('rendering-interval')}
+									onChange={handleRenderIntervalChange}
+									min={1}
+									max={100}
+									step={1}
+									defaultValue={parseInt( renderInterval / 1000, 10 )}
+								/>
+							</Col>
+							<Col md={7}>
+								<LanguageSwitcher />
+								<FormGroup className="settings-preamble-form-group">
+									<Card.Subtitle className="mb-2" >
+										<FormLabel className="title" >{t('preamble-template')}:</FormLabel>
+									</Card.Subtitle>
+									<FormControl
+										as="textarea"
+										onChange={handlePreambleTemplateChange}
+										rows={7}
+										defaultValue={preambleTemplate}
 									/>
-									<NumberInput
-										description={t('rendering-interval-description')}
-										legend={t('rendering-interval')}
-										onChange={this.handleRenderIntervalChange}
-										min={1}
-										max={100}
-										step={1}
-										defaultValue={parseInt( this.props.renderInterval / 1000, 10 )}
-									/>
-									<LanguageSwitcher />
-								</Col>
-								<Col md={7}>
-									<FormGroup className="settings-preamble-form-group">
-										<Card.Subtitle className="mb-2" ><FormLabel>{t('preamble-template')}:</FormLabel></Card.Subtitle>
-										<FormControl
-											as="textarea"
-											onChange={this.handlePreambleTemplateChange}
-											rows={7}
-											defaultValue={this.props.preambleTemplate}
-										/>
-									</FormGroup>
-								</Col>
-							</Row></Container>
-						</Card.Body>
-					</Card>
-				</div>
-			</Fragment>
-		);
-	}
-}
+								</FormGroup>
+							</Col>
+						</Row></Container>
+					</Card.Body>
+				</Collapse>
+			</div>
+		</Fragment>
+	);
+};
 
 
 // EXPORTS //
