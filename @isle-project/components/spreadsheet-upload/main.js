44379930c2fa6bfc422f199504d5509edfefb9ac
Planeshifter <pburckhardt@outlook.com>
2021-02-21 23:09:15 -0500

    Refactor as hook

--- @isle-project/components/spreadsheet-upload/main.js
+++ @isle-project/components/spreadsheet-upload/main.js
@@ -1,14 +1,11 @@
 // MODULES //
 
-import React, { Component } from 'react';
+import React, { useCallback, useContext, useRef, useState } from 'react';
 import PropTypes from 'prop-types';
-import { withTranslation } from 'react-i18next';
+import { useTranslation } from 'react-i18next';
 import Card from 'react-bootstrap/Card';
 import ProgressBar from 'react-bootstrap/ProgressBar';
-import parse from 'csv-parse';
-import detect from 'detect-csv';
-import replace from '@stdlib/string/replace';
-import isNull from '@stdlib/assert/is-null';
+import csv from 'csvtojson';
 import round from '@stdlib/math/base/special/round';
 import SessionContext from '@isle-project/session/context.js';
 import CheckboxInput from '@isle-project/components/input/checkbox';
@@ -20,6 +17,17 @@ import { addResources } from '@isle-project/locales';
 addResources( 'SpreadsheetUpload' );
 
 
+// FUNCTIONS //
+
+/**
+* Event handler ignoring default dragging behavior and preventing bubbling-up.
+*/
+function ignoreDrag( evt ) {
+	evt.stopPropagation();
+	evt.preventDefault();
+}
+
+
 // MAIN //
 
 /**
@@ -28,94 +36,61 @@ addResources( 'SpreadsheetUpload' );
 * @property {string} title - card title
 * @property {Function} onUpload - callback function called with ( err, output ) after loading of file
 */
-class SpreadsheetUpload extends Component {
-	constructor( props ) {
-		super( props );
-
-		this.state = {
-			percentCompleted: 0,
-			uploading: false,
-			header: true
-		};
-	}
-
-	/**
-	* Creates FileReader and attaches event listener for when the file is ready.
-	*/
-	handleFileUpload = () => {
-		this.setState({
-			uploading: true
-		}, () => {
-			const reader = new FileReader();
-			const selectedFile = this.fileUpload.files[ 0 ];
-			reader.addEventListener( 'load', this.onFileRead, false );
-			reader.addEventListener( 'progress', ( event ) => {
-				if ( event.lengthComputable ) {
-					const percentCompleted = event.loaded / event.total;
-					this.setState({
-						percentCompleted
-					});
-				}
-			}, false );
-			reader.readAsText( selectedFile, 'utf-8' );
-		});
-	}
+const SpreadsheetUpload = ({ title, onUpload }) => {
+	const [ percentCompleted, setPercentCompleted ] = useState( 0 );
+	const [ uploading, setUploading ] = useState( false );
+	const [ header, setHeader ] = useState( true );
+	const { t } = useTranslation( 'SpreadsheetUpload' );
+	const session = useContext( SessionContext );
+	const fileUpload = useRef( null );
 
 	/**
 	* Event handler invoked once student-supplied CSV file has been uploaded. Parses the file and extracts its categorical and quantitative variables.
 	*/
-	onFileRead = ( event ) => {
-		let text = event.target.result;
-		const csv = detect( text );
-		let delimiter;
-		if ( !isNull( csv ) ) {
-			delimiter = csv.delimiter;
-		} else {
-			delimiter = ',';
-		}
-		text = replace( text, '\r', '' ); // Remove carriage returns for search in L73 to return correct index
-		let columns;
-		const idx = text.indexOf( '\n' );
-		let firstLine = text.substring( 0, idx ).split( delimiter );
-		if ( this.state.header ) {
-			columns = firstLine;
-			text = text.substring( idx+1 );
-		} else {
-			columns = firstLine.map( ( x, i ) => {
-				return 'VAR_'+(i+1);
-			});
-		}
-		this.setState({
-			percentCompleted: 0,
-			uploading: false
-		});
-		parse( text, { delimiter, columns, cast: true }, ( err, output ) => {
-			if ( err ) {
-				const session = this.context;
+	const onFileRead = useCallback( ( event ) => {
+		const text = event.target.result;
+		setPercentCompleted( 0 );
+		setUploading( false );
+		csv({
+			noheader: !header,
+			checkType: true
+		})
+			.fromString( text )
+			.then( ( output ) => {
+				onUpload( null, output );
+			})
+			.catch( ( err ) => {
 				session.addNotification({
-					title: this.props.t('error-title'),
-					message: `${this.props.t('error-message')}: ${err.message}`,
+					title: t('error-title'),
+					message: `${t('error-message')}: ${err.message}`,
 					level: 'error',
 					position: 'tr'
 				});
-				return this.props.onUpload( err );
-			}
-			this.props.onUpload( null, output );
-		});
-	}
+				onUpload( err );
+			});
+	}, [ header, session, onUpload, t ] );
 
 	/**
-	* Event handler ignoring default dragging behavior and preventing bubbling-up.
+	* Creates FileReader and attaches event listener for when the file is ready.
 	*/
-	ignoreDrag = ( evt ) => {
-		evt.stopPropagation();
-		evt.preventDefault();
-	}
+	const handleFileUpload = useCallback( () => {
+		setUploading( true );
+		const reader = new FileReader();
+		const selectedFile = fileUpload.current.files[ 0 ];
+		reader.addEventListener( 'load', onFileRead, false );
+		reader.addEventListener( 'progress', ( event ) => {
+			if ( event.lengthComputable ) {
+				const percentage = event.loaded / event.total;
+				setPercentCompleted( percentage );
+			}
+		}, false );
+		reader.readAsText( selectedFile, 'utf-8' );
+	}, [ onFileRead ] );
 
 	/**
 	* Event handler invoked when user drags CSV file onto the upload area.
 	*/
-	onFileDrop = ( evt ) => {
+	const onFileDrop = useCallback( ( evt ) => {
 		evt.stopPropagation();
 		evt.preventDefault();
 		const dt = evt.dataTransfer;
@@ -131,68 +106,60 @@ class SpreadsheetUpload extends Component {
 		if ( file ) {
 			const mimeType = file.type;
 			if ( mimeType !== 'text/csv' ) {
-				const session = this.context;
 				return session.addNotification({
-					title: this.props.t('no-csv'),
-					message: this.props.t('no-csv-message'),
+					title: t('no-csv'),
+					message: t('no-csv-message'),
 					level: 'error',
 					position: 'tr'
 				});
 			}
-			reader.addEventListener( 'load', this.onFileRead, false );
+			reader.addEventListener( 'load', onFileRead, false );
 			reader.readAsText( file, 'utf-8' );
 		}
-	}
-
-	render() {
-		const completed = round( this.state.percentCompleted * 100.0 );
-		return ( <Card>
-			<Card.Header as="h2">
-				{this.props.title || this.props.t('title')}
-			</Card.Header>
-			<Card.Body>
-				<label htmlFor="CSVUpload">{this.props.t('csv-upload-prompt')}:</label>
-				<input
-					id="CSVUpload"
-					type="file"
-					accept=".csv"
-					onChange={this.handleFileUpload}
-					ref={fileUpload => {
-						this.fileUpload = fileUpload;
-					}}
-					style={{ margin: 'auto' }}
-				/>
-				<p>{this.props.t('or')}</p>
-				<div
-					onDrop={this.onFileDrop}
-					onDragOver={this.ignoreDrag}
-					onDragEnd={this.ignoreDrag}
-					style={{
-						minHeight: '150px',
-						width: '250px',
-						border: '1px solid blue',
-						padding: '10px'
-					}}
-				>
-					<span>{this.props.t('drop-file')}</span>
-				</div>
-				<CheckboxInput
-					legend={this.props.t('first-row-names')}
-					defaultValue={true}
-					onChange={( value ) => {
-						this.setState({
-							header: value
-						});
-					}}
-				/>
-				{ this.state.uploading ?
-					<ProgressBar now={completed} label={`${completed}%`} /> :
-					null
-				}
-			</Card.Body>
-		</Card> );
-	}
-}
+	}, [ session, onFileRead, t ] );
+	const completed = round( percentCompleted * 100.0 );
+	return ( <Card>
+		<Card.Header as="h2">
+			{title || t('title')}
+		</Card.Header>
+		<Card.Body>
+			<label htmlFor="CSVUpload">{t('csv-upload-prompt')}:</label>
+			<input
+				id="CSVUpload"
+				type="file"
+				accept=".csv"
+				onChange={handleFileUpload}
+				ref={fileUpload}
+				style={{ margin: 'auto' }}
+			/>
+			<p>{t('or')}</p>
+			<div
+				onDrop={onFileDrop}
+				onDragOver={ignoreDrag}
+				onDragEnd={ignoreDrag}
+				style={{
+					minHeight: '150px',
+					width: '250px',
+					border: '1px solid blue',
+					padding: '10px'
+				}}
+			>
+				<span>{t('drop-file')}</span>
+			</div>
+			<CheckboxInput
+				legend={t('first-row-names')}
+				defaultValue={true}
+				onChange={( value ) => {
+					setHeader( value );
+				}}
+			/>
+			{ uploading ?
+				<ProgressBar now={completed} label={`${completed}%`} /> :
+				null
+			}
+		</Card.Body>
+	</Card> );
+};
 
 
 // PROPERTIES //
@@ -212,4 +179,4 @@ SpreadsheetUpload.contextType = SessionContext;
 
 // EXPORTS //
 
-export default withTranslation( 'SpreadsheetUpload' )( SpreadsheetUpload );
+export default SpreadsheetUpload;
