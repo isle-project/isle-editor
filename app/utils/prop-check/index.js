5bd4956deaf213c3bb2e3a84fa03129a18091f4d
Planeshifter <pburckhardt@outlook.com>
2021-02-26 01:57:22 -0500

    Add prop check HOC

---
+++ app/utils/prop-check/index.js
@@ -0,0 +1,39 @@
+// MODULES //
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import ErrorBoundary from '@isle-project/components/internal/error-boundary';
+
+
+// FUNCTIONS //
+
+/* eslint-disable no-console */
+const consoleErrorFcn = console.error;
+if ( console.error === consoleErrorFcn ) {
+	console.error = function onWarning() {
+		if ( arguments[ 0 ].includes( 'prop type' ) ) {
+			throw new Error( arguments[ 0 ] );
+		} else {
+			consoleErrorFcn.apply( console, arguments );
+		}
+	};
+}
+/* eslint-enable no-console */
+
+
+// MAIN //
+
+export function withPropCheck( Component ) {
+	function WrappedComponent({ forwardedRef, ...rest }) {
+		if ( Component.propTypes ) {
+			PropTypes.checkPropTypes( Component.propTypes, rest, 'prop', Component.name );
+		}
+		return React.createElement( Component, { ...rest, ref: forwardedRef } );
+	}
+	const forwardRef = ( props, ref ) => {
+		return ( <ErrorBoundary component={Component.name} >
+			<WrappedComponent {...props} forwardedRef={ref} />
+		</ErrorBoundary> );
+	};
+	return React.forwardRef( forwardRef );
+}
