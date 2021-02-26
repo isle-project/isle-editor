40f04aee9026306578e8f690370071481eab9860
Planeshifter <pburckhardt@outlook.com>
2021-02-26 17:28:18 -0500

    Refactor as hook

--- @isle-project/components/draggable-grid/main.js
+++ @isle-project/components/draggable-grid/main.js
@@ -1,13 +1,14 @@
 // MODULES //
 
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
-import { withTranslation } from 'react-i18next';
+import { useTranslation } from 'react-i18next';
 import Alert from 'react-bootstrap/Alert';
 import { Responsive, WidthProvider } from 'react-grid-layout';
 import isArray from '@stdlib/assert/is-array';
 import pluck from '@stdlib/utils/pluck';
 import floor from '@stdlib/math/base/special/floor';
+import { withPropCheck } from '@isle-project/utils/prop-check';
 
 
 // VARIABLES //
@@ -17,12 +18,12 @@ const ResponsiveReactGridLayout = WidthProvider( Responsive );
 
 // FUNCTIONS //
 
-const createLayout = ( props ) => {
-	if ( !isArray( props.children ) ) {
+const createLayout = ( children ) => {
+	if ( !isArray( children ) ) {
 		return null;
 	}
 	/* eslint-disable i18next/no-literal-string */
-	let layouts = props.children.map( ( e, i ) => {
+	let layouts = children.map( ( e, i ) => {
 		return {
 			lg: { i: `cell-${i}`, x: i*6 % 18, y: floor( i / 3 ) * 4, w: 6, h: 4 },
 			md: { i: `cell-${i}`, x: i*6 % 12, y: floor( i / 2 ) * 4, w: 6, h: 4 },
@@ -52,42 +53,33 @@ const createLayout = ( props ) => {
 * @property {boolean} isResizable - controls whether grid elements are resizable
 * @property {number} rowHeight - height of a single row (in px)
 */
-class DraggableGrid extends Component {
-	constructor( props ) {
-		super( props );
-		this.state = {
-			layouts: createLayout( props )
-		};
+const DraggableGrid = ({ children, isDraggable, isResizable, rowHeight }) => {
+	const [ layouts, setLayouts ] = useState( createLayout( children ) );
+	const { t } = useTranslation( 'General' );
+	useEffect( () => {
+		const newLayouts = createLayout( children );
+		setLayouts( newLayouts );
+	}, [ children ] );
+	if ( !isArray( children ) ) {
+		return ( <Alert variant="danger" >
+			{t('grid-requires-two-children')}
+		</Alert> );
 	}
-
-	static getDerivedStateFromProps( nextProps ) {
-		return {
-			layouts: createLayout( nextProps )
-		};
-	}
-
-	render() {
-		if ( !isArray( this.props.children ) ) {
-			return ( <Alert variant="danger" >
-				{this.props.t('grid-requires-two-children')}
-			</Alert> );
-		}
-		return ( <ResponsiveReactGridLayout
-			layouts={this.state.layouts}
-			breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
-			cols={{ lg: 18, md: 12, sm: 12, xs: 12, xxs: 6 }}
-			isResizable={this.props.isResizable}
-			isDraggable={this.props.isDraggable}
-			rowHeight={this.props.rowHeight}
-		>
-			{this.props.children.map( ( element, idx ) => {
-				return ( <div key={`cell-${idx}`}>
-					{element}
-				</div> );
-			})}
-		</ResponsiveReactGridLayout> );
-	}
-}
+	return ( <ResponsiveReactGridLayout
+		layouts={layouts}
+		breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
+		cols={{ lg: 18, md: 12, sm: 12, xs: 12, xxs: 6 }}
+		isResizable={isResizable}
+		isDraggable={isDraggable}
+		rowHeight={rowHeight}
+	>
+		{children.map( ( element, idx ) => {
+			return ( <div key={`cell-${idx}`}>
+				{element}
+			</div> );
+		})}
+	</ResponsiveReactGridLayout> );
+};
 
 
 // PROPERTIES //
@@ -107,4 +99,4 @@ DraggableGrid.propTypes = {
 
 // EXPORTS //
 
-export default withTranslation( 'General' )( DraggableGrid );
+export default withPropCheck( DraggableGrid );
