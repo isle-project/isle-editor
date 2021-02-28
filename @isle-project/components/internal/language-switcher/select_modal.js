0a1cdd5ac883a1c567ca2cd085df40446990d161
Planeshifter <pburckhardt@outlook.com>
2021-02-28 17:46:36 -0500

    Add missing import

--- @isle-project/components/internal/language-switcher/select_modal.js
+++ @isle-project/components/internal/language-switcher/select_modal.js
@@ -1,5 +1,6 @@
 // MODULES //
 
+import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import { changeLanguage } from '@isle-project/locales';
