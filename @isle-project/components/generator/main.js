f2ebc719f6cf20512c3717bf30168d43880d818d
Planeshifter <pburckhardt@outlook.com>
2021-03-01 09:25:55 -0500

    Refactor as hook and attach id

--- @isle-project/components/generator/main.js
+++ @isle-project/components/generator/main.js
@@ -1,9 +1,15 @@
 // MODULES //
 
-import React, { Component } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import PropTypes from 'prop-types';
 import Button from 'react-bootstrap/Button';
 import Panel from '@isle-project/components/panel';
+import generateUID from '@isle-project/utils/uid';
+
+
+// VARIABLES //
+
+const uid = generateUID( 'generator' );
 
 
 // MAIN //
@@ -11,40 +17,37 @@ import Panel from '@isle-project/components/panel';
 /**
 * A component for generating other components (e.g., automatically generated questions).
 *
+* @property {boolean} autoStart - controls whether to automatically invoke the generator after mounting
 * @property {(string|node)} buttonLabel - label for button
 * @property {Function} onGenerate - function invoked that should return a new component to be rendered in the lesson
 */
-class Generator extends Component {
-	constructor( props ) {
-		super( props );
+const Generator = ({ id, autoStart, buttonLabel, onGenerate }) => {
+	const idRef = useRef( id || uid({ autoStart, buttonLabel, onGenerate }) );
+	const [ element, setElement ] = useState( null );
 
-		this.state = {
-			element: null
-		};
-	}
-
-	componentWillMount() {
-		if ( this.props.autoStart ) {
-			this.handleClick();
+	const handleClick = useCallback( () => {
+		let elem = onGenerate();
+		if ( elem ) {
+			elem = React.cloneElement( elem, {
+				id: idRef.current
+			});
+			setElement( elem );
 		}
-	}
+	}, [ onGenerate ] );
 
-	handleClick = () => {
-		const out = this.props.onGenerate();
-		this.setState({
-			element: out
-		});
-	}
+	useEffect( () => {
+		if ( autoStart ) {
+			handleClick();
+		}
+	}, [ autoStart, handleClick ] );
 
-	render() {
-		return (
-			<Panel>
-				{this.state.element}
-				<Button block onClick={this.handleClick}>{this.props.buttonLabel}</Button>
-			</Panel>
-		);
-	}
-}
+	return (
+		<Panel>
+			{element}
+			<Button block onClick={handleClick}>{buttonLabel}</Button>
+		</Panel>
+	);
+};
 
 
 // PROPERTIES //
