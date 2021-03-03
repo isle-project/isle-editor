94073acd07b11f38f0498eb23f5faeaddff39056
Planeshifter <pburckhardt@outlook.com>
2021-03-03 12:21:04 -0500

    Use loadable component

--- @isle-project/components/questions/multiple-choice-matrix/index.js
+++ @isle-project/components/questions/multiple-choice-matrix/index.js
@@ -1,6 +1,11 @@
 // MODULES //
 
-import MultipleChoiceMatrix from './main.js';
+import Loadable from '@isle-project/components/internal/loadable';
+
+
+// MAIN //
+
+const MultipleChoiceMatrix = Loadable( () => import( /* webpackChunkName: "MultipleChoiceMatrix" */ './main.js' ) );
 
 
 // EXPORTS //
