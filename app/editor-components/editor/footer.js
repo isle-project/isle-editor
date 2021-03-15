4806f02fedf7d365fcff0a5f77c783bd1b9ea576
Planeshifter <pburckhardt@outlook.com>
2021-03-15 02:33:08 -0400

    Add editor tutorial

--- app/editor-components/editor/footer.js
+++ app/editor-components/editor/footer.js
@@ -1,6 +1,6 @@
 // MODULES //
 
-import React, { Component } from 'react';
+import React, { useCallback, useState } from 'react';
 import PropTypes from 'prop-types';
 import Badge from 'react-bootstrap/Badge';
 import Button from 'react-bootstrap/Button';
@@ -10,9 +10,11 @@ import Tooltip from '@isle-project/components/tooltip';
 import Loadable from '@isle-project/components/internal/loadable';
 import electronStore from 'store/electron.js';
 import { LANGUAGES } from '@isle-project/constants/deepl';
+import { addResources } from '@isle-project/locales/editor';
 const AnimationHelp = Loadable( () => import( 'editor-components/animation-help' ) );
 const MarkdownHelp = Loadable( () => import( 'editor-components/markdown-help' ) );
 const KeyboardHelp = Loadable( () => import( 'editor-components/keyboard-help' ) );
+const EditorTutorial = Loadable( () => import( 'editor-components/tutorial' ) );
 
 
 // VARIABLES //
@@ -23,124 +25,122 @@ const ISLE_SERVER_TOKEN = electronStore.get( 'token' );
 
 // MAIN //
 
-class EditorFooter extends Component {
-	constructor( props ) {
-		super( props );
+const EditorFooter = ( props ) => {
+	const [ showAnimationHelp, setShowAnimationHelp ] = useState( false );
+	const [ showKeyboardHelp, setShowKeyboardHelp ] = useState( false );
+	const [ showMarkdownHelp, setShowMarkdownHelp ] = useState( false );
+	const [ showTutorial, setShowTutorial ] = useState( false );
 
-		this.state = {
-			showAnimationHelp: false,
-			showKeyboardHelp: false,
-			showMarkdownHelp: false
-		};
-	}
+	const toggleAnimationHelp = useCallback( () => {
+		setShowAnimationHelp( !showAnimationHelp );
+	}, [ showAnimationHelp ] );
+	const toggleKeyboardHelp = useCallback( () => {
+		setShowKeyboardHelp( !showKeyboardHelp );
+	}, [ showKeyboardHelp ] );
+	const toggleMarkdownHelp = useCallback( () => {
+		setShowMarkdownHelp( !showMarkdownHelp );
+	}, [ showMarkdownHelp ] );
+	const toggleTutorial = useCallback( () => {
+		addResources( 'Tutorial' );
+		setShowTutorial( !showTutorial );
+	}, [ showTutorial ] );
+	const { t, onTranslate } = props;
+	const handleTranslateClick = useCallback( ( _, data ) => {
+		onTranslate( data.language );
+	}, [ onTranslate] );
+	return (
+		<div className="editor-footer" >
+			<Tooltip tooltip={t('linting-tooltip')} placement="top" >
+				<Button
+					id="linting-and-spelling"
+					variant="light" size="sm" className="editor-footer-button" style={{ marginRight: 18 }}
+					onClick={() => {
+						props.editor.focus();
+						const evt = document.createEvent( 'KeyboardEvent' );
 
-	toggleAnimationHelp = () => {
-		this.setState({
-			showAnimationHelp: !this.state.showAnimationHelp
-		});
-	}
-
-	toggleKeyboardHelp = () => {
-		this.setState({
-			showKeyboardHelp: !this.state.showKeyboardHelp
-		});
-	}
-
-	toggleMarkdownHelp = () => {
-		this.setState({
-			showMarkdownHelp: !this.state.showMarkdownHelp
-		});
-	}
-
-	handleTranslateClick = ( _, data ) => {
-		this.props.onTranslate( data.language );
-	}
-
-	render() {
-		const { t } = this.props;
-		return (
-			<div className="editor-footer" >
-				<Tooltip tooltip={t('linting-tooltip')} placement="top" >
-					<Button
-						variant="light" size="sm" className="editor-footer-button" style={{ marginRight: 18 }}
-						onClick={() => {
-							this.props.editor.focus();
-							const evt = document.createEvent( 'KeyboardEvent' );
-
-							// Chromium Hack
-							Object.defineProperty(evt, 'keyCode', {
-								get() {
-									return this.keyCodeVal;
-								}
-							});
-							Object.defineProperty(evt, 'which', {
-								get() {
-									return this.keyCodeVal;
-								}
-							});
-							if ( evt.initKeyboardEvent ) {
-								evt.initKeyboardEvent( 'keydown', true, true, document.defaultView, 119, 119, '', '', false, '' );
-							} else {
-								evt.initKeyEvent( 'keydown', true, true, document.defaultView, false, false, false, false, 119, 0 );
+						// Chromium Hack
+						Object.defineProperty( evt, 'keyCode', {
+							get() {
+								return this.keyCodeVal;
 							}
-							evt.keyCodeVal = 119;
-							const div = document.getElementsByClassName( 'monaco-mouse-cursor-text' )[ 0 ];
-							div.dispatchEvent( evt );
-						}}
-						disabled={this.props.nErrors === 0}
-					>
-						{t('linting-and-spelling')}: <Badge variant="secondary">{this.props.nErrors}</Badge>
-					</Button>
-				</Tooltip>
-				<Tooltip tooltip={t('keyboard-shortcuts')} placement="top">
-					<Button className="editor-footer-button" variant="light" size="sm" onClick={this.toggleKeyboardHelp} >
-						<i className="far fa-keyboard"></i>
-					</Button>
-				</Tooltip>
-				<Tooltip tooltip={t('markdown-help')} placement="top" >
-					<Button className="editor-footer-button" variant="light" size="sm" onClick={this.toggleMarkdownHelp} >
-						<i className="fab fa-markdown" />
-					</Button>
-				</Tooltip>
-				<Tooltip tooltip={t('animation-help')} placement="top" >
-					<Button className="editor-footer-button" variant="light" size="sm" onClick={this.toggleAnimationHelp} >
-						<i className="fas fa-theater-masks" />
-					</Button>
-				</Tooltip>
-				{ ISLE_SERVER_TOKEN ?
-					<ContextMenuTrigger
-						id="language-menu" holdToDisplay={0}
-						renderTag="span"
-					>
-						<Tooltip tooltip={t('translate-lesson')} placement="left" >
-							<Button className="editor-footer-button" variant="light" size="sm" >
-								<i className="fas fa-globe-europe" />
-							</Button>
-						</Tooltip>
-					</ContextMenuTrigger> : null }
-				<ContextMenu id="language-menu" title={t('translate-entire-lesson')} >
-					<span style={{ marginLeft: 6 }}>{t('translate-entire-lesson')}</span>
-					<div className="react-contextmenu-item react-contextmenu-item--divider"></div>
-					{LANGUAGE_NAMES.map( ( name, idx ) => {
-						return (
-							<MenuItem
-								key={idx} data={{
-									language: LANGUAGES[ name ]
-								}}
-								onClick={this.handleTranslateClick}
-							>
-								{t(name)}
-							</MenuItem>
-						);
-					})}
-				</ContextMenu>
-				{ this.state.showAnimationHelp ? <AnimationHelp onHide={this.toggleAnimationHelp} /> : null }
-				{ this.state.showKeyboardHelp ? <KeyboardHelp onHide={this.toggleKeyboardHelp} /> : null }
-				{ this.state.showMarkdownHelp ? <MarkdownHelp onHide={this.toggleMarkdownHelp} /> : null }
-			</div>
-		);
-	}
-}
+						});
+						Object.defineProperty( evt, 'which', {
+							get() {
+								return this.keyCodeVal;
+							}
+						});
+						if ( evt.initKeyboardEvent ) {
+							evt.initKeyboardEvent( 'keydown', true, true, document.defaultView, 119, 119, '', '', false, '' );
+						} else {
+							evt.initKeyEvent( 'keydown', true, true, document.defaultView, false, false, false, false, 119, 0 );
+						}
+						evt.keyCodeVal = 119;
+						const div = document.getElementsByClassName( 'monaco-mouse-cursor-text' )[ 0 ];
+						div.dispatchEvent( evt );
+					}}
+					disabled={props.nErrors === 0}
+				>
+					{t('linting-and-spelling')}: <Badge variant="secondary">{props.nErrors}</Badge>
+				</Button>
+			</Tooltip>
+			<Tooltip tooltip={t('tutorial')} placement="top" >
+				<Button className="editor-footer-button" variant="light" size="sm" onClick={toggleTutorial} >
+					<i className="fas fa-info-circle"></i>
+				</Button>
+			</Tooltip>
+			<Tooltip tooltip={t('keyboard-shortcuts')} placement="top">
+				<Button id="keyboard-shortcuts" className="editor-footer-button" variant="light" size="sm" onClick={toggleKeyboardHelp} >
+					<i className="far fa-keyboard"></i>
+				</Button>
+			</Tooltip>
+			<Tooltip tooltip={t('markdown-help')} placement="top" >
+				<Button id="markdown-help" className="editor-footer-button" variant="light" size="sm" onClick={toggleMarkdownHelp} >
+					<i className="fab fa-markdown" />
+				</Button>
+			</Tooltip>
+			<Tooltip tooltip={t('animation-help')} placement="top" >
+				<Button id="animation-help" className="editor-footer-button" variant="light" size="sm" onClick={toggleAnimationHelp} >
+					<i className="fas fa-theater-masks" />
+				</Button>
+			</Tooltip>
+			{ ISLE_SERVER_TOKEN ?
+				<ContextMenuTrigger
+					id="language-menu" holdToDisplay={0}
+					renderTag="span"
+				>
+					<Tooltip tooltip={t('translate-lesson')} placement="left" >
+						<Button id="translate-lesson" className="editor-footer-button" variant="light" size="sm" >
+							<i className="fas fa-globe-europe" />
+						</Button>
+					</Tooltip>
+				</ContextMenuTrigger> : null }
+			<ContextMenu id="language-menu" title={t('translate-entire-lesson')} >
+				<span style={{ marginLeft: 6 }}>{t('translate-entire-lesson')}</span>
+				<div className="react-contextmenu-item react-contextmenu-item--divider"></div>
+				{LANGUAGE_NAMES.map( ( name, idx ) => {
+					return (
+						<MenuItem
+							key={idx} data={{
+								language: LANGUAGES[ name ]
+							}}
+							onClick={handleTranslateClick}
+						>
+							{t(name)}
+						</MenuItem>
+					);
+				})}
+			</ContextMenu>
+			{ showAnimationHelp ? <AnimationHelp onHide={toggleAnimationHelp} /> : null }
+			{ showKeyboardHelp ? <KeyboardHelp onHide={toggleKeyboardHelp} /> : null }
+			{ showMarkdownHelp ? <MarkdownHelp onHide={toggleMarkdownHelp} /> : null }
+			{ showTutorial ? <EditorTutorial
+				onFinish={() => {
+					setShowTutorial( false );
+				}}
+			/> : null }
+		</div>
+	);
+};
 
 
 // PROPERTIES //
---
+++ app/editor-components/tutorial/index.js
@@ -0,0 +1,195 @@
+// MODULES //
+
+import React, { useCallback, useState, useRef } from 'react';
+import Joyride, { ACTIONS, EVENTS, STATUS } from '@isle-project/components/joyride';
+import { useTranslation } from 'react-i18next';
+import { pasteInsertion } from 'actions';
+import { connect } from 'react-redux';
+
+
+// VARIABLES //
+
+const HIDE_NEXT_BUTTON = {
+	buttonNext: {
+		display: 'none'
+	}
+};
+
+
+// MAIN //
+
+const EditorTutorial = ({ onFinish, pasteInsertion }) => {
+	const [ run, setRun ] = useState( true );
+	const { t } = useTranslation( 'Tutorial' );
+	const [ stepIndex, setStepIndex ] = useState( 0 );
+	const addNextHandler = useCallback( ( node, index, triggerEvent = 'click' ) => {
+		function onEvent() {
+			node.removeEventListener( triggerEvent, onEvent );
+			setTimeout( () => {
+				setStepIndex( index + 2 );
+			}, 400 );
+		}
+		node.addEventListener( triggerEvent, onEvent );
+	}, [] );
+	const steps = useRef([
+		{
+			target: '.SplitPane',
+			content: t('two-panes'),
+			placement: 'auto',
+			disableBeacon: true
+		},
+		{
+			target: '.react-monaco-editor-container',
+			content: t('monaco-editor'),
+			placement: 'top-start',
+			disableBeacon: true
+		},
+		{
+			target: '#Lesson',
+			content: t('live-preview'),
+			placement: 'left-start'
+		},
+		{
+			target: '.line-buttons-jump',
+			content: t('click-button-to-jump'),
+			styles: HIDE_NEXT_BUTTON,
+			disableOverlayClose: true,
+			hideCloseButton: true
+		},
+		{
+			target: '.react-monaco-editor-container',
+			content: t('back-in-editor'),
+			placement: 'top-start',
+			disableBeacon: true
+		},
+		{
+			target: '.react-monaco-editor-container',
+			content: t('polaroid-hover'),
+			placement: 'auto',
+			disableBeacon: true
+		},
+		{
+			target: '.react-monaco-editor-container',
+			content: t('polaroid-lightbulb'),
+			placement: 'top-start',
+			disableOverlayClose: true,
+			hideCloseButton: true
+		},
+		{
+			target: '.react-monaco-editor-container',
+			content: t('polaroid-double-click'),
+			placement: 'top-start',
+			disableOverlayClose: true,
+			hideCloseButton: true
+		},
+		{
+			target: '#Lesson',
+			content: t('polaroid-wizard'),
+			placement: 'top-start'
+		},
+		{
+			target: '#linting-and-spelling',
+			content: t('linting-and-spelling')
+		},
+		{
+			target: '#keyboard-shortcuts',
+			content: t('keyboard-shortcuts')
+		},
+		{
+			target: '#markdown-help',
+			content: t('markdown-help')
+		},
+		{
+			target: '#animation-help',
+			content: t('animation-help')
+		},
+		{
+			target: '#translate-lesson',
+			content: t('translate-lesson')
+		},
+		{
+			target: '#link-export',
+			content: t('link-export')
+		},
+		{
+			target: '#link-settings',
+			content: t('link-settings')
+		},
+		{
+			target: '#link-docs',
+			content: t('link-docs')
+		},
+		{
+			target: '#change-online-mode',
+			content: t('change-online-mode')
+		},
+		{
+			target: '#header-role-in-preview',
+			content: t('header-role-in-preview')
+		},
+		{
+			target: '#header-preview-button',
+			content: t('header-preview-button')
+		}
+	]);
+
+	const handleCallback = useCallback( ( data ) => {
+		const { action, index, status, type } = data;
+		if ( [ EVENTS.STEP_AFTER ].includes( type ) ) {
+			// Update state to advance the tour
+			switch ( index ) {
+				case 2: {
+					setStepIndex( index + ( action === ACTIONS.PREV ? -1 : 1 ) );
+					const node = document.querySelector( steps.current[ index ].target );
+					addNextHandler( node, index );
+				}
+				break;
+				case 3:
+					setStepIndex( index + ( action === ACTIONS.PREV ? -1 : 1 ) );
+					pasteInsertion({
+						text: '<Polaroid image="https://bit.ly/2OlDrIY" />'
+					});
+				break;
+				default:
+					setStepIndex( index + ( action === ACTIONS.PREV ? -1 : 1 ) );
+				break;
+			}
+		}
+		else if ([ STATUS.FINISHED, STATUS.SKIPPED ].includes( status ) ) {
+			// Need to set our running state to false, so we can restart if we click start again.
+			setRun( false );
+		}
+		if ( type === EVENTS.TOUR_END ) {
+			setRun( false );
+			onFinish();
+		}
+	}, [ onFinish, pasteInsertion, steps, addNextHandler ] );
+	return (
+		<Joyride
+			onFinish={onFinish}
+			run={run}
+			stepIndex={stepIndex}
+			showProgress
+			disableScrolling
+			continuous
+			spotlightPadding={5}
+			spotlightClicks
+			steps={steps.current}
+			callback={handleCallback}
+			styles={{
+				options: {
+					zIndex: 1100
+				}
+			}}
+		/>
+	);
+};
+
+
+// EXPORTS //
+
+export default connect( mapStateToProps, { pasteInsertion } )( EditorTutorial );
+
+function mapStateToProps() {
+	return {};
+}
