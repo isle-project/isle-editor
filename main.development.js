e0ac96cac7defb3bd6d29cde0ec7a3188a1d0304
Planeshifter <pburckhardt@outlook.com>
2021-03-21 10:36:51 -0400

    Only load package to install extension when needed

--- main.development.js
+++ main.development.js
@@ -2,17 +2,16 @@
 
 // MODULES //
 
-import { app, Menu } from 'electron';
+import { app, BrowserWindow, Menu, screen } from 'electron';
 import Store from 'electron-store';
+import path from 'path';
+import { performance } from 'perf_hooks';
+import { i18n } from './@isle-project/locales/editor.main';
+import addCustomTemplates from './app/main/add_custom_templates.js';
 import configureMenu from './app/main/configure_menu.js';
 import createWindow from './app/main/create_window.js';
 import window from './app/main/window_manager.js';
-import { autoUpdater } from 'electron-updater';
-import { i18n } from './@isle-project/locales/editor.main';
-import pkg from './package.json';
-import installExtensions from './app/utils/install-extensions';
 import addRecentFilesMenu from './app/main/add_recent_files_menu.js';
-import addCustomTemplates from './app/main/add_custom_templates.js';
 import { initialize } from '@electron/remote/main';
 
 
@@ -20,20 +19,6 @@ import { initialize } from '@electron/remote/main';
 
 initialize();
 const mainConfig = new Store( 'isle-main' );
-const ELECTRON_REGEXP = /node_modules[\\/]electron[\\/]dist/;
-const IS_PACKAGED = !( ELECTRON_REGEXP.test( process.resourcesPath ) );
-
-let pathToOpen;
-const len = IS_PACKAGED ? 1 : 2;
-if ( process.env.NODE_ENV === 'production' && process.argv[ len ] ) {
-	pathToOpen = process.argv[ len ];
-}
-else if ( mainConfig.has( 'mostRecentFilePath' ) ) {
-	pathToOpen = mainConfig.get( 'mostRecentFilePath' );
-}
-autoUpdater.allowPrerelease = true;
-autoUpdater.autoDownload = false;
-autoUpdater.currentVersion = pkg.version;
 
 
 // FUNCTIONS //
@@ -42,15 +27,41 @@ autoUpdater.currentVersion = pkg.version;
 * Check for application updates and set application menu.
 */
 function onReady() {
-	console.log( 'Application is ready...' ); // eslint-disable-line no-console
+	console.log( 'Application is ready... (time: '+performance.now()+')' ); // eslint-disable-line no-console
 
+	const { width, height } = screen.getPrimaryDisplay().workAreaSize;
+	const splash = new BrowserWindow({
+		width, height, transparent: false, frame: false, alwaysOnTop: true
+	});
+	const splashscreenPath = path.resolve( __dirname, 'app', 'splashscreen.html' );
+	splash.loadURL( 'file://'+splashscreenPath );
+	console.log( 'Splashscreen has rendered... (time: '+performance.now()+')' ); // eslint-disable-line no-console
+
+	const ELECTRON_REGEXP = /node_modules[\\/]electron[\\/]dist/;
+	const IS_PACKAGED = !( ELECTRON_REGEXP.test( process.resourcesPath ) );
+	let pathToOpen;
+	const len = IS_PACKAGED ? 1 : 2;
+	if ( process.env.NODE_ENV === 'production' && process.argv[ len ] ) {
+		pathToOpen = process.argv[ len ];
+	}
+	else if ( mainConfig.has( 'mostRecentFilePath' ) ) {
+		pathToOpen = mainConfig.get( 'mostRecentFilePath' );
+	}
 	createWindow({
 		filePath: pathToOpen,
 		callback: ( mainWindow ) => {
+			console.log( 'Main window has been created... (time: '+performance.now()+')' ); // eslint-disable-line no-console
+			splash.destroy();
 			function sendStatusToWindow( text ) {
 				mainWindow.webContents.send( 'message', text );
 			}
 			i18n.changeLanguage( i18n.language );
+
+			const { autoUpdater } = require( 'electron-updater' );
+			autoUpdater.allowPrerelease = true;
+			autoUpdater.autoDownload = false;
+			const pkg = require( './package.json' );
+			autoUpdater.currentVersion = pkg.version;
 			if ( process.env.NODE_ENV === 'production' ) {
 				sendStatusToWindow( 'Check for updates and notify if available...' );
 
@@ -86,7 +97,10 @@ function onReady() {
 			}
 		}
 	});
-	installExtensions();
+
+	if ( process.env.NODE_ENV === 'development' ) {
+		installExtensions();
+	}
 	Menu.setApplicationMenu( null );
 
 	i18n.on( 'languageChanged', ( lng ) => {
@@ -98,6 +112,22 @@ function onReady() {
 		addRecentFilesMenu();
 		addCustomTemplates();
 	});
+
+	async function installExtensions() {
+		const { default: installExtension, REACT_DEVELOPER_TOOLS } = require( 'electron-devtools-installer' );
+		try {
+			const res = await installExtension( REACT_DEVELOPER_TOOLS, {
+				loadExtensionOptions: {
+					allowFileAccess: true
+				}
+			});
+			console.log( `Added Extension:  ${res}` ); // eslint-disable-line no-console
+			return res;
+		} catch ( err ) {
+			console.log( 'An error occurred: ', err ); // eslint-disable-line no-console
+			return null;
+		}
+	}
 }
 
 
