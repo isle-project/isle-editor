e24b47aff7bf817edc3bc7b83fa812fe187d94d4
Planeshifter <pburckhardt@outlook.com>
2021-04-07 17:04:26 -0400

    Refactor Plotly as function component using hooks

--- @isle-project/components/plotly/main.js
+++ @isle-project/components/plotly/main.js
@@ -1,10 +1,10 @@
 // MODULES //
 
-import React, { Component } from 'react';
+import React, { useCallback, useContext, useEffect, useMemo, useRef, useState } from 'react';
 import PropTypes from 'prop-types';
 import debounce from 'lodash.debounce';
 import logger from 'debug';
-import { withTranslation } from 'react-i18next';
+import { useTranslation } from 'react-i18next';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import createPlotlyComponent from 'react-plotly.js/factory';
@@ -14,14 +14,12 @@ import jsyaml from 'js-yaml';
 import omitBy from '@stdlib/utils/omit-by';
 import isNull from '@stdlib/assert/is-null';
 import isUndefined from '@stdlib/assert/is-undefined';
-import isEmptyObject from '@stdlib/assert/is-empty-object';
-import deepEqual from '@stdlib/assert/deep-equal';
-import copy from '@stdlib/utils/copy';
 import SessionContext from '@isle-project/session/context.js';
 import { PLOT_UPDATE } from '@isle-project/constants/actions.js';
 import html2clipboard from '@isle-project/utils/html-to-clipboard';
 import { ACCESS_TOKEN } from '@isle-project/constants/mapbox.js';
 import { withPropCheck } from '@isle-project/utils/prop-check';
+import usePrevious from '@isle-project/utils/hooks/use-previous';
 import PlotlyIcons from './icons.js';
 import calculateChanges from './calculate_changes.js';
 import './plotly.css';
@@ -85,262 +83,234 @@ Plotly.setPlotConfig({
 * @property {Function} onSelected - callback function invoked when elements are selected
 * @property {Function} onShare - callback function invoked when clicking on the "Share" button
 */
-class Wrapper extends Component {
-	constructor( props ) {
-		super( props );
+const Wrapper = ( props ) => {
+	const { t } = useTranslation( 'Plotly' );
+	const [ layout, setLayout ] = useState({
+		...props.layout,
+		autosize: true
+	});
+	const [ layoutUpdate, setLayoutUpdate ] = useState( 0 );
+	const oldLayout = usePrevious( layout );
+	const [ fullscreen, setFullscreen ] = useState( false );
+	const [ finishedDrawing, setFinishedDrawing ] = useState( false );
+	const figure = useRef();
+	const plotDataRef = useRef( null );
+	const debouncedChange = useRef();
+	const session = useContext( SessionContext );
 
-		const { layout } = props;
-		layout.autosize = true;
+	const toggleFullscreen = useCallback( () => {
+		debug( 'Toggle fullscreen view...' );
+		const newLayout = { ...layout };
+		if ( !fullscreen ) {
+			newLayout.width = null;
+			newLayout.height = null;
+		} else {
+			newLayout.width = props.layout.width;
+			newLayout.height = props.layout.height;
+		}
+		setFullscreen( !fullscreen );
+		setLayout( newLayout );
+	}, [ layout, fullscreen, props.layout ] );
 
-		this.state = {
-			fullscreen: false,
-			oldLayout: copy( layout, 1 ),
-			layout,
-			oldPropLayout: layout,
-			finishedDrawing: false
+	useEffect( () => {
+		debug( 'Layout property has changed...' );
+		setLayout({
+			...props.layout,
+			autosize: true
+		});
+	}, [ props.layout ] );
+
+	const config = useMemo( () => {
+		const toggleLegend = () => {
+			debug( 'Toggle legend...' );
+			const newLayout = { ...layout };
+			if (
+				isUndefined( layout.showlegend ) ||
+				layout.showlegend === true
+			) {
+				newLayout.showlegend = false;
+			} else {
+				newLayout.showlegend = true;
+			}
+			setLayout( newLayout );
+			setLayoutUpdate( layoutUpdate + 1 );
 		};
-		this.plotData = null;
-
+		const toggleLegendOrientation = () => {
+			debug( 'Toggle legend orientation...' );
+			const newLayout = { ...layout };
+			let newPos = 'h';
+			if ( newLayout.legend && newLayout.legend.orientation === 'h' ) {
+				newPos = 'v';
+			}
+			newLayout.legend = {
+				'orientation': newPos
+			};
+			setLayout( newLayout );
+			setLayoutUpdate( layoutUpdate + 1 );
+		};
 		const buttonsToAdd = [];
 		if ( props.legendButtons && !props.removeButtons ) {
 			buttonsToAdd.push({
-				name: this.props.t('toggle-legend'),
+				name: t('toggle-legend'),
 				icon: PlotlyIcons[ 'legend' ],
-				click: this.toggleLegend
+				click: toggleLegend
 			});
 			buttonsToAdd.push({
-				name: this.props.t('change-orientation'),
+				name: t('change-orientation'),
 				icon: PlotlyIcons[ 'changeLegendOrientation' ],
-				click: this.toggleLegendOrientation
+				click: toggleLegendOrientation
 			});
 		}
 		if ( props.onShare && !props.removeButtons ) {
 			buttonsToAdd.push({
-				name: this.props.t('share'),
+				name: t('share'),
 				icon: PlotlyIcons[ 'share' ],
 				click: props.onShare
 			});
 		}
 		if ( props.toggleFullscreen ) {
 			buttonsToAdd.push({
-				name: this.props.t('toggle-fullscreen'),
+				name: t('toggle-fullscreen'),
 				icon: PlotlyIcons[ 'fullscreen' ],
-				click: this.toggleFullscreen
+				click: toggleFullscreen
 			});
 		}
-
-		this.config = {
-			editable: this.props.editable,
+		return {
+			editable: props.editable,
 			displayModeBar: true,
 			displaylogo: false,
-			modeBarButtonsToRemove: this.props.removeButtons ? BUTTONS : BUTTONS_TO_REMOVE,
+			modeBarButtonsToRemove: props.removeButtons ? BUTTONS : BUTTONS_TO_REMOVE,
 			modeBarButtonsToAdd: buttonsToAdd,
-			...this.props.config
+			...props.config
 		};
-	}
-
-	static getDerivedStateFromProps( nextProps, prevState ) {
-		debug( 'Get derived state from props...' );
-		const newState = {};
-		if ( !deepEqual( nextProps.layout, prevState.oldPropLayout ) ) {
-			newState.oldPropLayout = nextProps.layout;
-			newState.oldLayout = copy( prevState.layout, 1 );
-			newState.layout = nextProps.layout;
-		}
-		if ( !isEmptyObject( newState ) ) {
-			return newState;
-		}
-		return null;
-	}
-
-	onInitialized = ( figure, gd ) => {
-		this.figure = figure;
-		this.drawPlot();
-		this.props.onInitialized( figure, gd );
-	}
-
-	handleUpdate = () => {
-		if ( this.props.id ) {
-			const session = this.context;
-			const changes = calculateChanges( this.figure.layout, this.state.oldLayout );
+	}, [ layout, layoutUpdate, props.config, props.editable, props.legendButtons, props.onShare, props.removeButtons, props.toggleFullscreen, toggleFullscreen, t ] );
+	const drawPlot = useCallback( () => {
+		const opts = { format: 'png', height: 425, width: 675 };
+		Plotly.toImage( figure.current, opts )
+			.then( ( data ) => {
+				let value;
+				if ( !props.meta ) {
+					value = `<img src="${data}" style="display: block; margin: 0 auto;" />`;
+				} else {
+					let meta = props.meta;
+					meta = omitBy( meta, ( _, value ) => {
+						return isNull( value );
+					});
+					meta = jsyaml.dump( meta );
+					value = `<img src="${data}" style="display: block; margin: 0 auto;" data-plot-id="${props.id}" data-plot-meta="${meta}"></img>`;
+				}
+				plotDataRef.current = value;
+				setFinishedDrawing( true );
+			});
+	}, [ props.id, props.meta ] );
+	const onInitialized = ( figure, gd ) => {
+		figure.current = figure;
+		drawPlot();
+		props.onInitialized( figure, gd );
+	};
+	const handleUpdate = useCallback( () => {
+		if ( props.id ) {
+			const changes = calculateChanges( figure.current.layout, oldLayout );
 			if ( changes.length > 0 ) {
 				session.log({
-					id: this.props.id,
+					id: props.id,
 					type: PLOT_UPDATE,
 					value: changes
 				});
 			}
 		}
-		this.setState({
-			oldLayout: copy( this.figure.layout, 1 ),
-			layout: this.figure.layout
-		}, this.drawPlot );
-	}
-
-	onUpdate = ( figure ) => {
-		this.setState({
-			finishedDrawing: false
-		}, () => {
-			this.figure = figure;
-			if ( this.debouncedChange ) {
-				this.debouncedChange();
-			} else {
-				this.debouncedChange = debounce( this.handleUpdate, 1000 );
-				this.debouncedChange();
-			}
-		});
-	}
-
-	drawPlot = () => {
-		const opts = { format: 'png', height: 425, width: 675 };
-		Plotly.toImage( this.figure, opts )
-			.then( ( data ) => {
-				let value;
-				if ( !this.props.meta ) {
-					value = `<img src="${data}" style="display: block; margin: 0 auto;" />`;
-				} else {
-					let meta = this.props.meta;
-					meta = omitBy( meta, ( _, value ) => {
-						return isNull( value );
-					});
-					meta = jsyaml.dump( meta );
-					value = `<img src="${data}" style="display: block; margin: 0 auto;" data-plot-id="${this.props.id}" data-plot-meta="${meta}"></img>`;
-				}
-				this.plotData = value;
-				this.setState({
-					finishedDrawing: true
+		setLayout( figure.current.layout );
+		drawPlot();
+	}, [ drawPlot, oldLayout, props.id, session ] );
+	const onUpdate = useCallback( ( newFigure ) => {
+		setFinishedDrawing( false );
+		figure.current = newFigure;
+		if ( debouncedChange.current ) {
+			debouncedChange.current();
+		} else {
+			debouncedChange.current = debounce( handleUpdate, 1000 );
+			debouncedChange.current();
+		}
+	}, [ handleUpdate ] );
+	let draggableBar;
+	if ( props.draggable && !fullscreen ) {
+		draggableBar = <button
+			className="plotly-draggable-bar"
+			draggable={finishedDrawing}
+			onDragStart={( ev ) => {
+				ev.dataTransfer.setData( 'text/html', plotDataRef.current );
+			}}
+			onClick={() => {
+				html2clipboard( plotDataRef.current );
+				session.addNotification({
+					title: t('copied-to-clipboard'),
+					message: t('plot-copied-to-clipboard'),
+					level: 'success',
+					position: 'tr'
 				});
-			});
+			}}
+			style={{
+				opacity: finishedDrawing ? 0.6 : 0.2
+			}}
+		>{t('drag-plot')}</button>;
 	}
-
-	toggleFullscreen = () => {
-		const newLayout = copy( this.state.layout, 1 );
-		if ( !this.state.fullscreen ) {
-			newLayout.width = null;
-			newLayout.height = null;
-		} else {
-			newLayout.width = this.props.layout.width;
-			newLayout.height = this.props.layout.height;
-		}
-		this.setState({
-			fullscreen: !this.state.fullscreen,
-			layout: newLayout,
-			oldLayout: this.state.layout
-		});
-	}
-
-	toggleLegend = () => {
-		const newLayout = copy( this.state.layout );
-		if (
-			isUndefined( this.state.layout.showlegend ) ||
-			this.state.layout.showlegend === true
-		) {
-			newLayout.showlegend = false;
-		} else {
-			newLayout.showlegend = true;
-		}
-		this.setState({
-			layout: newLayout
-		});
-	}
-
-	toggleLegendOrientation = () => {
-		const newLayout = copy( this.state.layout );
-		let newPos = 'h';
-		if ( newLayout.legend && newLayout.legend.orientation === 'h' ) {
-			newPos = 'v';
-		}
-		newLayout.legend = {
-			'orientation': newPos
-		};
-		this.setState({
-			layout: newLayout
-		});
-	}
-
-	render() {
-		let draggableBar;
-		const fullscreen = this.state.fullscreen;
-		if ( this.props.draggable && !fullscreen ) {
-			draggableBar = <button
-				className="plotly-draggable-bar"
-				draggable={this.state.finishedDrawing}
-				onDragStart={( ev ) => {
-					ev.dataTransfer.setData( 'text/html', this.plotData );
-				}}
-				onClick={() => {
-					html2clipboard( this.plotData );
-					const session = this.context;
-					session.addNotification({
-						title: this.props.t('copied-to-clipboard'),
-						message: this.props.t('plot-copied-to-clipboard'),
-						level: 'success',
-						position: 'tr'
-					});
-				}}
-				style={{
-					opacity: this.state.finishedDrawing ?
-						0.6 :
-						0.2
-				}}
-			>{this.props.t('drag-plot')}</button>;
-		}
-		const style = {
-			width: this.state.layout.width ? this.props.layout.width : '100%',
-			height: this.state.layout.height ? this.props.layout.height : '100%',
-			zIndex: 1
-		};
-		const outerStyle = {
-			...style,
-			...this.props.style
-		};
-		let plot = <div className="plotly-container" style={outerStyle} >
-			{draggableBar}
-			<Plot
-				className={this.props.draggable ? 'plotly-draggable' : null}
-				data={this.props.data}
-				layout={this.state.layout}
-				config={this.config}
-				onInitialized={this.onInitialized}
-				onUpdate={this.onUpdate}
-				useResizeHandler
-				onClick={this.props.onClick}
-				onSelected={this.props.onSelected}
-				onAfterPlot={this.props.onAfterPlot}
-				onRelayout={this.props.onRelayout}
-				onLegendClick={this.props.onLegendClick}
-				onLegendDoubleClick={this.props.onLegendDoubleClick}
-				style={style}
-				revision={this.props.revision}
-			/>
-		</div>;
-		if ( fullscreen ) {
-			return (
-				<Modal
-					show={fullscreen}
-					onHide={this.toggleFullscreen}
-					dialogClassName="modal-100w"
+	const style = {
+		width: layout.width ? layout.width : '100%',
+		height: layout.height ? layout.height : '100%',
+		zIndex: 1
+	};
+	const outerStyle = {
+		...style,
+		...props.style
+	};
+	let plot = <div className="plotly-container" style={outerStyle} >
+		{draggableBar}
+		<Plot
+			key={layoutUpdate}
+			className={props.draggable ? 'plotly-draggable' : null}
+			data={props.data}
+			layout={layout}
+			config={config}
+			onInitialized={onInitialized}
+			onUpdate={onUpdate}
+			useResizeHandler
+			onClick={props.onClick}
+			onSelected={props.onSelected}
+			onAfterPlot={props.onAfterPlot}
+			onRelayout={props.onRelayout}
+			onLegendClick={props.onLegendClick}
+			onLegendDoubleClick={props.onLegendDoubleClick}
+			style={style}
+			revision={props.revision}
+		/>
+	</div>;
+	console.log( 'RENDER PLOT' );
+	if ( fullscreen ) {
+		return (
+			<Modal
+				show={fullscreen}
+				onHide={toggleFullscreen}
+				dialogClassName="modal-100w"
+			>
+				<Modal.Body
+					style={{
+						height: 0.80 * window.innerHeight,
+						width: 0.90 * window.innerWidth,
+						marginBottom: 50
+					}}
 				>
-					<Modal.Body
-						style={{
-							height: 0.80 * window.innerHeight,
-							width: 0.90 * window.innerWidth,
-							marginBottom: 50
-						}}
-					>
-						{plot}
-					</Modal.Body>
-					<Modal.Footer>
-						<Button onClick={this.toggleFullscreen}>
-							{this.props.t('close')}
-						</Button>
-					</Modal.Footer>
-				</Modal>
-			);
-		}
-		return plot;
+					{plot}
+				</Modal.Body>
+				<Modal.Footer>
+					<Button onClick={toggleFullscreen} >
+						{t('close')}
+					</Button>
+				</Modal.Footer>
+			</Modal>
+		);
 	}
-}
+	return plot;
+};
 
 
 // PROPERTIES //
@@ -390,9 +360,7 @@ Wrapper.propTypes = {
 	toggleFullscreen: PropTypes.bool
 };
 
-Wrapper.contextType = SessionContext;
-
 
 // EXPORTS //
 
-export default withTranslation( 'Plotly' )( withPropCheck( Wrapper ) );
+export default withPropCheck( Wrapper );
