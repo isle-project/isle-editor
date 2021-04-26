a0595c7a8efc834f3a73af3df5df4f767c3bfa54
Planeshifter <pburckhardt@outlook.com>
2021-04-26 12:46:02 -0400

    Display tooltips on mobile devices

--- @isle-project/components/overlay-trigger/index.js
+++ @isle-project/components/overlay-trigger/index.js
@@ -2,14 +2,6 @@
 
 import React, { Component } from 'react';
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
-import isMobile from 'is-mobile';
-
-
-// VARIABLES //
-
-const IS_MOBILE = isMobile({
-	tablet: true
-});
 
 
 // MAIN //
@@ -19,10 +11,6 @@ const IS_MOBILE = isMobile({
 */
 class Placeholder extends Component {
 	render() {
-		if ( IS_MOBILE ) {
-			// Do not render tooltips on touch devices to prevent users from having to double tap buttons:
-			return this.props.children;
-		}
 		return <OverlayTrigger {...this.props} >{this.props.children}</OverlayTrigger>;
 	}
 }
