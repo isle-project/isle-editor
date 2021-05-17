082549d7fb4873108251f82479796a4ce355be0d
Planeshifter <pburckhardt@outlook.com>
2021-05-17 10:55:37 -0400

    Add snapshots

---
+++ test/components/sketchpad.spec.js
@@ -0,0 +1,25 @@
+// MODULES //
+
+import React from 'react';
+import renderer from 'react-test-renderer';
+import '@testing-library/jest-dom/extend-expect';
+import Session from '@isle-project/session';
+import Provider from '@isle-project/components/internal/provider';
+import Sketchpad from '@isle-project/components/sketchpad/main.js';
+
+
+// VARIABLES //
+
+const session = new Session( {}, true );
+
+
+// TESTS //
+
+describe( '<Sketchpad />', function test() {
+	it( 'renders a sketchpad', () => {
+		const sketchpad = renderer.create( <Provider session={session}>
+			<Sketchpad />
+		</Provider> );
+		expect( sketchpad ).toMatchSnapshot();
+	});
+});
---
+++ test/components/text_editor.spec.js
@@ -0,0 +1,29 @@
+// MODULES //
+
+import React from 'react';
+import renderer from 'react-test-renderer';
+import '@testing-library/jest-dom/extend-expect';
+import Session from '@isle-project/session';
+import Provider from '@isle-project/components/internal/provider';
+import './../mocks/exec_command_mock.js';
+import TextEditor from '@isle-project/components/text-editor/main.js';
+
+
+// FUNCTIONS //
+
+
+// VARIABLES //
+
+const session = new Session( {}, true );
+
+
+// TESTS //
+
+describe( '<TextEditor />', function test() {
+	it( 'renders a text editor', () => {
+		const editor = renderer.create( <Provider session={session}>
+			<TextEditor />
+		</Provider> );
+		expect( editor ).toMatchSnapshot();
+	});
+});
