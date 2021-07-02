291b30b7496f6d9283f08473ec27cad1b39bde07
Planeshifter <pburckhardt@outlook.com>
2021-07-02 10:15:09 -0400

    Add skeleton web config

---
+++ webpack.config.web.js
@@ -0,0 +1,114 @@
+/* eslint max-len: 0 */
+
+// MODULES //
+
+import path from 'path';
+import webpack from 'webpack';
+import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
+import baseConfig from './webpack.config.base';
+
+
+// VARIABLES //
+
+const port = process.env.PORT || 1212; // eslint-disable-line no-process-env
+const publicPath = `http://localhost:${port}/dist`; // eslint-disable-line i18next/no-literal-string
+
+
+// MAIN //
+
+const config = {
+	...baseConfig,
+	devtool: 'eval-cheap-source-map',
+
+	entry: [
+		path.join( __dirname, './app/index.js' )
+	],
+
+	mode: 'development',
+
+	output: {
+		...baseConfig.output,
+		publicPath: `http://localhost:${port}/dist/`, // eslint-disable-line i18next/no-literal-string
+		pathinfo: false,
+		filename: 'renderer.dev.js'
+	},
+
+	module: {
+		...baseConfig.module,
+		rules: [
+			...baseConfig.module.rules,
+			{
+				test: /\.svg$/i,
+				use: {
+					loader: 'svg-react-loader'
+				}
+			}
+		]
+	},
+
+	resolve: {
+		...baseConfig.resolve,
+		alias: {
+			...baseConfig.resolve.alias,
+			'v8-compile-cache': false
+		},
+		fallback: {
+			'path': path.resolve( './node_modules/path-browserify' ),
+			'stream': path.resolve( './node_modules/stream-browserify' ),
+			'os': path.resolve('./node_modules/os-browserify/browser' ),
+			'domain': false,
+			'fs': false
+		}
+	},
+
+	optimization: {
+		removeAvailableModules: false,
+		removeEmptyChunks: false,
+		splitChunks: false
+	},
+
+	plugins: [
+		...baseConfig.plugins,
+		new ReactRefreshWebpackPlugin(),
+		new webpack.HotModuleReplacementPlugin({
+			multiStep: true
+		}),
+		new webpack.NoEmitOnErrorsPlugin(),
+		new webpack.LoaderOptionsPlugin({
+			debug: true
+		})
+	],
+
+	target: 'web',
+
+	devServer: {
+		port,
+		publicPath,
+		compress: true,
+		stats: 'errors-only',
+
+		inline: true,
+		lazy: false,
+		hot: true,
+		headers: { 'Access-Control-Allow-Origin': '*' },
+		contentBase: path.join( __dirname, 'dist' ),
+		watchOptions: {
+			aggregateTimeout: 300,
+			ignored: /node_modules/,
+			poll: 1000 // Check for changes every second
+		},
+		historyApiFallback: {
+			verbose: true,
+			disableDotRule: false
+		},
+		overlay: {
+			warnings: false,
+			errors: true
+		}
+	}
+};
+
+
+// EXPORTS //
+
+export default config;
