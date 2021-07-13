5cb455e0318c2110ddacb98ef4f2a80b2b1a2c1c
Planeshifter <pburckhardt@outlook.com>
2021-07-13 12:12:21 -0400

    Add tests

--- test/components/image.spec.js
+++ test/components/image.spec.js
@@ -13,4 +13,19 @@ describe( '<Image />', function test() {
 		const { container } = render( <Image src="https://bit.ly/37cof7K" /> );
 		expect( container ).not.toBeEmptyDOMElement();
 	});
+
+	it( 'renders an image with a custom class', () => {
+		const { container } = render( <Image src="https://bit.ly/37cof7K" className="custom-class" /> );
+		expect( container.querySelector( '.custom-class' ) ).not.toBeNull();
+	});
+
+	it( 'renders an image with a custom style', () => {
+		const { container } = render( <Image src="https://bit.ly/37cof7K" style={{ color: 'red' }} /> );
+		expect( container.querySelector( 'img' ).style.color ).toBe( 'red' );
+	});
+
+	it( 'renders an image with an alt text', () => {
+		const { container } = render( <Image src="https://bit.ly/37cof7K" alt="alt text" /> );
+		expect( container.querySelector( 'img' ).alt ).toBe( 'alt text' );
+	});
 });
