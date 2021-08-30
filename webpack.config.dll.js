900f4e6607a0faf7b696c2f60774e5e06a08b2c5
Planeshifter <pburckhardt@outlook.com>
2021-08-30 14:45:10 -0400

    Bump dll version

--- webpack.config.dll.js
+++ webpack.config.dll.js
@@ -229,7 +229,7 @@ const config = {
 		path: join( __dirname, '@isle-project', 'dll' ),
 		filename: 'dll.[name].js',
 		library: '[name]_dll',
-		publicPath: 'https://cdn.jsdelivr.net/npm/@isle-project/dll@0.50.20/',
+		publicPath: 'https://cdn.jsdelivr.net/npm/@isle-project/dll@0.50.23/',
 		assetModuleFilename: 'static/media/[hash][ext][query]'
 	},
 	optimization: {
