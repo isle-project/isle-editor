cd2517f0d01b42d9b06cfa2eec9034ccb6daf69c
Planeshifter <pburckhardt@outlook.com>
2022-02-02 11:17:05 -0500

    chore: Bump dll versions

--- webpack.config.dll.js
+++ webpack.config.dll.js
@@ -244,7 +244,7 @@ const config = {
 		path: join( __dirname, '@isle-project', 'dll' ),
 		filename: 'dll.[name].js',
 		library: '[name]_dll',
-		publicPath: 'https://cdn.jsdelivr.net/npm/@isle-project/dll@0.50.38/',
+		publicPath: 'https://cdn.jsdelivr.net/npm/@isle-project/dll@0.50.39/',
 		assetModuleFilename: 'static/media/[hash][ext][query]'
 	},
 	optimization: {
