9c5da4a5e5685901f287275cc50ce317c8426611
Planeshifter <pburckhardt@outlook.com>
2022-02-01 19:35:44 -0500

    chore: Rewrite as function component

--- @isle-project/components/data-explorer/fullscreen_button.js
+++ @isle-project/components/data-explorer/fullscreen_button.js
@@ -1,6 +1,6 @@
 // MODULES //
 
-import React, { Component, Fragment } from 'react';
+import React, { useState, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
@@ -9,67 +9,57 @@ import Tooltip from '@isle-project/components/tooltip';
 
 // MAIN //
 
-class FullscreenButton extends Component {
-	constructor() {
-		super();
-
-		this.state = {
-			fullscreen: false
-		};
-	}
-
-	toggleFullscreen = () => {
-		this.setState({
-			fullscreen: !this.state.fullscreen
-		});
+const FullscreenButton = ( props ) => {
+	const [ showModal, setShowModal ] = useState( false );
+	const toggleFullscreen = () => {
+		setShowModal( !showModal );
 	};
-
-	render() {
-		const { t } = this.props;
-		return (
-			<Fragment>
-				<Tooltip
-					id="fullscreen_tooltip"
-					placement="bottom"
-					tooltip={t('toggle-fullscreen')}
+	const { t } = props;
+	return (
+		<Fragment>
+			<Tooltip
+				id="fullscreen_tooltip"
+				placement="bottom"
+				tooltip={t('toggle-fullscreen')}
+			>
+				<Button
+					aria-label={t('toggle-fullscreen')}
+					variant="outline-danger"
+					size="sm"
+					onClick={toggleFullscreen}
 				>
-					<Button
-						aria-label={t('toggle-fullscreen')}
-						variant="outline-danger"
-						size="sm"
-						onClick={this.toggleFullscreen}
-					>
-						<div className="fa fa-window-maximize" />
+					<div className="fa fa-window-maximize" />
+				</Button>
+			</Tooltip>
+			<Modal
+				show={showModal}
+				onHide={toggleFullscreen}
+				dialogClassName="modal-100w"
+				backdrop={true}
+				alt="fullscreen"
+			>
+				<Modal.Header closeButton>
+					<Modal.Title as="h1">
+						{props.header ? props.header : t('fullscreen-display')}
+					</Modal.Title>
+				</Modal.Header>
+				<Modal.Body
+					className="data-explorer-fullscreen-output"
+					style={{
+						height: 0.75 * window.innerHeight
+					}}
+				>
+					{props.body}
+				</Modal.Body>
+				<Modal.Footer>
+					<Button onClick={toggleFullscreen}>
+						{t('close')}
 					</Button>
-				</Tooltip>
-				<Modal
-					show={this.state.fullscreen}
-					onHide={this.toggleFullscreen}
-					dialogClassName="modal-100w"
-				>
-					<Modal.Header closeButton>
-						<Modal.Title as="h1">
-							{this.props.header ? this.props.header : t('fullscreen-display')}
-						</Modal.Title>
-					</Modal.Header>
-					<Modal.Body
-						className="data-explorer-fullscreen-output"
-						style={{
-							height: 0.75 * window.innerHeight
-						}}
-					>
-						{this.props.body}
-					</Modal.Body>
-					<Modal.Footer>
-						<Button onClick={this.toggleFullscreen}>
-							{t('close')}
-						</Button>
-					</Modal.Footer>
-				</Modal>
-			</Fragment>
-		);
-	}
-}
+				</Modal.Footer>
+			</Modal>
+		</Fragment>
+	);
+};
 
 
 // PROPERTIES //
