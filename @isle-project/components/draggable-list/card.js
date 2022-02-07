c4d7a5e9795555c23cb5d21c0ba2d8de4bf66fb1
Planeshifter <pburckhardt@outlook.com>
2022-02-07 17:31:34 -0500

    fix: Rewrite using hooks

--- @isle-project/components/draggable-list/card.js
+++ @isle-project/components/draggable-list/card.js
@@ -1,10 +1,8 @@
 // MODULES //
 
-import React, { Component } from 'react';
+import React, { useRef } from 'react';
 import PropTypes from 'prop-types';
-import { findDOMNode } from 'react-dom';
-import { DragSource, DropTarget } from 'react-dnd';
-
+import { useDrag, useDrop } from 'react-dnd'
 
 // VARIABLES //
 
@@ -16,96 +14,89 @@ const style = {
 	cursor: 'move'
 };
 
-const cardSource = {
-	beginDrag( props ) {
-		return {
-			id: props.id,
-			index: props.index
-		};
-	}
+
+// EXPORTS //
+
+export const Card = ({ id, text, index, moveCard }) => {
+	const ref = useRef( null );
+	const [{ handlerId }, drop] = useDrop({
+		accept: 'card',
+		collect( monitor ) {
+			return {
+				handlerId: monitor.getHandlerId()
+			};
+		},
+		hover( item, monitor ) {
+			if ( !ref.current ) {
+				return;
+			}
+			const dragIndex = item.index;
+			const hoverIndex = index;
+
+			// Don't replace items with themselves
+			if ( dragIndex === hoverIndex ) {
+				return;
+			}
+
+			// Determine rectangle on screen
+			const hoverBoundingRect = ref.current?.getBoundingClientRect();
+
+			// Get vertical middle
+			const hoverMiddleY =
+				(hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
+
+			// Determine mouse position
+			const clientOffset = monitor.getClientOffset();
+
+			// Get pixels to the top
+			const hoverClientY = clientOffset.y - hoverBoundingRect.top;
+
+			// Only perform the move when the mouse has crossed half of the items height
+			// When dragging downwards, only move when the cursor is below 50%
+			// When dragging upwards, only move when the cursor is above 50%
+			// Dragging downwards
+			if ( dragIndex < hoverIndex && hoverClientY < hoverMiddleY ) {
+				return;
+			}
+			// Dragging upwards
+			if ( dragIndex > hoverIndex && hoverClientY > hoverMiddleY ) {
+				return;
+			}
+
+			// Time to actually perform the action
+			moveCard( dragIndex, hoverIndex );
+
+			// Note: we're mutating the monitor item here!
+			// Generally it's better to avoid mutations,
+			// but it's good here for the sake of performance
+			// to avoid expensive index searches.
+			item.index = hoverIndex;
+		}
+	});
+	const [{ isDragging }, drag] = useDrag({
+		type: 'card',
+		item: () => {
+			return { id, index };
+		},
+		collect: ( monitor ) => ({
+			isDragging: monitor.isDragging()
+		})
+	});
+	const opacity = isDragging ? 0 : 1;
+	drag( drop( ref ) );
+	return (
+		<div ref={ref} style={{ ...style, opacity }} data-handler-id={handlerId}>
+		{text}
+		</div>
+	);
 };
-
-const cardTarget = {
-	hover( props, monitor, component ) {
-		const dragIndex = monitor.getItem().index;
-		const hoverIndex = props.index;
-
-		// Don't replace items with themselves
-		if ( dragIndex === hoverIndex ) {
-			return;
-		}
-
-		// Determine rectangle on screen
-		const hoverBoundingRect = findDOMNode( component ).getBoundingClientRect();
-
-		// Get vertical middle
-		const hoverMiddleY = ( hoverBoundingRect.bottom - hoverBoundingRect.top ) / 2;
-
-		// Determine mouse position
-		const clientOffset = monitor.getClientOffset();
-
-		// Get pixels to the top
-		const hoverClientY = clientOffset.y - hoverBoundingRect.top;
-
-		// Only perform the move when the mouse has crossed half of the items height
-		// When dragging downwards, only move when the cursor is below 50%
-		// When dragging upwards, only move when the cursor is above 50%
-
-		// Dragging downwards
-		if ( dragIndex < hoverIndex && hoverClientY < hoverMiddleY ) {
-			return;
-		}
-
-		// Dragging upwards
-		if ( dragIndex > hoverIndex && hoverClientY > hoverMiddleY ) {
-			return;
-		}
-
-		// Time to actually perform the action
-		props.moveCard( dragIndex, hoverIndex );
-
-		// Note: we're mutating the monitor item here!
-		// Generally it's better to avoid mutations, but it's good here for the sake of performance to avoid expensive index searches.
-		monitor.getItem().index = hoverIndex;
-	}
-};
-
-
-// FUNCTIONS //
-
-const sourceCollect = ( connect, monitor ) => ({
-	connectDragSource: connect.dragSource(),
-	isDragging: monitor.isDragging()
-});
-
-const targetCollect = connect => ({
-	connectDropTarget: connect.dropTarget()
-});
-
-
-// MAIN //
-
-class Card extends Component {
-	render() {
-		const { text, isDragging, connectDragSource, connectDropTarget } = this.props;
-		const opacity = isDragging ? 0 : 1;
-		return connectDragSource( connectDropTarget(
-			<div style={{ ...style, opacity }}>
-				{text}
-			</div>
-		) );
-	}
-}
 
 
 // PROPERTIES //
 
 Card.propTypes = {
-	connectDragSource: PropTypes.func.isRequired,
-	connectDropTarget: PropTypes.func.isRequired,
 	id: PropTypes.any.isRequired,   // eslint-disable-line
 	index: PropTypes.number.isRequired, // eslint-disable-line
-	isDragging: PropTypes.bool.isRequired,
 	moveCard: PropTypes.func.isRequired, // eslint-disable-line
 	text: PropTypes.string.isRequired
 };
@@ -113,6 +104,4 @@ Card.propTypes = {
 
 // EXPORTS //
 
-export default DragSource( 'card', cardSource, sourceCollect )(
-	DropTarget( 'card', cardTarget, targetCollect )( Card )
-);
+export default Card;
