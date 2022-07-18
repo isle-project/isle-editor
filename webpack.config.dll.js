1458e70653c7cca9330f3a9f4b3a81c9c1ce5dbd
Philipp Burckhardt <pburckhardt@outlook.com>
2022-07-17 23:14:44 -0400

    Bump dll version

--- webpack.config.dll.js
+++ webpack.config.dll.js
@@ -243,7 +243,7 @@ const config = {
 		path: join( __dirname, '@isle-project', 'dll' ),
 		filename: 'dll.[name].js',
 		library: '[name]_dll',
-		publicPath: 'https://cdn.jsdelivr.net/npm/@isle-project/dll@0.50.48/',
+		publicPath: 'https://cdn.jsdelivr.net/npm/@isle-project/dll@0.50.49/',
 		assetModuleFilename: 'static/media/[hash][ext][query]'
 	},
 	optimization: {
