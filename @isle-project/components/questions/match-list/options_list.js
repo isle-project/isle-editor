ead53008952893bcf39c9690437e08406c655eeb
Philipp Burckhardt <pburckhardt@outlook.com>
2022-07-26 21:11:12 -0400

    fix: Convert to function and use hash check for changes

--- @isle-project/components/questions/match-list/options_list.js
+++ @isle-project/components/questions/match-list/options_list.js
@@ -1,59 +1,60 @@
 // MODULES //
 
-import React, { Component } from 'react';
+import React, { useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
-import shuffle from '@stdlib/random/shuffle';
+import randomShuffle from '@stdlib/random/shuffle';
+import hash from 'object-hash';
 
 
 // MAIN //
 
-class OptionsList extends Component {
-	constructor( props ) {
-		super( props );
-	}
-
-	render() {
-		const options = this.props.shuffle ? shuffle( this.props.options ) : this.props.options;
-		this.options = this.options || options;
-		const { active, onSelect, answers } = this.props;
-		return (
-			<ul className="match-list-question-ul" >
-				{
-					this.options.map( ( q, i ) => {
-						if ( !q ) {
-							return null;
-						}
-						const match = answers.filter( answer => {
-							return answer.a === q || answer.b === q;
-						});
-						const style = {};
-						if ( match[ 0 ] ) {
-							style.backgroundColor = match[ 0 ].color;
-						} else {
-							style.backgroundColor = ( q === active ) ? 'gainsboro' : this.props.baseColor;
-						}
-						const selectItem = () => onSelect( q );
-						return (
-							<div
-								role="button" tabIndex={0}
-								onClick={selectItem}
-								onKeyPress={selectItem}
-								key={i}
+const OptionsList = ({ active, answers, baseColor, options, shuffle, onSelect }) => {
+	const optionsRef = useRef( shuffle ? randomShuffle( options ) : options );
+	const hashRef = useRef( hash( options ) );
+	useEffect( () => {
+		const newHash = hash( options );
+		if ( hashRef.current !== newHash ) {
+			optionsRef.current = shuffle ? randomShuffle( options ) : options;
+			hashRef.current = newHash;
+		}
+	}, [ options, shuffle ] );
+	return (
+		<ul className="match-list-question-ul" >
+			{
+				optionsRef.current.map( ( q, i ) => {
+					if ( !q ) {
+						return null;
+					}
+					const match = answers.filter( answer => {
+						return answer.a === q || answer.b === q;
+					});
+					const style = {};
+					if ( match[ 0 ] ) {
+						style.backgroundColor = match[ 0 ].color;
+					} else {
+						style.backgroundColor = ( q === active ) ? 'gainsboro' : baseColor;
+					}
+					const selectItem = () => onSelect( q );
+					return (
+						<div
+							role="button" tabIndex={0}
+							onClick={selectItem}
+							onKeyPress={selectItem}
+							key={i}
+						>
+							<li
+								className="match-list-question-item"
+								style={style}
 							>
-								<li
-									className="match-list-question-item"
-									style={style}
-								>
-									{q}
-								</li>
-							</div>
-						);
-					})
-				}
-			</ul>
-		);
-	}
-}
+								{q}
+							</li>
+						</div>
+					);
+				})
+			}
+		</ul>
+	);
+};
 
 
 // PROPERTIES //
