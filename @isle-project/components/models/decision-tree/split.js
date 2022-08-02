e6dc1e4c9e1b7d50fd1bb0f2e001003c7a892708
Philipp Burckhardt <pburckhardt@outlook.com>
2022-08-02 08:52:51 -0400

    fix: Use typed arrays to improve memory usage

--- @isle-project/components/models/decision-tree/split.js
+++ @isle-project/components/models/decision-tree/split.js
@@ -4,19 +4,21 @@
 * Splitting observations using specific predicate and pivot.
 */
 function split( data, indices, attr, predicate, pivot ) {
-	const match = [];
-	const notMatch = [];
 	const values = data[ attr ];
-
+	const out = new Int32Array( indices.length );
+	let runningIndex = 0;
 	for ( let i = indices.length - 1; i >= 0; i-- ) {
 		const idx = indices[ i ];
 		const attrValue = values[ idx ];
 		if ( predicate( attrValue, pivot ) ) {
-			match.push( idx );
+			out[ runningIndex ] = idx;
+			runningIndex += 1;
 		} else {
-			notMatch.push( idx );
+			out[ i ] = idx;
 		}
 	}
+	const match = new Int32Array( out.buffer, 0, runningIndex );
+	const notMatch = new Int32Array( out.buffer, runningIndex * 4, out.length - runningIndex );
 	return {
 		match: match,
 		notMatch: notMatch
