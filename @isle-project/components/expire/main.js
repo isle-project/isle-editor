bd6e8eba1dd3849e3535d5366c3fd1cc30543162
Philipp Burckhardt <pburckhardt@outlook.com>
2022-08-04 09:59:21 -0400

    chore: Convert to function component

--- @isle-project/components/expire/main.js
+++ @isle-project/components/expire/main.js
@@ -1,7 +1,8 @@
 // MODULES //
 
-import React, { Component, Fragment } from 'react';
+import React, { Fragment, useCallback, useEffect, useRef, useState } from 'react';
 import PropTypes from 'prop-types';
+import usePrevious from '@isle-project/utils/hooks/use-previous';
 
 
 // MAIN //
@@ -12,83 +13,44 @@ import PropTypes from 'prop-types';
 * @property {boolean} active - controls whether the counter should be active
 * @property {number} delay - number of milliseconds before component disappears
 */
-class Expire extends Component {
-	/**
-	* Constructor function
-	*/
-	constructor( props) {
-		super();
+const Expire = ( props ) => {
+	const { active, children, delay } = props;
+	const [ visible, setVisible ] = useState( active );
+	const prevChildren = usePrevious( props.children );
+	const timerRef = useRef( null );
 
-		// Set initial component states...
-		this.state = {
-			visible: true,
-			children: props.children
+	const setTimer = useCallback( () => {
+		if ( timerRef.current ) {
+			clearTimeout( timerRef.current );
+		}
+		timerRef.current = setTimeout( () => {
+			setVisible( false );
+			timerRef.current = null;
+		}, delay );
+	}, [ delay ] );
+
+	useEffect( () => {
+		if ( active ) {
+			setTimer();
+		}
+		return () => {
+			if ( timerRef.current ) {
+				clearTimeout( timerRef.current );
+			}
 		};
-	}
+	}, [ active, setTimer ] );
 
-	static getDerivedStateFromProps( nextProps, prevState ) {
-		if ( nextProps.children !== prevState.children ) {
-			return {
-				visible: true,
-				children: nextProps.children
-			};
+	useEffect( () => {
+		if ( children !== prevChildren ) {
+			setVisible( true );
+			if ( active ) {
+				setTimer();
+			}
 		}
-		return null;
-	}
+	}, [ active, setTimer, children, prevChildren ] );
 
-	/*
-	* Invoked once on client after the initial rendering. Sets up component timer.
-	*/
-	componentDidMount() {
-		if ( this.props.active ) {
-			this.setTimer();
-		}
-	}
-
-	componentDidUpdate( prevProps ) {
-		if (
-			this.props.children !== prevProps.children &&
-			this.props.active
-		) {
-			this.setTimer();
-		}
-		else if (
-			this.props.active &&
-			this.props.active !== prevProps.active
-		) {
-			this.setTimer();
-		}
-	}
-
-	/*
-	* Invoked immediately before component is unmounted from DOM. Performs clean-up of
-	* component timer.
-	*/
-	componentWillUnmount() {
-		if ( this._timer ) {
-			clearTimeout( this._timer );
-		}
-	}
-
-	setTimer() {
-		if ( this._timer !== null ) {
-			clearTimeout( this._timer );
-		}
-		this._timer = setTimeout( () => {
-			this.setState({ visible: false });
-			this._timer = null;
-		}, this.props.delay );
-	}
-
-	/**
-	* React component render method
-	*/
-	render() {
-		return this.state.visible ?
-			<Fragment>{this.props.children}</Fragment> :
-			<span />;
-	}
-}
+	return visible ? <Fragment>{children}</Fragment> : <span />;
+};
 
 
 // PROPERTIES //
