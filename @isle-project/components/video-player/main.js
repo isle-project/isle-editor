ec26646422f7cb618671c64b29e84d3a3677724a
Philipp Burckhardt <pburckhardt@outlook.com>
2022-08-05 17:28:26 -0400

    Refactor as function component

--- @isle-project/components/video-player/main.js
+++ @isle-project/components/video-player/main.js
@@ -1,27 +1,25 @@
 // MODULES //
 
-import React, { Component, Fragment } from 'react';
+import React, { useEffect, useRef, useState, Fragment, useCallback } from 'react';
 import PropTypes from 'prop-types';
 import logger from 'debug';
-import { withTranslation } from 'react-i18next';
+import { useTranslation } from 'react-i18next';
 import ReactPlayer from 'react-player';
 import Alert from 'react-bootstrap/Alert';
 import Button from 'react-bootstrap/Button';
-import omit from '@stdlib/utils/omit';
 import contains from '@stdlib/assert/contains';
 import Tooltip from '@isle-project/components/tooltip';
 import SessionContext from '@isle-project/session/context.js';
 import isHidden from '@isle-project/utils/is-hidden';
 import isElectron from '@isle-project/utils/is-electron';
 import { END, PLAY, START, PAUSE, SEEK } from '@isle-project/constants/actions.js';
-import { withActionLogger } from '@isle-project/session/action_logger.js';
+import { useActionLogger } from '@isle-project/session/action_logger.js';
 import { withPropCheck } from '@isle-project/utils/prop-check';
 
 
 // VARIABLES //
 
 const debug = logger( 'isle:video-player' );
-const OMITTED_PROPS = [ 'center', 'startTime', 't', 'tReady', 'retrieveLastAction', 'onAction', 'logAction', 'logScore' ];
 
 
 // FUNCTIONS //
@@ -45,6 +43,21 @@ function respondToVisibility( element, callback ) {
 	observer.observe( element );
 }
 
+const ErrorDisplay = ({ error, t, onClick }) => {
+	const button = <Tooltip tooltip={t('retry')}>
+		<Button
+			size="sm" variant="light-outline" onClick={onClick}
+			style={{ position: 'absolute', top: 5, right: 5 }}
+		>
+			<i className="fas fa-redo"></i>
+		</Button>
+	</Tooltip>;
+	return ( <div style={{ position: 'relative' }}>
+		<pre>{error}</pre>
+		{button}
+	</div> );
+};
+
 
 // MAIN //
 
@@ -67,211 +80,147 @@ function respondToVisibility( element, callback ) {
 * @property {Function} onStart - callback invoked once the video pauses
 * @property {Function} onSeek - callback when media seeks called with `seconds` parameter
 */
-class Video extends Component {
-	constructor( props ) {
-		super( props );
-		this.state = {
-			encounteredError: null,
-			progress: {},
-			inViewport: true
-		};
-	}
+const Video = ( props ) => {
+	const { url, center, playing, startTime, width, height, style, onEnded, onStart, onPlay, onPause, onSeek, ...rest } = props;
+	const [ encounteredError, setEncounteredError ] = useState( null );
+	const [ inViewport, setInViewport ] = useState( false );
+	const [ progress, setProgress ] = useState( {} );
+	const { id, logAction } = useActionLogger( 'VIDEO', props );
+	const { t } = useTranslation( 'video' );
+	const videoPlayer = useRef( null );
+	const internalPlayer = useRef( null );
 
-	componentDidMount() {
-		if ( this.props.playing ) {
-			const node = isElectron ? document.getElementById( 'Lesson' ) : document;
-			node.addEventListener( 'scroll', this.isInViewport );
-		}
-	}
-
-	componentDidUpdate( prevProps ) {
-		if ( this.props.playing && !prevProps.playing ) {
-			const node = isElectron ? document.getElementById( 'Lesson' ) : document;
-			node.addEventListener( 'scroll', this.isInViewport );
-		}
-	}
-
-	componentWillUnmount() {
-		const node = isElectron ? document.getElementById( 'Lesson' ) : document;
-		node.removeEventListener( 'scroll', this.isInViewport );
-	}
-
-	isInViewport = () => {
+	const isInViewport = useCallback( () => {
 		debug( 'Check whether video player is in viewport...' );
-		if ( !this.videoPlayer ) {
+		if ( !videoPlayer.current ) {
 			return false;
 		}
-		const top = this.videoPlayer.getBoundingClientRect().top;
-		this.setState({
-			inViewport: top >= 0 && top <= window.innerHeight
-		});
-	};
+		const top = videoPlayer.current.getBoundingClientRect().top;
+		setInViewport( top >= 0 && top <= window.innerHeight );
+	}, [] );
 
-	handleStart = () => {
-		this.props.logAction( START, this.state.progress.playedSeconds );
-		this.props.onStart();
-	};
-
-	handlePlay = () => {
-		this.props.logAction( PLAY, this.state.progress.playedSeconds );
-		this.props.onPlay();
-	};
-
-	handlePause = () => {
-		this.props.logAction( PAUSE, this.state.progress.playedSeconds );
-		this.props.onPause();
-	};
-
-	handleSeek = ( seconds ) => {
-		this.props.logAction( SEEK, seconds );
-		this.props.onSeek( seconds );
-	};
-
-	handleReady = ( player ) => {
-		this.player = player;
-		if ( this.props.startTime ) {
-			const internalPlayer = this.player.getInternalPlayer();
+	useEffect( () => {
+		const node = isElectron ? document.getElementById( 'Lesson' ) : document;
+		if ( playing ) {
+			node.addEventListener( 'scroll', isInViewport );
+		}
+		return () => {
+			node.removeEventListener( 'scroll', isInViewport );
+		};
+	}, [ playing, isInViewport ] );
+	const handleStart = useCallback( () => {
+		logAction( START, progress.playedSeconds );
+		onStart();
+	}, [ logAction, onStart, progress ] );
+	const handlePlay = useCallback( () => {
+		logAction( PLAY, progress.playedSeconds );
+		onPlay();
+	}, [ logAction, onPlay, progress ] );
+	const handlePause = useCallback( () => {
+		logAction( PAUSE, progress.playedSeconds );
+		onPause();
+	}, [ logAction, onPause, progress ] );
+	const handleSeek = useCallback( ( seconds ) => {
+		logAction( SEEK, seconds );
+		onSeek( seconds );
+	}, [ logAction, onSeek ] );
+	const handleReady = useCallback( ( player ) => {
+		internalPlayer.current = player;
+		if ( startTime ) {
+			const player = internalPlayer.current.getInternalPlayer();
 
 			// Skip ahead to chosen start time:
-			internalPlayer.seekTo( this.props.startTime );
+			player.seekTo( startTime );
 		}
-	};
-
-	handleEnded = () => {
-		this.props.logAction( END, this.state.progress.playedSeconds );
-		this.props.onEnded();
-	};
-
-	handleError = ( errorCode ) => {
+	}, [ startTime ] );
+	const handleEnded = useCallback( () => {
+		logAction( END, progress.playedSeconds );
+		onEnded();
+	}, [ logAction, onEnded, progress ] );
+	const handleError = useCallback( ( errorCode ) => {
 		debug( 'Encountered an error: '+errorCode );
-		let encounteredError;
+		let error;
 		switch ( errorCode ) {
 			case 150:
-				encounteredError = this.props.t('playback-disabled');
+				error = t('playback-disabled');
 				break;
 			default:
-				encounteredError = this.props.t('video-not-loaded');
+				error = t('video-not-loaded');
 				break;
 		}
-		this.setState({
-			encounteredError
-		});
-	};
-
-	handleProgress = ( progress ) => {
-		this.setState({
-			progress: progress
-		});
-	};
-
-	pausePlayer = () => {
-		const player = this.player.getInternalPlayer();
-		player.pauseVideo();
-	};
-
-	startPlayer = () => {
-		const player = this.player.getInternalPlayer();
-		player.playVideo();
-	};
-
-	stopPlayer = () => {
-		const player = this.player.getInternalPlayer();
-		player.stopVideo();
-	};
-
-	renderError() {
-		if ( !this.state.encounteredError ) {
-			return null;
-		}
-		const button = <Tooltip tooltip={this.props.t('retry')}>
-			<Button size="sm" variant="light-outline" onClick={() => {
-				this.setState({ encounteredError: null });
-			}} style={{ position: 'absolute', top: 5, right: 5 }}>
-				<i className="fas fa-redo"></i>
-			</Button>
-		</Tooltip>;
-		return ( <div style={{ position: 'relative' }}>
-			<pre>{this.state.encounteredError}</pre>
-			{button}
-		</div> );
+		setEncounteredError( error );
+	}, [ t ] );
+	if ( !url ) {
+		return <Alert variant="danger" >{t('url-missing')}</Alert>;
 	}
-
-	render() {
-		if ( !this.props.url ) {
-			return <Alert variant="danger" >{this.props.t('url-missing')}</Alert>;
-		}
-		let props = this.props;
-		const style = {
-			width: props.width,
-			height: props.height,
-			marginTop: '10px',
-			marginBottom: '10px',
-			...props.style
-		};
-		if ( props.center ) {
-			style.marginLeft = 'auto';
-			style.marginRight = 'auto';
-		}
-		props = omit( props, OMITTED_PROPS );
-		const hidden = isHidden( this.videoPlayer );
-		let player;
-		if (
-			!this.props.playing ||
-			( this.state.inViewport && !hidden ) ||
-			this.player // Keep video player in case it was ready before...
-		) {
-			let config = {};
-			if ( contains( props.url, 'yout' ) ) {
-				config = {
-					youtube: {
-						playerVars: {
-							showinfo: 0,
-							rel: 0,
-							modestbranding: 1
-						}
+	const customStyle = {
+		width: width,
+		height: height,
+		marginTop: '10px',
+		marginBottom: '10px',
+		...style
+	};
+	if ( center ) {
+		customStyle.marginLeft = 'auto';
+		customStyle.marginRight = 'auto';
+	}
+	const hidden = isHidden( videoPlayer.current );
+	let player;
+	if (
+		!playing ||
+		( inViewport && !hidden ) ||
+		internalPlayer.current // Keep video player in case it was ready before...
+	) {
+		let config = {};
+		if ( contains( url, 'yout' ) ) {
+			config = {
+				youtube: {
+					playerVars: {
+						showinfo: 0,
+						rel: 0,
+						modestbranding: 1
 					}
-				};
-			}
-			if ( hidden ) {
-				props.playing = false;
-			}
-			player = <Fragment>
-				{ this.state.encounteredError ?
-					this.renderError() :
-					<ReactPlayer
-						{...props}
-						onStart={this.handleStart}
-						onPlay={this.handlePlay}
-						onPause={this.handlePause}
-						onEnded={this.handleEnded}
-						onProgress={this.handleProgress}
-						onReady={this.handleReady}
-						onSeek={this.handleSeek}
-						onError={this.handleError}
-						progressInterval={1000}
-						config={config}
-					/>
 				}
-			</Fragment>;
-		} else {
-			player = null;
+			};
 		}
-		const out = <div
-			id={this.id}
-			style={style}
-			className="video"
-			ref={( el ) => {
-				if ( !this.videoPlayer ) {
-					this.videoPlayer = el;
-					respondToVisibility( this.videoPlayer, this.isInViewport );
-				}
-			}}
-		>
-			{player}
-		</div>;
-		return out;
+		player = <Fragment>
+			{encounteredError && <ErrorDisplay error={encounteredError} t={t} onClick={() => {
+				setEncounteredError( null );
+			}} />}
+			<ReactPlayer
+				{...rest}
+				url={url}
+				playing={playing && !hidden}
+				onStart={handleStart}
+				onPlay={handlePlay}
+				onPause={handlePause}
+				onEnded={handleEnded}
+				onProgress={setProgress}
+				onReady={handleReady}
+				onSeek={handleSeek}
+				onError={handleError}
+				progressInterval={1000}
+				config={config}
+			/>
+		</Fragment>;
+	} else {
+		player = null;
 	}
-}
+	const out = <div
+		id={id}
+		style={customStyle}
+		className="video"
+		ref={( el ) => {
+			if ( !videoPlayer.current ) {
+				respondToVisibility( el, isInViewport );
+				videoPlayer.current = el;
+			}
+		}}
+	>
+		{player}
+	</div>;
+	return out;
+};
 
 
 // PROPERTIES //
@@ -323,4 +272,4 @@ Video.contextType = SessionContext;
 
 // EXPORTS //
 
-export default withActionLogger( 'VIDEO' )( withTranslation( 'video' )( withPropCheck( Video ) ) );
+export default withPropCheck( Video );
