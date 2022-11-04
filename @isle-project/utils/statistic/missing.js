91f651505effa632012540039f4c37fce4097efc
Philipp Burckhardt <pburckhardt@outlook.com>
2022-11-04 13:08:25 -0400

    Add statistic to calculate proportion of missing values

---
+++ @isle-project/utils/statistic/missing.js
@@ -0,0 +1,21 @@
+// MODULES //
+
+import isMissing from '@isle-project/utils/is-missing';
+
+
+// MAIN //
+
+/**
+* Returns the proportion of missing values.
+*
+* @param {Array} arr - array of values
+* @returns {number} proportion of missing values (between 0 and 1)
+*/
+function missing( arr ) {
+
+}
+
+
+// EXPORTS //
+
+export default missing;
