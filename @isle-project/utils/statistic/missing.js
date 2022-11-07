146eb675341d04c555e519f81b4b2285f844cf4d
Philipp Burckhardt <pburckhardt@outlook.com>
2022-11-07 10:19:22 -0500

    feat: Add function to calculate proportion of missing values

--- @isle-project/utils/statistic/missing.js
+++ @isle-project/utils/statistic/missing.js
@@ -12,7 +12,17 @@ import isMissing from '@isle-project/utils/is-missing';
 * @returns {number} proportion of missing values (between 0 and 1)
 */
 function missing( arr ) {
-
+	const len = arr.length;
+	if ( !len ) {
+		return null;
+	}
+	let nMissing = 0;
+	for ( let i = 0; i < len; i++ ) {
+		if ( isMissing( arr[ i ] ) ) {
+			nMissing += 1;
+		}
+	}
+	return nMissing / len;
 }
 
 
