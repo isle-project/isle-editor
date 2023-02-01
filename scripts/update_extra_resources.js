b9805412ebb5846b4d7f3cfbac6e7aad7e0d3948
Philipp Burckhardt <pburckhardt@outlook.com>
2023-01-31 21:21:26 -0500

    fix: Rewrite update resources script

--- scripts/update_extra_resources.js
+++ scripts/update_extra_resources.js
@@ -3,15 +3,14 @@
 // MODULES //
 
 const path = require( 'path' );
-const { spawn } = require( 'child_process' );
 const fs = require( 'fs' );
 const replace = require( '@stdlib/string/replace' );
+const packageLockFile = require( './../package-lock.json' );
 
 
 // VARIABLES //
 
-const RE_NODE_MODULE = /^[\s\S]*?\/node_modules\/([\s\S]+$)/;
-const RE_EOL = /\r?\n/;
+const RE_NODE_MODULE = /^[\s\S]*?\/?node_modules\/([\s\S]+$)/;
 const pkgPath = path.join( __dirname, '..', 'package.json' );
 const pkg = require( pkgPath );
 const IGNORE_LIST = [
@@ -59,45 +58,23 @@ const extraResources = [
 	'!@isle-project/session/dist/*'
 ];
 const DEV_DEPS = new Set();
-const listDevDeps = spawn( 'npm', [ 'ls', '--only=dev', '--parseable', '--all' ] );
 
-listDevDeps.stdout.on( 'data', ( data ) => {
-	const lines = data.toString().split( RE_EOL );
-	for ( let i = 1; i < lines.length - 1; i++ ) {
-		DEV_DEPS.add( lines[ i ] );
+
+const entries = Object.entries( packageLockFile.packages );
+for ( let i = 0; i < entries.length; i++ ) {
+	const [ pkg, entry ] = entries[ i ];
+	if ( entry.dev ) {
+		DEV_DEPS.add( pkg );
+	}
+}
+
+DEV_DEPS.forEach( ( value ) => {
+	const ignorePattern = replace( value, RE_NODE_MODULE, '!node_modules/$1/**/*' );
+	if ( !IGNORE_LIST.includes( ignorePattern ) ) {
+		extraResources.push( ignorePattern );
 	}
 });
 
-listDevDeps.stdout.on( 'close', ( code ) => {
-	console.log( `npm ls --dev exited with code ${code}` );
-	console.log( `The isle-editor has ${DEV_DEPS.size} development dependencies.` );
+pkg.build.extraResources = extraResources;
 
-	const listProdDeps = spawn( 'npm', [ 'ls', '--only=prod', '--parseable', '-all' ]);
-
-	listProdDeps.stdout.on( 'data', ( data ) => {
-		const lines = data.toString().split( RE_EOL );
-		for ( let i = 1; i < lines.length - 1; i++ ) {
-			const val = lines[ i ];
-			if ( DEV_DEPS.has( val ) ) {
-				DEV_DEPS.delete( val );
-			}
-		}
-	});
-
-	listProdDeps.stdout.on( 'close', ( code ) => {
-		console.log( `npm ls --prod exited with code ${code}` );
-		console.log( `The isle-editor has ${DEV_DEPS.size} development dependencies that are not also used by production dependencies.` );
-
-		DEV_DEPS.forEach( ( value ) => {
-			const ignorePattern = replace( value, RE_NODE_MODULE, '!node_modules/$1/**/*' );
-			if ( !IGNORE_LIST.includes( ignorePattern ) ) {
-				extraResources.push( ignorePattern );
-			}
-		});
-
-		console.log( extraResources );
-		pkg.build.extraResources = extraResources;
-
-		fs.writeFileSync( pkgPath, JSON.stringify( pkg, null, 2 ).concat( '\n' ) );
-	});
-});
+fs.writeFileSync( pkgPath, JSON.stringify( pkg, null, 2 ).concat( '\n' ) );
