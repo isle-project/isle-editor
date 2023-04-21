8669d91a14682ba23a6d3b9aae82aa558f5b69f3
Philipp Burckhardt <pburckhardt@outlook.com>
2023-04-21 18:04:03 -0400

    feat: implement chi2 table in JS

--- @isle-project/components/tests/chi2/index.js
+++ @isle-project/components/tests/chi2/index.js
@@ -4,6 +4,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { useTranslation } from 'react-i18next';
 import Alert from 'react-bootstrap/Alert';
+import roundn from '@stdlib/math/base/special/roundn';
+import sqrt from '@stdlib/math/base/special/sqrt';
 import extractCategoriesFromValues from '@isle-project/utils/extract-categories-from-values';
 import { withPropCheck } from '@isle-project/utils/prop-check';
 import { Factor } from '@isle-project/utils/factor-variable';
@@ -35,7 +37,7 @@ function createContingencyTable( data, rowVar, colVar ) {
 		const yIndex = yCategories.indexOf( yVal );
 		out[ xIndex ][ yIndex ] += 1;
 	}
-	return out;
+	return { table: out, xCategories, yCategories };
 }
 
 
@@ -48,14 +50,51 @@ function Chi2Test({ data, var1, var2, showDecision }) {
 	}
 	// TODO: Filter our missing values and keep count of them
 
-	const out = createContingencyTable( data, var1, var2 );
-	const result = chi2test( out );
+	const { table, xCategories, yCategories } = createContingencyTable( data, var1, var2 );
+	const result = chi2test( table );
+
+	const contribTable = new Array( table.length );
+	for ( let i = 0; i < table.length; i++ ) {
+		contribTable[ i ] = new Array( table[ i ].length );
+		for ( let j = 0; j < table[ i ].length; j++ ) {
+			const observed = table[ i ][ j ];
+			const expected = result.expected.get( i, j );
+			const residual = ( observed - expected ) / sqrt( expected );
+			const residualSquared = residual * residual;
+			const contribution = 100 * residualSquared / result.statistic;
+			contribTable[ i ][ j ] = `${observed}/${roundn(expected, -2)} (${roundn(contribution, -2 )}%)`;
+		}
+	}
+	const contributionsTable = <table className="table table-bordered table-condensed">
+		<thead>
+			<tr>
+				<th></th>
+				{yCategories.map( ( y, i ) => {
+					return <th key={i}>{y}</th>;
+				})}
+			</tr>
+		</thead>
+		<tbody>
+			{xCategories.map( ( x, i ) => {
+				return (
+					<tr key={i}>
+						<th>{x}</th>
+						{contribTable[ i ].map( ( y, j ) => {
+							return <td key={j}>{y}</td>;
+						})}
+					</tr>
+				);
+			})}
+		</tbody>
+	</table>;
 	return (
 		<div style={{ overflowX: 'auto', width: '100%' }}>
 			<label>{t('hypothesis-test-for-independence', { var1, var2 })}:</label>
 			{result.print({
 					decision: showDecision
 			})}
+			{contributionsTable}
+			<p>observed/expected (contribution to chi-square in %)</p>
 		</div>
 	);
 }
